08b7c2bd32ca17db4573b439a8c309b03dc7e6a1
Tom Ware <tom.ware@oracle.com>
2007-10-25 20:22:15 +0000

    Updated to work with current package renamer

--- foundation/bin/packageRename.sh
+++ foundation/bin/packageRename.sh
@@ -4,10 +4,7 @@
 # Please do not change any of the following lines:
 SRC_DIR=$1
 DEST_DIR=$2
-LOG_DIR=$3
 JVM_ARGS=-Xmx256M
-CLASSPATH=`dirname $0`/../lib/java/internal/packageRename.jar
-JAVA_ARGS=`dirname $0`/../config/packageRename.properties \
-${SRC_DIR} ${DEST_DIR} ${LOG_DIR}
-${JAVA_HOME}/bin/java ${JVM_ARGS} -classpath ${CLASSPATH} \
-    org.eclipse.persistence.tools.PackageRenamer ${JAVA_ARGS}
+CLASSPATH=`dirname $0`/../jlib/package-rename.jar
+
+${JAVA_HOME}/bin/java ${JVM_ARGS} -classpath ${CLASSPATH} utilities.MigrateTopLinkToEclipseLink ${SRC_DIR} ${DEST_DIR} ../config/package-rename.properties
