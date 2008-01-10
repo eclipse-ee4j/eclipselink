544e5a43d1b2d4dbbdb32836e6cb454b7e973c38
Andrei Ilitchev <andrei.ilitchev@oracle.com>
2008-01-10 21:13:27 +0000

    Adding the new file I forgot while in today's fix for bugs  214826 and 214827

---
+++ foundation/eclipselink.core/src/org/eclipse/persistence/internal/sequencing/SequencingCallbackFactory.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 1998, 2007 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0, which accompanies this distribution
+ * and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Contributors:
+ *     Oracle - initial API and implementation from Oracle TopLink
+ ******************************************************************************/  
+package org.eclipse.persistence.internal.sequencing;
+
+/**
+ * <p>
+ * <b>Purpose</b>: Define interface for sequencing callback factory.
+ * <p>
+ * <b>Description</b>: Instantiated internaly by SequencingManager.
+ * <p>
+ * <b>Responsibilities</b>:
+ * <ul>
+ * <li> Provides sequencing callback to be called after transaction has committed.
+ * </ul>
+ * @see org.eclipse.persistence.sequencing.SequencingControl
+ */
+public interface SequencingCallbackFactory {
+
+    /**
+    * INTERNAL:
+    * Create SequencingCallback.
+    */
+    SequencingCallback createSequencingCallback();
+}
