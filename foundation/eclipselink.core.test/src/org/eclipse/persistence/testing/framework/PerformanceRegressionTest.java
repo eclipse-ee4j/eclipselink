6060e4cb87115a39722d9735aec85bbfadd42523
James Sutherland <james.sutherland@oracle.com>
2008-02-12 15:50:36 +0000

    Adding performance tests framework classes missed from last checkin.

---
+++ foundation/eclipselink.core.test/src/org/eclipse/persistence/testing/framework/PerformanceRegressionTest.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 1998, 2007 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0, which accompanies this distribution
+ * and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Contributors:
+ *     Oracle - initial API and implementation from Oracle TopLink
+ ******************************************************************************/  
+package org.eclipse.persistence.testing.framework;
+
+/**
+ * Performance test interface used for sharing behavoir and
+ * result with EJB tests.
+ */
+public interface PerformanceRegressionTest extends PerformanceComparisonTest {
+}
