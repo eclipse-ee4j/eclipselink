d8a416233b89a5648b00a14776ce1118618bf7bc
Peter Krogh <peter.krogh@oracle.com>
2008-02-20 14:38:49 +0000

    219593 - Dual Licensing EclipseLink

--- jpa/eclipselink.jpa/src/org/eclipse/persistence/internal/jpa/metadata/tables/UniqueConstraintMetadata.java
+++ jpa/eclipselink.jpa/src/org/eclipse/persistence/internal/jpa/metadata/tables/UniqueConstraintMetadata.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 1998, 2008 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
+ * which accompanies this distribution. 
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * @author Guy Pelletier
+ * @since EclipseLink 1.0
+ *******************************************************************************/
 package org.eclipse.persistence.internal.jpa.metadata.tables;
 
 import java.util.ArrayList;
@@ -8,9 +20,6 @@ import javax.persistence.UniqueConstraint;
 /**
  * INTERNAL:
  * Object to hold onto a unique constraint metadata.
- * 
- * @author Guy Pelletier
- * @since EclipseLink 1.0
  */
 public class UniqueConstraintMetadata {
 	private List<String> m_columnNames;
