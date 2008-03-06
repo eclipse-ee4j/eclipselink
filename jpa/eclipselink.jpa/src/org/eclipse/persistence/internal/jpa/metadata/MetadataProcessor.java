745ecc5a760feca72758c008160e601500dafeb0
Guy Pelletier <guy.pelletier@oracle.com>
2008-03-06 16:13:13 +0000

    Fix incorrect merging

--- jpa/eclipselink.jpa/src/org/eclipse/persistence/internal/jpa/metadata/MetadataProcessor.java
+++ jpa/eclipselink.jpa/src/org/eclipse/persistence/internal/jpa/metadata/MetadataProcessor.java
@@ -237,7 +237,7 @@ public class MetadataProcessor {
      * and no XML.
      */
     public void processAnnotations() {
-    	for (ClassAccessor accessor : m_project.getClassAccessors()) {
+    	for (EntityAccessor accessor : m_project.getEntityAccessors()) {
     	    // If the accessor hasn't been processed yet, then process it.
             if (! accessor.isProcessed()) {
                 accessor.process();
