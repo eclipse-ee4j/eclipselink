5c3a6dc8dbe7f49a60bb60bc9714a6d450e91824
Guy Pelletier <guy.pelletier@oracle.com>
2008-03-14 14:58:47 +0000

    Minor code formatting.

--- jpa/eclipselink.jpa/src/org/eclipse/persistence/internal/jpa/metadata/transformers/ReadTransformerMetadata.java
+++ jpa/eclipselink.jpa/src/org/eclipse/persistence/internal/jpa/metadata/transformers/ReadTransformerMetadata.java
@@ -28,38 +28,62 @@ public class ReadTransformerMetadata {
     private String m_transformerClassName;
     private String m_method;
 
+    /**
+     * INTERNAL:
+     */
     public ReadTransformerMetadata() {
         super();
     }
 
+    /**
+     * INTERNAL:
+     * Class was specified - apply it to the mapping.
+     * Note that because this method is called from predeploy with temporary 
+     * classloader the class name should be set. However having the class allows 
+     * validation.
+     */
+    protected void applyClass(TransformationMapping mapping) {
+        if(AttributeTransformer.class.isAssignableFrom(getTransformerClass())) {
+            mapping.setAttributeTransformerClassName(getTransformerClass().getName());
+        } else {
+            throw ValidationException.readTransformerClassDoesntImplementAttributeTransformer(mapping.getAttributeName(), mapping.getDescriptor().getJavaClassName());
+        }
+    }
+    
+    /**
+     * INTERNAL:
+     * Method was specified - apply it to the mapping.
+     */
+    protected void applyMethod(TransformationMapping mapping) {
+        mapping.setAttributeTransformation(getMethod());
+    }
+    
+    /**
+     * INTERNAL:
+     * Used for OX mapping.
+     */
+    public String getMethod() {
+        return m_method;
+    }
+    
+    /**
+     * INTERNAL:
+     */
     public Class getTransformerClass() {
         return m_transformerClass;
     }
 
-    public void setTransformerClass(Class transformerClass) {
-        m_transformerClass = transformerClass;
-    }
-    
+    /**
+     * INTERNAL:
+     * Used for OX mapping.
+     */
     public String getTransformerClassName() {
         return m_transformerClassName;
     }
-
-    public void setTransformerClassName(String transformerClassName) {
-        m_transformerClassName = transformerClassName;
-    }
     
-    public String getMethod() {
-        return m_method;
-    }
-
-    public void setMethod(String method) {
-        m_method = method;
-    }
-    
-    public boolean hasMethod() {
-        return m_method != null && m_method.length() > 0;
-    }
-    
+    /**
+     * INTERNAL:
+     */
     public boolean hasClass() {
         return m_transformerClass != null && !m_transformerClass.equals(void.class);
     }
@@ -75,8 +99,16 @@ public class ReadTransformerMetadata {
     
     /**
      * INTERNAL:
-     * When this method is called there must be either method or class (but not both!).
-     * If there was not class but className, then by now the class should have been set.
+     */
+    public boolean hasMethod() {
+        return m_method != null && m_method.length() > 0;
+    }
+    
+    /**
+     * INTERNAL:
+     * When this method is called there must be either method or class (but not 
+     * both!). If there was not class but className, then by now the class 
+     * should have been set.
      */
     public void process(TransformationMapping mapping) {
         if(hasMethod()) {
@@ -96,30 +128,37 @@ public class ReadTransformerMetadata {
 
     /**
      * INTERNAL:
-     * Method was specified - apply it to the mapping.
+     * Used for OX mapping.
      */
-    protected void applyMethod(TransformationMapping mapping) {
-        mapping.setAttributeTransformation(getMethod());
+    public void setMethod(String method) {
+        m_method = method;
     }
-
+    
     /**
      * INTERNAL:
-     * Class was specified - apply it to the mapping.
-     * Note that because this method is called from predeploy with temporary classloader
-     * the class name should be set. However having the class allows validation.
      */
-    protected void applyClass(TransformationMapping mapping) {
-        if(AttributeTransformer.class.isAssignableFrom(getTransformerClass())) {
-            mapping.setAttributeTransformerClassName(getTransformerClass().getName());
-        } else {
-            throw ValidationException.readTransformerClassDoesntImplementAttributeTransformer(mapping.getAttributeName(), mapping.getDescriptor().getJavaClassName());
-        }
+    public void setTransformerClass(Class transformerClass) {
+        m_transformerClass = transformerClass;
     }
-
+    
+    /**
+     * INTERNAL:
+     * Used for OX mapping.
+     */
+    public void setTransformerClassName(String transformerClassName) {
+        m_transformerClassName = transformerClassName;
+    }
+    
+    /**
+     * INTERNAL:
+     */
     protected void throwBothClassAndMethodSpecifiedException(TransformationMapping mapping) {
         throw ValidationException.readTransformerHasBothClassAndMethod(mapping.getAttributeName(), mapping.getDescriptor().getJavaClassName());
     }
 
+    /**
+     * INTERNAL:
+     */
     protected void throwNeitherClassNorMethodSpecifiedException(TransformationMapping mapping) {
         throw ValidationException.readTransformerHasNeitherClassNorMethod(mapping.getAttributeName(), mapping.getDescriptor().getJavaClassName());
     }
