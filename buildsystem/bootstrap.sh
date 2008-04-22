31dafa965f82d0836739f2aee5055e2c3681bed3
Tom Ware <tom.ware@oracle.com>
2008-04-22 20:06:17 +0000

    Automated build updates checked in for Eric Gwin

--- buildsystem/bootstrap.sh
+++ buildsystem/bootstrap.sh
@@ -4,36 +4,47 @@ umask 0002
 TARGET=$1
 BRANCH=$2
 if [ ! "$TARGET" = "" ]
-then 
+then
+    TARG_NM=${TARGET}
     echo "Target=${TARGET}"
+else
+    TARG_NM="default"
 fi
 if [ ! "$BRANCH" = "" ]
-then 
+then
+    BRANCH_NM=${BRANCH}
     BRANCH=branch/${BRANCH}
     echo "Branch=${BRANCH}"
+else
+    BRANCH_NM="trunk"
 fi
 
 BOOTSTRAP_BLDFILE=bootstrap.xml
 SVN_EXEC=`which svn`
 if [ $? -ne 0 ]
-then 
+then
     echo "Error finding svn install!"
     exit
 fi
 
-#Define common variables and assumed dependencies
+#Define common variables
 JAVA_HOME=/shared/common/ibm-java2-ppc-50
 ANT_HOME=/shared/common/apache-ant-1.6.5
 #JAVA_HOME=/shared/common/jdk1.6.0_05
 #ANT_HOME=/shared/common/apache-ant-1.7.0
+CUR_DIR=`pwd`
 HOME_DIR=/shared/technology/eclipselink/staging2
-LOG_DIR=${HOME_DIR}/log
-JUNIT_HOME=${HOME_DIR}/org.junit_3.8.2.v200706111738
-MAVENANT_DIR=${HOME_DIR}/staging
+LOG_DIR=${HOME_DIR}/logs
 BRANCH_DIR=${HOME_DIR}/${BRANCH}trunk
-
+BLD_DEPS_DIR=${HOME_DIR}/bld_deps/${BRANCH_NM}
+START_DATE=`date '+%y%m%d-%H%M'`
+DATED_LOG=${LOG_DIR}/bsb-${BRANCH_NM}_${TARG_NM}_${START_DATE}.log
 JDBC_LOGIN_INFO_FILE=${HOME_DIR}/db.dat
-DATED_LOG=${LOG_DIR}/bsb_`date '+%y%m%d-%H%M'`.log
+
+#Define build dependency dirs (build needs em, NOT compile dependencies)
+JUNIT_HOME=${BLD_DEPS_DIR}/junit
+MAVENANT_DIR=${BLD_DEPS_DIR}/mavenant
+MAILLIB_DIR=${BRANCH_DIR}/foundation/eclipselink.core.lib
 
 PATH=${JAVA_HOME}/bin:${ANT_HOME}/bin:/usr/bin:/usr/local/bin:${PATH}
 CLASSPATH=${CLASSPATH}:${JUNIT_HOME}/junit.jar:${ANT_HOME}/lib/ant-junit.jar:${MAILLIB_DIR}/mail.jar:${MAILLIB_DIR}/activation.jar:${MAVENANT_DIR}/maven-ant-tasks-2.0.8.jar
@@ -46,35 +57,29 @@ CreatePath() {
     do
         newdir=${newdir}/${directory}
         if [ ! -d "/${newdir}" ]
-	then
-	    #echo "creating ${newdir}"
-	    mkdir ${newdir}
+        then
+            #echo "creating ${newdir}"
+            mkdir ${newdir}
             if [ $? -ne 0 ]
-            then 
+            then
                 echo "    Create failed!"
                 exit
             fi
-       fi
+        fi
     done
 }
 
-#GetBuildFromSVN () {
-#    whereami=`pwd`
-#    cd $HOME_DIR
-#    svn co -q ${BUILD_URL}
-#    cd $whereami    
-#}
 #--------- MAIN --------#
 
 #Test for existence of dependencies, send email and fail if any of the above not found.
-if [ ! -d ${JAVA_HOME} ] 
+if [ ! -d ${JAVA_HOME} ]
 then
     echo "Java not found!"
     echo "Expecting Java at: $JAVA_HOME"
     exit
 fi
-if [ ! -d ${ANT_HOME} ] 
-then 
+if [ ! -d ${ANT_HOME} ]
+then
     echo "Java not found!"
     echo "Expecting Java at: $ANT_HOME"
     exit
@@ -89,25 +94,34 @@ then
     echo "Need to create LOG_DIR (${LOG_DIR})"
     CreatePath ${LOG_DIR}
 fi
+if [ ! -d ${BLD_DEPS_DIR} ]
+then
+    echo "Need to create BLD_DEPS_DIR (${BLD_DEPS_DIR})"
+    CreatePath ${BLD_DEPS_DIR}
+fi
 if [ ! -f ./${BOOTSTRAP_BLDFILE} ]
 then
     echo "Need bootstrap buildfile (${BOOTSTRAP_BLDFILE}) in the current directory to proceed."
     exit 1
 else
-    cp ./${BOOTSTRAP_BLDFILE} ${HOME_DIR}/${BOOTSTRAP_BLDFILE}
+    if [ ! "${HOME_DIR}" = "${CUR_DIR}" ]
+    then
+        cp ./${BOOTSTRAP_BLDFILE} ${HOME_DIR}/${BOOTSTRAP_BLDFILE}
+    fi
 fi
 if [ ! -f $JDBC_LOGIN_INFO_FILE ]
 then
     echo "No db Login info available!"
     exit
-else 
+else
     DB_USER=`cat $JDBC_LOGIN_INFO_FILE | cut -d: -f1`
     DB_PWD=`cat $JDBC_LOGIN_INFO_FILE | cut -d: -f2`
 fi
 
 #Set appropriate max Heap for VM and let Ant inherit JavaVM (OS's) proxy settings
+ANT_ARGS=" "
 ANT_OPTS="-Xmx128m"
-ANT_BASEARG="-f \"${BOOTSTRAP_BLDFILE}\" -l \"$DATED_LOG\" -Dbranch.name=\"${BRANCH}\""
+ANT_BASEARG="-f \"${BOOTSTRAP_BLDFILE}\" -Dbranch.name=\"${BRANCH}\""
 
 if [ "$TARGET" = "test" ]
 then
@@ -116,13 +130,19 @@ then
     ANT_ARGS="-autoproxy"
     TARGET=build
     ANT_BASEARG="${ANT_BASEARG} -D_Test=1"
-fi    
+fi
 
-export JAVA_HOME ANT_HOME HOME_DIR JUNIT_HOME MAVENANT_DIR BRANCH_DIR PATH CLASSPATH SVN_EXEC ANT_ARGS ANT_OPTS
+export ANT_ARGS ANT_OPTS ANT_HOME BRANCH_DIR HOME_DIR LOG_DIR JAVA_HOME JUNIT_HOME MAVENANT_DIR PATH CLASSPATH
+export SVN_EXEC BLD_DEPS_DIR JUNIT_HOME
 
-cd ${HOME_DIR} 
+cd ${HOME_DIR}
+echo "Results logged to: ${DATED_LOG}"
+
 touch $DATED_LOG
+echo "Build started at: `date`" >> ${DATED_LOG}
+source ~/.ssh-agent >> ${DATED_LOG}
+echo "ant ${ANT_BASEARG} $TARGET" >> ${DATED_LOG}
+ant ${ANT_BASEARG} -Ddb.user="$DB_USER" -Ddb.pwd="$DB_PWD" $TARGET >> ${DATED_LOG}
+echo "Build completed at: `date`" >> ${DATED_LOG}
 
-echo "ant ${ANT_BASEARG} $TARGET" >> $DATED_LOG
-ant ${ANT_BASEARG} -Ddb.user="$DB_USER" -Ddb.pwd="$DB_PWD" $TARGET &
-tail -f $DATED_LOG
+#tail -f $DATED_LOG
