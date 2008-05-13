3858020226f7ebef50316a5a535ab43fceefe60c
Mike Norman <mike.norman@oracle.com>
2008-05-13 18:39:50 +0000

    part of https://bugs.eclipse.org/bugs/show_bug.cgi?id=231312

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/sessions/factories/ObjectPersistenceWorkbenchXMLProject.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/sessions/factories/ObjectPersistenceWorkbenchXMLProject.java
@@ -13,12 +13,13 @@
 package org.eclipse.persistence.sessions.factories;
 
 import org.eclipse.persistence.descriptors.ClassDescriptor;
-import org.eclipse.persistence.oxm.mappings.*;
+import org.eclipse.persistence.oxm.mappings.XMLCompositeDirectCollectionMapping;
+import org.eclipse.persistence.oxm.mappings.XMLDirectMapping;
 
 /**
  * INTERNAL:
- * Define the TopLink OX project and descriptor information to read a OracleAS TopLink 10<i>g</i> (10.0.3) project from an XML file.
- * Note any changes must be reflected in the OPM XML schema.
+ * Amend the EclipseLink project and descriptor information to write out an EclipseLink project
+ * to an XML file, replacing actual .class references with String equivalents
  */
 public class ObjectPersistenceWorkbenchXMLProject extends EclipseLinkObjectPersistenceRuntimeXMLProject {
 
@@ -33,7 +34,8 @@ public class ObjectPersistenceWorkbenchXMLProject extends EclipseLinkObjectPersi
     protected ClassDescriptor buildAggregateMappingDescriptor() {
         ClassDescriptor descriptor = super.buildAggregateMappingDescriptor();
 
-        XMLDirectMapping referenceClassMapping = (XMLDirectMapping)descriptor.getMappingForAttributeName("referenceClass");
+        XMLDirectMapping referenceClassMapping = 
+        	(XMLDirectMapping)descriptor.getMappingForAttributeName("referenceClass");
         referenceClassMapping.setGetMethodName("getReferenceClassName");
         referenceClassMapping.setSetMethodName("setReferenceClassName");
 
@@ -43,7 +45,8 @@ public class ObjectPersistenceWorkbenchXMLProject extends EclipseLinkObjectPersi
     protected ClassDescriptor buildForeignReferenceMappingDescriptor() {
         ClassDescriptor descriptor = super.buildForeignReferenceMappingDescriptor();
 
-        XMLDirectMapping referenceClassMapping = (XMLDirectMapping)descriptor.getMappingForAttributeName("referenceClass");
+        XMLDirectMapping referenceClassMapping = 
+        	(XMLDirectMapping)descriptor.getMappingForAttributeName("referenceClass");
         referenceClassMapping.setGetMethodName("getReferenceClassName");
         referenceClassMapping.setSetMethodName("setReferenceClassName");
         
@@ -53,7 +56,8 @@ public class ObjectPersistenceWorkbenchXMLProject extends EclipseLinkObjectPersi
     protected ClassDescriptor buildInheritancePolicyDescriptor() {
         ClassDescriptor descriptor = super.buildInheritancePolicyDescriptor();
 
-        XMLDirectMapping referenceClassMapping = (XMLDirectMapping)descriptor.getMappingForAttributeName("parentClass");
+        XMLDirectMapping referenceClassMapping = 
+        	(XMLDirectMapping)descriptor.getMappingForAttributeName("parentClass");
         referenceClassMapping.setGetMethodName("getParentClassName");
         referenceClassMapping.setSetMethodName("setParentClassName");
         
@@ -63,7 +67,8 @@ public class ObjectPersistenceWorkbenchXMLProject extends EclipseLinkObjectPersi
     protected ClassDescriptor buildInstantiationPolicyDescriptor() {
         ClassDescriptor descriptor = super.buildInstantiationPolicyDescriptor();
 
-        XMLDirectMapping referenceClassMapping = (XMLDirectMapping)descriptor.getMappingForAttributeName("factoryClass");
+        XMLDirectMapping referenceClassMapping = 
+        	(XMLDirectMapping)descriptor.getMappingForAttributeName("factoryClass");
         referenceClassMapping.setGetMethodName("getFactoryClassName");
         referenceClassMapping.setSetMethodName("setFactoryClassName");
 
@@ -73,11 +78,13 @@ public class ObjectPersistenceWorkbenchXMLProject extends EclipseLinkObjectPersi
     protected ClassDescriptor buildInterfacePolicyDescriptor() {
         ClassDescriptor descriptor = super.buildInterfacePolicyDescriptor();
 
-        XMLCompositeDirectCollectionMapping referenceClassMapping = (XMLCompositeDirectCollectionMapping)descriptor.getMappingForAttributeName("parentInterfaces");
+        XMLCompositeDirectCollectionMapping referenceClassMapping = 
+        	(XMLCompositeDirectCollectionMapping)descriptor.getMappingForAttributeName("parentInterfaces");
         referenceClassMapping.setGetMethodName("getParentInterfaceNames");
         referenceClassMapping.setSetMethodName("setParentInterfaceNames");
 
-        XMLDirectMapping implementorDescriptorMapping = (XMLDirectMapping)descriptor.getMappingForAttributeName("implementorDescriptor");
+        XMLDirectMapping implementorDescriptorMapping = 
+        	(XMLDirectMapping)descriptor.getMappingForAttributeName("implementorDescriptor");
         implementorDescriptorMapping.setGetMethodName("getImplementorDescriptorClassName");
         implementorDescriptorMapping.setSetMethodName("setImplementorDescriptorClassName");
                 
@@ -87,7 +94,8 @@ public class ObjectPersistenceWorkbenchXMLProject extends EclipseLinkObjectPersi
     protected ClassDescriptor buildInterfaceContainerPolicyDescriptor() {
         ClassDescriptor descriptor = super.buildInterfaceContainerPolicyDescriptor();
 
-        XMLDirectMapping referenceClassMapping = (XMLDirectMapping)descriptor.getMappingForAttributeName("containerClass");
+        XMLDirectMapping referenceClassMapping = 
+        	(XMLDirectMapping)descriptor.getMappingForAttributeName("containerClass");
         referenceClassMapping.setGetMethodName("getContainerClassName");
         referenceClassMapping.setSetMethodName("setContainerClassName");
 
@@ -97,7 +105,8 @@ public class ObjectPersistenceWorkbenchXMLProject extends EclipseLinkObjectPersi
     protected ClassDescriptor buildSortedCollectionContainerPolicyDescriptor() {
         ClassDescriptor descriptor = super.buildSortedCollectionContainerPolicyDescriptor();
 
-        XMLDirectMapping referenceClassMapping = (XMLDirectMapping)descriptor.getMappingForAttributeName("comparatorClass");
+        XMLDirectMapping referenceClassMapping = 
+        	(XMLDirectMapping)descriptor.getMappingForAttributeName("comparatorClass");
         referenceClassMapping.setGetMethodName("getComparatorClassName");
         referenceClassMapping.setSetMethodName("setComparatorClassName");
 
@@ -108,7 +117,8 @@ public class ObjectPersistenceWorkbenchXMLProject extends EclipseLinkObjectPersi
     protected ClassDescriptor buildMethodBaseQueryRedirectorDescriptor() {
         ClassDescriptor descriptor = super.buildMethodBaseQueryRedirectorDescriptor();
 
-        XMLDirectMapping referenceClassMapping = (XMLDirectMapping)descriptor.getMappingForAttributeName("methodClass");
+        XMLDirectMapping referenceClassMapping = 
+        	(XMLDirectMapping)descriptor.getMappingForAttributeName("methodClass");
         referenceClassMapping.setGetMethodName("getMethodClassName");
         referenceClassMapping.setSetMethodName("setMethodClassName");
 
@@ -118,7 +128,8 @@ public class ObjectPersistenceWorkbenchXMLProject extends EclipseLinkObjectPersi
     protected ClassDescriptor buildObjectLevelReadQueryDescriptor() {
         ClassDescriptor descriptor = super.buildObjectLevelReadQueryDescriptor();
 
-        XMLDirectMapping referenceClassMapping = (XMLDirectMapping)descriptor.getMappingForAttributeName("referenceClass");
+        XMLDirectMapping referenceClassMapping = 
+        	(XMLDirectMapping)descriptor.getMappingForAttributeName("referenceClass");
         referenceClassMapping.setGetMethodName("getReferenceClassName");
         referenceClassMapping.setSetMethodName("setReferenceClassName");
         
@@ -128,7 +139,8 @@ public class ObjectPersistenceWorkbenchXMLProject extends EclipseLinkObjectPersi
     protected ClassDescriptor buildQueryArgumentDescriptor() {
         ClassDescriptor descriptor = super.buildQueryArgumentDescriptor();
 
-        XMLDirectMapping referenceClassMapping = (XMLDirectMapping)descriptor.getMappingForAttributeName("type");     
+        XMLDirectMapping referenceClassMapping = 
+        	(XMLDirectMapping)descriptor.getMappingForAttributeName("type");     
         referenceClassMapping.setGetMethodName("getTypeName");
         referenceClassMapping.setSetMethodName("setTypeName");
 
@@ -138,7 +150,8 @@ public class ObjectPersistenceWorkbenchXMLProject extends EclipseLinkObjectPersi
     protected ClassDescriptor buildRelationshipQueryKeyDescriptor() {
         ClassDescriptor descriptor = super.buildRelationshipQueryKeyDescriptor();
 
-        XMLDirectMapping referenceClassMapping = (XMLDirectMapping)descriptor.getMappingForAttributeName("referenceClass");
+        XMLDirectMapping referenceClassMapping = 
+        	(XMLDirectMapping)descriptor.getMappingForAttributeName("referenceClass");
         referenceClassMapping.setGetMethodName("getReferenceClassName");
         referenceClassMapping.setSetMethodName("setReferenceClassName");
 
@@ -148,7 +161,8 @@ public class ObjectPersistenceWorkbenchXMLProject extends EclipseLinkObjectPersi
     protected ClassDescriptor buildReturningFieldInfoDescriptor() {
         ClassDescriptor descriptor = super.buildReturningFieldInfoDescriptor();
 
-        XMLDirectMapping referenceClassMapping = (XMLDirectMapping)descriptor.getMappingForAttributeName("referenceClass");
+        XMLDirectMapping referenceClassMapping = 
+        	(XMLDirectMapping)descriptor.getMappingForAttributeName("referenceClass");
         referenceClassMapping.setGetMethodName("getReferenceClassName");
         referenceClassMapping.setSetMethodName("setReferenceClassName");
 
@@ -158,11 +172,13 @@ public class ObjectPersistenceWorkbenchXMLProject extends EclipseLinkObjectPersi
     protected ClassDescriptor buildClassDescriptorDescriptor() {
         ClassDescriptor descriptor = super.buildClassDescriptorDescriptor();
 
-        XMLDirectMapping referenceClassMapping = (XMLDirectMapping)descriptor.getMappingForAttributeName("javaClass");
+        XMLDirectMapping referenceClassMapping = 
+        	(XMLDirectMapping)descriptor.getMappingForAttributeName("javaClass");
         referenceClassMapping.setGetMethodName("getJavaClassName");
         referenceClassMapping.setSetMethodName("setJavaClassName");
         
-        XMLDirectMapping amendmentClassMapping = (XMLDirectMapping)descriptor.getMappingForAttributeName("amendmentClass");
+        XMLDirectMapping amendmentClassMapping = 
+        	(XMLDirectMapping)descriptor.getMappingForAttributeName("amendmentClass");
         amendmentClassMapping.setGetMethodName("getAmendmentClassName");
         amendmentClassMapping.setSetMethodName("setAmendmentClassName");
         
@@ -183,11 +199,13 @@ public class ObjectPersistenceWorkbenchXMLProject extends EclipseLinkObjectPersi
     protected ClassDescriptor buildTypeConversionConverterDescriptor() {
         ClassDescriptor descriptor = super.buildTypeConversionConverterDescriptor();
 
-        XMLDirectMapping objectClassMapping = (XMLDirectMapping)descriptor.getMappingForAttributeName("objectClass");
+        XMLDirectMapping objectClassMapping = 
+        	(XMLDirectMapping)descriptor.getMappingForAttributeName("objectClass");
         objectClassMapping.setGetMethodName("getObjectClassName");
         objectClassMapping.setSetMethodName("setObjectClassName");
         
-        XMLDirectMapping dataClassNameMapping = (XMLDirectMapping)descriptor.getMappingForAttributeName("dataClass");
+        XMLDirectMapping dataClassNameMapping = 
+        	(XMLDirectMapping)descriptor.getMappingForAttributeName("dataClass");
         dataClassNameMapping.setGetMethodName("getDataClassName");
         dataClassNameMapping.setSetMethodName("setDataClassName");
 
@@ -208,7 +226,8 @@ public class ObjectPersistenceWorkbenchXMLProject extends EclipseLinkObjectPersi
     protected ClassDescriptor buildAbstractTransformationMappingDescriptor() {
         ClassDescriptor descriptor = super.buildAbstractTransformationMappingDescriptor();
 
-        XMLDirectMapping objectClassMapping = (XMLDirectMapping)descriptor.getMappingForAttributeName("attributeTransformerClass");
+        XMLDirectMapping objectClassMapping = 
+        	(XMLDirectMapping)descriptor.getMappingForAttributeName("attributeTransformerClass");
         objectClassMapping.setGetMethodName("getAttributeTransformerClassName");
         objectClassMapping.setSetMethodName("setAttributeTransformerClassName");
 
@@ -218,7 +237,8 @@ public class ObjectPersistenceWorkbenchXMLProject extends EclipseLinkObjectPersi
     protected ClassDescriptor buildTransformerBasedFieldTransformationDescriptor() {
         ClassDescriptor descriptor = super.buildTransformerBasedFieldTransformationDescriptor();
 
-        XMLDirectMapping objectClassMapping = (XMLDirectMapping)descriptor.getMappingForAttributeName("transformerClass");
+        XMLDirectMapping objectClassMapping = 
+        	(XMLDirectMapping)descriptor.getMappingForAttributeName("transformerClass");
         objectClassMapping.setGetMethodName("getTransformerClassName");
         objectClassMapping.setSetMethodName("setTransformerClassName");
         
