4c68b7785069801165f9e0ca71fb4145ed4ab56e
David Twelves <david.twelves@oracle.com>
2008-05-27 17:08:44 +0000

    Update EPL header

--- dbws/org.eclipse.persistence.dbws/src/org/eclipse/persistence/tools/dbws/MetroPackager.java
+++ dbws/org.eclipse.persistence.dbws/src/org/eclipse/persistence/tools/dbws/MetroPackager.java
@@ -1,4 +1,17 @@
-package org.eclipse.persistence.tools.dbws;
+/*******************************************************************************
+ * Copyright (c) 1998, 2008 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
+ * which accompanies this distribution. 
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     Mike Norman - May 26 2008, creating packager for JAX-WS RI
+ ******************************************************************************/
+ 
+ package org.eclipse.persistence.tools.dbws;
 
 // javase imports
 import java.io.BufferedOutputStream;
