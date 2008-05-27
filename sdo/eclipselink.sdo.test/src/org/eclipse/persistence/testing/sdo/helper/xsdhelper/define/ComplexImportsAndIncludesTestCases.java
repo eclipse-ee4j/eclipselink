d754c4d92fc6f6a509ca349a2c4541d54efd0a33
David Twelves <david.twelves@oracle.com>
2008-05-27 17:06:37 +0000

    Update EPL header

--- sdo/eclipselink.sdo.test/src/org/eclipse/persistence/testing/sdo/helper/xsdhelper/define/ComplexImportsAndIncludesTestCases.java
+++ sdo/eclipselink.sdo.test/src/org/eclipse/persistence/testing/sdo/helper/xsdhelper/define/ComplexImportsAndIncludesTestCases.java
@@ -1,12 +1,16 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2007 Oracle. All rights reserved.
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0, which accompanies this distribution
- * and is available at http://www.eclipse.org/legal/epl-v10.html.
+ * Copyright (c) 1998, 2008 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
+ * which accompanies this distribution. 
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ * http://www.eclipse.org/org/documents/edl-v10.php.
  *
  * Contributors:
- *     Oracle - initial API and implementation from Oracle TopLink
- ******************************************************************************/
+ *     Denise Smith - Feb 20 2008 Initial test case creation 
+ ******************************************************************************/ 
+
 package org.eclipse.persistence.testing.sdo.helper.xsdhelper.define;
 
 import java.io.FileInputStream;
