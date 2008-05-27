8faee26adafad2813178d95a758250d3c4c94db5
Peter Krogh <peter.krogh@oracle.com>
2008-05-27 15:57:41 +0000

    no bug number - fixed header issues.  Files contained old header.  Some files had incorrect contributors.

--- plugins/org.eclipse.persistence.antlr/src/org/eclipse/persistence/internal/libraries/antlr/runtime/ClassicToken.java
+++ plugins/org.eclipse.persistence.antlr/src/org/eclipse/persistence/internal/libraries/antlr/runtime/ClassicToken.java
@@ -1,12 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 1998, 2007 Oracle. All rights reserved.
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0, which accompanies this distribution
- * and is available at http://www.eclipse.org/legal/epl-v10.html.
- *
- * Contributors:
- *     Oracle - initial API and implementation from Oracle TopLink
- ******************************************************************************/  
 package org.eclipse.persistence.internal.libraries.antlr.runtime;
 
 /** A Token object like we'd use in ANTLR 2.x; has an actual string created
