23176bb19160bc740bc88f6186e7abc84698dc38
Doug Clarke <douglas.clarke@oracle.com>
2008-06-06 15:32:39 +0000

    Bug 209534: Fixed typo in static and error message string for error 6147 "PARTIAL"

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/exceptions/QueryException.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/exceptions/QueryException.java
@@ -157,7 +157,7 @@ public class QueryException extends ValidationException {
     public final static int QUERY_HINT_DID_NOT_CONTAIN_ENOUGH_TOKENS = 6144;  
     public final static int DISTINCT_COUNT_ON_OUTER_JOINED_COMPOSITE_PK = 6145;
     public final static int QUERY_HINT_CONTAINED_INVALID_INTEGER_VALUE = 6146;
-    public final static int EXPRESSION_DOES_NOT_SUPPORT_PARTAIL_ATTRIBUTE_READING = 6147;    
+    public final static int EXPRESSION_DOES_NOT_SUPPORT_PARTIAL_ATTRIBUTE_READING = 6147;    
     public final static int ADD_ARGS_NOT_SUPPORTED = 6148;
     public final static int UNNAMED_ARG_NOT_SUPPORTED = 6149;
     public final static int MAP_KEY_IS_NULL = 6150;
@@ -1322,8 +1322,8 @@ public class QueryException extends ValidationException {
     public static QueryException expressionDoesNotSupportPartialAttributeReading(Expression expression) {
         Object[] args = {expression};
     
-        QueryException queryException = new QueryException(ExceptionMessageGenerator.buildMessage(QueryException.class, EXPRESSION_DOES_NOT_SUPPORT_PARTAIL_ATTRIBUTE_READING, args));
-        queryException.setErrorCode(EXPRESSION_DOES_NOT_SUPPORT_PARTAIL_ATTRIBUTE_READING);
+        QueryException queryException = new QueryException(ExceptionMessageGenerator.buildMessage(QueryException.class, EXPRESSION_DOES_NOT_SUPPORT_PARTIAL_ATTRIBUTE_READING, args));
+        queryException.setErrorCode(EXPRESSION_DOES_NOT_SUPPORT_PARTIAL_ATTRIBUTE_READING);
         return queryException;
     }
 
