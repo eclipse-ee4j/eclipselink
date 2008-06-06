c93f19ba9545713aefafd41ceccfab095d697403
Rick Barkhouse <rick.barkhouse@oracle.com>
2008-06-06 17:18:24 +0000

    Initial check-in for simple root element support (bug 217508)

---
+++ sdo/eclipselink.sdo.test/src/org/eclipse/persistence/testing/sdo/model/dataobject/SDODataObjectTestAdditionalSuite.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 1998, 2008 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
+ * which accompanies this distribution. 
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     Oracle - initial API and implementation from Oracle TopLink
+ ******************************************************************************/  
+package org.eclipse.persistence.testing.sdo.model.dataobject;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import org.eclipse.persistence.testing.sdo.helper.datahelper.DataHelperToYearMonthDayTest;
+import org.eclipse.persistence.testing.sdo.helper.datahelper.DataHelperToYearMonthDayWithCalnTest;
+import org.eclipse.persistence.testing.sdo.helper.datahelper.DataHelperToYearMonthTest;
+import org.eclipse.persistence.testing.sdo.helper.datahelper.DataHelperToYearMonthWithCalnTest;
+import org.eclipse.persistence.testing.sdo.helper.datahelper.DataHelperToYearTest;
+import org.eclipse.persistence.testing.sdo.helper.datahelper.DataHelperToYearWithCalnTest;
+import org.eclipse.persistence.testing.sdo.model.dataobject.xpathpositional.SDODataObjectXPathPositionalTestSuite;
+import org.eclipse.persistence.testing.sdo.model.dataobject.xpathquery.SDODataObjectXPathQueryTestSuite;
+
+public class SDODataObjectTestAdditionalSuite {
+    public SDODataObjectTestAdditionalSuite() {
+    }
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    /**
+    *  Inherited suite mthod for generating all test cases.
+    * @return
+    */
+    public static Test suite() {
+        TestSuite suite = new TestSuite("Additional SDODataObject Tests");
+
+        // TODO: order is significant run getPathTest before ListWrapperTest
+        //suite.addTest(new TestSuite(SDODataObjectGetPathTest.class));
+        //suite.addTest(new TestSuite(SDODataObjectListWrapperTest.class));
+       
+        return suite;
+    }
+}
