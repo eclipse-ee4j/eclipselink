6dbd5071d9dfc5e92275cf5a4acffd5d6f89ea08
Peter Krogh <peter.krogh@oracle.com>
2008-06-15 01:55:21 +0000

    move to rt

--- buildsystem/buildNightlyList.sh
+++ buildsystem/buildNightlyList.sh
@@ -15,30 +15,30 @@ tmp=$tmp/somedir.$RANDOM.$RANDOM.$RANDOM.$$
   exit 1
 }
 cat ./trunk/buildsystem/phphead.txt > $tmp/index.xml
-find /home/data/httpd/download.eclipse.org/technology/eclipselink/nightly -name eclipselink-incubation-[1-9]\*.zip -printf '%h        <p> <a href="http://www.eclipse.org/downloads/download.php?file=/technology/eclipselink/nightly/%f"> %f </a>    -----    <a href="http://www.eclipse.org/eclipselink/testing/index.php"> Test Results </a></p>\n' | grep -v '/2008' | cut -d' ' -f2- | sort -r >> $tmp/index.xml
+find /home/data/httpd/download.eclipse.org/rt/eclipselink/nightly -name eclipselink-incubation-[1-9]\*.zip -printf '%h        <p> <a href="http://www.eclipse.org/downloads/download.php?file=/rt/eclipselink/nightly/%f"> %f </a>    -----    <a href="http://www.eclipse.org/eclipselink/testing/index.php"> Test Results </a></p>\n' | grep -v '/2008' | cut -d' ' -f2- | sort -r >> $tmp/index.xml
 echo \<\/description\> >> $tmp/index.xml
 echo \<\/section\> >> $tmp/index.xml
 echo \<section class=\"main\" name=\"1.0 Nightly Builds - OSGi Plugins\"\> >> $tmp/index.xml
 echo \<description\> >> $tmp/index.xml
 
-find /home/data/httpd/download.eclipse.org/technology/eclipselink/nightly -name eclipselink-plugins-incubation\*.zip -printf '%h        <p> <a href="http://www.eclipse.org/downloads/download.php?file=/technology/eclipselink/nightly/%f"> %f </a></p>\n' | grep -v '/2008' | cut -d' ' -f2- | sort -r >> $tmp/index.xml
+find /home/data/httpd/download.eclipse.org/rt/eclipselink/nightly -name eclipselink-plugins-incubation\*.zip -printf '%h        <p> <a href="http://www.eclipse.org/downloads/download.php?file=/rt/eclipselink/nightly/%f"> %f </a></p>\n' | grep -v '/2008' | cut -d' ' -f2- | sort -r >> $tmp/index.xml
 cat ./trunk/buildsystem/phptail.txt >> $tmp/index.xml
 
 cat ./trunk/buildsystem/testinghead.txt > $tmp/testing.xml
 #core test results
 
-find /home/data/httpd/download.eclipse.org/technology/eclipselink/nightly/test-results/core -name \*.html -printf '        <p> <a href="http://download.eclipse.org/technology/eclipselink/nightly/test-results/core/%f"> %f </a></p>\n' | sort -r >> $tmp/testing.xml
+find /home/data/httpd/download.eclipse.org/rt/eclipselink/nightly/test-results/core -name \*.html -printf '        <p> <a href="http://download.eclipse.org/rt/eclipselink/nightly/test-results/core/%f"> %f </a></p>\n' | sort -r >> $tmp/testing.xml
 
-find /home/data/httpd/download.eclipse.org/technology/eclipselink/nightly/test-results/jpa -name \*.html -printf '        <p> <a href="http://download.eclipse.org/technology/eclipselink/nightly/test-results/jpa/%f"> %f </a></p>\n' | sort -r >> $tmp/testing.xml
+find /home/data/httpd/download.eclipse.org/rt/eclipselink/nightly/test-results/jpa -name \*.html -printf '        <p> <a href="http://download.eclipse.org/rt/eclipselink/nightly/test-results/jpa/%f"> %f </a></p>\n' | sort -r >> $tmp/testing.xml
 
-find /home/data/httpd/download.eclipse.org/technology/eclipselink/nightly/test-results/moxy -name \*.html -printf '        <p> <a href="http://download.eclipse.org/technology/eclipselink/nightly/test-results/moxy/%f"> %f </a></p>\n' | sort -r >> $tmp/testing.xml
+find /home/data/httpd/download.eclipse.org/rt/eclipselink/nightly/test-results/moxy -name \*.html -printf '        <p> <a href="http://download.eclipse.org/rt/eclipselink/nightly/test-results/moxy/%f"> %f </a></p>\n' | sort -r >> $tmp/testing.xml
 
-find /home/data/httpd/download.eclipse.org/technology/eclipselink/nightly/test-results/sdo -name \*.html -printf '        <p> <a href="http://download.eclipse.org/technology/eclipselink/nightly/test-results/sdo/%f"> %f </a></p>\n' | sort -r >> $tmp/testing.xml
+find /home/data/httpd/download.eclipse.org/rt/eclipselink/nightly/test-results/sdo -name \*.html -printf '        <p> <a href="http://download.eclipse.org/rt/eclipselink/nightly/test-results/sdo/%f"> %f </a></p>\n' | sort -r >> $tmp/testing.xml
 
 cat ./trunk/buildsystem/testingtail.txt >> $tmp/testing.xml
 
-mv -f $tmp/index.xml  /home/data/httpd/download.eclipse.org/technology/eclipselink/downloads.xml
-mv -f $tmp/testing.xml  /home/data/httpd/download.eclipse.org/technology/eclipselink/testing.xml
+mv -f $tmp/index.xml  /home/data/httpd/download.eclipse.org/rt/eclipselink/downloads.xml
+mv -f $tmp/testing.xml  /home/data/httpd/download.eclipse.org/rt/eclipselink/testing.xml
 rm -rf $tmp
 
 
--- buildsystem/cleanNightly.sh
+++ buildsystem/cleanNightly.sh
@@ -7,7 +7,7 @@ num_files=10
 # leave only the last $num_files (10) builds on the download server
 # OSGi plugin install
 index=0
-for zipfile in `ls -r /home/data/httpd/download.eclipse.org/technology/eclipselink/nightly/eclipselink-plugins-incubation*.zip ` ; do
+for zipfile in `ls -r /home/data/httpd/download.eclipse.org/rt/eclipselink/nightly/eclipselink-plugins-incubation*.zip ` ; do
 	index=`expr $index + 1`
         if [ $index -gt $num_files ] ; then
            rm $zipfile
@@ -16,7 +16,7 @@ done
 
 # SRC install
 index=0
-for zipfile in `ls -r /home/data/httpd/download.eclipse.org/technology/eclipselink/nightly/eclipselink-incubation-src*.zip ` ; do
+for zipfile in `ls -r /home/data/httpd/download.eclipse.org/rt/eclipselink/nightly/eclipselink-incubation-src*.zip ` ; do
 	index=`expr $index + 1`
         if [ $index -gt $num_files ] ; then
            rm $zipfile
@@ -24,7 +24,7 @@ for zipfile in `ls -r /home/data/httpd/download.eclipse.org/technology/eclipseli
 done
 # EL install
 index=0
-for zipfile in `ls -r /home/data/httpd/download.eclipse.org/technology/eclipselink/nightly/eclipselink-incubation-[0-9]*.zip ` ; do
+for zipfile in `ls -r /home/data/httpd/download.eclipse.org/rt/eclipselink/nightly/eclipselink-incubation-[0-9]*.zip ` ; do
 	index=`expr $index + 1`
         if [ $index -gt $num_files ] ; then
            rm $zipfile
@@ -34,46 +34,46 @@ done
 for suite in core jpa sdo  ; do
     # Clean SRG Results
     index=0
-    for htmlfile in `ls -r /home/data/httpd/download.eclipse.org/technology/eclipselink/nightly/test-results/${suite}/eclipse*srg*.html ` ; do
+    for htmlfile in `ls -r /home/data/httpd/download.eclipse.org/rt/eclipselink/nightly/test-results/${suite}/eclipse*srg*.html ` ; do
         index=`expr $index + 1`
         if [ $index -gt $num_files ] ; then
             rm $htmlfile
         fi
         if [ $index -eq 1 ] ; then
-            cp $htmlfile /home/data/httpd/download.eclipse.org/technology/eclipselink/nightly/test-results/${suite}/latest-srg-test-results.html
+            cp $htmlfile /home/data/httpd/download.eclipse.org/rt/eclipselink/nightly/test-results/${suite}/latest-srg-test-results.html
         fi 
     done
 
     # Clean LRG Results
     index=0
-    for htmlfile in `ls -r /home/data/httpd/download.eclipse.org/technology/eclipselink/nightly/test-results/${suite}/eclipse*lrg*.html ` ; do
+    for htmlfile in `ls -r /home/data/httpd/download.eclipse.org/rt/eclipselink/nightly/test-results/${suite}/eclipse*lrg*.html ` ; do
         index=`expr $index + 1`
         if [ $index -gt $num_files ] ; then
             rm $htmlfile
         fi
         if [ $index -eq 1 ] ; then
-            cp $htmlfile /home/data/httpd/download.eclipse.org/technology/eclipselink/nightly/test-results/${suite}/latest-lrg-test-results.html
+            cp $htmlfile /home/data/httpd/download.eclipse.org/rt/eclipselink/nightly/test-results/${suite}/latest-lrg-test-results.html
         fi 
     done
 done
 index=0
-for htmlfile in `ls -r /home/data/httpd/download.eclipse.org/technology/eclipselink/nightly/test-results/moxy/eclipse*oxm*.html ` ; do
+for htmlfile in `ls -r /home/data/httpd/download.eclipse.org/rt/eclipselink/nightly/test-results/moxy/eclipse*oxm*.html ` ; do
     index=`expr $index + 1`
     if [ $index -gt $num_files ] ; then
         rm $htmlfile
     fi
     if [ $index -eq 1 ] ; then
-        cp $htmlfile /home/data/httpd/download.eclipse.org/technology/eclipselink/nightly/test-results/moxy/latest-oxm-test-results.html
+        cp $htmlfile /home/data/httpd/download.eclipse.org/rt/eclipselink/nightly/test-results/moxy/latest-oxm-test-results.html
     fi 
 done
 index=0
-for htmlfile in `ls -r /home/data/httpd/download.eclipse.org/technology/eclipselink/nightly/test-results/moxy/eclipse*jaxb*.html ` ; do
+for htmlfile in `ls -r /home/data/httpd/download.eclipse.org/rt/eclipselink/nightly/test-results/moxy/eclipse*jaxb*.html ` ; do
     index=`expr $index + 1`
     if [ $index -gt $num_files ] ; then
         rm $htmlfile
     fi
     if [ $index -eq 1 ] ; then
-        cp $htmlfile /home/data/httpd/download.eclipse.org/technology/eclipselink/nightly/test-results/moxy/latest-jaxb-test-results.html
+        cp $htmlfile /home/data/httpd/download.eclipse.org/rt/eclipselink/nightly/test-results/moxy/latest-jaxb-test-results.html
     fi 
 done
 
@@ -81,7 +81,7 @@ done
 index=0
 # 5 days worth of files - 9 files per day
 num_files=45
-for mvnfile in `ls -r /home/data/httpd/download.eclipse.org/technology/eclipselink/maven.repo/org/eclipse/persistence/eclipselink/1.0-SNAPSHOT/eclipse*.* ` ; do
+for mvnfile in `ls -r /home/data/httpd/download.eclipse.org/rt/eclipselink/maven.repo/org/eclipse/persistence/eclipselink/1.0-SNAPSHOT/eclipse*.* ` ; do
         index=`expr $index + 1`
         if [ $index -gt $num_files ] ; then
            rm $mvnfile
