296e67f394dbfb1a4d86272eef9907fb49184b20
Tom Ware <tom.ware@oracle.com>
2008-08-15 13:30:58 +0000

    Maintain backwards compatibility for 1.0 stream

---
+++ foundation/org.eclipse.persistence.oracle/src/org/eclipse/persistence/platform/database/oracle/OraclePlatform.java
@@ -0,0 +1,18 @@
+package org.eclipse.persistence.platform.database.oracle;
+
+/**
+ * <p><b>Purpose</b>: Provides Oracle specific behavior.
+ * <p><b>Responsibilities</b>:<ul>
+ * <li> Native SQL for byte[], Date, Time, & Timestamp.
+ * <li> Native sequencing named sequences.
+ * <li> Native SQL/ROWNUM support for MaxRows and FirstResult filtering.
+ * </ul>
+ *
+ * @deprecated
+ * @see org.eclipse.persistence.platform.database.OraclePlatform
+ * @since TOPLink/Java 1.0
+ */
+public class OraclePlatform extends
+		org.eclipse.persistence.platform.database.OraclePlatform {
+
+}
