c2342a0820084da0ce07f5e145b8af49e2eb45b3
Eric Gwin <eric.gwin@oracle.com>
2008-10-16 19:01:39 +0000

    Added class to call "Version.printVersion()" method.

---
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/printVersion.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 1998, 2008 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     Eric Gwin - initial implementation 
+ ******************************************************************************/
+package org.eclipse.persistence;
+
+public class printVersion {
+
+    public static void main ( String[] args ) {
+        org.eclipse.persistence.Version.printVersion();          
+    }
+}
