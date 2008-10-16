cf5664d26bd0089eeff13ad9f1555dff19ea0099
Mike Norman <mike.norman@oracle.com>
2008-10-16 17:54:54 +0000

    https://bugs.eclipse.org/bugs/show_bug.cgi?id=251107  - part of work to package DBWSBuilder as part of EclipseLink installer.zip

---
+++ utils/eclipselink.utils.dbws/dbwsbuilder.cmd
@@ -0,0 +1,34 @@
+@echo off
+setlocal
+call "%~dp0setenv.cmd"
+
+@REM User may increase Java memory setting(s) if desired:
+set JVM_ARGS=-Xmx256m
+
+REM Please do not change any of the following lines:
+set _FIXPATH=
+call :fixpath "%~dp0"
+set THIS=%_FIXPATH:~1%
+
+set CLASSPATH=%THIS%..\jlib\javax.wsdl_1.6.2.jar
+set CLASSPATH=%CLASSPATH%;%DRIVER_CLASSPATH%
+set CLASSPATH=%CLASSPATH%;%THIS%..\eclipselink-dbwsutils.jar
+
+set DBWSBUILDER_ARGS=%*
+ 
+start %JAVA_HOME%\bin\javaw.exe %JVM_ARGS% -cp %CLASSPATH% org.eclipse.persistence.tools.dbws.DBWSBuilder %DBWSBUILDER_ARGS%
+
+endlocal
+goto :EOF
+
+:fixpath
+if not %1.==. (
+  for /f "tokens=1* delims=;" %%a in (%1) do (
+    call :shortfilename "%%a" & call :fixpath "%%b"
+  )
+)
+goto :EOF
+
+:shortfilename
+for %%i in (%1) do set _FIXPATH=%_FIXPATH%;%%~fsi
+goto :EOF
---
+++ utils/eclipselink.utils.dbws/setenv.cmd
@@ -0,0 +1,29 @@
+@REM User MUST set DRIVER_CLASSPATH to point to their desired JDBC driver jar(s).
+@REM For example:
+@REM DRIVER_CLASSPATH=/some/directory/mysql-connector-java-5.1.6-bin.jar
+@REM 
+set DRIVER_CLASSPATH=
+
+@REM User MUST set JAVA_HOME to point a JavaSE 6 JDK installation
+set JAVA_HOME=
+
+@REM Please do not change any of the following lines:
+
+set _FIXPATH=
+call :fixpath "%DRIVER_CLASSPATH%"
+set DRIVER_CLASSPATH=%_FIXPATH:~1%
+set _FIXPATH=
+call :fixpath "%JAVA_HOME%"
+set JAVA_HOME=%_FIXPATH:~1%
+goto :EOF
+
+:fixpath
+if not %1.==. (
+  for /f "tokens=1* delims=;" %%a in (%1) do (
+    call :shortfilename "%%a" & call :fixpath "%%b"
+  )
+)
+goto :EOF
+:shortfilename
+for %%i in (%1) do set _FIXPATH=%_FIXPATH%;%%~fsi
+goto :EOF
