a2eddb213ca239cd166efeeea1a6db5932603680
Edwin Tang <edwin.tang@oracle.com>
2008-10-21 19:25:24 +0000

    Comment out two tests which have validation error in ASKERNEL SRG per Yiping's request

--- moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/oxm/OXMSRGTestSuite.java
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/oxm/OXMSRGTestSuite.java
@@ -16,12 +16,15 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestResult;
 import junit.framework.TestSuite;
-import org.eclipse.persistence.testing.oxm.inheritance.InheritanceTestSuite;
+import org.eclipse.persistence.testing.oxm.inheritance.*;
+import org.eclipse.persistence.testing.oxm.inheritance.typetests.TypeTestSuite;
+import org.eclipse.persistence.testing.oxm.inheritance.classextractor.CarClassExtractorTestCases;
 import org.eclipse.persistence.testing.oxm.xmlmarshaller.XMLMarshallerTestSuite;
 import org.eclipse.persistence.testing.oxm.descriptor.rootelement.RootElementTestSuite;
 import org.eclipse.persistence.testing.oxm.xpathengine.XPathEngineTestSuite;
 import org.eclipse.persistence.testing.oxm.xmllogin.XMLLoginTestSuite;
-import org.eclipse.persistence.testing.oxm.converter.ConverterTestSuite;
+import org.eclipse.persistence.testing.oxm.converter.*;
+import org.eclipse.persistence.testing.oxm.converter.typesafeenum.TypeSafeEnumConverterTestCases;
 
 public class OXMSRGTestSuite extends TestCase {
     public OXMSRGTestSuite(String name) {
@@ -34,14 +37,25 @@ public class OXMSRGTestSuite extends TestCase {
 
     public static Test suite() {
         TestSuite suite = new TestSuite("OXM SRG Test Suite");
-
+        //line 40 and 55 are commented out due to test logic validation error, add individual inheritance and converter tests instead
         //suite.addTest(InheritanceTestSuite.suite());
+        //suite.addTestSuite(InheritanceTestCases.class);
+        suite.addTestSuite(InheritanceMissingDescriptorTestCases.class);
+        suite.addTestSuite(InheritanceCarTestCases.class);
+        suite.addTestSuite(InheritanceCarDiffPrefixTestCases.class);
+        suite.addTestSuite(InheritanceCarNoPrefixTestCases.class);
+        suite.addTestSuite(InheritanceVehicleTestCases.class);
+        suite.addTestSuite(InheritanceVehicleDiffPrefixTestCases.class);
+        suite.addTestSuite(InheritanceDiffPrefixNonRootTestCases.class);
+        suite.addTest(TypeTestSuite.suite());
+        suite.addTestSuite(CarClassExtractorTestCases.class);
         suite.addTest(XMLMarshallerTestSuite.suite()); 
         suite.addTest(RootElementTestSuite.suite());
         suite.addTest(XPathEngineTestSuite.suite());
         suite.addTest(XMLLoginTestSuite.suite());
         //suite.addTest(ConverterTestSuite.suite());
-     
+        //suite.addTestSuite(ObjectTypeConverterTestCases.class);
+        suite.addTestSuite(TypeSafeEnumConverterTestCases.class);
         return suite;
     }
 }
