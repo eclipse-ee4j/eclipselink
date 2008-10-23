1853db5ec5e607c506658b0506ef48c2c4913afe
Mike Norman <mike.norman@oracle.com>
2008-10-23 14:42:23 +0000

    trivial fix - remove unused imports

--- foundation/eclipselink.extension.oracle.test/src/org/eclipse/persistence/testing/models/jpa/timestamptz/TStamp.java
+++ foundation/eclipselink.extension.oracle.test/src/org/eclipse/persistence/testing/models/jpa/timestamptz/TStamp.java
@@ -13,18 +13,12 @@
 package org.eclipse.persistence.testing.models.jpa.timestamptz;
 
 import java.io.Serializable;
-import java.util.Date;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.persistence.Version;
 
 import java.util.Calendar;
 import oracle.sql.TIMESTAMPTZ;
