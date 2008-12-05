f674ab5f52c0d3b108bcd2156b1d7c8cf86c7196
Tom Ware <tom.ware@oracle.com>
2008-12-05 14:23:17 +0000

    - Added definition of Cache

---
+++ jpa/plugins/javax.persistence/src/javax/persistence/Cache.java
@@ -0,0 +1,25 @@
+package javax.persistence;
+/**
+* Interface used to interact with the second-level cache.
+* If a cache is not in use, the methods of this interface have
+* no effect, except for contains, which returns false.
+*/
+public interface Cache {
+/**
+* Whether the cache contains data for the given entity.
+*/
+public boolean contains(Class cls, Object primaryKey);
+/**
+* Remove the data for the given entity from the cache.
+*/
+public void evict(Class cls, Object primaryKey);
+/**
+* Remove the data for entities of the specified class (and its
+subclasses) from the cache.
+*/
+public void evict(Class cls);
+/**
+* Clear the cache.
+*/
+public void evictAll();
+}
