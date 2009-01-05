35038494ed756aeae7244063a148c8928f06fba0
Mike Norman <mike.norman@oracle.com>
2009-01-05 16:32:30 +0000

    trivial fix - remove unused imports

--- jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/tests/jpa/fieldaccess/relationships/VirtualAttributeTestSuite.java
+++ jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/tests/jpa/fieldaccess/relationships/VirtualAttributeTestSuite.java
@@ -14,7 +14,6 @@ package org.eclipse.persistence.testing.tests.jpa.fieldaccess.relationships;
 
 import javax.persistence.EntityManager;
 
-import junit.extensions.TestSetup;
 import junit.framework.*;
 
 import org.eclipse.persistence.testing.framework.junit.JUnitTestCase;
