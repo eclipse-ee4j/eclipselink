87cc778797c9fc9c29e31e374f8954f9c711530f
Mike Norman <mike.norman@oracle.com>
2009-01-05 16:22:17 +0000

    trivial fix - remove unused imports

--- jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/tests/jpa/advanced/OptimisticConcurrencyJUnitTestSuite.java
+++ jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/tests/jpa/advanced/OptimisticConcurrencyJUnitTestSuite.java
@@ -14,11 +14,9 @@ package org.eclipse.persistence.testing.tests.jpa.advanced;
 
 import org.eclipse.persistence.testing.framework.junit.JUnitTestCase;
 import org.eclipse.persistence.testing.models.jpa.advanced.*;
-import org.eclipse.persistence.sessions.DatabaseSession;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import junit.extensions.TestSetup;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
