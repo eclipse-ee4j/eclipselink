1020cf79f2c19efebb4d1f19974f47971cc4f166
Rick Barkhouse <rick.barkhouse@oracle.com>
2009-02-10 17:03:42 +0000

    Improved error message for assertXMLIdentical()

--- moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/oxm/XMLTestCase.java
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/oxm/XMLTestCase.java
@@ -24,7 +24,25 @@ public class XMLTestCase extends junit.framework.TestCase {
     }
     
     public void assertXMLIdentical(Document control, Document test) {
-        assertTrue("Node " + control + " is not equal to node " + test, xmlComparer.isNodeEqual(control, test));
+        boolean isEqual = xmlComparer.isNodeEqual(control, test);
+        String controlString = "";
+        String testString = "";
+        
+        if (!isEqual) {
+            org.eclipse.persistence.platform.xml.XMLTransformer t = 
+                org.eclipse.persistence.platform.xml.XMLPlatformFactory.getInstance().getXMLPlatform().newXMLTransformer();
+            java.io.StringWriter controlWriter = new java.io.StringWriter();
+            t.transform(control, controlWriter);
+            
+            t = org.eclipse.persistence.platform.xml.XMLPlatformFactory.getInstance().getXMLPlatform().newXMLTransformer();
+            java.io.StringWriter testWriter = new java.io.StringWriter();
+            t.transform(test, testWriter);
+
+            controlString = controlWriter.toString();
+            testString = testWriter.toString();
+        }
+        
+        assertTrue("Documents are not equal.\nCONTROL:\n" + controlString + "\nTEST:\n" + testString, isEqual);
     }
     
     public XMLComparer getXMLComparer() {
