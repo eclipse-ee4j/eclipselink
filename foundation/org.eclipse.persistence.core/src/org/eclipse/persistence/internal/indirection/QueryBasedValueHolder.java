0579b04845e12d82e35162501b19313a6ef17bfa
Doug Clarke <douglas.clarke@oracle.com>
2009-03-27 15:25:37 +0000

    Bug 270272 - Simple fix making getQuery public - reviewed with pkrogh

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/indirection/QueryBasedValueHolder.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/indirection/QueryBasedValueHolder.java
@@ -9,7 +9,7 @@
  *
  * Contributors:
  *     Oracle - initial API and implementation from Oracle TopLink
- ******************************************************************************/  
+ ******************************************************************************/
 package org.eclipse.persistence.internal.indirection;
 
 import org.eclipse.persistence.queries.*;
@@ -18,12 +18,12 @@ import org.eclipse.persistence.internal.sessions.AbstractRecord;
 import org.eclipse.persistence.internal.sessions.AbstractSession;
 
 /**
- * QueryBasedValueHolder wraps a database-stored object and
- * implements behavior to access it. The object is read from
- * the database by invoking a user-specified query.
- *
- * @see        ObjectLevelReadQuery
- * @author    Dorin Sandu
+ * QueryBasedValueHolder wraps a database-stored object and implements behavior
+ * to access it. The object is read from the database by invoking a
+ * user-specified query.
+ * 
+ * @see ObjectLevelReadQuery
+ * @author Dorin Sandu
  */
 public class QueryBasedValueHolder extends DatabaseValueHolder {
 
@@ -33,7 +33,8 @@ public class QueryBasedValueHolder extends DatabaseValueHolder {
     protected transient ReadQuery query;
 
     /**
-     * Store the uow identity so that it can be used to determine new transaction logic
+     * Store the uow identity so that it can be used to determine new
+     * transaction logic
      */
     /**
      * Initialize the query-based value holder.
@@ -44,11 +45,14 @@ public class QueryBasedValueHolder extends DatabaseValueHolder {
 
         // Make sure not to put a ClientSession or IsolatedClientSession in
         // the shared cache (indirectly).
-        // Skip this if unitOfWork, for we use session.isUnitOfWork() to implement
-        // isTransactionalValueholder(), saving us from needing a boolean instance variable.
+        // Skip this if unitOfWork, for we use session.isUnitOfWork() to
+        // implement
+        // isTransactionalValueholder(), saving us from needing a boolean
+        // instance variable.
         // If unitOfWork this safety measure is deferred until merge time with
         // releaseWrappedValuehHolder.
-        // Note that if isolated session & query will return itself, which is safe
+        // Note that if isolated session & query will return itself, which is
+        // safe
         // for if isolated it will not go in the shared cache.
         if (!session.isUnitOfWork()) {
             this.session = session.getRootSession(query);
@@ -57,9 +61,13 @@ public class QueryBasedValueHolder extends DatabaseValueHolder {
     }
 
     /**
-     * Return the query.
+     * Return the query. The query for a QueryBasedValueHolder is constructed
+     * dynamically based on the original query on the parent object and the
+     * mapping configuration. If modifying a query the developer must be careful
+     * not to change the results returned as that may cause the application to
+     * see incorrect results.
      */
-    protected ReadQuery getQuery() {
+    public ReadQuery getQuery() {
         return query;
     }
 
@@ -71,7 +79,7 @@ public class QueryBasedValueHolder extends DatabaseValueHolder {
      * Instantiate the object by executing the query on the session.
      */
     protected Object instantiate(AbstractSession session) throws DatabaseException {
-        if (session == null){
+        if (session == null) {
             throw ValidationException.instantiatingValueholderWithNullSession();
         }
         return session.executeQuery(getQuery(), getRow());
@@ -81,25 +89,26 @@ public class QueryBasedValueHolder extends DatabaseValueHolder {
      * Triggers UnitOfWork valueholders directly without triggering the wrapped
      * valueholder (this).
      * <p>
-     * When in transaction and/or for pessimistic locking the UnitOfWorkValueHolder
-     * needs to be triggered directly without triggering the wrapped valueholder.
-     * However only the wrapped valueholder knows how to trigger the indirection,
-     * i.e. it may be a batchValueHolder, and it stores all the info like the row
-     * and the query.
-     * Note: This method is not thread-safe.  It must be used in a synchronized manner
+     * When in transaction and/or for pessimistic locking the
+     * UnitOfWorkValueHolder needs to be triggered directly without triggering
+     * the wrapped valueholder. However only the wrapped valueholder knows how
+     * to trigger the indirection, i.e. it may be a batchValueHolder, and it
+     * stores all the info like the row and the query. Note: This method is not
+     * thread-safe. It must be used in a synchronized manner
      */
     public Object instantiateForUnitOfWorkValueHolder(UnitOfWorkValueHolder unitOfWorkValueHolder) {
         return instantiate(unitOfWorkValueHolder.getUnitOfWork());
     }
 
     /**
-     * Releases a wrapped valueholder privately owned by a particular unit of work.
+     * Releases a wrapped valueholder privately owned by a particular unit of
+     * work.
      * <p>
-     * When unit of work clones are built directly from rows no object in the shared
-     * cache points to this valueholder, so it can store the unit of work as its
-     * session.  However once that UnitOfWork commits and the valueholder is merged
-     * into the shared cache, the session needs to be reset to the root session, ie.
-     * the server session.
+     * When unit of work clones are built directly from rows no object in the
+     * shared cache points to this valueholder, so it can store the unit of work
+     * as its session. However once that UnitOfWork commits and the valueholder
+     * is merged into the shared cache, the session needs to be reset to the
+     * root session, ie. the server session.
      */
     public void releaseWrappedValueHolder() {
         AbstractSession session = getSession();
@@ -124,30 +133,32 @@ public class QueryBasedValueHolder extends DatabaseValueHolder {
     }
 
     /**
-     * INTERNAL:
-     * Answers if this valueholder is a pessimistic locking one.  Such valueholders
-     * are special in that they can be triggered multiple times by different
-     * UnitsOfWork.  Each time a lock query will be issued.  Hence even if
-     * instantiated it may have to be instantiated again, and once instantiated
-     * all fields can not be reset.
+     * INTERNAL: Answers if this valueholder is a pessimistic locking one. Such
+     * valueholders are special in that they can be triggered multiple times by
+     * different UnitsOfWork. Each time a lock query will be issued. Hence even
+     * if instantiated it may have to be instantiated again, and once
+     * instantiated all fields can not be reset.
      * <p>
      * Since locks will be issued each time this valueholder is triggered,
      * triggering this directly on the session in auto commit mode will generate
-     * an exception.  This only UnitOfWorkValueHolder's wrapping this can trigger
-     * it.
-     * Note: This method is not thread-safe.  It must be used in a synchronized manner
+     * an exception. This only UnitOfWorkValueHolder's wrapping this can trigger
+     * it. Note: This method is not thread-safe. It must be used in a
+     * synchronized manner
      */
     public boolean isPessimisticLockingValueHolder() {
         // Get the easy checks out of the way first.
         if ((getQuery() == null) || !getQuery().isObjectLevelReadQuery()) {
             return false;
         }
-        ObjectLevelReadQuery query = (ObjectLevelReadQuery)getQuery();
+        ObjectLevelReadQuery query = (ObjectLevelReadQuery) getQuery();
 
-        // Note even if the reference class is not locked, but the valueholder query
+        // Note even if the reference class is not locked, but the valueholder
+        // query
         // has joined attributes, then this may count as a lock query.
-        // This means it is possible to trigger a valueholder to get an object which
-        // is not to be pess. locked and get an exception for triggering it on the
+        // This means it is possible to trigger a valueholder to get an object
+        // which
+        // is not to be pess. locked and get an exception for triggering it on
+        // the
         // session outside a transaction.
         return query.isLockQuery(getSession());
     }
