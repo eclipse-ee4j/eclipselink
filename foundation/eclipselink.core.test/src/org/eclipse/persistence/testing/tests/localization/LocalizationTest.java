a94b84bdc5b7ec0a12f46a0d3bfcd2becbb8c30e
Tom Ware <tom.ware@oracle.com>
2009-03-30 19:49:44 +0000

    Bug 245923 update - localization fix

--- foundation/eclipselink.core.test/src/org/eclipse/persistence/testing/tests/localization/LocalizationTest.java
+++ foundation/eclipselink.core.test/src/org/eclipse/persistence/testing/tests/localization/LocalizationTest.java
@@ -33,7 +33,7 @@ public class LocalizationTest extends AutoVerifyTestCase {
             throw new TestErrorException("LoggingLocalization.buildMessage could not find the correct translation.");
         }
 
-        if (!nonLocalizedString.equals("message_not_exist")) {
+        if (!nonLocalizedString.equals("message_not_exist (There is no English translation for this message.)")) {
             throw new TestErrorException("LoggingLocalization.buildMessage could not find NoTranslationForThisLocale message.");
         }
     }
