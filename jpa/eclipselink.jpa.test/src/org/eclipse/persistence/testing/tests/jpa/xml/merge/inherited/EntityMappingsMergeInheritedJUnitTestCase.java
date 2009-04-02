81e09dbfb3c2144fc8bcbf9311f06c1844602a13
Guy Pelletier <guy.pelletier@oracle.com>
2009-04-02 14:55:24 +0000

    Changing copyright and bug information

--- jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/tests/jpa/xml/merge/inherited/EntityMappingsMergeInheritedJUnitTestCase.java
+++ jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/tests/jpa/xml/merge/inherited/EntityMappingsMergeInheritedJUnitTestCase.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2008 Oracle. All rights reserved.
+ * Copyright (c) 1998, 2009 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the 
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
  * which accompanies this distribution. 
@@ -11,6 +11,8 @@
  *     Oracle - initial API and implementation from Oracle TopLink
  *     09/23/2008-1.1 Guy Pelletier 
  *       - 241651: JPA 2.0 Access Type support
+ *     04/02/2009-2.0 Guy Pelletier 
+ *       - 270853: testBeerLifeCycleMethodAnnotationIgnored within xml merge testing need to be relocated
  ******************************************************************************/  
 package org.eclipse.persistence.testing.tests.jpa.xml.merge.inherited;
 
--- jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/internal/jpa/metadata/listeners/EntityClassListenerMetadata.java
+++ jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/internal/jpa/metadata/listeners/EntityClassListenerMetadata.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2008 Oracle. All rights reserved.
+ * Copyright (c) 1998, 2009 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the 
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
  * which accompanies this distribution. 
@@ -13,6 +13,8 @@
  *       - 218084: Implement metadata merging functionality between mapping files
  *     06/20/2008-1.0 Guy Pelletier 
  *       - 232975: Failure when attribute type is generic
+ *     04/02/2009-2.0 Guy Pelletier 
+ *       - 270853: testBeerLifeCycleMethodAnnotationIgnored within xml merge testing need to be relocated      
  ******************************************************************************/  
 package org.eclipse.persistence.internal.jpa.metadata.listeners;
 
