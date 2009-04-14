c34007535ebeaafb4ee525781c72341addd491e1
Andrei Ilitchev <andrei.ilitchev@oracle.com>
2009-04-14 15:41:24 +0000

    Rolled back my fix to bug 220402: use sequence connection pool by default. To enable sequencing connection pool use property "eclipselink.jdbc.sequence-connection-pool" -> "true" See fixed bug 270802: Improve default connection pooling introduced more properties to configure sequencing connection pool - see properties with JDBC_SEQUENCE_ prefix in PersistenceUnitProperties class.

--- jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/internal/jpa/EntityManagerSetupImpl.java
+++ jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/internal/jpa/EntityManagerSetupImpl.java
@@ -978,10 +978,6 @@ public class EntityManagerSetupImpl {
             return;
         }
         
-        if (session.getProject().usesSequencing()) {
-            session.getSequencingControl().setShouldUseSeparateConnection(nonjtaDatasource !=  null);
-        }
-        
         login.setUsesExternalTransactionController(transactionType == PersistenceUnitTransactionType.JTA);
 
         javax.sql.DataSource mainDatasource = null;
