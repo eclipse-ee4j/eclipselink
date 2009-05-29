be613d3512bbeaa1579618982819036a74b1c59c
Guy Pelletier <guy.pelletier@oracle.com>
2009-05-29 14:18:24 +0000

    Updating copyright and removing double copyright tag from some testing files.

--- jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/models/jpa/xml/inherited/Consumer.java
+++ jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/models/jpa/xml/inherited/Consumer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2008 Oracle. All rights reserved.
+ * Copyright (c) 1998, 2009 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the 
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
  * which accompanies this distribution. 
@@ -10,8 +10,6 @@
  * Contributors:
  *     Oracle - initial API and implementation from Oracle TopLink
  ******************************************************************************/
-
-
 package org.eclipse.persistence.testing.models.jpa.xml.inherited;
 
 public class Consumer  {
--- jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/models/jpa/xml/inherited/TelephoneNumber.java
+++ jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/models/jpa/xml/inherited/TelephoneNumber.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2008 Oracle. All rights reserved.
+ * Copyright (c) 1998, 2009 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the 
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
  * which accompanies this distribution. 
@@ -9,21 +9,7 @@
  *
  * Contributors:
  *     Oracle - initial API and implementation from Oracle TopLink
- ******************************************************************************/  
-
-
-/*******************************************************************************
- * Copyright (c) 1998, 2008 Oracle. All rights reserved.
- * This program and the accompanying materials are made available under the 
- * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
- * which accompanies this distribution. 
- * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at 
- * http://www.eclipse.org/org/documents/edl-v10.php.
- *
- * Contributors:
- *     Oracle - initial API and implementation from Oracle TopLink
- ******************************************************************************/  
+ ******************************************************************************/   
 package org.eclipse.persistence.testing.models.jpa.xml.inherited;
 
 import java.io.*;
