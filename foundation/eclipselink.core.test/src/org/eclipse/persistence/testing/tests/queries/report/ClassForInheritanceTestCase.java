e55db4d6dc5ad1b0081adc3a52245187026d50d9
Chris Delahunt <chris.delahunt@oracle.com>
2009-06-02 14:42:55 +0000

    patch for feature 277509 - "Need native expressions to support JPQL Entity Type filtering" Changes:  Expression now use a 'type()' to access an indicator field on classes that use inheritance, converting class objects to/from field values Reviewed by Tom Ware --Missed new test classes in the last checkin
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=277509

---
+++ foundation/eclipselink.core.test/src/org/eclipse/persistence/testing/tests/queries/report/ClassForInheritanceTestCase.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (c) 1998, 2008 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
+ * which accompanies this distribution. 
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     May 26, 2009-1.0M6 Chris Delahunt 
+ *       - TODO Bug#: Bug Description 
+ ******************************************************************************/  
+package org.eclipse.persistence.testing.tests.queries.report;
+
+import java.math.BigDecimal;
+import java.util.Enumeration;
+import java.util.Vector;
+
+import org.eclipse.persistence.expressions.ExpressionBuilder;
+import org.eclipse.persistence.internal.helper.ConversionManager;
+import org.eclipse.persistence.internal.helper.Helper;
+import org.eclipse.persistence.queries.ReportQuery;
+import org.eclipse.persistence.queries.ReportQueryResult;
+import org.eclipse.persistence.testing.framework.TestErrorException;
+import org.eclipse.persistence.testing.models.employee.domain.*;
+
+/**
+ * @author cdelahun
+ *
+ */
+public class ClassForInheritanceTestCase extends ReportQueryTestCase {
+
+    /* (non-Javadoc)
+     * @see org.eclipse.persistence.testing.tests.queries.report.ReportQueryTestCase#buildExpectedResults()
+     */
+    @Override
+    protected void buildExpectedResults() throws Exception {
+
+        Vector projects = getSession().readAllObjects(Project.class);
+        Class projClass = null;
+        Project proj = null;
+        for (Enumeration e = projects.elements(); e.hasMoreElements(); ) {
+            proj = (Project)e.nextElement();
+            addResult(new Object[] {proj.getClass(), proj}, null );
+        }
+    }
+
+    protected void setup() throws Exception {
+        super.setup();
+        ExpressionBuilder builder = new ExpressionBuilder();
+        reportQuery = new ReportQuery(Project.class, builder);
+
+        reportQuery.addItem("Type", builder.type());
+        reportQuery.addItem("project", builder);
+    }
+
+}
