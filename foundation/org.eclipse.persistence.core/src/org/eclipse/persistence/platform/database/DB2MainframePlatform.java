2e97aed77faf5d30e9f8ec8f5581e30f2222cfbb
Peter Krogh <peter.krogh@oracle.com>
2009-06-18 12:51:54 +0000

    bug 270881.  User submitted patch.  No new test.

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/platform/database/DB2MainframePlatform.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/platform/database/DB2MainframePlatform.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2008 Oracle. All rights reserved.
+ * Copyright (c) 1998, 2009 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the 
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
  * which accompanies this distribution. 
@@ -24,6 +24,10 @@ import org.eclipse.persistence.expressions.ExpressionOperator;
  * @since TopLink 3.0.3
  */
 public class DB2MainframePlatform extends DB2Platform {
+    public DB2MainframePlatform() {
+        super();
+        this.pingSQL = "SELECT COUNT(*) from SYSIBM.SYSDUMMY1 WHERE 1 = 0";
+    }
 
     /**
      * Initialize any platform-specific operators
