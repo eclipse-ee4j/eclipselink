e3303ba5cd3c954a16efe714acb0e496b832161e
Peter Krogh <peter.krogh@oracle.com>
2009-07-08 16:30:19 +0000

    Copyright date information.
    Change the header from 2008 to 2009.  1.1.0 stream

--- dbws/eclipselink.dbws.test.oracle/src/dbws/testing/plsqlcollection/PLSQLcollectionTestSuite.java
+++ dbws/eclipselink.dbws.test.oracle/src/dbws/testing/plsqlcollection/PLSQLcollectionTestSuite.java
@@ -1,4 +1,17 @@
-package dbws.testing.plsqlcollection;
+/*******************************************************************************
+ * Copyright (c) 1998, 2009 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     Mike Norman - May 2008, created DBWS test package
+ ******************************************************************************/
+ 
+ package dbws.testing.plsqlcollection;
 
 //javase imports
 import java.io.StringReader;
