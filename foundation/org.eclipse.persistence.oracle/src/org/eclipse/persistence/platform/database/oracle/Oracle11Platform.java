90006a43e2d0f7812fa5b9c73acf82e8b02aea6f
Andrei Ilitchev <andrei.ilitchev@oracle.com>
2009-08-21 16:23:41 +0000

    Bug 287318: 5 TESTS OF UNWRAPCONNECTIONNCHARTESTMODEL FAILED AGAINST ORACLE 11GR2 BETA

--- foundation/org.eclipse.persistence.oracle/src/org/eclipse/persistence/platform/database/oracle/Oracle11Platform.java
+++ foundation/org.eclipse.persistence.oracle/src/org/eclipse/persistence/platform/database/oracle/Oracle11Platform.java
@@ -12,9 +12,32 @@
  ******************************************************************************/  
 package org.eclipse.persistence.platform.database.oracle;
 
+import java.sql.ResultSet;
+
+import org.eclipse.persistence.internal.helper.ClassConstants;
+import org.eclipse.persistence.internal.sessions.AbstractSession;
+
 /**
  * <p><b>Purpose:</b>
  * Supports usage of certain Oracle JDBC specific APIs for the Oracle 11 database.
  */
 public class Oracle11Platform extends Oracle10Platform {
+    /* NCLOB sql type is defined in OracleTypes starting with Oracle jdbc version 11.
+     * The constant redefined here because for backward compatibility:
+     * OracleTypes.NCLOB won't compile with Oracle jdbc 10 and earlier.
+     */
+    public final static int OracleTypes_NCLOB = 2011; 
+    /**
+     * INTERNAL:
+     * Get a timestamp value from a result set.
+     * Overrides the default behavior to specifically return a timestamp.  Added
+     * to overcome an issue with the oracle 9.0.1.4 JDBC driver.
+     */
+    public Object getObjectFromResultSet(ResultSet resultSet, int columnNumber, int type, AbstractSession session) throws java.sql.SQLException {
+        Object value = super.getObjectFromResultSet(resultSet, columnNumber, type, session);
+        if(type == OracleTypes_NCLOB) {
+            value = convertObject(value, ClassConstants.STRING);
+        }
+        return value;
+    }
 }
