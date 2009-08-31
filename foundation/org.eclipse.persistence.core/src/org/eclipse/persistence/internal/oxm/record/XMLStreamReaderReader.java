17837125e614de729bcbab3b36854dd1f9cf146e
Denise Smith <denise.smith@oracle.com>
2009-08-31 17:45:32 +0000

    Fix for bug 288131 Reviewed by Blaise Doughan

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/oxm/record/XMLStreamReaderReader.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/oxm/record/XMLStreamReaderReader.java
@@ -90,6 +90,18 @@ public class XMLStreamReaderReader extends XMLReader {
     public void parse(String systemId) throws SAXException {}
 
     private void parse(XMLStreamReader xmlStreamReader) throws SAXException {
+    	try{    
+            parseEvent(xmlStreamReader);	
+            while(depth > 0 && xmlStreamReader.hasNext()) {
+                xmlStreamReader.next();
+                parseEvent(xmlStreamReader);                            
+            }
+        }catch(XMLStreamException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    private void parseEvent(XMLStreamReader xmlStreamReader) throws SAXException {
         if(null == getContentHandler()) {
             return;
         }
@@ -172,14 +184,6 @@ public class XMLStreamReaderReader extends XMLReader {
                 }
                 break;
             }
-        }
-        try {
-            if(depth > 0 && xmlStreamReader.hasNext()) {
-                xmlStreamReader.next();
-                parse(xmlStreamReader);
-            }
-        } catch(XMLStreamException e) {
-            throw new RuntimeException(e);
         }
     }
 
