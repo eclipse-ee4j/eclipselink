a0f6dbe39b9c8754a518a3dfdc7c76b6152f7a89
Tom Ware <tom.ware@oracle.com>
2009-10-02 15:05:30 +0000

    update to comment

--- jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/internal/jpa/deployment/URLArchive.java
+++ jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/internal/jpa/deployment/URLArchive.java
@@ -9,6 +9,7 @@
  *
  * Contributors:
  *     Oracle - initial API and implementation from Oracle TopLink
+ *     Flaivio Stutz - bug fix for getEntryAsURL
  ******************************************************************************/
 package org.eclipse.persistence.internal.jpa.deployment;
 
