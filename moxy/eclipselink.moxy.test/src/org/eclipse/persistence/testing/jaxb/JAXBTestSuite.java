b5af5df82370e50264bc94b84961219d54258911
Rick Barkhouse <rick.barkhouse@oracle.com>
2009-10-20 20:38:08 +0000

    Fix for bug 291411, reviewed by bdoughan.

--- moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/JAXBTestSuite.java
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/JAXBTestSuite.java
@@ -1,15 +1,15 @@
 /*******************************************************************************
  * Copyright (c) 1998, 2009 Oracle. All rights reserved.
- * This program and the accompanying materials are made available under the 
- * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
- * which accompanies this distribution. 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
  * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at 
+ * and the Eclipse Distribution License is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *
  * Contributors:
  *     Oracle - initial API and implementation from Oracle TopLink
- ******************************************************************************/  
+ ******************************************************************************/
 package org.eclipse.persistence.testing.jaxb;
 
 import org.eclipse.persistence.testing.jaxb.events.RootWithCompositeObjectTestCases;
@@ -36,18 +36,19 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class JAXBTestSuite extends TestCase {
+
     public static Test suite() {
         TestSuite suite = new TestSuite("JAXB20 Compiler Test Suite");
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.employee.JAXBEmployeeTestCases.class);
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.employee.EmployeeNamespaceCancellationTestCases.class);
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.xmlrootelement.XmlRootElementNamespaceTestCases.class);
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.xmlrootelement.XmlRootElementNoNamespaceTestCases.class);
-		suite.addTestSuite(org.eclipse.persistence.testing.jaxb.xmlelement.XmlElementNamespaceTestCases.class);
-		suite.addTestSuite(org.eclipse.persistence.testing.jaxb.xmlelement.XmlElementNoNamespaceTestCases.class);
-		suite.addTestSuite(org.eclipse.persistence.testing.jaxb.xmlelement.XmlElementCollectionTestCases.class);
-		suite.addTestSuite(org.eclipse.persistence.testing.jaxb.xmlattribute.XmlAttributeNamespaceTestCases.class);
-		suite.addTestSuite(org.eclipse.persistence.testing.jaxb.xmlattribute.XmlAttributeNoNamespaceTestCases.class);
-		suite.addTestSuite(org.eclipse.persistence.testing.jaxb.xmlattribute.XmlAttributeCollectionTestCases.class);
+        suite.addTestSuite(org.eclipse.persistence.testing.jaxb.xmlelement.XmlElementNamespaceTestCases.class);
+        suite.addTestSuite(org.eclipse.persistence.testing.jaxb.xmlelement.XmlElementNoNamespaceTestCases.class);
+        suite.addTestSuite(org.eclipse.persistence.testing.jaxb.xmlelement.XmlElementCollectionTestCases.class);
+        suite.addTestSuite(org.eclipse.persistence.testing.jaxb.xmlattribute.XmlAttributeNamespaceTestCases.class);
+        suite.addTestSuite(org.eclipse.persistence.testing.jaxb.xmlattribute.XmlAttributeNoNamespaceTestCases.class);
+        suite.addTestSuite(org.eclipse.persistence.testing.jaxb.xmlattribute.XmlAttributeCollectionTestCases.class);
         suite.addTestSuite(XmlValueTestCases.class);
         suite.addTestSuite(XmlValueWithAttributesTestCases.class);
         suite.addTestSuite(XmlValueListTestCases.class);
@@ -60,11 +61,10 @@ public class JAXBTestSuite extends TestCase {
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.xmlelementref.EmployeeCollectionTestCases.class);
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.xmlelementref.EmployeeSingleTestCases.class);
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.multiplepackage.MultiplePackageTestCases.class);
-        
-        
-        //jaxb-mats bug due to time/timestamp changes to TopLink after 070609
-        //suite.addTestSuite(XmlSchemaTypeDateTestCases.class);
-        //suite.addTestSuite(XmlSchemaTypeTwoDatesTestCases.class);
+
+        // jaxb-mats bug due to time/timestamp changes to TopLink after 070609
+        // suite.addTestSuite(XmlSchemaTypeDateTestCases.class);
+        // suite.addTestSuite(XmlSchemaTypeTwoDatesTestCases.class);
         suite.addTestSuite(XmlIdRefTestCases.class);
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.xmlenum.XmlEnumElementTestCases.class);
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.xmlenum.XmlEnumAttributeTestCases.class);
@@ -78,23 +78,24 @@ public class JAXBTestSuite extends TestCase {
         suite.addTestSuite(JAXBElementBase64TestCases.class);
         suite.addTestSuite(JAXBElementDataHandlerTestCases.class);
         suite.addTestSuite(RootWithCompositeObjectTestCases.class);
-       // suite.addTestSuite(JAXBInheritanceTestCases.class);
+        // suite.addTestSuite(JAXBInheritanceTestCases.class);
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.events.RootWithCompositeCollectionTestCases.class);
         suite.addTest(XmlAdapterTestSuite.suite());
         suite.addTest(SchemaGenTestSuite.suite());
         suite.addTest(JAXBDOMTestSuite.suite());
         suite.addTest(JAXBSAXTestSuite.suite());
-        
+
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.simpledocument.SimpleDocumentStringTestCases.class);
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.simpledocument.SimpleDocumentIntegerTestCases.class);
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.simpledocument.SimpleDocumentDateTestCases.class);
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.simpledocument.SimpleDocumentByteArrayTestCases.class);
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.simpledocument.SimpleDocumentWhitespaceNullTestCases.class);
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.annotations.xmlcontainerproperty.ContainerPropertyTestCases.class);
-        
+
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.namespaceuri.schemacontext.SchemaContextAsQNameTest.class);
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.events.sessionevents.SessionEventTestCases.class);
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.jaxbcontext.JaxbContextCreationTests.class);
+        suite.addTestSuite(org.eclipse.persistence.testing.jaxb.jaxbcontext.JaxbContextReturnTypesTests.class);
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.binder.adapter.BinderWithAdapterTestCases.class);
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.annotations.PropertyTypeTestCases.class);
         suite.addTestSuite(org.eclipse.persistence.testing.jaxb.annotations.required.RequiredAnnotationTestCases.class);
@@ -110,8 +111,9 @@ public class JAXBTestSuite extends TestCase {
 
     public static void main(String[] args) {
         String[] arguments = { "-c", "org.eclipse.persistence.testing.jaxb.JAXBTestSuite" };
-        //junit.swingui.TestRunner.main(arguments);
-        //System.setProperty("useLogging", "true");
+        // junit.swingui.TestRunner.main(arguments);
+        // System.setProperty("useLogging", "true");
         junit.textui.TestRunner.main(arguments);
     }
+
 }
--- moxy/org.eclipse.persistence.moxy/src/org/eclipse/persistence/jaxb/JAXBContext.java
+++ moxy/org.eclipse.persistence.moxy/src/org/eclipse/persistence/jaxb/JAXBContext.java
@@ -1,36 +1,28 @@
 /*******************************************************************************
  * Copyright (c) 1998, 2009 Oracle. All rights reserved.
- * This program and the accompanying materials are made available under the 
- * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
- * which accompanies this distribution. 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
  * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at 
+ * and the Eclipse Distribution License is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *
  * Contributors:
  *     Oracle - initial API and implementation from Oracle TopLink
- ******************************************************************************/  
+ ******************************************************************************/
 package org.eclipse.persistence.jaxb;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
 
-import javax.xml.bind.Binder;
-import javax.xml.bind.Marshaller;
 import javax.xml.bind.SchemaOutputResolver;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.bind.Validator;
 import javax.xml.namespace.QName;
-import java.util.HashMap;
 
-import org.eclipse.persistence.oxm.XMLConstants;
-import org.eclipse.persistence.oxm.XMLContext;
-import org.eclipse.persistence.oxm.XMLDescriptor;
-import org.eclipse.persistence.sessions.Session;
 import org.eclipse.persistence.exceptions.JAXBException;
 import org.eclipse.persistence.internal.helper.ClassConstants;
 import org.eclipse.persistence.internal.jaxb.JAXBSchemaOutputResolver;
@@ -42,6 +34,10 @@ import org.eclipse.persistence.jaxb.compiler.Generator;
 import org.eclipse.persistence.jaxb.compiler.MarshalCallback;
 import org.eclipse.persistence.jaxb.compiler.UnmarshalCallback;
 import org.eclipse.persistence.jaxb.javamodel.JavaClass;
+import org.eclipse.persistence.oxm.XMLConstants;
+import org.eclipse.persistence.oxm.XMLContext;
+import org.eclipse.persistence.oxm.XMLDescriptor;
+import org.eclipse.persistence.sessions.Session;
 
 /**
  * INTERNAL:
@@ -57,13 +53,13 @@ import org.eclipse.persistence.jaxb.javamodel.JavaClass;
  * <p>This is the TopLink JAXB 2.0 implementation of javax.xml.bind.JAXBContext. This class
  * is created by the JAXBContextFactory and is used to create Marshallers, Unmarshallers, Validators,
  * Binders and Introspectors. A JAXBContext can also be used to create Schema Files.
- * 
+ *
  * @see javax.xml.bind.JAXBContext
  * @see org.eclipse.persistence.jaxb.JAXBMarshaller
  * @see org.eclipse.persistence.jaxb.JAXBUnmarshaller
  * @see org.eclipse.persistence.jaxb.JAXBBinder
  * @see org.eclipse.persistence.jaxb.JAXBIntrospector
- * 
+ *
  * @author mmacivor
  * @since Oracle TopLink 11.1.1.0.0
  */
@@ -73,24 +69,24 @@ public class JAXBContext extends javax.xml.bind.JAXBContext {
     private static final Map<String, Boolean> PARSER_FEATURES = new HashMap<String, Boolean>(2);
     static {
         PARSER_FEATURES.put("http://apache.org/xml/features/validation/schema/normalized-value", false);
-        PARSER_FEATURES.put("http://apache.org/xml/features/validation/schema/element-default", false);            
+        PARSER_FEATURES.put("http://apache.org/xml/features/validation/schema/element-default", false);
     }
 
     private XMLContext xmlContext;
-    private org.eclipse.persistence.jaxb.compiler.Generator generator;    
+    private org.eclipse.persistence.jaxb.compiler.Generator generator;
     private HashMap<QName, Class> qNameToGeneratedClasses;
     private HashMap<String, Class> classToGeneratedClasses;
     private HashMap<QName, Class> qNamesToDeclaredClasses;
     private HashMap<java.lang.reflect.Type, QName> typeToSchemaType;
     private Type[] boundTypes;
-    
+
     public JAXBContext(XMLContext context) {
         super();
         xmlContext = context;
     }
-    
+
     public JAXBContext(XMLContext context, Generator generator, Type[] boundTypes) {
-        
+
         this(context);
         this.generator = generator;
         this.qNameToGeneratedClasses = generator.getMappingsGenerator().getQNamesToGeneratedClasses();
@@ -98,15 +94,15 @@ public class JAXBContext extends javax.xml.bind.JAXBContext {
         this.qNamesToDeclaredClasses = generator.getMappingsGenerator().getQNamesToDeclaredClasses();
         this.boundTypes = boundTypes;
     }
-    
+
     public XMLContext getXMLContext() {
         return this.xmlContext;
     }
-    
+
     public void generateSchema(SchemaOutputResolver outputResolver) {
         generateSchema(outputResolver, null);
     }
-    
+
     public void generateSchema(SchemaOutputResolver outputResolver, Map<QName, Type> additonalGlobalElements) {
         if (generator == null) {
             List<XMLDescriptor> descriptorsToProcess = new ArrayList<XMLDescriptor>();
@@ -123,9 +119,9 @@ public class JAXBContext extends javax.xml.bind.JAXBContext {
             generator.generateSchemaFiles(outputResolver, additonalGlobalElements);
         }
     }
-    
-    public Marshaller createMarshaller() {
-    	// create a JAXBIntrospector and set it on the marshaller
+
+    public JAXBMarshaller createMarshaller() {
+        // create a JAXBIntrospector and set it on the marshaller
         JAXBMarshaller marshaller = new JAXBMarshaller(xmlContext.createMarshaller(), new JAXBIntrospector(xmlContext));
         if (generator != null && generator.hasMarshalCallbacks()) {
             // initialize each callback in the map
@@ -141,7 +137,7 @@ public class JAXBContext extends javax.xml.bind.JAXBContext {
         return marshaller;
     }
 
-    public Unmarshaller createUnmarshaller() {
+    public JAXBUnmarshaller createUnmarshaller() {
         JAXBUnmarshaller unmarshaller = new JAXBUnmarshaller(xmlContext.createUnmarshaller(PARSER_FEATURES));
         if (generator != null && generator.hasUnmarshalCallbacks()) {
             // initialize each callback in the map
@@ -150,133 +146,133 @@ public class JAXBContext extends javax.xml.bind.JAXBContext {
                 cb.initialize(generator.getClass().getClassLoader());
             }
             unmarshaller.setUnmarshalCallbacks(generator.getUnmarshalCallbacks());
-        }        
+        }
         unmarshaller.setJaxbContext(this);
         return unmarshaller;
     }
 
-    public Validator createValidator() {
+    public JAXBValidator createValidator() {
         return new JAXBValidator(xmlContext.createValidator());
     }
-    
-    public Binder createBinder() {
+
+    public JAXBBinder createBinder() {
         return new JAXBBinder(this.xmlContext);
     }
-    
-    public Binder createBinder(Class nodeClass) {
-    	if(nodeClass.getName().equals("org.w3c.dom.Node")){
-    		return new JAXBBinder(this.xmlContext);
-    	}else{
-    		throw new UnsupportedOperationException(JAXBException.unsupportedNodeClass(nodeClass.getName()));
-    	}
+
+    public <T> JAXBBinder createBinder(Class<T> nodeClass) {
+        if (nodeClass.getName().equals("org.w3c.dom.Node")) {
+            return new JAXBBinder(this.xmlContext);
+        } else {
+            throw new UnsupportedOperationException(JAXBException.unsupportedNodeClass(nodeClass.getName()));
+        }
     }
-    
+
     public JAXBIntrospector createJAXBIntrospector() {
         return new JAXBIntrospector(xmlContext);
-    }      
-    
+    }
+
     public void setQNameToGeneratedClasses(HashMap<QName, Class> qNameToClass) {
-    	this.qNameToGeneratedClasses = qNameToClass;
+        this.qNameToGeneratedClasses = qNameToClass;
     }
-    
+
     public HashMap<String, Class> getClassToGeneratedClasses() {
-    	return classToGeneratedClasses;
+        return classToGeneratedClasses;
     }
-    
+
     public void setClassToGeneratedClasses(HashMap<String, Class> classToClass) {
-    	this.classToGeneratedClasses = classToClass;
+        this.classToGeneratedClasses = classToClass;
     }
-    
+
     /**
      * ADVANCED:
      * Adjust the OXM metadata to take into accound ORM mapping metadata,
      */
      public void applyORMMetadata(AbstractSession ormSession) {
-    	 this.xmlContext.applyORMMetadata(ormSession);
+        this.xmlContext.applyORMMetadata(ormSession);
      }
 
-	public HashMap<QName, Class> getQNamesToDeclaredClasses() {
-		return qNamesToDeclaredClasses;
-	}
+    public HashMap<QName, Class> getQNamesToDeclaredClasses() {
+        return qNamesToDeclaredClasses;
+    }
 
-	public void setQNamesToDeclaredClasses(
-			HashMap<QName, Class> nameToDeclaredClasses) {
-		qNamesToDeclaredClasses = nameToDeclaredClasses;
-	}
-	
-	public Map<String, Class>getArrayClassesToGeneratedClasses(){
-		return generator.getAnnotationsProcessor().getArrayClassesToGeneratedClasses();
-	}
-	
-	public Map<Type, Class>getCollectionClassesToGeneratedClasses(){
-		return generator.getAnnotationsProcessor().getCollectionClassesToGeneratedClasses();
-	}
-	
-	public void initTypeToSchemaType() {
-	    this.typeToSchemaType = new HashMap<Type, QName>();
-	    Iterator descriptors = xmlContext.getSession(0).getProject().getOrderedDescriptors().iterator();
-	    
-	    HashMap defaults = XMLConversionManager.getDefaultJavaTypes();
-	    
-	    //Add schema types generated for mapped domain classes
-	    while(descriptors.hasNext()) {
-	        XMLDescriptor next = (XMLDescriptor)descriptors.next();
-	        Class javaClass = next.getJavaClass();
-	        
-	        if(next.getSchemaReference() != null){
-		        QName schemaType = next.getSchemaReference().getSchemaContextAsQName(next.getNamespaceResolver());
-		        Type type;
-		        if(generator != null) {
-		            type = generator.getAnnotationsProcessor().getGeneratedClassesToCollectionClasses().get(javaClass);
-		            if(type == null) {
-		                JavaClass arrayClass = (JavaClass)generator.getAnnotationsProcessor().getGeneratedClassesToArrayClasses().get(javaClass);
-		                if(arrayClass != null) {
-		                    String arrayClassName = arrayClass.getName();
-		                    try {
-		                        type = PrivilegedAccessHelper.getClassForName(arrayClassName);
-		                    } catch (Exception ex){}
-		                }
-		            }
-		            if(type == null) {
-		                type = javaClass;
-		            }
-		        } else {
-		            type = javaClass;
-		        }
-		        this.typeToSchemaType.put(type, schemaType);
-	        }
-	    }
-	    
-	    //Add any types that we didn't generate descriptors for (built in types)
-	    if(boundTypes != null) {
-	        for(Type next:this.boundTypes) {
-	            if(this.typeToSchemaType.get(next) == null) {
-	                QName name = null;
-	                //Check for annotation overrides
-	                if(next instanceof Class) {
-	                    name = this.generator.getAnnotationsProcessor().getUserDefinedSchemaTypes().get(((Class)next).getName());
-	                }
-	                if(name == null) {
-	                    //Change default for byte[] to Base64 (JAXB 2.0 default)
-	                    if(next == ClassConstants.ABYTE || next == ClassConstants.APBYTE) {
-	                        name = XMLConstants.BASE_64_BINARY_QNAME;
-	                    } else {
-	                        name = (QName)defaults.get(next);
-	                    }
-	                }
-	                if(name != null) {
-	                    this.typeToSchemaType.put(next, name);
-	                }
-	            }
-	        }
-	    }
-	}
+    public void setQNamesToDeclaredClasses(
+            HashMap<QName, Class> nameToDeclaredClasses) {
+        qNamesToDeclaredClasses = nameToDeclaredClasses;
+    }
 
-	public HashMap<Type, QName> getTypeToSchemaType() {
-	    if(typeToSchemaType == null) {
-	        initTypeToSchemaType();
-	    }
-	    return typeToSchemaType;
-	}
-	
+    public Map<String, Class>getArrayClassesToGeneratedClasses(){
+        return generator.getAnnotationsProcessor().getArrayClassesToGeneratedClasses();
+    }
+
+    public Map<Type, Class>getCollectionClassesToGeneratedClasses(){
+        return generator.getAnnotationsProcessor().getCollectionClassesToGeneratedClasses();
+    }
+
+    public void initTypeToSchemaType() {
+        this.typeToSchemaType = new HashMap<Type, QName>();
+        Iterator descriptors = xmlContext.getSession(0).getProject().getOrderedDescriptors().iterator();
+
+        HashMap defaults = XMLConversionManager.getDefaultJavaTypes();
+
+        //Add schema types generated for mapped domain classes
+        while (descriptors.hasNext()) {
+            XMLDescriptor next = (XMLDescriptor)descriptors.next();
+            Class javaClass = next.getJavaClass();
+
+            if (next.getSchemaReference() != null){
+                QName schemaType = next.getSchemaReference().getSchemaContextAsQName(next.getNamespaceResolver());
+                Type type;
+                if (generator != null) {
+                    type = generator.getAnnotationsProcessor().getGeneratedClassesToCollectionClasses().get(javaClass);
+                    if (type == null) {
+                        JavaClass arrayClass = (JavaClass)generator.getAnnotationsProcessor().getGeneratedClassesToArrayClasses().get(javaClass);
+                        if (arrayClass != null) {
+                            String arrayClassName = arrayClass.getName();
+                            try {
+                                type = PrivilegedAccessHelper.getClassForName(arrayClassName);
+                            } catch (Exception ex) {}
+                        }
+                    }
+                    if (type == null) {
+                        type = javaClass;
+                    }
+                } else {
+                    type = javaClass;
+                }
+                this.typeToSchemaType.put(type, schemaType);
+            }
+        }
+
+        //Add any types that we didn't generate descriptors for (built in types)
+        if (boundTypes != null) {
+            for (Type next:this.boundTypes) {
+                if (this.typeToSchemaType.get(next) == null) {
+                    QName name = null;
+                    //Check for annotation overrides
+                    if (next instanceof Class) {
+                        name = this.generator.getAnnotationsProcessor().getUserDefinedSchemaTypes().get(((Class)next).getName());
+                    }
+                    if (name == null) {
+                        //Change default for byte[] to Base64 (JAXB 2.0 default)
+                        if (next == ClassConstants.ABYTE || next == ClassConstants.APBYTE) {
+                            name = XMLConstants.BASE_64_BINARY_QNAME;
+                        } else {
+                            name = (QName)defaults.get(next);
+                        }
+                    }
+                    if (name != null) {
+                        this.typeToSchemaType.put(next, name);
+                    }
+                }
+            }
+        }
+    }
+
+    public HashMap<Type, QName> getTypeToSchemaType() {
+        if (typeToSchemaType == null) {
+            initTypeToSchemaType();
+        }
+        return typeToSchemaType;
+    }
+
 }
