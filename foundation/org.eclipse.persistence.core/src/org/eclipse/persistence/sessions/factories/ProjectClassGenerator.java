cade4b14303a6042125773b93725aaf5af70826a
Andrei Ilitchev <andrei.ilitchev@oracle.com>
2010-02-08 16:49:27 +0000

    Bug 300123 - NullPointerException occuring in ProjectClassGenerator

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/sessions/factories/ProjectClassGenerator.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/sessions/factories/ProjectClassGenerator.java
@@ -338,7 +338,10 @@ public class ProjectClassGenerator {
     }
 
     protected void addDirectMapMappingLines(NonreflectiveMethodDefinition method, String mappingName, DirectMapMapping mapping) {
-        method.addLine(mappingName + ".setDirectKeyFieldName(\"" + mapping.getDirectKeyField().getQualifiedName() + "\");");
+        DatabaseField  directKeyField = mapping.getDirectKeyField();
+        if(directKeyField != null) {
+            method.addLine(mappingName + ".setDirectKeyFieldName(\"" + directKeyField.getQualifiedName() + "\");");
+        }
 
         Converter converter = mapping.getKeyConverter();
         if (converter != null) {
