37d7d9507a489828e2b7343739d97020c7c5320c
Denise Smith <denise.smith@oracle.com>
2010-03-01 14:25:13 +0000

    Fix for bug 303631 Reviewed by Blaise Doughan

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/oxm/XMLConversionManager.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/oxm/XMLConversionManager.java
@@ -1605,10 +1605,14 @@ public class XMLConversionManager extends ConversionManager implements TimeZoneH
         return appendTimeZone(string);
     }
 
-    private String stringFromXMLGregorianCalendar(XMLGregorianCalendar cal, QName schemaTypeQName) {
-        return stringFromCalendar(cal.toGregorianCalendar(), schemaTypeQName);
+private String stringFromXMLGregorianCalendar(XMLGregorianCalendar 
+cal, QName schemaTypeQName) {
+        GregorianCalendar gCal = cal.toGregorianCalendar();
+        if(cal.getTimezone() == DatatypeConstants.FIELD_UNDEFINED) {
+            gCal.clear(Calendar.ZONE_OFFSET);
+        }
+        return stringFromCalendar(gCal, schemaTypeQName);
     }
-    
     private String stringFromXMLGregorianCalendar(XMLGregorianCalendar cal) {
         return cal.toXMLFormat();
     }
