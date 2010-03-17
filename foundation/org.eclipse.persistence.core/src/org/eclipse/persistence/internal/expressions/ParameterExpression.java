a6c0fc45641939f8ac29b4185ddfe2aaec518e99
David Minsky <david.minsky@oracle.com>
2010-03-17 18:19:29 +0000

    Fix for bug 306068 -  NPE in ParameterExpression.validateParameterValueAgainstMapping for DirectCollectionMapping Added null check in ParameterExpression validateParameterValueAgainstMapping to prevent NullPointerException, same code is present in 2.0 and trunk (backport to 1.2). Reviewed: Chris Delahunt

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/expressions/ParameterExpression.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/expressions/ParameterExpression.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2009 Oracle. All rights reserved.
+ * Copyright (c) 1998, 2010 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the 
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
  * which accompanies this distribution. 
@@ -363,7 +363,7 @@ public class ParameterExpression extends BaseExpression {
                     }
                 } else if (mapping.isDirectCollectionMapping()){
                     Class targetClass = ((DirectCollectionMapping)mapping).getDirectField().getType();
-                    if (((Class)targetClass).equals(BasicTypeHelperImpl.ElementPlaceHolder.class)){
+                    if (targetClass == null || ((Class)targetClass).equals(BasicTypeHelperImpl.ElementPlaceHolder.class)){
                         // we do not know the class of the target and cannot do validation
                         return;
                     } else {
