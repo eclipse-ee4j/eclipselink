04fe52f925e61cbd724f401ee78f7025bd0783ab
Gordon Yorke <gordon.yorke@oracle.com>
2010-03-19 20:28:03 +0000

    305246-Simple-deadlock-occurs-when-triggering-indirection-during-clone-through-post-load Update to fix some RMI and Server Tests

--- jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/tests/jpa/advanced/AdvancedJPAJunitTest.java
+++ jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/tests/jpa/advanced/AdvancedJPAJunitTest.java
@@ -1197,7 +1197,7 @@ public class AdvancedJPAJunitTest extends JUnitTestCase {
     
     public void testRelationshipReadDuringClone(){
         EntityManager em = createEntityManager();
-        Session session = JpaHelper.getServerSession(em.getEntityManagerFactory());
+        Session session = JpaHelper.getServerSession(getEntityManagerFactory());
         ClassDescriptor departmentDesc = session.getDescriptor(Department.class);
         DescriptorEventAdapter listener = new DescriptorEventAdapter(){
             public void postClone(DescriptorEvent event) {
