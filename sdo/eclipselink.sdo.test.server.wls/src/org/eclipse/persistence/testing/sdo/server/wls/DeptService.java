e15db583ee210b747593d132b9f76e539e50ce96
Edwin Tang <edwin.tang@oracle.com>
2010-05-12 18:11:24 +0000

    Remove sdo/eclipselink.sdo.test.server.wls and close bug 308880 Code reviewed by: David Mccann

--- sdo/eclipselink.sdo.test.server.wls/src/org/eclipse/persistence/testing/sdo/server/wls/DeptService.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.persistence.testing.sdo.server.wls;
-
-import javax.ejb.Remote;
-
-@Remote
-public interface DeptService {
-    Dept getDept(Integer deptno);
-    boolean updateDept(Dept dept);
-}
