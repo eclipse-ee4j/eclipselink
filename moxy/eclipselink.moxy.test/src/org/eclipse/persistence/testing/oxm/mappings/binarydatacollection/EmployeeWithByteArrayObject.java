5573223ebb65106620db8d944d64583fada63f38
Matt MacIvor <matt.macivor@oracle.com>
2010-06-03 12:38:20 +0000

    Update for failed compile.

--- moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/oxm/mappings/binarydatacollection/EmployeeWithByteArrayObject.java
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/oxm/mappings/binarydatacollection/EmployeeWithByteArrayObject.java
@@ -18,9 +18,7 @@ import java.util.Vector;
 import javax.activation.DataHandler;
 
 import org.eclipse.persistence.internal.helper.ConversionManager;
-
-import com.sun.org.apache.xml.internal.security.utils.Base64;
-
+import org.eclipse.persistence.internal.oxm.conversion.Base64;
 
 public class EmployeeWithByteArrayObject {
     public static final int DEFAULT_ID = 123;
@@ -78,7 +76,7 @@ public class EmployeeWithByteArrayObject {
             for (int i = 0; i < getPhotos().size(); i++) {
                 Object next = getPhotos().elementAt(i);
                 if (next != null) {
-                    returnString += (next + "\n" + "-->" + (Base64.encode((byte[])ConversionManager.getDefaultManager().convertObject(next, byte[].class))));
+                    returnString += (next + "\n" + "-->" + new String(Base64.base64Encode(((byte[])ConversionManager.getDefaultManager().convertObject(next, byte[].class)))));
                 } else {
                     returnString += ("null_item" + " ");
                 }
