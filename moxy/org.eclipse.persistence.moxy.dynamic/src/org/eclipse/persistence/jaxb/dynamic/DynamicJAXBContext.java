ddeb1da9e42e6174396c1085c3d748e7dffa8b96
Matt MacIvor <matt.macivor@oracle.com>
2010-06-03 19:03:15 +0000

    Updated Copyright info in headers

--- moxy/org.eclipse.persistence.moxy.dynamic/src/org/eclipse/persistence/jaxb/dynamic/DynamicJAXBContext.java
+++ moxy/org.eclipse.persistence.moxy.dynamic/src/org/eclipse/persistence/jaxb/dynamic/DynamicJAXBContext.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2010 Oracle. All rights reserved.
+ * Copyright (c) 2010 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
  * which accompanies this distribution.
@@ -8,7 +8,7 @@
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *
  * Contributors:
- *     rbarkhouse - initial implementation
+ *     Rick Barkhouse = 2.1 - initial implementation
  ******************************************************************************/
 package org.eclipse.persistence.jaxb.dynamic;
 
--- moxy/org.eclipse.persistence.moxy/src/org/eclipse/persistence/internal/jaxb/many/ArrayValue.java
+++ moxy/org.eclipse.persistence.moxy/src/org/eclipse/persistence/internal/jaxb/many/ArrayValue.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 1998, 2010 Oracle. All rights reserved.
+* Copyright (c) 2010 Oracle. All rights reserved.
 * This program and the accompanying materials are made available under the
 * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
 * which accompanies this distribution.
@@ -8,7 +8,7 @@
 * http://www.eclipse.org/org/documents/edl-v10.php.
 *
 * Contributors:
-*     bdoughan - April 22/2010 - 2.1 - Initial implementation
+*     Blaise Doughan = 2.1 - Initial implementation
 ******************************************************************************/
 package org.eclipse.persistence.internal.jaxb.many;
 
