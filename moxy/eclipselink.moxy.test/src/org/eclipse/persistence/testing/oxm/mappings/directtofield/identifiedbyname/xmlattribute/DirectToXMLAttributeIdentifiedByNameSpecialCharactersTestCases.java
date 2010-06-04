803bc6e63a7e11a940c847b47dda9eaec6226b0d
David McCann <david.mccann@oracle.com>
2010-06-04 18:21:40 +0000

    Fix for bug# 315786 (MOXy - Need to handle double quote char in attributes and text nodes) Reviewed by:  matt.macivor@oracle.com Tests: all unit tests pass as expected; oxm/mappings/directtofield/identifiedbyname/xmlattribute/DirectToXMLAttributeIdentifiedByNameSpecialCharactersTestCases; oxm/mappings/directtofield/identifiedbyname/xmlelement/DirectToXMLElementIdentifiedByNameSpecialCharactersTestCases

--- moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/oxm/mappings/directtofield/identifiedbyname/xmlattribute/DirectToXMLAttributeIdentifiedByNameSpecialCharactersTestCases.java
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/oxm/mappings/directtofield/identifiedbyname/xmlattribute/DirectToXMLAttributeIdentifiedByNameSpecialCharactersTestCases.java
@@ -21,9 +21,9 @@ import org.eclipse.persistence.internal.helper.Helper;
 
 public class DirectToXMLAttributeIdentifiedByNameSpecialCharactersTestCases extends OXTestCase {
     private final static int CONTROL_ID = 123;
-    private final static String CONTROL_FIRST_NAME = "A<B&C<";
+    private final static String CONTROL_FIRST_NAME = "A<\"B&C<";
     private final static String CONTROL_LAST_NAME = null;
-    private final static String CONTROL_XML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + Helper.cr() + "<employee id=\"123\" first-name=\"A&lt;B&amp;C&lt;\"/>";
+    private final static String CONTROL_XML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + Helper.cr() + "<employee id=\"123\" first-name=\"A&lt;&quot;B&amp;C&lt;\"/>";
     private XMLMarshaller xmlMarshaller;
 
     public DirectToXMLAttributeIdentifiedByNameSpecialCharactersTestCases(String name) {
