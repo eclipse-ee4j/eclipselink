57630e38d824269cd17c8441c94e2f2dcae70716
Guy Pelletier <guy.pelletier@oracle.com>
2010-06-24 15:56:19 +0000

    Adding copyright and removing some duplicates

--- jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/models/jpa/xml/advanced/DoNotRedirect.java
+++ jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/models/jpa/xml/advanced/DoNotRedirect.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
+ * which accompanies this distribution. 
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     11/19/2009-2.0 Gordon Yorke 
+ *       - 239825: XML configuration for Interceptors and Default redirectors
+ ******************************************************************************/ 
 package org.eclipse.persistence.testing.models.jpa.xml.advanced;
 
 import org.eclipse.persistence.internal.sessions.AbstractRecord;
