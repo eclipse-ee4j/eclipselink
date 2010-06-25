9b169b15a66def9950c0b87f7a5ffacda2ee7086
David Minsky <david.minsky@oracle.com>
2010-06-25 21:33:21 +0000

    Backport of Bug 311023 (Requested in Bug 312441) - setShouldOuterJoinSubclasses generates incorrect outer joins Backported code is from Bug 307497 - Query populates attributes with wrong select list items of query result This transaction contains a fix to InheritancePolicy, adding a test to JUnitJPQLInheritanceTestSuite (including testing model class SoftwareEngineer) Previously checked into trunk by James (and previously reviewed by Andrei)

---
+++ jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/models/jpa/inheritance/SoftwareEngineer.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 1998, 2010 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
+ * which accompanies this distribution. 
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     James Sutherland - initial API and implementation
+ ******************************************************************************/  
+
+package org.eclipse.persistence.testing.models.jpa.inheritance;
+
+import javax.persistence.*;
+
+/**
+ * This tests having two independent subclasses sharing the same table.
+ * @author James Sutherland
+ */
+@Entity
+@Table(name="CMP3_ENGINEER")
+@DiscriminatorValue("4")
+public class SoftwareEngineer extends Person {
+    private String title;
+    private Company company;
+
+    @ManyToOne
+    public Company getCompany() {
+        return company;
+    }
+    
+    @Column(name="TITLE")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
