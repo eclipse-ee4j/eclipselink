867fb36f896d2469fd36ab58a6c68dc8549ba607
Eric Gwin <eric.gwin@oracle.com>
2010-07-06 15:12:19 +0000

    Fixes to CleanRevision.

--- buildsystem/ant_customizations/src/org/eclipse/persistence/buildtools/ant/taskdefs/CleanRevision.java
+++ buildsystem/ant_customizations/src/org/eclipse/persistence/buildtools/ant/taskdefs/CleanRevision.java
@@ -1,6 +1,7 @@
 package org.eclipse.persistence.buildtools.ant.taskdefs;
 
 import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
 
 public class CleanRevision extends Task {
@@ -8,6 +9,8 @@ public class CleanRevision extends Task {
     private String property = null;
     
     public void execute() throws BuildException {
+        boolean input_error=false;
+        
         if (input == null) {
             throw new BuildException("'input' attribute must be set.", getLocation());
         }
@@ -18,17 +21,28 @@ public class CleanRevision extends Task {
             throw new BuildException("'property' cannot be an empty string.", getLocation());
         }
         if ( input.startsWith("${") || input =="" ) {
-            getProject().setNewProperty(property, "NA");
+            // If input empty or unexpanded then set value of property to 'NA'
+            input_error = true;
         }
         else {            
             // Last changed revision (branch specific) is stored 2nd to last line of
             //    "svn info --revision HEAD <url>" output
-            // Get "endString" Index based on search for last instance of "Last Changed"
+            // Get "endString" Index based on search for last instance of "Last Changed" (Last line of output)
             int endLastRev = input.lastIndexOf("Last Changed");
-            // Get "beginString" index based upon the last space in the line above the endString index
+            // Get "beginString" index based upon the last space in the line above the endString index (and don't include the space)
             int beginLastRev = input.lastIndexOf(" ", endLastRev);
-            // Trim the string to not include the leading space, or the end-of-line
-            getProject().setNewProperty( property, input.substring(beginLastRev+1, endLastRev-1));
+            if( (beginLastRev >=0)&& (endLastRev > 0) && (beginLastRev < endLastRev)) {
+                // Trim the string to not include the leading space, or the end-of-line
+                getProject().setProperty( property, input.substring(beginLastRev, endLastRev).trim());
+                log("CleanRev Finished. New value of: '" + property + "' is '" + input.substring(beginLastRev, endLastRev).trim() + "'.", Project.MSG_VERBOSE);  
+            }
+            else {
+                input_error = true;               
+            }
+        }
+        if(input_error) {
+            log("CleanRev Finished. Input empty or search failed! Setting value to 'NA'", Project.MSG_VERBOSE);  
+            getProject().setProperty(property, "NA");                
         }
     }
     
