32b8f8f3ca7faa33ca2122ad56bb10d970285f25
Guy Pelletier <guy.pelletier@oracle.com>
2010-11-30 16:48:40 +0000

    Bug 331343 - NPE from Canonical model generator when using generic class types
    Reviewed by: Tom Ware
    
    Tested manually through Eclipse and our build scripts.
    
    ** Changes didn't get submitted properly the first time **

--- jpa/org.eclipse.persistence.jpa.modelgen/src/org/eclipse/persistence/internal/jpa/modelgen/MetadataMirrorFactory.java
+++ jpa/org.eclipse.persistence.jpa.modelgen/src/org/eclipse/persistence/internal/jpa/modelgen/MetadataMirrorFactory.java
@@ -29,6 +29,7 @@ import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.TypeParameterElement;
 import javax.lang.model.type.TypeMirror;
 import javax.tools.Diagnostic.Kind;
 
@@ -115,7 +116,7 @@ public class MetadataMirrorFactory extends MetadataFactory {
         
         if (metadataClass == null) {
             // As a performance gain, avoid visiting this class if it is not a 
-            // round element.
+            // round element. We must re-visit round elements.
             if (isRoundElement(element)) {
                 processingEnv.getMessager().printMessage(Kind.NOTE, "Building metadata class for round element: " + element);
                 metadataClass = new MetadataClass(MetadataMirrorFactory.this, "");
@@ -131,13 +132,28 @@ public class MetadataMirrorFactory extends MetadataFactory {
                 addMetadataClass(metadataClass);
             } else {
                 // So we are not a round element, the outcome is as follows:
-                //  - Not a TypeElement (should not happen since we filter them 
-                //    out from the root elements ... but like everything
-                //    else, who knows for sure!), return null;
+                //  - TypeParameterElement: they are stored by simple name, so 
+                //    if it exists in our map return it, otherwise visit it.
                 //  - TypeElement, and not in the existing class map, return 
                 //    simple MetadataClass with only a name.
                 //  - TypeElement, in existing class map, return it.
-                metadataClass = getMetadataClass((element instanceof TypeElement) ? ((TypeElement) element).getQualifiedName().toString() : null);
+                //  - Everything else ... look up the metadata class using the
+                //    toString value.
+                if (element instanceof TypeParameterElement) {
+                    // You could in theory just visit it since we don't do much with it.
+                    String simpleName = element.getSimpleName().toString();
+                    
+                    if (metadataClassExists(simpleName)) {
+                        metadataClass = getMetadataClass(simpleName);
+                    } else {
+                        metadataClass = new MetadataClass(MetadataMirrorFactory.this, "");
+                        element.accept(elementVisitor, metadataClass);
+                        addMetadataClass(metadataClass);
+                    }
+                } else {
+                    //processingEnv.getMessager().printMessage(Kind.NOTE, "Element: " + element.toString());
+                    metadataClass = getMetadataClass((element instanceof TypeElement) ? ((TypeElement) element).getQualifiedName().toString() : element.toString());    
+                }
             }
         }
         
