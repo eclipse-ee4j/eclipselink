ad4f6875a465b16ba2343cfc2bcaa235f516d16d
Michael O'Brien <michael.obrien@oracle.com>
2011-02-25 21:13:43 +0000

    333316: JMX: ModuleName string extraction code does not handle -1 not found index in 3 of 5 cases https://bugs.eclipse.org/bugs/attachment.cgi?id=189853&action=diff 333336: Add AccessController.doPrivileged private method for shouldUsePrivilegedAccess() when SecurityManager is enabled on the EE container This will enable the MBeanServerFactory.findMBeanServer(null) JMX API call to run in secure mode https://bugs.eclipse.org/bugs/attachment.cgi?id=189854&action=diff

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/platform/server/jboss/JBossPlatform.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/platform/server/jboss/JBossPlatform.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2010 Oracle. All rights reserved.
+ * Copyright (c) 1998, 2011 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the 
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
  * which accompanies this distribution. 
@@ -53,7 +53,7 @@ public class JBossPlatform extends JMXServerPlatformBase implements JMXEnabledPl
         /** Override by subclass: Search String in application server session for war modules */
         APP_SERVER_CLASSLOADER_MODULE_WAR_SEARCH_STRING_PREFIX = ".war/";
         APP_SERVER_CLASSLOADER_APPLICATION_PU_SEARCH_STRING_POSTFIX = "/}";
-        APP_SERVER_CLASSLOADER_MODULE_EJB_WAR_SEARCH_STRING_POSTFIX = "";
+        APP_SERVER_CLASSLOADER_MODULE_EJB_WAR_SEARCH_STRING_POSTFIX = "postfix,match~not;required^";
     }
     
     /**
