db7621988ac8b6e1303ba516e12cbc79f03bd99b
Denise Smith <denise.smith@oracle.com>
2011-04-21 13:09:51 +0000

    Additional test files for  bug 333601

---
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/annotations/xmlnametransformer/classlevel/upper/package-info.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ * Denise Smith - 2.3
+ ******************************************************************************/
+@XmlSchema(namespace="examplenamespace",
+	xmlns = {@XmlNs(prefix="x", namespaceURI="examplenamespace")}
+)
+@XmlNameTransformer(MyLowerNameTransformer.class)
+package org.eclipse.persistence.testing.jaxb.annotations.xmlnametransformer.classlevel.upper;
+import javax.xml.bind.annotation.*;
+import org.eclipse.persistence.oxm.annotations.XmlNameTransformer;
+import org.eclipse.persistence.testing.jaxb.annotations.xmlnametransformer.upper.MyUpperNameTransformer;
---
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/annotations/xmlnametransformer/package-info.java
@@ -0,0 +1,5 @@
+@XmlSchema(namespace="examplenamespace",
+	xmlns = {@XmlNs(prefix="x", namespaceURI="examplenamespace")}
+)
+package org.eclipse.persistence.testing.jaxb.annotations.xmlnametransformer;
+import javax.xml.bind.annotation.*;
