b7e7cf43e46cdcaeb5ac688323b9852f00a214fb
Blaise Doughan <blaise.doughan@oracle.com>
2011-05-10 17:15:49 +0000

    MINOR FIX - Missed test case files

---
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/xmltype/proporder/ChildOfTransientRoot.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     Blaise Doughan - 2.3 - initial implementation
+ ******************************************************************************/
+package org.eclipse.persistence.testing.jaxb.xmltype.proporder;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name="child")
+@XmlType(propOrder={"childProp", "parentProp"})
+public class ChildOfTransientRoot extends TransientRoot {
+
+    private String childProp;
+
+    public String getChildProp() {
+        return childProp;
+    }
+
+    public void setChildProp(String childProp) {
+        this.childProp = childProp;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!super.equals(obj)) {
+            return false;
+        }
+        ChildOfTransientRoot test = (ChildOfTransientRoot) obj;
+        if(null == childProp) {
+            return null == test.getChildProp();
+        } else {
+            return childProp.equals(test.getChildProp());
+        }
+    }
+
+}
