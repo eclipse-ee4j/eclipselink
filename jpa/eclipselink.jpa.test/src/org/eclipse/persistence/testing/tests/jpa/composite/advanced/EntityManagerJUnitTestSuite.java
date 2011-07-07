dc55e719cbbae841c534e9f8a72446afc3a8fe5a
Tom Ware <tom.ware@oracle.com>
2011-07-07 14:58:05 +0000

    Reduce length of sequence name for testing failure

--- jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/tests/jpa/composite/advanced/EntityManagerJUnitTestSuite.java
+++ jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/tests/jpa/composite/advanced/EntityManagerJUnitTestSuite.java
@@ -5710,7 +5710,7 @@ public class EntityManagerJUnitTestSuite extends JUnitTestCase {
             return;
         }
         EntityManager em = createEntityManager();
-        String seqName = "testCOMPSequenceObjectDefinition";
+        String seqName = "testCOMPSeqObjectDefinition";
         try {
             // first param is preallocationSize, second is startValue
             // both should be positive
