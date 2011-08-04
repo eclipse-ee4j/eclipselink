d18ab35ee8c585dd1d5df1297e4c674c61b33cbd
Kevin Yuan <kevin.yuan@oracle.com>
2011-08-04 19:21:35 +0000

    Back port the fix for wls domain testing in other env by passing dynamic value for domain name

--- jpa/eclipselink.jpa.test/resource/weblogic/wls_domain_setup.py
+++ jpa/eclipselink.jpa.test/resource/weblogic/wls_domain_setup.py
@@ -18,7 +18,7 @@ connect('%%WL_USR%%','%%WL_PWD%%','t3://%%WL_HOST%%:%%WL_PORT%%')
 edit()
 startEdit()
 
-cd('/JPA/mydomain')
+cd('/JPA/%%WL_DOMAIN%%')
 cmo.setDefaultJPAProvider('org.eclipse.persistence.jpa.PersistenceProvider')
 
 save()
