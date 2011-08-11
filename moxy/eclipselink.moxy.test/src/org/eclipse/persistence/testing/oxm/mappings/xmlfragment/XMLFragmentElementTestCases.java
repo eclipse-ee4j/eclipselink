9acf371c1bc177be360009f446f7b2e5da516f8f
Matt MacIvor <matt.macivor@oracle.com>
2011-08-11 16:25:32 +0000

    Fix for bug 354504 Reviewed by Blaise Doughan

--- moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/oxm/mappings/xmlfragment/XMLFragmentElementTestCases.java
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/oxm/mappings/xmlfragment/XMLFragmentElementTestCases.java
@@ -22,6 +22,10 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
 
 import org.eclipse.persistence.testing.oxm.mappings.XMLMappingTestCases;
 
@@ -51,4 +55,56 @@ public class XMLFragmentElementTestCases extends XMLMappingTestCases {
     } catch(Exception ex) {}
     return employee;
   }
+  
+  public void testObjectToContentHandlerNull() {
+      Object obj = getControlObject();
+      try {
+          xmlMarshaller.marshal(obj, new ContentHandler() {
+
+              public void setDocumentLocator(Locator locator) {
+              }
+
+              public void startDocument() throws SAXException {
+              }
+
+              public void endDocument() throws SAXException {
+              }
+
+              public void startPrefixMapping(String prefix, String uri) throws SAXException {
+              }
+
+              public void endPrefixMapping(String prefix) throws SAXException {
+              }
+
+              public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
+                  if(atts.getLength() > 0) {
+                      for(int i = 0; i < atts.getLength(); i++) {
+                          if(atts.getURI(i) == null) {
+                              throw new SAXException();
+                          }
+                      }
+                  }
+              }
+
+              public void endElement(String uri, String localName, String qName) throws SAXException {
+              }
+
+              public void characters(char[] ch, int start, int length) throws SAXException {
+              }
+
+              public void ignorableWhitespace(char[] ch, int start, int length) throws SAXException {
+              }
+
+              public void processingInstruction(String target, String data) throws SAXException {
+              }
+
+              public void skippedEntity(String name) throws SAXException {
+              }
+          });
+      }catch (Exception ex) {
+          fail("An unexpected exception was thrown due to a null namespace on an attribute");
+      }
+  }
+  
+  
 }
