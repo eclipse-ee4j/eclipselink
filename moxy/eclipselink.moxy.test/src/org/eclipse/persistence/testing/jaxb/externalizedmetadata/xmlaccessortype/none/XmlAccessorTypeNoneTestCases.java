1544ea93278b4afaa6610c935710e30eb4e9ae94
Denise Smith <denise.smith@oracle.com>
2011-08-19 18:21:41 +0000

    New test files

---
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlaccessortype/none/XmlAccessorTypeNoneTestCases.java
@@ -0,0 +1,93 @@
+/*******************************************************************************
+ * Copyright (c) 1998, 2011 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ * dmccann - June 17/2009 - 2.0 - Initial implementation
+ ******************************************************************************/
+package org.eclipse.persistence.testing.jaxb.externalizedmetadata.xmlaccessortype.none;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+
+import org.eclipse.persistence.jaxb.JAXBContextFactory;
+import org.eclipse.persistence.testing.jaxb.JAXBWithJSONTestCases;
+
+/**
+ * Tests XmlAccessorType via eclipselink-oxm.xml
+ *
+ */
+public class XmlAccessorTypeNoneTestCases extends JAXBWithJSONTestCases {    
+    private static final String XML_RESOURCE = "org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlaccessortype/employee-no-access.xml";
+    private static final String JSON_RESOURCE = "org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlaccessortype/employee-no-access.json";
+    
+    /**
+     * This is the preferred (and only) constructor.
+     * 
+     * @param name
+     */
+    public XmlAccessorTypeNoneTestCases(String name) throws Exception{
+        super(name);
+        setClasses(new Class[]{Employee.class});
+        setControlDocument(XML_RESOURCE);
+        setControlJSON(JSON_RESOURCE);
+    }
+    
+    protected Object getControlObject() {
+		Employee emp = new Employee(0);	
+		emp.firstName = "firstName";
+		emp.lastName = "lastName";		
+		return emp;
+	}	   
+    
+    
+    public Object getWriteControlObject() {
+		Employee emp = new Employee(666);	
+		emp.firstName = "firstName";
+		emp.lastName = "lastName";		
+		return emp;
+	}	
+    public Map getProperties(){
+		InputStream inputStream = ClassLoader.getSystemResourceAsStream("org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlaccessortype/none/eclipselink-oxm.xml");
+
+		HashMap<String, Source> metadataSourceMap = new HashMap<String, Source>();
+	    metadataSourceMap.put("org.eclipse.persistence.testing.jaxb.externalizedmetadata.xmlaccessortype.none", new StreamSource(inputStream));
+	    Map<String, Map<String, Source>> properties = new HashMap<String, Map<String, Source>>();
+	    properties.put(JAXBContextFactory.ECLIPSELINK_OXM_XML_KEY, metadataSourceMap);		
+        
+        return properties;
+	}
+	
+    public void testInstanceDocValidation() throws Exception {
+    	InputStream schema = ClassLoader.getSystemResourceAsStream("org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlaccessortype/none/employee-none.xsd");        
+        StreamSource schemaSource = new StreamSource(schema); 
+                
+        MyMapStreamSchemaOutputResolver outputResolver = new MyMapStreamSchemaOutputResolver();
+        getJAXBContext().generateSchema(outputResolver);
+        
+        InputStream instanceDocStream = ClassLoader.getSystemResourceAsStream(XML_RESOURCE);
+        String result = validateAgainstSchema(instanceDocStream, schemaSource, outputResolver );        
+        assertTrue("Instance doc validation (employee.xml) failed unxepectedly: " + result, result == null);
+    }
+    
+    public void testSchemaGen() throws Exception {
+    	List controlSchemas = new ArrayList();
+    	InputStream is = ClassLoader.getSystemResourceAsStream("org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlaccessortype/none/employee-none.xsd");
+    	controlSchemas.add(is);
+    	super.testSchemaGen(controlSchemas);
+    }
+   public void testRoundTrip() throws Exception{
+       // Not applicable since id is a write only mapping
+    }   
+   }
---
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlaccessortype/property/XmlAccessorTypePropertyTestCases.java
@@ -0,0 +1,95 @@
+/*******************************************************************************
+ * Copyright (c) 1998, 2011 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ * dmccann - June 17/2009 - 2.0 - Initial implementation
+ ******************************************************************************/
+package org.eclipse.persistence.testing.jaxb.externalizedmetadata.xmlaccessortype.property;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+
+import org.eclipse.persistence.jaxb.JAXBContextFactory;
+import org.eclipse.persistence.testing.jaxb.JAXBWithJSONTestCases;
+
+/**
+ * Tests class level @XmlAccessorType override via eclipselink-oxm.xml.
+ * Here, the Employee object has the access set to 'NONE', but this is
+ * overridden as 'PROPERTY'.
+ * 
+ * Positive test.
+ */
+public class XmlAccessorTypePropertyTestCases extends JAXBWithJSONTestCases {    
+    private static final String XML_RESOURCE = "org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlaccessortype/employee-property.xml";
+    private static final String JSON_RESOURCE = "org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlaccessortype/employee-property.json";
+
+    /**
+     * This is the preferred (and only) constructor.
+     * 
+     * @param name
+     */
+    public XmlAccessorTypePropertyTestCases(String name) throws Exception{
+        super(name);
+        setClasses(new Class[]{Employee.class});
+        setControlDocument(XML_RESOURCE);
+        setControlJSON(JSON_RESOURCE);
+    }
+    
+    protected Object getControlObject() {
+		Employee emp = new Employee(0);	
+		return emp;
+	}
+
+    public Object getWriteControlObject() {
+		Employee emp = new Employee(66);	
+		emp.firstName = "firstName";
+		emp.lastName = "lastName";		
+		return emp;
+	}	
+    
+    public Map getProperties(){
+		InputStream inputStream = ClassLoader.getSystemResourceAsStream("org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlaccessortype/property/eclipselink-oxm.xml");
+
+		HashMap<String, Source> metadataSourceMap = new HashMap<String, Source>();
+	    metadataSourceMap.put("org.eclipse.persistence.testing.jaxb.externalizedmetadata.xmlaccessortype.property", new StreamSource(inputStream));
+	    Map<String, Map<String, Source>> properties = new HashMap<String, Map<String, Source>>();
+	    properties.put(JAXBContextFactory.ECLIPSELINK_OXM_XML_KEY, metadataSourceMap);		
+        
+        return properties;
+	}
+	
+    public void testInstanceDocValidation() throws Exception {
+    	InputStream schema = ClassLoader.getSystemResourceAsStream("org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlaccessortype/property/employee-property.xsd");        
+        StreamSource schemaSource = new StreamSource(schema); 
+                
+        MyMapStreamSchemaOutputResolver outputResolver = new MyMapStreamSchemaOutputResolver();
+        getJAXBContext().generateSchema(outputResolver);
+        
+        InputStream instanceDocStream = ClassLoader.getSystemResourceAsStream(XML_RESOURCE);
+        String result = validateAgainstSchema(instanceDocStream, schemaSource, outputResolver );        
+        assertTrue("Instance doc validation (employee.xml) failed unxepectedly: " + result, result == null);
+    }
+    
+    public void testSchemaGen() throws Exception {
+    	List controlSchemas = new ArrayList();
+    	InputStream is = ClassLoader.getSystemResourceAsStream("org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlaccessortype/property/employee-property.xsd");
+    	controlSchemas.add(is);
+    	super.testSchemaGen(controlSchemas);
+    }
+    public void testRoundTrip() throws Exception{
+        //Not applicable since id is a write only mapping
+    }
+    
+}
