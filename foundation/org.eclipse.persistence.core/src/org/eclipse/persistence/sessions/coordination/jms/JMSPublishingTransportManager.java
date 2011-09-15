0de513e6d64f906971ef6f20e31d77be9cc28391
Guy Pelletier <guy.pelletier@oracle.com>
2011-09-15 14:58:34 +0000

    Fixing remote command manager exception string (removing security information)

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/sessions/coordination/jms/JMSPublishingTransportManager.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/sessions/coordination/jms/JMSPublishingTransportManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************  
- * Copyright (c) 2010 Oracle. All rights reserved.  
+ * Copyright (c) 2011 Oracle. All rights reserved.  
  * This program and the accompanying materials are made available under the  
  * terms of the Eclipse Public License v1.0, which accompanies this distribution  
  * and is available at http://www.eclipse.org/legal/epl-v10.html.  
@@ -72,9 +72,9 @@ public class JMSPublishingTransportManager extends BroadcastTransportManager {
         } catch (Exception ex) {
             RemoteCommandManagerException rcmException;
             if(isLocalConnectionBeingCreated) {
-                rcmException = RemoteCommandManagerException.errorCreatingLocalJMSConnection(topicName, connectionFactoryName, getRemoteContextProperties(), ex);
+                rcmException = RemoteCommandManagerException.errorCreatingLocalJMSConnection(topicName, connectionFactoryName, ex);
             } else {
-                rcmException = RemoteCommandManagerException.errorCreatingJMSConnection(topicName, connectionFactoryName, getRemoteContextProperties(), ex);
+                rcmException = RemoteCommandManagerException.errorCreatingJMSConnection(topicName, connectionFactoryName, ex);
             }
             throw rcmException;
         } finally {
