97f4459d3a6e2f6a1b959d7550a6ae5dc50021be
Tom Ware <tom.ware@oracle.com>
2011-09-21 13:19:31 +0000

    Bug 358261 - Cache Coordination fails to keep OneToMany with order column updated when using CacheCoordinationType.SEND_NEW_OBJECTS_WITH_CHANGES

--- foundation/eclipselink.core.test/src/org/eclipse/persistence/testing/tests/distributedservers/rcm/RCMDistributedServersModel.java
+++ foundation/eclipselink.core.test/src/org/eclipse/persistence/testing/tests/distributedservers/rcm/RCMDistributedServersModel.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2010 Oracle. All rights reserved.
+ * Copyright (c) 1998, 2011 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the 
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
  * which accompanies this distribution. 
@@ -108,6 +108,8 @@ public class RCMDistributedServersModel extends DistributedServersModel {
         addTest(new UpdateObjectInvalidationTest());
         addTest(new NativeUpdateObjectInvalidationTest());
         addTest(new NewObjectWithOptimisticLockingTest());
+        addTest(new OrderedListNewObjectTest());
+
     }
 
     public void startCacheSynchronization() {
