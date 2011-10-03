6cb3d091f9ee7314aa81775a2af4c79b4c26859d
Tom Ware <tom.ware@oracle.com>
2011-10-03 19:11:24 +0000

    Bug 239829 - Please do not log and rethrow exception

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/logging/DefaultSessionLog.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/logging/DefaultSessionLog.java
@@ -9,7 +9,7 @@
  *
  * Contributors:
  *     Oracle - initial API and implementation from Oracle TopLink
- ******************************************************************************/  
+ ******************************************************************************/
 package org.eclipse.persistence.logging;
 
 import java.io.FileWriter;
@@ -24,52 +24,50 @@ import org.eclipse.persistence.exceptions.ValidationException;
 import org.eclipse.persistence.internal.helper.Helper;
 
 /**
- * <p><b>Purpose</b>: Default log used for the session when message logging is enabled.
- * The session can log information such as,<ul>
- * <li> all SQL executed
- * <li> informational messages
- * <li> debugging information
- * <li> all exceptions that occur within TopLink
+ * <b>Purpose</b>: Default log used for the session when message logging is
+ * enabled. The session can log information such as,
+ * <ul>
+ * <li>all SQL executed
+ * <li>informational messages
+ * <li>debugging information
+ * <li>all exceptions that occur within TopLink
  * </ul>
- * As well information about the message can be logged such as,<ul>
- * <li> the session logging the message
- * <li> the connection executing the SQL
- * <li> the thread in which the log entry occurred
- * <li> the exact time (to milliseconds) that the log entry occurred
- * <li> the stack trace to the exception
+ * As well information about the message can be logged such as,
+ * <ul>
+ * <li>the session logging the message
+ * <li>the connection executing the SQL
+ * <li>the thread in which the log entry occurred
+ * <li>the exact time (to milliseconds) that the log entry occurred
+ * <li>the stack trace to the exception
  * </ul>
+ * 
  * @see SessionLog
  * @see AbstractSessionLog
- *
+ * 
  * @author Big Country
  */
 public class DefaultSessionLog extends AbstractSessionLog implements Serializable {
 
-    /** The filename associated with this DefaultSessionLog, if it is being written out to a file **/
+    /**
+     * The filename associated with this DefaultSessionLog, if it is being
+     * written out to a file
+     **/
     protected String fileName;
 
     /**
-     * Represents the Map that stores log levels per the name space strings.
-     * The keys are category names. The values are log levels.
+     * Represents the Map that stores log levels per the name space strings. The
+     * keys are category names. The values are log levels.
      */
     private Map<String, Integer> categoryLogLevelMap = new HashMap();
 
-    /**
-     * PUBLIC:
-     * Create a new default session log.
-     */
     public DefaultSessionLog() {
         super();
         for (int i = 0; i < loggerCatagories.length; i++) {
-            String loggerCategory = loggerCatagories[i]; 
+            String loggerCategory = loggerCatagories[i];
             categoryLogLevelMap.put(loggerCategory, null);
         }
     }
 
-    /**
-     * PUBLIC:
-     * Create a new default session log for the given writer.
-     */
     public DefaultSessionLog(Writer writer) {
         this();
         this.initialize(writer);
@@ -77,9 +75,9 @@ public class DefaultSessionLog extends AbstractSessionLog implements Serializabl
 
     @Override
     public void setLevel(int level, String category) {
-        if(category == null) {
+        if (category == null) {
             this.level = level;
-        } else if(categoryLogLevelMap.containsKey(category)) {
+        } else if (categoryLogLevelMap.containsKey(category)) {
             categoryLogLevelMap.put(category, level);
         }
     }
@@ -88,23 +86,22 @@ public class DefaultSessionLog extends AbstractSessionLog implements Serializabl
     public int getLevel(String category) {
         if (category != null) {
             Integer level = this.categoryLogLevelMap.get(category);
-            
+
             if (level != null) {
                 return level;
             }
         }
-        
+
         return super.getLevel(category);
     }
 
     /**
-     * PUBLIC:
+     * Check if a message of the given level would actually be logged by the
+     * logger with name space built from the given session and category.
      * <p>
-     * Check if a message of the given level would actually be logged by the logger
-     * with name space built from the given session and category.
-     * </p><p>
-     * @return true if the given message level will be logged for the given category
-     * </p>
+     * 
+     * @return true if the given message level will be logged for the given
+     *         category
      */
     @Override
     public boolean shouldLog(int level, String category) {
@@ -112,8 +109,8 @@ public class DefaultSessionLog extends AbstractSessionLog implements Serializabl
     }
 
     /**
-     * Initialize the log to be backward-compatible with
-     * the original TopLink log.
+     * Initialize the log to be backward-compatible with the original TopLink
+     * log.
      */
     protected void initialize() {
         this.setShouldPrintSession(true);
@@ -128,9 +125,8 @@ public class DefaultSessionLog extends AbstractSessionLog implements Serializabl
     }
 
     /**
-     * PUBLIC:
-     * Log the entry.
-     * This writes the log entries information to a writer such as System.out or a file.
+     * Log the entry. This writes the log entries information to a
+     * writer such as System.out or a file.
      */
     public void log(SessionLogEntry entry) {
         if (!shouldLog(entry.getLevel(), entry.getNameSpace())) {
@@ -141,22 +137,22 @@ public class DefaultSessionLog extends AbstractSessionLog implements Serializabl
             try {
                 printPrefixString(entry.getLevel());
                 this.getWriter().write(getSupplementDetailString(entry));
-    
-                if (entry.hasException()) {
-                    if (entry.getLevel() == SEVERE) {
-                        entry.getException().printStackTrace(new PrintWriter(getWriter()));
-                    } else if (entry.getLevel() <= WARNING) {
-                        if (shouldLogExceptionStackTrace()) {
-                            entry.getException().printStackTrace(new PrintWriter(getWriter()));
-                        } else {
-                            writeMessage(entry.getException().toString());
-                        }
-                    }
-                } else {
+
+                if (entry.hasMessage()) {
                     writeMessage(formatMessage(entry));
+                    getWriter().write(Helper.cr());
+                    getWriter().flush();
                 }
-                getWriter().write(Helper.cr());
-                getWriter().flush();
+                
+                if (entry.hasException()) {
+                    if (shouldLogExceptionStackTrace()) {
+                        entry.getException().printStackTrace(new PrintWriter(getWriter()));
+                    } else {
+                        writeMessage(entry.getException().toString());
+                    }
+                    getWriter().write(Helper.cr());
+                    getWriter().flush();
+                } 
             } catch (IOException exception) {
                 throw ValidationException.logIOError(exception);
             }
@@ -164,9 +160,8 @@ public class DefaultSessionLog extends AbstractSessionLog implements Serializabl
     }
 
     /**
-     * PUBLIC:
-     * Set the writer that will receive the
-     * formatted log entries for a file name.
+     * Set the writer that will receive the formatted log entries for a
+     * file name.
      */
     public void setWriter(String aFileName) {
         if (aFileName != null) {
@@ -180,11 +175,9 @@ public class DefaultSessionLog extends AbstractSessionLog implements Serializabl
     }
 
     /**
-     * PUBLIC:
-     * For the given writer, return it's associated filename.
-     * If associated writer does not have a filename, return null.
+     * For the given writer, return it's associated filename. If
+     * associated writer does not have a filename, return null.
      */
-    //Added for F2104: Properties.XML  .. gn
     public String getWriterFilename() {
         return fileName;
     }
