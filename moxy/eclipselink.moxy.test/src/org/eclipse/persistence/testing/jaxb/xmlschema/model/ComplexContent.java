b6f2bf9db646a7883a936b0d9c3c91bd36626f48
Denise Smith <denise.smith@oracle.com>
2011-10-25 18:23:11 +0000

    Missing files from checkin for bug 360860

---
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/xmlschema/model/ComplexContent.java
@@ -0,0 +1,131 @@
+/*******************************************************************************
+ * Copyright (c) 1998, 2011 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ * Denise Smith - 2.3
+ ******************************************************************************/
+package org.eclipse.persistence.testing.jaxb.xmlschema.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;extension base="{http://www.w3.org/2001/XMLSchema}annotated">
+ *       &lt;choice>
+ *         &lt;element name="restriction" type="{http://www.w3.org/2001/XMLSchema}complexRestrictionType"/>
+ *         &lt;element name="extension" type="{http://www.w3.org/2001/XMLSchema}extensionType"/>
+ *       &lt;/choice>
+ *       &lt;attribute name="mixed" type="{http://www.w3.org/2001/XMLSchema}boolean" />
+ *       &lt;anyAttribute processContents='lax' namespace='##other'/>
+ *     &lt;/extension>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "restriction",
+    "extension"
+})
+@XmlRootElement(name = "complexContent")
+public class ComplexContent
+    extends Annotated
+{
+
+    protected ComplexRestrictionType restriction;
+    protected ExtensionType extension;
+    @XmlAttribute(name = "mixed")
+    protected Boolean mixed;
+
+    /**
+     * Gets the value of the restriction property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link ComplexRestrictionType }
+     *     
+     */
+    public ComplexRestrictionType getRestriction() {
+        return restriction;
+    }
+
+    /**
+     * Sets the value of the restriction property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link ComplexRestrictionType }
+     *     
+     */
+    public void setRestriction(ComplexRestrictionType value) {
+        this.restriction = value;
+    }
+
+    /**
+     * Gets the value of the extension property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link ExtensionType }
+     *     
+     */
+    public ExtensionType getExtension() {
+        return extension;
+    }
+
+    /**
+     * Sets the value of the extension property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link ExtensionType }
+     *     
+     */
+    public void setExtension(ExtensionType value) {
+        this.extension = value;
+    }
+
+    /**
+     * Gets the value of the mixed property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Boolean }
+     *     
+     */
+    public Boolean isMixed() {
+        return mixed;
+    }
+
+    /**
+     * Sets the value of the mixed property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Boolean }
+     *     
+     */
+    public void setMixed(Boolean value) {
+        this.mixed = value;
+    }
+
+}
