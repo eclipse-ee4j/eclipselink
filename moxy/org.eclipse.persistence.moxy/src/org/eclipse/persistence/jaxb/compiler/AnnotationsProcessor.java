355f1575176f1cb533d4d6faab4b8cfe639bfb76
Matt MacIvor <matt.macivor@oracle.com>
2011-11-14 19:50:28 +0000

    Fix for bug 360860 Reviewed by Denise Smith

--- moxy/org.eclipse.persistence.moxy/src/org/eclipse/persistence/jaxb/compiler/AnnotationsProcessor.java
+++ moxy/org.eclipse.persistence.moxy/src/org/eclipse/persistence/jaxb/compiler/AnnotationsProcessor.java
@@ -765,6 +765,14 @@ public class AnnotationsProcessor {
                         throw JAXBException.propertyOrFieldShouldBeAnAttribute(property.getPropertyName());
                     }
                 }
+                
+                
+                // handle XmlElementRef(s) - validate and build the required
+                // ElementDeclaration object               
+                 if (property.isReference()) {
+                    processReferenceProperty(property, tInfo, jClass);
+                }
+                 
                 if (property.isSwaAttachmentRef() && !this.hasSwaRef) {
                     this.hasSwaRef = true;
                 }
@@ -880,14 +888,11 @@ public class AnnotationsProcessor {
             	if (property.isTransient()) {
                     continue;
                 }
-                
-                // handle XmlElementRef(s) - validate and build the required
-                // ElementDeclaration object               
-                 if (property.isReference()) {
-                    processReferenceProperty(property, info, next);
-                }
-                 
                 JavaClass type = property.getActualType();
+                
+                if(property.isReference()) {
+                    processReferencePropertyTypes(property, info, next);
+                }
                 
                 if (!(this.typeInfo.containsKey(type.getQualifiedName())) && shouldGenerateTypeInfo(type)) {
                 	PackageInfo pInfo = getPackageInfoForPackage(next);                	
@@ -2160,25 +2165,11 @@ public class AnnotationsProcessor {
                     typeName = type.getQualifiedName();
                 }
             }
-            
-            if(JAVAX_XML_BIND_JAXBELEMENT.equals(typeName) && type.getActualTypeArguments().size() >0){
-            	JavaClass theType = (JavaClass) type.getActualTypeArguments().iterator().next();
-            	processPropertyTypes(theType);
-            }
 
-            // for DEFAULT, if from XML the type will be
-            // "XmlElementRef.DEFAULT",
-            // and from annotations the value will be "XmlElementref$DEFAULT"
             if (!(nextRef.getType().equals("javax.xml.bind.annotation.XmlElementRef.DEFAULT") || nextRef.getType().equals("javax.xml.bind.annotation.XmlElementRef$DEFAULT"))) {
                 typeName = nextRef.getType();
                 type = helper.getJavaClass(typeName);
             }
-            TypeInfo refTypeInfo = typeInfo.get(type.getQualifiedName());
-            if (refTypeInfo==null && shouldGenerateTypeInfo(type)) {
-                JavaClass[] jClassArray = new JavaClass[] { type };
-                buildNewTypeInfo(jClassArray);
-            }
-            
             
             boolean missingReference = true;
             for (Entry<String, ElementDeclaration> entry : xmlRootElements.entrySet()) {
@@ -2218,6 +2209,41 @@ public class AnnotationsProcessor {
             }
         }
         return property;
+    }
+    
+    private void processReferencePropertyTypes(Property property, TypeInfo info, JavaClass theClass) {
+        for (org.eclipse.persistence.jaxb.xmlmodel.XmlElementRef nextRef : property.getXmlElementRefs()) {
+            JavaClass type = property.getType();
+            String typeName = type.getQualifiedName();
+            if (isCollectionType(property)) {
+                if (type.hasActualTypeArguments()) {
+                    type = property.getGenericType();
+                    typeName = type.getQualifiedName();
+                }
+            }
+            
+            if(JAVAX_XML_BIND_JAXBELEMENT.equals(typeName) && type.getActualTypeArguments().size() >0){
+            	JavaClass theType = (JavaClass) type.getActualTypeArguments().iterator().next();
+                TypeInfo refTypeInfo = typeInfo.get(theType.getQualifiedName());
+                if (refTypeInfo==null && shouldGenerateTypeInfo(theType)) {
+                    JavaClass[] jClassArray = new JavaClass[] { theType };
+                    buildNewTypeInfo(jClassArray);            
+                }
+            }
+
+            // for DEFAULT, if from XML the type will be
+            // "XmlElementRef.DEFAULT",
+            // and from annotations the value will be "XmlElementref$DEFAULT"
+            if (!(nextRef.getType().equals("javax.xml.bind.annotation.XmlElementRef.DEFAULT") || nextRef.getType().equals("javax.xml.bind.annotation.XmlElementRef$DEFAULT"))) {
+                typeName = nextRef.getType();
+                type = helper.getJavaClass(typeName);
+            }
+            TypeInfo refTypeInfo = typeInfo.get(type.getQualifiedName());
+            if (refTypeInfo==null && shouldGenerateTypeInfo(type)) {
+                JavaClass[] jClassArray = new JavaClass[] { type };
+                buildNewTypeInfo(jClassArray);
+            }
+        }
     }
 
     private void processPropertyAnnotations(TypeInfo info, JavaClass cls, JavaHasAnnotations javaHasAnnotations, Property property) {
