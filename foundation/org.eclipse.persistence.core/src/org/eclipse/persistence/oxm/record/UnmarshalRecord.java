97162250326127cadf5709e543e53ed297999c66
Matt MacIvor <matt.macivor@oracle.com>
2011-11-15 17:44:59 +0000

    Perf optimization. Reviewed by Blaise Doughan

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/oxm/record/UnmarshalRecord.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/oxm/record/UnmarshalRecord.java
@@ -104,6 +104,8 @@ public class UnmarshalRecord extends XMLRecord implements ExtendedContentHandler
     private Map<XPathFragment, Integer> indexMap;
     private List<NullCapableValue> nullCapableValues;
     private Map<ContainerValue, Object> containersMap;
+    private Object lastAccessedContainer;
+    private ContainerValue lastAccessedContainerNode;
     private boolean isBufferCDATA;
     private Attributes attributes;
     private QName typeQName;
@@ -170,6 +172,8 @@ public class UnmarshalRecord extends XMLRecord implements ExtendedContentHandler
         if(null != containersMap) {
             containersMap.clear();
         }
+        lastAccessedContainer = null;
+        lastAccessedContainerNode = null;
         isBufferCDATA = false;
         attributes = null;
         typeQName = null;
@@ -299,7 +303,13 @@ public class UnmarshalRecord extends XMLRecord implements ExtendedContentHandler
         if (null == containersMap) {
             return null;
         }
-        return containersMap.get(containerValue);
+        if(containerValue == lastAccessedContainerNode && lastAccessedContainer != null) {
+        	return lastAccessedContainer;
+        }
+        Object container = containersMap.get(containerValue);
+        lastAccessedContainer = container;
+        lastAccessedContainerNode = containerValue;
+        return container;
     }
 
     /**
