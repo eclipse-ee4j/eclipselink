7919126dbd1f1ae5c0412816a4d2b6269d57621f
Denise Smith <denise.smith@oracle.com>
2011-12-21 15:21:01 +0000

    Fix for bug 367239 Reviewed by Blaise Doughan

--- moxy/org.eclipse.persistence.moxy/src/org/eclipse/persistence/jaxb/compiler/AnnotationsProcessor.java
+++ moxy/org.eclipse.persistence.moxy/src/org/eclipse/persistence/jaxb/compiler/AnnotationsProcessor.java
@@ -2469,15 +2469,15 @@ public class AnnotationsProcessor {
             org.eclipse.persistence.jaxb.xmlmodel.XmlNullPolicy policy = new org.eclipse.persistence.jaxb.xmlmodel.XmlNullPolicy();
             policy.setEmptyNodeRepresentsNull(nullPolicy.emptyNodeRepresentsNull());
             policy.setIsSetPerformedForAbsentNode(nullPolicy.isSetPerformedForAbsentNode());
-            policy.setXsiNilRepresentsNull(new Boolean(nullPolicy.xsiNilRepresentsNull()));
+            policy.setXsiNilRepresentsNull(Boolean.valueOf(nullPolicy.xsiNilRepresentsNull()));
             policy.setNullRepresentationForXml(org.eclipse.persistence.jaxb.xmlmodel.XmlMarshalNullRepresentation.valueOf(nullPolicy.nullRepresentationForXml().toString()));
             property.setNullPolicy(policy);
 
         } else if (helper.isAnnotationPresent(property.getElement(), XmlIsSetNullPolicy.class)) {
             XmlIsSetNullPolicy nullPolicy = (XmlIsSetNullPolicy) helper.getAnnotation(property.getElement(), XmlIsSetNullPolicy.class);
             org.eclipse.persistence.jaxb.xmlmodel.XmlIsSetNullPolicy policy = new org.eclipse.persistence.jaxb.xmlmodel.XmlIsSetNullPolicy();
-            policy.setEmptyNodeRepresentsNull(nullPolicy.emptyNodeRepresentsNull());
-            policy.setXsiNilRepresentsNull(new Boolean(nullPolicy.xsiNilRepresentsNull()));
+            policy.setEmptyNodeRepresentsNull(nullPolicy.emptyNodeRepresentsNull());       
+            policy.setXsiNilRepresentsNull(Boolean.valueOf(nullPolicy.xsiNilRepresentsNull()));
             policy.setNullRepresentationForXml(org.eclipse.persistence.jaxb.xmlmodel.XmlMarshalNullRepresentation.valueOf(nullPolicy.nullRepresentationForXml().toString()));
             policy.setIsSetMethodName(nullPolicy.isSetMethodName());
             for (XmlParameter next : nullPolicy.isSetParameters()) {
