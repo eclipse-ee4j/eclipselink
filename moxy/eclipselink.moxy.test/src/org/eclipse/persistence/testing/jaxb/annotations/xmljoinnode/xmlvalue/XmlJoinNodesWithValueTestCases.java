e832f87774b41c1036f87f74fe11a300d16d329d
Matt MacIvor <matt.macivor@oracle.com>
2012-01-06 20:06:19 +0000

    Fix for bug 367803 Reviewed by Blaise Doughan

---
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/annotations/xmljoinnode/xmlvalue/XmlJoinNodesWithValueTestCases.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     Matt MacIvor - 2.4 - initial implementation
+ ******************************************************************************/
+package org.eclipse.persistence.testing.jaxb.annotations.xmljoinnode.xmlvalue;
+
+import java.util.ArrayList;
+
+import org.eclipse.persistence.testing.jaxb.JAXBTestCases;
+
+public class XmlJoinNodesWithValueTestCases extends JAXBTestCases {
+
+    private static final String XML_RESOURCE="org/eclipse/persistence/testing/jaxb/annotations/xmljoinnode/xmlvalue/root.xml";
+    public XmlJoinNodesWithValueTestCases(String name) throws Exception {
+        super(name);
+        setClasses(new Class[]{Root.class, Order.class, Customer.class});
+        setControlDocument(XML_RESOURCE);
+    }
+
+    @Override
+    protected Object getControlObject() {
+        Root root = new Root();
+        root.customers = new ArrayList<Customer>();
+        root.orders = new ArrayList<Order>();
+        
+        Order order = new Order();
+        order.id = "123";
+        order.itemId = "456";
+        root.orders.add(order);
+        
+        Customer customer = new Customer();
+        customer.info = "value";
+        customer.order = order;
+        root.customers.add(customer);
+        
+        return root;
+    }
+
+}
--- moxy/org.eclipse.persistence.moxy/src/org/eclipse/persistence/jaxb/compiler/Property.java
+++ moxy/org.eclipse.persistence.moxy/src/org/eclipse/persistence/jaxb/compiler/Property.java
@@ -1163,6 +1163,9 @@ public class Property implements Cloneable {
      */
     public void setXmlJoinNodes(XmlJoinNodes xmlJoinNodes) {
         this.xmlJoinNodes = xmlJoinNodes;
+        if(xmlJoinNodes != null && !CompilerHelper.hasNonAttributeJoinNodes(this)) {
+            this.isAttribute = true;
+        }
     }
 
     /**
@@ -1214,6 +1217,9 @@ public class Property implements Cloneable {
      */
     public void setXmlJoinNodesList(List<XmlJoinNodes> xmlJoinNodesList) {
         this.xmlJoinNodesList = xmlJoinNodesList;
+        if(xmlJoinNodesList != null && !(xmlJoinNodesList.isEmpty()) && !CompilerHelper.hasNonAttributeJoinNodes(this)) {
+            this.isAttribute = true;
+        }
     }
     
     /**
