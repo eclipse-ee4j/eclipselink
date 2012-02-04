78ddc50cfef2ec3f6b1d8ad0752a075fa0c41810
Denise Smith <denise.smith@oracle.com>
2012-02-04 13:19:32 +0000

    Fix for bug 370601 Reviewed by Matt MacIvor

---
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/innerclasses/notincontext/InnerClassNotInContextTestCases.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     Denise Smith - February 2012
+ ******************************************************************************/
+package org.eclipse.persistence.testing.jaxb.innerclasses.notincontext;
+
+import org.eclipse.persistence.testing.jaxb.JAXBTestCases;
+import org.eclipse.persistence.testing.jaxb.javadoc.xmlaccessororder.Employee;
+
+public class InnerClassNotInContextTestCases extends JAXBTestCases{
+	private final static String XML_RESOURCE = "org/eclipse/persistence/testing/jaxb/innerclasses/notincontext.xml";
+	  
+	public InnerClassNotInContextTestCases(String name) throws Exception {
+		super(name);
+	    setControlDocument(XML_RESOURCE);    
+        Class[] classes = new Class[1];
+        classes[0] = TestObjectWrapper.class;
+        setClasses(classes);
+	}
+
+	@Override
+	protected Object getControlObject() {
+		TestObjectWrapper tow = new TestObjectWrapper();
+		TestObject testObject = new TestObject();
+		testObject.testString = "testStringValue";
+		tow.testObject = testObject;
+		return tow;
+	}
+
+	
+}
