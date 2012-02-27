34e4db38de86b25ff54433d7d75fc5d0881b6596
Andrei Ilitchev <andrei.ilitchev@oracle.com>
2012-02-27 14:59:49 +0000

    Bug 362913 - Aggregate mappings do not support "isNullAllowed' when aggregate has OneToOneMapping relationships Correction to the previous patch In AggregateObjectMapping don't need to do anything if isNullAllowed is false. In DDLGenerationJUnitTestSuite.testEmbeddedOneToOne made sure that correct table name is used by custom sql on all database platforms.

--- jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/tests/jpa/ddlgeneration/DDLGenerationJUnitTestSuite.java
+++ jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/tests/jpa/ddlgeneration/DDLGenerationJUnitTestSuite.java
@@ -60,6 +60,7 @@ import org.eclipse.persistence.config.EntityManagerProperties;
 import org.eclipse.persistence.exceptions.ValidationException;
 import org.eclipse.persistence.internal.helper.ConversionManager;
 import org.eclipse.persistence.sessions.Project;
+import org.eclipse.persistence.sessions.server.ServerSession;
 import org.eclipse.persistence.testing.framework.junit.JUnitTestCase;
 import org.eclipse.persistence.testing.models.jpa.ddlgeneration.multitenant.Boss;
 import org.eclipse.persistence.testing.models.jpa.ddlgeneration.multitenant.Capo;
@@ -1182,7 +1183,7 @@ public class DDLGenerationJUnitTestSuite extends JUnitTestCase {
             currentPatent.setId(2);
             em.persist(currentPatent);
             em.flush();
-            em.createNativeQuery("UPDATE PatentInvestigator SET CURRENT_DESRIPTION = 'Current', CURRENT_PATENT = " + currentPatent.getId() + " WHERE ID = " + investigator.getId()).executeUpdate();            
+            em.createNativeQuery("UPDATE " + em.unwrap(ServerSession.class).getDescriptor(PatentInvestigator.class).getDefaultTable().getName() + " SET CURRENT_DESRIPTION = 'Current', CURRENT_PATENT = " + currentPatent.getId() + " WHERE ID = " + investigator.getId()).executeUpdate();            
             em.refresh(investigatorRead);
             assertTrue("after refresh investigatorRead.getLast() == null", investigatorRead.getLast() != null);
             assertTrue("after refresh investigatorRead.getLast.getPatent() == null", investigatorRead.getLast().getPatent() != null);
