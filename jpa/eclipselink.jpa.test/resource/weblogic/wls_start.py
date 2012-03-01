6e2cbb2da770d73e12dfae7d36fd6f1ef00c4ed7
Kevin Yuan <kevin.yuan@oracle.com>
2012-03-01 16:44:50 +0000

    Change start-weblogic-server using jdk as default.
    Code reviewed by Edwin Tang

--- jpa/eclipselink.jpa.test/resource/weblogic/wls_start.py
+++ jpa/eclipselink.jpa.test/resource/weblogic/wls_start.py
@@ -9,14 +9,14 @@
 # Start server using wlst command
 #===========================================================================
 
-startServer('%%TARGET_SERVER%%', 'eclipselink', url='t3://%%WL_HOST%%:%%WL_PORT%%', username='%%WL_USR%%', password='%%WL_PWD%%', domainDir='%%WL_DOMAIN%%', jvmArgs='-Xms256m -Xmx960m -Dweblogic.Stdout=stdout.log -Dweblogic.Stderr=stderr.log')
+#startServer('%%TARGET_SERVER%%', 'eclipselink', url='t3://%%WL_HOST%%:%%WL_PORT%%', username='%%WL_USR%%', password='%%WL_PWD%%', domainDir='%%WL_DOMAIN%%', jvmArgs='-Xms256m -Xmx960m -Dweblogic.Stdout=stdout.log -Dweblogic.Stderr=stderr.log')
 
 #===========================================================================
 # Using the following instead of above "jvmarg" setting when using SUN jdk 
 # because jrockit doesn't support PermSize when the server run on SUN jdk
 #===========================================================================
 
-#startServer('%%TARGET_SERVER%%', 'eclipselink', url='t3://%%WL_HOST%%:%%WL_PORT%%', username='%%WL_USR%%', password='%%WL_PWD%%', domainDir='%%WL_DOMAIN%%', jvmArgs='-XX:PermSize=128m -XX:MaxPermSize=256m -Dweblogic.Stdout=stdout.log -Dweblogic.Stderr=stderr.log')
+startServer('%%TARGET_SERVER%%', 'eclipselink', url='t3://%%WL_HOST%%:%%WL_PORT%%', username='%%WL_USR%%', password='%%WL_PWD%%', domainDir='%%WL_DOMAIN%%', jvmArgs='-XX:PermSize=128m -XX:MaxPermSize=256m -Dweblogic.Stdout=stdout.log -Dweblogic.Stderr=stderr.log')
 
 #===========================================================================
 # Add the following jvmarg(s) into wlst command when you try to debug
