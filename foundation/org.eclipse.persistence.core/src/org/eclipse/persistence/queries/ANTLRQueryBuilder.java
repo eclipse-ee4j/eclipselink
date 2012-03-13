c1e7fb8d5fafb74482f8f5d4d7e51b11a6226106
Pascal Filion <pascal.filion@oracle.com>
2012-03-13 15:58:02 +0000

    Changed String jpqlQuery to CharSequence jpqlQuery

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/queries/ANTLRQueryBuilder.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/queries/ANTLRQueryBuilder.java
@@ -1,9 +1,9 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2010 Oracle. All rights reserved.
+ * Copyright (c) 1998, 2012 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
  * which accompanies this distribution.
- * The Eclipse Public License is available athttp://www.eclipse.org/legal/epl-v10.html
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
  * and the Eclipse Distribution License is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *
@@ -13,88 +13,100 @@
  ******************************************************************************/
 package org.eclipse.persistence.queries;
 
+import org.eclipse.persistence.expressions.Expression;
 import org.eclipse.persistence.internal.jpa.parsing.JPQLParseTree;
 import org.eclipse.persistence.internal.jpa.parsing.jpql.JPQLParser;
 import org.eclipse.persistence.internal.jpa.parsing.jpql.JPQLParserFactory;
 import org.eclipse.persistence.internal.sessions.AbstractSession;
-import org.eclipse.persistence.queries.DatabaseQuery;
-
 
 /**
- * This class defines the default {@link JPAQueryBuilder} implementation 
- * that uses an Antlr-based parser and {@link DatabaseQuery} conversion 
- * mechanism.
- * 
+ * This class defines the default {@link JPAQueryBuilder} implementation that uses an Antlr-based
+ * parser and {@link DatabaseQuery} conversion mechanism.
+ *
  * @see JPQLParser
  * @see JPQLParser
  * @see JPQLParserFactory
  * @see JPAQueryBuilder
  * @see JPAQueryBuilderManager
  *
- * @version 2.2
+ * @version 2.4
  * @since 2.2
  * @author John Bracken
  */
 public final class ANTLRQueryBuilder implements JPAQueryBuilder {
-   
-    /**
-     * Constructs a new instance of 
-     * {@link ANTLRQueryBuilder}.
-     */
-    public ANTLRQueryBuilder() {
-        super();
-    }
 
-    /**
-     * {@inheritDoc}
-     */
-    public DatabaseQuery buildQuery(String jpqlQuery, AbstractSession session) {
-        // TODO - must set class loader
-        
-        JPQLParseTree parseTree = buildParseTree(jpqlQuery, session);
-        DatabaseQuery databaseQuery = parseTree.createDatabaseQuery();
-        databaseQuery.setJPQLString(jpqlQuery);
+   /**
+    * Creates a new <code>ANTLRQueryBuilder</code>.
+    */
+	public ANTLRQueryBuilder() {
+		super();
+	}
 
-        populateQueryInternal(jpqlQuery, session, parseTree, databaseQuery);
-        
-        return databaseQuery;
-    }
+	/**
+    * Builds a {@link JPQLParseTree} based on the given query and associated {@link AbstractSession}.
+    *
+    * @param jpqlQuery The JPQL query
+    * @param session The associated session
+    * @return The {@link JPQLParseTree}
+    */
+   private JPQLParseTree buildParseTree(CharSequence jpqlQuery, AbstractSession session) {
+   	JPQLParseTree parseTree = JPQLParser.buildParseTree(jpqlQuery.toString());
+   	parseTree.setClassLoader(session.getDatasourcePlatform().getConversionManager().getLoader());
+   	return parseTree;
+   }
 
-    /**
-     * {@inheritDoc}
-     */
-    public void populateQuery(String jpqlQuery, DatabaseQuery query, AbstractSession session) {
-        new JPQLParserFactory().populateQuery(jpqlQuery, (ObjectLevelReadQuery)query, session);
-    }
+   /**
+    * {@inheritDoc}
+    */
+   public DatabaseQuery buildQuery(CharSequence jpqlQuery, AbstractSession session) {
+   	// TODO - must set class loader
 
-    
-    /**
-     * Builds a {@link JPQLParseTree} based on the given query and associated
-     * {@link AbstractSession}.
-     * 
-     * @param jpqlQuery The jpql query.
-     * @param session The associated session.
-     * @return The {@link JPQLParseTree}.
-     */
-    private JPQLParseTree buildParseTree(String jpqlQuery, AbstractSession session) {
-        JPQLParseTree parseTree = JPQLParser.buildParseTree(jpqlQuery);
-        parseTree.setClassLoader(session.getDatasourcePlatform().getConversionManager().getLoader());
-        return parseTree;
-    }
+   	JPQLParseTree parseTree = buildParseTree(jpqlQuery, session);
+   	DatabaseQuery databaseQuery = parseTree.createDatabaseQuery();
+   	databaseQuery.setJPQLString(jpqlQuery.toString());
 
-    /**
-     * Populates the given query based on the provided parse tree, jpql, and session.
-     * 
-     * @param jpqlQuery the jpql query.
-     * @param session The associated {@link AbstractSession}.
-     * @param parseTree The parse tree for the given query.
-     * @param databaseQuery The database query to be updated.
-     */
-    private void populateQueryInternal(String jpqlQuery, AbstractSession session, JPQLParseTree parseTree, DatabaseQuery databaseQuery) {
-        parseTree.populateQuery(databaseQuery, session);
-        // TODO - must set class loader.
+   	populateQueryInternal(session, parseTree, databaseQuery);
 
-        // Bug#4646580 Add arguments to query.
-        parseTree.addParametersToQuery(databaseQuery);
-    }
+   	return databaseQuery;
+   }
+
+   /**
+	 * {@inheritDoc}
+	 */
+	public Expression buildSelectionCriteria(String entityName,
+	                                         String additionalCriteria,
+	                                         AbstractSession session) {
+
+      StringBuilder jpqlQuery = new StringBuilder();
+      jpqlQuery.append("select this from ");
+      jpqlQuery.append(entityName);
+      jpqlQuery.append(" this where ");
+      jpqlQuery.append(additionalCriteria.trim());
+      return buildQuery(jpqlQuery, session).getSelectionCriteria();
+	}
+
+   /**
+    * {@inheritDoc}
+    */
+   public void populateQuery(CharSequence jpqlQuery, DatabaseQuery query, AbstractSession session) {
+   	new JPQLParserFactory().populateQuery(jpqlQuery.toString(), (ObjectLevelReadQuery) query, session);
+   }
+
+   /**
+    * Populates the given query based on the provided parse tree and session.
+    *
+    * @param session The associated {@link AbstractSession}
+    * @param parseTree The parse tree for the given query
+    * @param databaseQuery The database query to be updated
+    */
+   private void populateQueryInternal(AbstractSession session,
+                                      JPQLParseTree parseTree,
+                                      DatabaseQuery databaseQuery) {
+
+   	// TODO - must set class loader.
+   	parseTree.populateQuery(databaseQuery, session);
+
+   	// Bug#4646580 Add arguments to query.
+   	parseTree.addParametersToQuery(databaseQuery);
+   }
 }
--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/queries/JPAQueryBuilder.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/queries/JPAQueryBuilder.java
@@ -1,9 +1,9 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2010 Oracle. All rights reserved.
+ * Copyright (c) 1998, 2012 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
  * which accompanies this distribution.
- * The Eclipse Public License is available athttp://www.eclipse.org/legal/epl-v10.html
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
  * and the Eclipse Distribution License is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *
@@ -13,50 +13,57 @@
  ******************************************************************************/
 package org.eclipse.persistence.queries;
 
-import org.eclipse.persistence.config.PersistenceUnitProperties;
 import org.eclipse.persistence.internal.sessions.AbstractSession;
-import org.eclipse.persistence.queries.DatabaseQuery;
 
 /**
- * This interface defines the entry point for implementing a JPA Query parsing system in EclipseLink.
+ * This interface defines the entry point for implementing a JPQL query parsing system in EclipseLink.
  * <p>
- * By default, EclipseLink uses {@link ANTLRQueryBuilder} for parsing a query and
- * converting it into a {@link DatabaseQuery}.
+ * By default, EclipseLink uses {@link ANTLRQueryBuilder} for parsing a query and converting it into
+ * a {@link DatabaseQuery}.
  * <p>
  * Third-parties can implement this interface and provide a different JPQL parsing system if required.
- * The {@link JPAQueryBuilder} implementing class can be registered via a persistence unit property and
- * the implementation class must have a public, zero-arg constructor.
+ * The {@link JPAQueryBuilder} implementing class can be registered via a persistence unit property
+ * and the implementation class must have a public, zero-arg constructor.
  *
- * @see PersistenceUnitProperties#JPQL_QUERY_BUILDER
- * @see PersistenceUnitProperties#JPQL_QUERY_VALIDATION
- *
- * @version 2.2
+ * @version 2.4
  * @since 2.2
  * @author John Bracken
  * @author Pascal Filion
  */
 public interface JPAQueryBuilder {
 
-	/**
-	 * Creates a fully initialized {@link DatabaseQuery} by parsing the given Java Persistence query.
-	 *
-	 * @param jpqlQuery A non-<code>null</code> string representation of the query to parse and to
-	 * convert into a {@link DatabaseQuery}
-	 * @param query The query to populate with the derived jpql criteria
-	 * @param session The EclipseLink {@link AbstractSession} that this query will execute against
-	 * @return The fully initialized {@link DatabaseQuery}
-	 */
-        // TODO - must set class loader
-	DatabaseQuery buildQuery(String jpqlQuery, AbstractSession session);
+   /**
+    * Creates a fully initialized {@link DatabaseQuery} by parsing the given JPQL query.
+    *
+    * @param jpqlQuery A non-<code>null</code> string representation of the query to parse and to
+    * convert into a {@link DatabaseQuery}
+    * @param query The query to populate with the derived JPQL query
+    * @param session The EclipseLink {@link AbstractSession} that this query will execute against
+    * @return The fully initialized {@link DatabaseQuery}
+    */
+   // TODO - must set class loader
+   DatabaseQuery buildQuery(CharSequence jpqlQuery, AbstractSession session);
+
+   /**
+    * Creates a new {@link Expression} that represents the given selection criteria.
+    *
+    * @param entityName The name of the entity for which a criteria is created
+    * @param selectionCriteria The string representation of a conditional expression to parse
+    * @param session The EclipseLink {@link AbstractSession} that this query will execute against
+    * @return The fully initialized {@link Expression}
+    */
+//   Expression buildSelectionCriteria(String entityName,
+//                                     String selectionCriteria,
+//                                     AbstractSession session);
 
 	/**
-	 * Populates an existing {@link DatabaseQuery} by parsing the given Java Persistence query.
-	 *
-	 * @param jpqlQuery A non-<code>null</code> string representation of the query to parse and to
-	 * convert into a {@link DatabaseQuery}
-	 * @param query The query to populate with the derived jpql criteria
-	 * @param session The EclipseLink {@link AbstractSession} that this query will execute against
-	 */
-        // TODO - must set class loader
-	void populateQuery(String jpqlQuery, DatabaseQuery query, AbstractSession session);
+    * Populates the given {@link DatabaseQuery} by parsing the given JPQL query.
+    *
+    * @param jpqlQuery A non-<code>null</code> string representation of the query to parse and to
+    * convert into a {@link DatabaseQuery}
+    * @param query The query to populate with the derived JPQL query
+    * @param session The EclipseLink {@link AbstractSession} that this query will execute against
+    */
+   // TODO - must set class loader
+   void populateQuery(CharSequence jpqlQuery, DatabaseQuery query, AbstractSession session);
 }
