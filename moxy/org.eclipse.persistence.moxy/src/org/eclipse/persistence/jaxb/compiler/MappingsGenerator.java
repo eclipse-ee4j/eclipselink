2c81b42778f366b15988ac8d940da0f92106b3f5
Blaise Doughan <blaise.doughan@oracle.com>
2012-03-28 16:29:16 +0000

    Fix for Bug 375446 - Missing default namespace declaration - 2 packages, difference namespaces, depending upon processing order
    Code reviewed by:  Denise Smith
    
    Fix Details:
    In MappingsGenerator.getPrefixForNamespace we now take into account that the namespace URI may correspond to the default namespace on the global NamespaceResolver.

--- moxy/org.eclipse.persistence.moxy/src/org/eclipse/persistence/jaxb/compiler/MappingsGenerator.java
+++ moxy/org.eclipse.persistence.moxy/src/org/eclipse/persistence/jaxb/compiler/MappingsGenerator.java
@@ -1921,7 +1921,10 @@ public class MappingsGenerator {
         }           
         String prefix = globalNamespaceResolver.resolveNamespaceURI(URI);
         if(prefix == null){
-            if(suggestedPrefix != null){
+            if(URI.equals(globalNamespaceResolver.getDefaultNamespaceURI())) { 
+                namespaceResolver.setDefaultNamespaceURI(URI);
+                return null;
+            } else if(suggestedPrefix != null){
         	    prefix = globalNamespaceResolver.generatePrefix(suggestedPrefix);
             }else{
         	    prefix = globalNamespaceResolver.generatePrefix();
