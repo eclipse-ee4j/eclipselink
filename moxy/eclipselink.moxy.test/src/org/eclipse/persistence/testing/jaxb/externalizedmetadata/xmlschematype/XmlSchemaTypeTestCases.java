9bac89dabd9456fc0108b9a0eec0e018f947981d
Rick Barkhouse <rick.barkhouse@oracle.com>
2012-05-08 16:10:42 +0000

    Fix for bug 317717

--- moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlschematype/XmlSchemaTypeTestCases.java
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlschematype/XmlSchemaTypeTestCases.java
@@ -14,8 +14,8 @@ package org.eclipse.persistence.testing.jaxb.externalizedmetadata.xmlschematype;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.TimeZone;
 
-import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
@@ -29,10 +29,10 @@ import org.w3c.dom.Document;
 public class XmlSchemaTypeTestCases extends ExternalizedMetadataTestCases {
     private static final String CONTEXT_PATH = "org.eclipse.persistence.testing.jaxb.externalizedmetadata.xmlschematype";
     private static final String PATH = "org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlschematype/";
-    
+
     /**
      * This is the preferred (and only) constructor.
-     * 
+     *
      * @param name
      */
     public XmlSchemaTypeTestCases(String name) {
@@ -40,10 +40,10 @@ public class XmlSchemaTypeTestCases extends ExternalizedMetadataTestCases {
     }
 
     /**
-     * Tests @XmlSchemaType schema generation via eclipselink-oxm.xml.  Here, a 
-     * package-level xml-schema-type declaration exists.  It's value "date" 
+     * Tests @XmlSchemaType schema generation via eclipselink-oxm.xml.  Here, a
+     * package-level xml-schema-type declaration exists.  It's value "date"
      * should override the one set in code ("year").
-     * 
+     *
      * Positive test.
      */
     public void testXmlSchemaTypePkgSchemaGen() {
@@ -55,10 +55,10 @@ public class XmlSchemaTypeTestCases extends ExternalizedMetadataTestCases {
     }
 
     /**
-     * Tests @XmlSchemaType schema generation via eclipselink-oxm.xml.  Here, a 
-     * property-level xml-schema-type declaration exists.  It's value "date" 
+     * Tests @XmlSchemaType schema generation via eclipselink-oxm.xml.  Here, a
+     * property-level xml-schema-type declaration exists.  It's value "date"
      * should override the one set in code ("year").
-     * 
+     *
      * Positive test.
      */
     public void testXmlSchemaTypePropSchemaGen() {
@@ -74,7 +74,7 @@ public class XmlSchemaTypeTestCases extends ExternalizedMetadataTestCases {
      * and property-level xml-schema-type declarations exist.  It is expected that the property-
      * level value "date" will override the package-level one ("time"), as well as the package-
      * level one set in code ("year").
-     * 
+     *
      * Positive test.
      */
     public void testXmlSchemaTypeOverrideSchemaGen() {
@@ -88,9 +88,9 @@ public class XmlSchemaTypeTestCases extends ExternalizedMetadataTestCases {
     /**
      * Tests @XmlSchemaType schema generation via eclipselink-oxm.xml.  Here, both package-level
      * and property-level @XmlSchemaType annotations exist in code.  In Xml, there is a package-
-     * level declaration ("day"), but it is expected that the property-level value "date" in code will 
+     * level declaration ("day"), but it is expected that the property-level value "date" in code will
      * override it.
-     * 
+     *
      * Positive test.
      */
     public void testXmlSchemaTypeClassOverridesPackageSchemaGen() {
@@ -98,14 +98,13 @@ public class XmlSchemaTypeTestCases extends ExternalizedMetadataTestCases {
         MySchemaOutputResolver outputResolver = generateSchemaWithFileName(new Class[] { EmployeeWithAnnotation.class }, CONTEXT_PATH, metadataFile, 1);
         // validate schema
         String controlSchema = PATH + "schema1.xsd";
-        //System.out.println(ExternalizedMetadataTestCases.getFileAsText(outputResolver.schemaFiles.get(EMPTY_NAMESPACE)));
         compareSchemas(outputResolver.schemaFiles.get(EMPTY_NAMESPACE), new File(controlSchema));
     }
 
     /**
-     * Tests xml-schema-type schema generation via eclipselink-oxm.xml when 
+     * Tests xml-schema-type schema generation via eclipselink-oxm.xml when
      * combined with xml-id.
-     * 
+     *
      * Positive test.
      */
     public void testXmlSchemaTypeWithIDSchemaGen() {
@@ -113,34 +112,31 @@ public class XmlSchemaTypeTestCases extends ExternalizedMetadataTestCases {
         MySchemaOutputResolver outputResolver = generateSchemaWithFileName(new Class[] { EmployeeWithMethods.class }, CONTEXT_PATH, metadataFile, 1);
         // validate schema
         String controlSchema = PATH + "schema2.xsd";
-        //System.out.println(ExternalizedMetadataTestCases.getFileAsText(outputResolver.schemaFiles.get(EMPTY_NAMESPACE)));
         compareSchemas(outputResolver.schemaFiles.get(EMPTY_NAMESPACE), new File(controlSchema));
     }
 
     /**
      * Tests @XmlSchemaType schema generation combined with @XmlID.
-     * 
+     *
      * Positive test.
      */
     public void testXmlSchemaTypeWithIDAnnotationSchemaGen() {
         MySchemaOutputResolver outputResolver = generateSchema(new Class[] { EmployeeWithAnnotationOnMethod.class }, 1);
         // validate schema
         String controlSchema = PATH + "schema3.xsd";
-        //System.out.println(ExternalizedMetadataTestCases.getFileAsText(outputResolver.schemaFiles.get(EMPTY_NAMESPACE)));
         compareSchemas(outputResolver.schemaFiles.get(EMPTY_NAMESPACE), new File(controlSchema));
     }
 
-    /**
-     * Tests @XmlSchemaType via eclipselink-oxm.xml.
-     * 
-     * Positive test.
-     */
-    /*public void testXmlSchemaType() {
-        // load XML metadata
+    public void testXmlSchemaType() {
+        // load XML metadata and create JAXBContext
         String metadataFile = PATH + "eclipselink-oxm-property.xml";
-
         MySchemaOutputResolver outputResolver = generateSchemaWithFileName(new Class[] { Employee.class }, CONTEXT_PATH, metadataFile, 1);
-        
+
+        // This test's instance document contains a date in EST time zone, so temporarily
+        // reset the VM time zone
+        TimeZone currentTimeZone = TimeZone.getDefault();
+        TimeZone.setDefault(TimeZone.getTimeZone("America/New_York"));
+
         // load instance doc
         String src = PATH + "employee.xml";
         InputStream iDocStream = loader.getResourceAsStream(src);
@@ -150,13 +146,12 @@ public class XmlSchemaTypeTestCases extends ExternalizedMetadataTestCases {
 
         // unmarshal
         Object obj = null;
-        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
         try {
+            Unmarshaller unmarshaller = getJAXBContext().createUnmarshaller();
             obj = unmarshaller.unmarshal(iDocStream);
             assertFalse("Unmarshalled object is null.", obj == null);
-        } catch (JAXBException e) {
-            e.printStackTrace();
-            fail("Unmarshal operation failed.");
+        } catch (Exception e) {
+            fail("Unmarshal operation failed. " + e);
         }
 
         Document testDoc = parser.newDocument();
@@ -164,19 +159,19 @@ public class XmlSchemaTypeTestCases extends ExternalizedMetadataTestCases {
         try {
             ctrlDoc = getControlDocument(src);
         } catch (Exception e) {
-            e.printStackTrace();
-            fail("An unexpected exception occurred loading control document [" + src + "].");
+            fail("An unexpected exception occurred loading control document [" + src + "]. " + e);
         }
 
         // marshal
-        Marshaller marshaller = jaxbContext.createMarshaller();
         try {
+            Marshaller marshaller = jaxbContext.createMarshaller();
             marshaller.marshal(obj, testDoc);
-            marshaller.marshal(obj, System.out);
             assertTrue("Document comparison failed unxepectedly: ", compareDocuments(ctrlDoc, testDoc));
-        } catch (JAXBException e) {
-            e.printStackTrace();
-            fail("Unmarshal operation failed.");
+        } catch (Exception e) {
+            fail("Marshal operation failed. " + e);
         }
-    }*/
+
+        TimeZone.setDefault(currentTimeZone);
+    }
+
 }
--- moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlschematypes/XmlSchemaTypesTestCases.java
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlschematypes/XmlSchemaTypesTestCases.java
@@ -20,6 +20,7 @@ import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TimeZone;
 
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
@@ -32,12 +33,15 @@ import org.eclipse.persistence.testing.jaxb.externalizedmetadata.xmlschematypes.
  * Tests XmlSchemaTypes via eclipselink-oxm.xml
  *
  */
-public class XmlSchemaTypesTestCases extends JAXBWithJSONTestCases {    
+public class XmlSchemaTypesTestCases extends JAXBWithJSONTestCases {
     private static final String XML_RESOURCE = "org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlschematypes/employee.xml";
     private static final String JSON_RESOURCE = "org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlschematypes/employee.json";
+
+    private TimeZone currentTimeZone;
+
     /**
      * This is the preferred (and only) constructor.
-     * 
+     *
      * @param name
      */
     public XmlSchemaTypesTestCases(String name) throws Exception{
@@ -47,40 +51,55 @@ public class XmlSchemaTypesTestCases extends JAXBWithJSONTestCases {
         setClasses(new Class[]{Employee.class});
     }
 
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        // This test's instance document contains a date in EST time zone, so temporarily
+        // reset the VM time zone
+        currentTimeZone = TimeZone.getDefault();
+        TimeZone.setDefault(TimeZone.getTimeZone("America/New_York"));
+    }
+
+    @Override
+    public void tearDown() {
+        super.tearDown();
+        TimeZone.setDefault(currentTimeZone);
+    }
+
     protected Object getControlObject() {
-		Employee emp = new Employee();
-		
-		//setup control Employee
+        Employee emp = new Employee();
+
+        //setup control Employee
         GregorianCalendar calendar = new GregorianCalendar();
         Date theDate = new Date(new Long("1262840400000"));
         calendar.setTime(theDate);
-        
+
         emp.hireDate = calendar;
         emp.lengthOfEmployment = new BigDecimal("1.000010");
-		
-		return emp;
-	}
-	
-	public Map getProperties(){
-		InputStream inputStream = ClassLoader.getSystemResourceAsStream("org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlschematypes/eclipselink-oxm.xml");
 
-		HashMap<String, Source> metadataSourceMap = new HashMap<String, Source>();
-	    metadataSourceMap.put("org.eclipse.persistence.testing.jaxb.externalizedmetadata.xmlschematypes", new StreamSource(inputStream));
-	    Map<String, Map<String, Source>> properties = new HashMap<String, Map<String, Source>>();
-	    properties.put(JAXBContextFactory.ECLIPSELINK_OXM_XML_KEY, metadataSourceMap);		
-        
-        return properties;
-	}
-    
-    
-    public void testSchemaGen() throws Exception{
-    	List controlSchemas = new ArrayList();
-    	InputStream is = ClassLoader.getSystemResourceAsStream("org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlschematypes/schema.xsd");
-    	
-    	controlSchemas.add(is);
-    	
-    	super.testSchemaGen(controlSchemas);
-    
+        return emp;
     }
-    
+
+    public Map getProperties(){
+        InputStream inputStream = ClassLoader.getSystemResourceAsStream("org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlschematypes/eclipselink-oxm.xml");
+
+        HashMap<String, Source> metadataSourceMap = new HashMap<String, Source>();
+        metadataSourceMap.put("org.eclipse.persistence.testing.jaxb.externalizedmetadata.xmlschematypes", new StreamSource(inputStream));
+        Map<String, Map<String, Source>> properties = new HashMap<String, Map<String, Source>>();
+        properties.put(JAXBContextFactory.ECLIPSELINK_OXM_XML_KEY, metadataSourceMap);
+
+        return properties;
+    }
+
+
+    public void testSchemaGen() throws Exception{
+        List controlSchemas = new ArrayList();
+        InputStream is = ClassLoader.getSystemResourceAsStream("org/eclipse/persistence/testing/jaxb/externalizedmetadata/xmlschematypes/schema.xsd");
+
+        controlSchemas.add(is);
+
+        super.testSchemaGen(controlSchemas);
+
+    }
+
 }
