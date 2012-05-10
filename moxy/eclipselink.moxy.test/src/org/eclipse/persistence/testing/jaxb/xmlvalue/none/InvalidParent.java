6a28fdac6516e05a5821f8057685d425ada76e85
Peter Krogh <peter.krogh@oracle.com>
2012-05-10 18:40:35 +0000

    Change copyright from 2011 - 2012

--- moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/xmlvalue/none/InvalidParent.java
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/xmlvalue/none/InvalidParent.java
@@ -1,7 +1,7 @@
 package org.eclipse.persistence.testing.jaxb.xmlvalue.none;
 
 /*******************************************************************************
- * Copyright (c) 2011 Oracle. All rights reserved.
+ * Copyright (c) 2011, 2012 Oracle and/or its affiliates. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
  * which accompanies this distribution.
