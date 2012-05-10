90b64100649731e6e3753af5b4ed7f67b18c58a6
David McCann <david.mccann@oracle.com>
2012-05-10 14:47:07 +0000

    Fix for Bug 379138 - DBWS JUnit: WeaklyTypedRefCursor suite should have StoredFunction tests Reviewed by:  matt.macivor@oracle.com Tests:  WeakRefCursorTestSuite

--- utils/eclipselink.dbws.builder.test.oracle/src/dbws/testing/weakrefcursor/WeakRefCursorTestSuite.java
+++ utils/eclipselink.dbws.builder.test.oracle/src/dbws/testing/weakrefcursor/WeakRefCursorTestSuite.java
@@ -68,18 +68,23 @@ public class WeakRefCursorTestSuite extends DBWSTestSuite {
     static final String CREATE_WEAKLY_TYPED_REF_CURSOR_TEST_PACKAGE =
         "CREATE OR REPLACE PACKAGE " + WEAKLY_TYPED_REF_CURSOR_TEST_PACKAGE + " AS" +
             "\nTYPE " + WEAKLY_TYPED_REF_CURSOR + " IS REF CURSOR;" +
-            "\nPROCEDURE GET_EMS(P_EMS " + WEAKLY_TYPED_REF_CURSOR_TABLE+".NAME%TYPE, P_EMS_SET OUT " +
-            WEAKLY_TYPED_REF_CURSOR + ");" +
+            "\nPROCEDURE GET_EMS(P_EMS " + WEAKLY_TYPED_REF_CURSOR_TABLE+".NAME%TYPE, P_EMS_SET OUT " + WEAKLY_TYPED_REF_CURSOR + ");" +
+            "\nFUNCTION GET_EMS_FUNC(P_EMS VARCHAR) RETURN " + WEAKLY_TYPED_REF_CURSOR + ";" + 
         "\nEND " + WEAKLY_TYPED_REF_CURSOR_TEST_PACKAGE + ";";
     static final String CREATE_WEAKLY_TYPED_REF_CURSOR_TEST_PACKAGE_BODY =
         "CREATE OR REPLACE PACKAGE BODY " + WEAKLY_TYPED_REF_CURSOR_TEST_PACKAGE + " AS" +
-            "\nPROCEDURE GET_EMS(P_EMS " + WEAKLY_TYPED_REF_CURSOR_TABLE+".NAME%TYPE, P_EMS_SET OUT " +
-            WEAKLY_TYPED_REF_CURSOR + ") AS" +
+            "\nPROCEDURE GET_EMS(P_EMS " + WEAKLY_TYPED_REF_CURSOR_TABLE+".NAME%TYPE, P_EMS_SET OUT " + WEAKLY_TYPED_REF_CURSOR + ") AS" +
             "\nBEGIN" +
-            "\n    OPEN P_EMS_SET FOR" +
-            "\n        SELECT ID, NAME, SINCE FROM " + WEAKLY_TYPED_REF_CURSOR_TABLE +
-              " WHERE NAME LIKE P_EMS;" +
+                "\nOPEN P_EMS_SET FOR" +
+                "\nSELECT ID, NAME, SINCE FROM " + WEAKLY_TYPED_REF_CURSOR_TABLE + " WHERE NAME LIKE P_EMS;" +
             "\nEND GET_EMS;" +
+            "\nFUNCTION GET_EMS_FUNC(P_EMS VARCHAR) RETURN " + WEAKLY_TYPED_REF_CURSOR + " IS" +
+            "\nP_EMS_SET " + WEAKLY_TYPED_REF_CURSOR + ";"+
+            "\nBEGIN" +
+                "\nOPEN P_EMS_SET FOR" +
+                "\nSELECT ID, NAME, SINCE FROM " + WEAKLY_TYPED_REF_CURSOR_TABLE + " WHERE NAME LIKE P_EMS;" +
+                "\nRETURN P_EMS_SET;" +
+            "\nEND GET_EMS_FUNC;" +
         "\nEND " + WEAKLY_TYPED_REF_CURSOR_TEST_PACKAGE + ";";
 
     static final String DROP_WEAKLY_TYPED_REF_CURSOR_TEST_PACKAGE =
@@ -154,6 +159,13 @@ public class WeakRefCursorTestSuite extends DBWSTestSuite {
                   "isCollection=\"true\" " +
                   "isSimpleXMLFormat=\"true\" " +
               "/>" +
+              "<plsql-procedure " +
+                  "name=\"weakRefCursorTest2\" " +
+                  "catalogPattern=\"" + WEAKLY_TYPED_REF_CURSOR_TEST_PACKAGE + "\" " +
+                  "procedurePattern=\"GET_EMS_FUNC\" " +
+                  "isCollection=\"true\" " +
+                  "isSimpleXMLFormat=\"true\" " +
+              "/>" +
             "</dbws-builder>";
           builder = new DBWSBuilder();
           DBWSTestSuite.setUp(".");
@@ -171,6 +183,20 @@ public class WeakRefCursorTestSuite extends DBWSTestSuite {
     @Test
     public void weakRefCursorTest() {
         Invocation invocation = new Invocation("weakRefCursorTest");
+        invocation.setParameter("P_EMS", "mike%");
+        Operation op = xrService.getOperation(invocation.getName());
+        Object result = op.invoke(xrService, invocation);
+        assertNotNull("result is null", result);
+        XMLMarshaller marshaller = xrService.getXMLContext().createMarshaller();
+        Document doc = xmlPlatform.createDocument();
+        marshaller.marshal(result, doc);
+        Document controlDoc = xmlParser.parse(new StringReader(EMPLOYEES_IN_DEPT10_XML));
+        assertTrue("Expected:\n" + documentToString(controlDoc) + "\nActual:\n" + documentToString(doc), comparer.isNodeEqual(controlDoc, doc));
+    }
+    @SuppressWarnings("rawtypes")
+    @Test
+    public void weakRefCursorTest2() {
+        Invocation invocation = new Invocation("weakRefCursorTest2");
         invocation.setParameter("P_EMS", "mike%");
         Operation op = xrService.getOperation(invocation.getName());
         Object result = op.invoke(xrService, invocation);
