a374ea57b1d1ac1df16c0becfac51afd187de7b9
Peter Krogh <peter.krogh@oracle.com>
2012-05-10 19:18:51 +0000

    Change copyright from 2011 - 2012

--- utils/org.eclipse.persistence.dbws.builder/src/org/eclipse/persistence/tools/dbws/WarArchiver.java
+++ utils/org.eclipse.persistence.dbws.builder/src/org/eclipse/persistence/tools/dbws/WarArchiver.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2011 Oracle. All rights reserved.
+ * Copyright (c) 1998, 2012 Oracle and/or its affiliates. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
