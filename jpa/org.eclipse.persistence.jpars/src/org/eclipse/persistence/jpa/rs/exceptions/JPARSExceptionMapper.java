1fda6838838df0e978464d66a99a2cf4a0521fca
Tom Ware <tom.ware@oracle.com>
2012-05-11 14:19:02 +0000

    Add JPARSException and JPARSExceptionMapper

---
+++ jpa/org.eclipse.persistence.jpars/src/org/eclipse/persistence/jpa/rs/exceptions/JPARSExceptionMapper.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
+ * which accompanies this distribution. 
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *      tware - initial 
+ ******************************************************************************/
+package org.eclipse.persistence.jpa.rs.exceptions;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class JPARSExceptionMapper implements ExceptionMapper<JPARSException> {
+    public Response toResponse(JPARSException exception){
+        return Response.status(Status.BAD_REQUEST).build();
+    }
+}
