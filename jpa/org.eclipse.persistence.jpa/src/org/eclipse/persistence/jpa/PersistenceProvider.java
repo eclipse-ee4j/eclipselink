3c10b1618ee64418526bb77015407c0aa8bc2855
Andrei Ilitchev <andrei.ilitchev@oracle.com>
2012-05-11 18:56:00 +0000

    Bug 377531 - createContainerEMF does not close factory that attempted to deploy and failed. createContainerEntityManagerFactory method in PersistenceProvider class should immediately close the factory that attempted to deploy and failed - just like createEntityManagerFactory method does.

--- jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/jpa/PersistenceProvider.java
+++ jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/jpa/PersistenceProvider.java
@@ -234,15 +234,24 @@ public class PersistenceProvider implements javax.persistence.spi.PersistencePro
                 info.addTransformer(transformer);
             }
         }
-        // When EntityManagerFactory is created, the session is only partially created
-        // When the factory is actually accessed, the emSetupImpl will be used to complete the session construction
-        EntityManagerFactoryImpl factory = new EntityManagerFactoryImpl(emSetupImpl, nonNullProperties);
 
-        // This code has been added to allow validation to occur without actually calling createEntityManager
-        if (emSetupImpl.shouldGetSessionOnCreateFactory(nonNullProperties)) {
-            factory.getDatabaseSession();
+        EntityManagerFactoryImpl factory = null;
+        try {
+            factory = new EntityManagerFactoryImpl(emSetupImpl, nonNullProperties);
+        
+            // This code has been added to allow validation to occur without actually calling createEntityManager
+            if (emSetupImpl.shouldGetSessionOnCreateFactory(nonNullProperties)) {
+                factory.getDatabaseSession();
+            }
+            return factory;
+        } catch (RuntimeException ex) {
+            if(factory != null) {
+                factory.close();
+            } else {
+                emSetupImpl.undeploy();
+            }
+            throw ex;
         }
-        return factory;
     }
 
     /**
