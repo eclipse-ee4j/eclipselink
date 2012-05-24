7b35a06893cf30d1044bec308939184044498af2
Blaise Doughan <blaise.doughan@oracle.com>
2012-05-24 20:26:52 +0000

    Fix for Bug 340673 - Unexpected JAXBElement returned when unmarshalling with inheritance / XmlSeeAlso
    Code reviewed by:  Rick Barkhouse
    
    ---
    
    Resolution
    
    There is a difference in how MOXy and the RI handle this use case.  As MOXy is able to round trip the following document and the RI is not, I believe the MOXy behaviour to be the correct behaviour for this use case.
    
    <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
    <animal xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="cat">
        <name>Fluffy</name>
        <sleepTime>10</sleepTime>
        <enemy>
            <name>Buster</name>
            <barkLevel>LOUD</barkLevel>
        </enemy>
    </animal>
    
    Note both MOXy and the RI handle the following document the same:
    
    <cat>
        <name>Fluffy</name>
        <sleepTime>10</sleepTime>
        <enemy>
            <name>Buster</name>
            <barkLevel>LOUD</barkLevel>
        </enemy>
    </cat>

--- moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/javadoc/xmlseealso/XmlSeeAlsoTest2.java
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/javadoc/xmlseealso/XmlSeeAlsoTest2.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
  * which accompanies this distribution.
@@ -8,18 +8,21 @@
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *
  * Contributors:
- *     Praba Vijayaratnam - 2.3 - initial implementation
+ *     Blaise Doughan - 2.4 - initial implementation
  ******************************************************************************/
 package org.eclipse.persistence.testing.jaxb.javadoc.xmlseealso;
 
+import javax.xml.bind.JAXBElement;
+import javax.xml.namespace.QName;
+
 import org.eclipse.persistence.testing.jaxb.JAXBWithJSONTestCases;
 
-public class XmlSeeAlsoTest extends JAXBWithJSONTestCases {
+public class XmlSeeAlsoTest2 extends JAXBWithJSONTestCases {
 
-    private final static String XML_RESOURCE = "org/eclipse/persistence/testing/jaxb/javadoc/xmlseealso/xmlseealso.xml";
-    private final static String JSON_RESOURCE = "org/eclipse/persistence/testing/jaxb/javadoc/xmlseealso/xmlseealso.json";
+    private final static String XML_RESOURCE = "org/eclipse/persistence/testing/jaxb/javadoc/xmlseealso/xmlseealso2.xml";
+    private final static String JSON_RESOURCE = "org/eclipse/persistence/testing/jaxb/javadoc/xmlseealso/xmlseealso2.json";
 
-    public XmlSeeAlsoTest(String name) throws Exception {
+    public XmlSeeAlsoTest2(String name) throws Exception {
         super(name);
         setControlDocument(XML_RESOURCE);
         setControlJSON(JSON_RESOURCE);
@@ -33,7 +36,8 @@ public class XmlSeeAlsoTest extends JAXBWithJSONTestCases {
         exampleC.owner = "JANE DOE";
         exampleC.name = "meow";
         exampleC.sleephours = 10;
-        return exampleC;
+        return new JAXBElement<Animal>(new QName("animal"), Animal.class,
+                exampleC);
     }
 
 }
