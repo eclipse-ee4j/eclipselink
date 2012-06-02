8152629df6893bbc4535f269025704eb3700902d
Eric Gwin <eric.gwin@oracle.com>
2012-06-02 19:58:18 +0000

    update properties

--- moxy/bin/jaxb-compiler.sh
+++ moxy/bin/jaxb-compiler.sh
@@ -9,8 +9,8 @@ JVM_ARGS="-Xmx256m"
 
 # Please do not change any of the following lines:
 CLASSPATH=`dirname $0`/../jlib/moxy/javax.xml.stream_1.0.1.v201004272200.jar:\
-`dirname $0`/../jlib/moxy/javax.xml.bind_2.2.0.v201005080402.jar:\
-`dirname $0`/../jlib/moxy/javax.activation_1.1.0.v201005080500.jar:\
+`dirname $0`/../jlib/moxy/javax.xml.bind_2.2.0.v201105210648.jar:\
+`dirname $0`/../jlib/moxy/javax.activation_1.1.0.v201108011116.jar:\
 `dirname $0`/../jlib/moxy/com.sun.xml.bind_2.2.0.v201004141950.jar:\
 `dirname $0`/../jlib/moxy/com.sun.tools.xjc_2.2.0.jar:\
 `dirname $0`/../jlib/eclipselink.jar
