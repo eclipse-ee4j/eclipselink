fc13071f2fff76058ca3b65b8cd405d169cbf1ae
Andrei Ilitchev <andrei.ilitchev@oracle.com>
2012-06-25 16:22:58 +0000

    static-eclipselink-annotations.jar model got too big, that causes exceptions in static tests. Moved test entity Customer2 from relationships to fieldaccess.relationship package. Moved db creation and testRemoveReference accordingly.

--- jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/models/jpa/fieldaccess/relationships/Customer2.java
+++ jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/models/jpa/fieldaccess/relationships/Customer2.java
@@ -10,7 +10,7 @@
  * Contributors:
  *     Oracle - initial API and implementation from Oracle TopLink
  ******************************************************************************/  
-package org.eclipse.persistence.testing.models.jpa.relationships;
+package org.eclipse.persistence.testing.models.jpa.fieldaccess.relationships;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -22,8 +22,8 @@ import javax.persistence.*;
 import static javax.persistence.GenerationType.*;
 import static javax.persistence.CascadeType.*;
 
-@Entity
-@Table(name="CMP3_CUSTOMER2")
+@Entity(name="FieldAccessCustomer2")
+@Table(name="CMP3_FIELDACCESS_CUSTOMER2")
 public class Customer2 implements java.io.Serializable {
     @Id
     @GeneratedValue
