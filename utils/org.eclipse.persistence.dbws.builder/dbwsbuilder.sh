73272128676b6dd52362e6640d217d927e52bbb8
dmccann <david.mccann@oracle.com>
2012-08-15 11:07:37 -0400

    Fix for Bug387292 (DBWS - builder scripts reference wrong DDLParser jar version, and should use setenv from parent bin)

--- utils/org.eclipse.persistence.dbws.builder/dbwsbuilder.sh
+++ utils/org.eclipse.persistence.dbws.builder/dbwsbuilder.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-. `dirname $0`/setenv.sh 
+. `dirname $0`/../../bin/setenv.sh 
 
 # User may increase Java memory setting(s) if desired:
 JVM_ARGS=-Xmx256m
@@ -7,7 +7,7 @@ JVM_ARGS=-Xmx256m
 # Please do not change any of the following lines:
 CLASSPATH=`dirname $0`/javax.servlet_2.4.0.v200806031604.jar:\
 `dirname $0`/javax.wsdl_1.6.2.v201012040545.jar:\
-`dirname $0`/org.eclipse.persistence.oracleddlparser_1.0.0.v20120530.jar:\
+`dirname $0`/org.eclipse.persistence.oracleddlparser_1.0.0.v20120628.jar:\
 `dirname $0`/../../jlib/eclipselink.jar:\
 `dirname $0`/eclipselink-dbwsutils.jar:\
 ${DRIVER_CLASSPATH}:\
