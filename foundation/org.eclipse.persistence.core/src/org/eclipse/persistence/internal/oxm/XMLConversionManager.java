894f3608ceca41a6cafbe03eb4fd9a519fdf3d34
mmacivor <matt.macivor@oracle.com>
2012-08-29 09:25:35 -0400

    Updated fix for bug 388256 to handle empty elements for other date types.

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/oxm/XMLConversionManager.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/oxm/XMLConversionManager.java
@@ -809,11 +809,17 @@ public class XMLConversionManager extends ConversionManager implements TimeZoneH
      * @return
      */
     public Duration convertStringToDuration(String sourceString) {
+        if(sourceString == null || sourceString.length() == 0) {
+            return null;
+        }
         return getDatatypeFactory().newDuration(sourceString);
     }
 
     public Calendar convertStringToCalendar(String sourceString, QName schemaTypeQName) {
         XMLGregorianCalendar xmlGregorianCalender = convertStringToXMLGregorianCalendar(sourceString, schemaTypeQName);
+        if(xmlGregorianCalender == null) {
+            return null;
+        }
         return toCalendar(xmlGregorianCalender);
     }
 
@@ -878,7 +884,9 @@ public class XMLConversionManager extends ConversionManager implements TimeZoneH
                  throw XMLConversionException.incorrectTimestampDateTimeFormat(sourceString);
              }
         }
-
+        if(xmlGregorianCalender == null) {
+            return null;
+        }
         GregorianCalendar cal = xmlGregorianCalender.toGregorianCalendar();
         if(xmlGregorianCalender.getTimezone() == DatatypeConstants.FIELD_UNDEFINED) {
             cal.setTimeZone(getTimeZone());
@@ -1128,7 +1136,9 @@ public class XMLConversionManager extends ConversionManager implements TimeZoneH
 
     public java.util.Date convertStringToDate(String sourceString, QName schemaType) {
         XMLGregorianCalendar xmlGregorianCalender = convertStringToXMLGregorianCalendar(sourceString, schemaType);
-
+        if(xmlGregorianCalender == null) {
+            return null;
+        }
         Calendar cal = toCalendar(xmlGregorianCalender);
         Date returnDate = cal.getTime();
 
