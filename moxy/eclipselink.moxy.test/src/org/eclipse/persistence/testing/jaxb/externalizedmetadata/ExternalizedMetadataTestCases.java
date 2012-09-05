eeaf5468df4f3df90487bf1886781adf7730af7b
Rick Barkhouse <rick.barkhouse@oracle.com>
2012-09-05 15:07:21 -0400

    Fix for JAXB test failures relating to eclipselink_oxm rename

--- moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/externalizedmetadata/ExternalizedMetadataTestCases.java
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/externalizedmetadata/ExternalizedMetadataTestCases.java
@@ -560,9 +560,9 @@ public class ExternalizedMetadataTestCases extends TestCase {
         SchemaFactory sFact = SchemaFactory.newInstance(XMLConstants.SCHEMA_URL);
         Schema theSchema;
         try {
-            InputStream bindingsFileXSDInputStream = getClass().getClassLoader().getResourceAsStream("eclipselink_oxm_2_4.xsd");
+            InputStream bindingsFileXSDInputStream = getClass().getClassLoader().getResourceAsStream("eclipselink_oxm_2_5.xsd");
             if (bindingsFileXSDInputStream == null){
-                bindingsFileXSDInputStream = getClass().getClassLoader().getResourceAsStream("org/eclipse/persistence/jaxb/eclipselink_oxm_2_4.xsd");
+                bindingsFileXSDInputStream = getClass().getClassLoader().getResourceAsStream("org/eclipse/persistence/jaxb/eclipselink_oxm_2_5.xsd");
             }
             if (bindingsFileXSDInputStream == null){
                 fail("ERROR LOADING eclipselink_oxm_2_4.xsd");
