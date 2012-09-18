edfe3c13f774c290cb8dca866db7ff03200da381
mmacivor <matt.macivor@oracle.com>
2012-09-18 10:36:41 -0400

    Backport of fix for bug 389407 Reviewed by Denise Smith

---
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/xmlelement/EmployeeWithConstants.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     Matt MacIvor - 2.4.1 - initial implementation
+ ******************************************************************************/
+package org.eclipse.persistence.testing.jaxb.xmlelement;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="employee-data")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class EmployeeWithConstants {
+    
+    @XmlElement(name = "constant")
+    private static final String ANNOTATED_CONSTANT= "VALUE";
+
+    private static final String UN_ANNOTATED_CONSTANT = "ANOTHER VALUE";
+    
+    public String id;
+    
+    public boolean equals(Object obj) {
+        return this.id.equals(((EmployeeWithConstants)obj).id);
+    }
+}
