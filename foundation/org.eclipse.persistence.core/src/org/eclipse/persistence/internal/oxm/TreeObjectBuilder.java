a46036dda2df0f4ccc9cebb338057e36d4c364fa
Rick Barkhouse <rick.barkhouse@oracle.com>
2012-11-20 10:25:46 -0500

    Fix for bug 394596, reviewed by bdougan

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/oxm/TreeObjectBuilder.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/oxm/TreeObjectBuilder.java
@@ -60,6 +60,7 @@ import org.eclipse.persistence.oxm.mappings.XMLObjectReferenceMapping;
 import org.eclipse.persistence.oxm.record.MarshalRecord;
 import org.eclipse.persistence.oxm.record.NodeRecord;
 import org.eclipse.persistence.oxm.record.UnmarshalRecord;
+import org.eclipse.persistence.oxm.record.XMLRecord;
 import org.eclipse.persistence.oxm.sequenced.SequencedObject;
 import org.w3c.dom.Node;
 
@@ -580,6 +581,15 @@ public class TreeObjectBuilder extends XMLObjectBuilder {
      */
     public AbstractRecord createRecord(int size, AbstractSession session) {
         return createRecord(session);
+    }
+
+    @Override
+    protected List addExtraNamespacesToNamespaceResolver(XMLDescriptor desc, XMLRecord marshalRecord, AbstractSession session, boolean allowOverride, boolean ignoreEqualResolvers) {
+        if (rootXPathNode.getNonAttributeChildren() == null) {
+            return null;
+        } else {
+            return super.addExtraNamespacesToNamespaceResolver(desc, marshalRecord, session, allowOverride, ignoreEqualResolvers);
+        }
     }
 
 }
