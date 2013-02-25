271dae54645a3ccc7b0b3a6ca74090593538b315
Blaise Doughan <blaise.doughan@oracle.com>
2013-02-25 13:52:29 -0500

    Fix for Bug 401731 - JAXBIntrospector returning wrong info for classes annotated with @XmlElementDecl
    Code reviewed by:  Denise Smith

--- moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/jaxbintrospector/elementname/JAXBIntrospectorGetElementNameTestCases.java
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/jaxbintrospector/elementname/JAXBIntrospectorGetElementNameTestCases.java
@@ -13,6 +13,7 @@
 package org.eclipse.persistence.testing.jaxb.jaxbintrospector.elementname;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBIntrospector;
 import javax.xml.namespace.QName;
 
@@ -20,19 +21,54 @@ import org.eclipse.persistence.jaxb.JAXBContextFactory;
 
 import junit.framework.TestCase;
 
-public class JAXBIntrospectorGetElementNameTestCases extends TestCase{
+public class JAXBIntrospectorGetElementNameTestCases extends TestCase {
 
-	JAXBContext jaxbContext;
-	JAXBIntrospector jaxbIntrospector;
-	
-	public void setUp() throws Exception{
-		Class[] classesToBeBound = new Class[]{TestObject.class};
-		jaxbContext = JAXBContextFactory.createContext(classesToBeBound, null);
-		jaxbIntrospector = jaxbContext.createJAXBIntrospector();
-	}
-	public void testGetElementNameEmptyStringUri(){
+    JAXBContext jaxbContext;
+    JAXBIntrospector jaxbIntrospector;
+
+    public void setUp() throws Exception {
+        Class[] classesToBeBound = new Class[] { TestObject.class,
+                ElementDeclObject.class, ObjectFactory.class };
+        jaxbContext = JAXBContextFactory.createContext(classesToBeBound, null);
+        jaxbIntrospector = jaxbContext.createJAXBIntrospector();
+    }
+
+    public void testIsElement_XmlRootElement() {
+        TestObject testObject = new TestObject();
+        assertTrue(jaxbIntrospector.isElement(testObject));
+    }
+
+    public void testGetElementName_XmlRootElement() {
         TestObject obj = new TestObject();
         QName controlQname = new QName("someUri", "theRoot");
         assertEquals(controlQname, jaxbIntrospector.getElementName(obj));
-	}
+    }
+
+    public void testIsElement_XmlElementDecl() {
+        ElementDeclObject elementDeclObject = new ElementDeclObject();
+        assertFalse(jaxbIntrospector.isElement(elementDeclObject));
+    }
+
+    public void testGetElementName_XmlElementDecl() {
+        ElementDeclObject elementDeclObject = new ElementDeclObject();
+        assertNull(jaxbIntrospector.getElementName(elementDeclObject));
+    }
+
+    public void testIsElement_JAXBElement() {
+        ElementDeclObject elementDeclObject = new ElementDeclObject();
+        ObjectFactory objectFactory = new ObjectFactory();
+        JAXBElement<ElementDeclObject> jaxbElement = objectFactory
+                .createElementDeclObject(elementDeclObject);
+        assertTrue(jaxbIntrospector.isElement(jaxbElement));
+    }
+
+    public void testGetElementName_JAXBElement() {
+        ElementDeclObject elementDeclObject = new ElementDeclObject();
+        ObjectFactory objectFactory = new ObjectFactory();
+        JAXBElement<ElementDeclObject> jaxbElement = objectFactory
+                .createElementDeclObject(elementDeclObject);
+        QName controlQName = new QName("edo");
+        assertEquals(controlQName, jaxbIntrospector.getElementName(jaxbElement));
+    }
+
 }
