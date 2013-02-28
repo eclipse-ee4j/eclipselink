ba85647768fef5d6380fd62d83ae9ff910da40f2
tware <tom.ware@oracle.com>
2013-02-28 15:09:40 -0500

    Add comment headers

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/jpa/rs/metadata/model/LinkTemplate.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/jpa/rs/metadata/model/LinkTemplate.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Oracle and/or its affiliates. All rights reserved.
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
+ * which accompanies this distribution. 
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     tware - initial implementation
+ ******************************************************************************/
 package org.eclipse.persistence.internal.jpa.rs.metadata.model;
 
 import javax.xml.bind.annotation.XmlRootElement;
