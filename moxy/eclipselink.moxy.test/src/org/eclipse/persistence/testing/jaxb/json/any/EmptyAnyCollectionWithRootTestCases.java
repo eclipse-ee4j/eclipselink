0d839095f088097c094c6b29ceafff99fc60fffa
Blaise Doughan <blaise.doughan@oracle.com>
2013-03-12 13:06:37 -0400

    Fix for Bug 403042 - Invalid JSON output when marshalling empty array for @XmlAnyElement when class has no root element
    Code reviewed by:  Rick Barkhouse
    
    Fix Details:
    Now JSONWriterRecord won't call startCollection or endEmptyCollection for Any mappings when the collection is empty.

---
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/json/any/EmptyAnyCollectionWithRootTestCases.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Oracle and/or its affiliates. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     Blaise Doughan - 2.4.2 - initial implementation
+ ******************************************************************************/
+package org.eclipse.persistence.testing.jaxb.json.any;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.namespace.QName;
+
+import org.eclipse.persistence.jaxb.JAXBContextProperties;
+import org.eclipse.persistence.testing.jaxb.json.JSONMarshalUnmarshalTestCases;
+
+public class EmptyAnyCollectionWithRootTestCases extends JSONMarshalUnmarshalTestCases {
+
+    private String JSON = "org/eclipse/persistence/testing/jaxb/json/any/NoAnyCollectionWithRoot.json";
+
+    public EmptyAnyCollectionWithRootTestCases(String name) throws Exception {
+        super(name);
+        setClasses(new Class[] {Root.class});
+        setControlJSON(JSON);
+    }
+
+    @Override
+    protected Root getControlObject() {
+        return new Root();
+    }
+
+}
