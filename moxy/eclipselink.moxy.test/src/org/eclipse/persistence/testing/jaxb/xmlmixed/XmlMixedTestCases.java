b132f0da5f2a803772a52591e2aaab4cd0745339
Rick Barkhouse <rick.barkhouse@oracle.com>
2013-03-21 12:50:04 -0400

    Fix for bug 372545, reviewed by bdoughan

--- moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/xmlmixed/XmlMixedTestCases.java
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/xmlmixed/XmlMixedTestCases.java
@@ -16,17 +16,21 @@ import java.util.ArrayList;
 
 import org.eclipse.persistence.jaxb.MarshallerProperties;
 import org.eclipse.persistence.jaxb.UnmarshallerProperties;
+import org.eclipse.persistence.testing.jaxb.JAXBTestCases;
 import org.eclipse.persistence.testing.jaxb.JAXBWithJSONTestCases;
 
-public class XmlMixedTestCases extends JAXBWithJSONTestCases {
+public class XmlMixedTestCases extends JAXBTestCases { // extends JAXBWithJSONTestCases
+
     private final static String XML_RESOURCE = "org/eclipse/persistence/testing/jaxb/xmlmixed/root.xml";
+    private final static String XML_RESOURCE_WRITE = "org/eclipse/persistence/testing/jaxb/xmlmixed/root-write.xml";    
     private final static String JSON_RESOURCE = "org/eclipse/persistence/testing/jaxb/xmlmixed/root.json";
 
     public XmlMixedTestCases(String name) throws Exception {
         super(name);
         setClasses(new Class[] {Root.class});
         setControlDocument(XML_RESOURCE);
-        setControlJSON(JSON_RESOURCE);
+        setWriteControlDocument(XML_RESOURCE_WRITE);        
+        //setControlJSON(JSON_RESOURCE);
         jaxbMarshaller.setProperty(MarshallerProperties.JSON_ATTRIBUTE_PREFIX, "@");
         jaxbUnmarshaller.setProperty(UnmarshallerProperties.JSON_ATTRIBUTE_PREFIX, "@");
     }
@@ -37,7 +41,6 @@ public class XmlMixedTestCases extends JAXBWithJSONTestCases {
         root.setElem("element value");
         root.setObjects(new ArrayList<Object>());
         root.getObjects().add("Text Value");
-        
         root.getObjects().add("Text Value2");
         return root;
     }
@@ -57,4 +60,5 @@ public class XmlMixedTestCases extends JAXBWithJSONTestCases {
     
     public void testObjectToXMLDocument() throws Exception {    	
     }
+
 }
