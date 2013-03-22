de969e9f19cc32a0538bd4e6072940eda16164c7
Blaise Doughan <blaise.doughan@oracle.com>
2013-03-22 11:31:37 -0400

    MINOR CHANGE Cleaned up javadoc comment on @XmlInverseReference

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/oxm/annotations/XmlInverseReference.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/oxm/annotations/XmlInverseReference.java
@@ -18,12 +18,13 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * This annotation is used to map a back-pointer.  When configuring an 
- * XmlInverseReference, the "mappedBy" attribute must be set to the field
- * on the reference class that maps to this field.  For example:<br><br>
+ * This annotation is used to map a back-pointer during the unmarshal operation.  
+ * When configuring an &#64;XmlInverseReference, the "mappedBy" attribute must 
+ * be set to the field on the reference class that maps to this field.  For 
+ * example:<br><br>
  * 
  * <code>
- * &nbsp;@XmlRootElement(name="employee")<br>
+ * &nbsp;@XmlRootElement<br>
  * &nbsp;public class Employee {<br>
  * &nbsp;&nbsp;&nbsp;&nbsp;...<br>
  * &nbsp;&nbsp;&nbsp;&nbsp;@XmlElementWrapper(name="phone-numbers")<br>
@@ -38,7 +39,21 @@ import java.lang.annotation.Target;
  * &nbsp;&nbsp;&nbsp;&nbsp;public Employee owningEmployee;<br>
  * &nbsp;&nbsp;&nbsp;&nbsp;...<br>
  * &nbsp;}<br>
- * </code>     
+ * </code>
+ * 
+ * <p>By default using &#64;XmlInverseReference will make the property act the 
+ * same as &#64;XmlTransient for the marshal operation.  You can make the 
+ * property writeable by combining it will &#64;XmlElement.</p>
+ 
+ * <code>
+ * &nbsp;public class PhoneNumber {<br>
+ * &nbsp;&nbsp;&nbsp;&nbsp;...<br>
+ * &nbsp;&nbsp;&nbsp;&nbsp;@XmlInverseReference(mappedBy="phoneNumbers")<br>
+ * &nbsp;&nbsp;&nbsp;&nbsp;@XmlElement<br>
+ * &nbsp;&nbsp;&nbsp;&nbsp;public Employee owningEmployee;<br>
+ * &nbsp;&nbsp;&nbsp;&nbsp;...<br>
+ * &nbsp;}<br>
+ * </code>
  */
 @Target({ElementType.FIELD, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
