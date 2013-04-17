cafc3318fd80aa975a1aab2a4492a6f200fe5fe7
Gul Onural <gul.onural@oracle.com>
2013-04-17 10:49:01 -0400

    Added code to joinTransaction in JPARS

--- dbws/org.eclipse.persistence.dbws/src/org/eclipse/persistence/jpa/rs/util/JTATransactionWrapper.java
+++ dbws/org.eclipse.persistence.dbws/src/org/eclipse/persistence/jpa/rs/util/JTATransactionWrapper.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
  * Copyright (c) 2011, 2013 Oracle and/or its affiliates. All rights reserved.
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
- * which accompanies this distribution.
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
+ * which accompanies this distribution. 
  * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at
+ * and the Eclipse Distribution License is available at 
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *
  * Contributors:
- *      tware - initial
+ *      tware - initial 
  ******************************************************************************/
 package org.eclipse.persistence.jpa.rs.util;
 
@@ -23,6 +23,10 @@ public class JTATransactionWrapper extends TransactionWrapper {
     public void beginTransaction(EntityManager em) {
         AbstractSession session = JpaHelper.getEntityManagerFactory(em).getDatabaseSession();
         session.getExternalTransactionController().beginTransaction(session);
+        // EMs obtained outside the transaction is now prevented from participating or flushing to the trans unless join is called. 
+        if (!em.isJoinedToTransaction()) {
+            em.joinTransaction();
+        }
     }
 
     @Override
@@ -35,7 +39,5 @@ public class JTATransactionWrapper extends TransactionWrapper {
     public void rollbackTransaction(EntityManager em) {
         AbstractSession session = JpaHelper.getEntityManagerFactory(em).getDatabaseSession();
         session.getExternalTransactionController().rollbackTransaction(session);
-
     }
-
 }
