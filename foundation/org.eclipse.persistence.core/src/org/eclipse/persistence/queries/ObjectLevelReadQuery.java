4ed5aa91d0c5134699c35809c65bdb51470afe26
Gordon Yorke <gordon.yorke@oracle.com>
2013-05-16 08:49:04 -0300

    Bug 408057 - Exception not raised when a fetchgroup is executed against a Class that is not configured for fetchgroups Small change

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/queries/ObjectLevelReadQuery.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/queries/ObjectLevelReadQuery.java
@@ -2045,7 +2045,7 @@ public abstract class ObjectLevelReadQuery extends ObjectBuildingQuery {
             }
         } else {
             // FetchGroupManager is null
-            if ((this.fetchGroup != null && !this.fetchGroup.isValidated()) || this.fetchGroupName != null) {
+            if (this.fetchGroup != null || this.fetchGroupName != null) {
                 throw QueryException.fetchGroupValidOnlyIfFetchGroupManagerInDescriptor(getDescriptor().getJavaClassName(), getName());
             }
         }
