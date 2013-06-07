ad997da0e03710c40f434920a9a74ad1210fed76
tware <tom.ware@oracle.com>
2013-06-07 10:14:08 -0400

    Bug 400941 - jpa.jpql.JUnitJPQLUnitTestSuite.testFirstResultOnNamedQuery failed on Derby with 2.5.0.v20130206-c1267ce

--- jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/internal/jpa/EntityManagerFactoryDelegate.java
+++ jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/internal/jpa/EntityManagerFactoryDelegate.java
@@ -737,6 +737,9 @@ public class EntityManagerFactoryDelegate implements EntityManagerFactory, Persi
         }
         if (unwrapped.isReadQuery()){
             ((ReadQuery)unwrapped).setInternalMax((((QueryImpl)query).getMaxResultsInternal()));
+            if (query.getFirstResult() != QueryImpl.UNDEFINED){
+                ((ReadQuery)unwrapped).setFirstResult(query.getFirstResult());
+            }
         }
         this.getServerSession().addQuery(name, unwrapped, true);
     }
