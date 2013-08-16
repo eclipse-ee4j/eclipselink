a8c90752a05a79fdc1d0c579bdea7d7daffdc701
Kevin Yuan <kevin.yuan@oracle.com>
2013-08-16 15:04:48 -0400

    Take remote jpa tests from test-lrg21

--- jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/tests/jpa/FullRegressionTestSuite.java
+++ jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/tests/jpa/FullRegressionTestSuite.java
@@ -97,7 +97,6 @@ import org.eclipse.persistence.testing.tests.jpa.relationships.IsolatedCacheTest
 import org.eclipse.persistence.testing.tests.jpa.relationships.RelationshipModelJUnitTestSuite;
 import org.eclipse.persistence.testing.tests.jpa.relationships.UniAndBiDirectionalMappingTestSuite;
 import org.eclipse.persistence.testing.tests.jpa.relationships.VirtualAttributeTestSuite;
-import org.eclipse.persistence.testing.tests.jpa.remote.RemoteEntityManagerTestSuite;
 import org.eclipse.persistence.testing.tests.jpa.validation.QueryParameterValidationTestSuite;
 import org.eclipse.persistence.testing.tests.jpa.validation.ValidationTestSuite;
 import org.eclipse.persistence.testing.tests.jpa.xml.EntityMappingsJUnitTestSuite;
@@ -301,7 +300,8 @@ public class FullRegressionTestSuite extends TestSuite {
         fullSuite.addTest(PartitionedXMLTestSuite.suite());
         fullSuite.addTest(PLSQLTestSuite.suite());
         fullSuite.addTest(XMLPLSQLTestSuite.suite());
-        fullSuite.addTest(RemoteEntityManagerTestSuite.suite());
+        // Has security manager issues on some JVMs.
+        //fullSuite.addTest(RemoteEntityManagerTestSuite.suite());
 
         // Composite tests.
         fullSuite.addTest(org.eclipse.persistence.testing.tests.jpa.composite.advanced.EntityManagerJUnitTestSuite.suite());
