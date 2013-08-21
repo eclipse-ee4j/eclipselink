f9e2b2bff3188418f27b54434d6e7cadae1c1d36
Chris Delahunt <christopher.delahunt@oracle.com>
2013-08-21 15:29:58 -0400

    Simple test added to allow clearing the schema on MySQL

---
+++ foundation/eclipselink.core.test/src/org/eclipse/persistence/testing/tests/ClearDatabaseSchemaTest.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 1998, 2013 Oracle and/or its affiliates. All rights reserved.
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
+ * which accompanies this distribution. 
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     21/08/2013-2.6 Chris Delahunt 
+ ******************************************************************************/  
+package org.eclipse.persistence.testing.tests;
+
+import org.eclipse.persistence.internal.sessions.ArrayRecord;
+import org.eclipse.persistence.testing.framework.*;
+
+public class ClearDatabaseSchemaTest extends TestCase {
+    
+    public ClearDatabaseSchemaTest() {
+        setDescription("Clears the database for MYSQL.");
+    }
+    
+    public void test() {
+        if (getSession().getDatasourcePlatform().isMySQL()) {
+            //for now, only MySQL is targeted
+            ArrayRecord o = (ArrayRecord)getSession().executeSQL("select DATABASE()").get(0);
+            getSession().executeNonSelectingSQL("drop database "+o.get("DATABASE()"));
+            getSession().executeNonSelectingSQL("create database "+o.get("DATABASE()"));
+        }
+    }
+}
