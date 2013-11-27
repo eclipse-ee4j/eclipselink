7f5ceefe6c886156d6f413c50b08c89ad88c8a46
David Minsky <david.minsky@oracle.com>
2013-11-27 14:10:17 -0500

    Fix for bug 422610 - Null aggregate with lock field using TimestampLockingPolicy causes NPE on change calculation - Issue is resolved with a straightforward null check in ObjectBuilder getBaseValueForField(DatabaseField databaseField, Object domainObject), which now returns null for a null aggregate - New testing: WriteNullAggregateWithTimestampLockingTest added to OptimisticLockingTestModel - Testing changes: TimestampInAggregateObject (aggregate) from org/eclipse/persistence/testing/models/optimisticlocking modified to allow null aggregate values. - Reviewed: Guy Pelletier

---
+++ foundation/eclipselink.core.test/src/org/eclipse/persistence/testing/tests/optimisticlocking/WriteNullAggregateWithTimestampLockingTest.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 1998, 2013 Oracle and/or its affiliates. All rights reserved.
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
+ * which accompanies this distribution. 
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     dminsky - initial API and implementation
+ ******************************************************************************/  
+package org.eclipse.persistence.testing.tests.optimisticlocking;
+
+import org.eclipse.persistence.sessions.UnitOfWork;
+import org.eclipse.persistence.testing.models.optimisticlocking.*;
+import org.eclipse.persistence.testing.framework.TransactionalTestCase;
+
+public class WriteNullAggregateWithTimestampLockingTest extends TransactionalTestCase {
+    
+    public WriteNullAggregateWithTimestampLockingTest() {
+        super();
+        setDescription("Test writing an object with a null aggregate and a TimestampLockingPolicy");
+    }
+    
+    @Override
+    public void test() {
+        UnitOfWork uow = getSession().acquireUnitOfWork();
+        TimestampInAggregateObject original = new TimestampInAggregateObject();
+        original.version = null; // set to null
+        TimestampInAggregateObject clone = (TimestampInAggregateObject)uow.registerObject(original);
+        clone.value = "Timestamp in null aggregate";
+        uow.commit();
+    }
+
+}
