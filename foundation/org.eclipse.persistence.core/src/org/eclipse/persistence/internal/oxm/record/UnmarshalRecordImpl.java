9ad6abde31d9e01e5a1c8780c046fafd93fcbc0c
desmith <denise.mahar@oracle.com>
2014-03-17 11:00:10 -0400

    Fix for SDO bug 430104 Reviewed by Blaise Doughan

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/oxm/record/UnmarshalRecordImpl.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/oxm/record/UnmarshalRecordImpl.java
@@ -1034,20 +1034,33 @@ public class UnmarshalRecordImpl extends CoreAbstractRecord implements Unmarshal
                 }
             } else {
                 XPathNode textNode = xPathNode.getTextNode();
-                if (null != textNode && textNode.isWhitespaceAware() && getStringBuffer().length() == 0) {
+                
+                if (null != textNode && getStringBuffer().length() == 0) {
                     NodeValue textNodeUnmarshalNodeValue = textNode.getUnmarshalNodeValue();
-                    if (textNodeUnmarshalNodeValue.isMappingNodeValue()) {
-                        Mapping mapping = ((MappingNodeValue)textNodeUnmarshalNodeValue).getMapping();
-                        if(mapping.isAbstractDirectMapping() && isXsiNil) {
-                            Object nullValue = ((DirectMapping)mapping).getNullValue();
-                            if(!(Constants.EMPTY_STRING.equals(nullValue))) {
-                                setAttributeValue(null, mapping);
-                                this.removeNullCapableValue((NullCapableValue)textNodeUnmarshalNodeValue);
+                    if(textNode.isWhitespaceAware()){
+	                    if (textNodeUnmarshalNodeValue.isMappingNodeValue()) {
+	                        Mapping mapping = ((MappingNodeValue)textNodeUnmarshalNodeValue).getMapping();
+	                        if(mapping.isAbstractDirectMapping() && isXsiNil) {
+	                            Object nullValue = ((DirectMapping)mapping).getNullValue();
+	                            if(!(Constants.EMPTY_STRING.equals(nullValue))) {
+	                                setAttributeValue(null, mapping);
+	                                this.removeNullCapableValue((NullCapableValue)textNodeUnmarshalNodeValue);
+	                            }
+	                        } else {
+	                            textNodeUnmarshalNodeValue.endElement(xPathFragment, this);
+	                        }
+	                        isXsiNil = false;
+	                    }
+                    }else{
+                    	
+                        //This means empty tag
+                       if(textNodeUnmarshalNodeValue.isMappingNodeValue()) {
+                            Mapping mapping = ((MappingNodeValue)textNodeUnmarshalNodeValue).getMapping();
+                            if(mapping.isAbstractDirectMapping() && !isXsiNil && ((DirectMapping)mapping).getNullPolicy().isNullRepresentedByXsiNil()){
+                                removeNullCapableValue((NullCapableValue)textNodeUnmarshalNodeValue);                		
                             }
-                        } else {
-                            textNodeUnmarshalNodeValue.endElement(xPathFragment, this);
                         }
-                        isXsiNil = false;
+
                     }
                 }
             }
