b3ddc9ed1bfdf920b4571006929fe1b7cb2fd902
Martin Grebac <martin.grebac@oracle.com>
2014-04-30 18:44:10 +0200

    Fix for 433684 - removing unnecessary cast
    Signed-off-by: Martin Grebac <martin.grebac@oracle.com>

--- sdo/org.eclipse.persistence.sdo/src/org/eclipse/persistence/sdo/SDOResolvable.java
+++ sdo/org.eclipse.persistence.sdo/src/org/eclipse/persistence/sdo/SDOResolvable.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2012 Oracle and/or its affiliates. All rights reserved.
- * This program and the accompanying materials are made available under the 
- * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
- * which accompanies this distribution. 
+ * Copyright (c) 1998, 2014 Oracle and/or its affiliates. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
  * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
  * and the Eclipse Distribution License is available at 
  * http://www.eclipse.org/org/documents/edl-v10.php.
@@ -225,7 +225,7 @@ public class SDOResolvable implements ExternalizableDelegator.Resolvable {
             objectOutput.writeUTF(aPath);
 
             // write root (via indirect recursion to SDOResolvable)
-            ((ObjectOutputStream)objectOutput).writeObject(theSDODataObject.getRootObject());
+            objectOutput.writeObject(theSDODataObject.getRootObject());
         }
     }
 
