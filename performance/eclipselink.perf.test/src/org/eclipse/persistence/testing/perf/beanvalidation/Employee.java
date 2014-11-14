6e859832a2a7943dbdd5a0bdeb1f29cf64c93590
Marcel Valovy <marcel.valovy@oracle.com>
2014-11-14 11:21:12 +1000

    Bug #447339 - Skip Bean Validation on objects that are not constrained.
    Signed-off-by: Marcel Valovy <marcel.valovy@oracle.com>
    Reviewed-by: Miroslav Kos, Martin Vojtek

---
+++ performance/eclipselink.perf.test/src/org/eclipse/persistence/testing/perf/beanvalidation/Employee.java
@@ -0,0 +1,110 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Oracle and/or its affiliates. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     Marcel Valovy - 2.6.0 - initial implementation
+ ******************************************************************************/
+package org.eclipse.persistence.testing.perf.beanvalidation;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+/**
+ * Employee that will undergo bean validation callbacks from both JPA and MOXy but will not trigger bean validation.
+ */
+@Entity(name = "Employee")
+@XmlRootElement
+public class Employee implements Serializable {
+
+    @Id
+    @XmlAttribute
+    private Integer id;
+
+    @XmlAttribute
+    private int age;
+
+    @XmlElement
+    private String personalName;
+
+    @XmlElement
+    private String phoneNumber;
+
+    public Employee(){
+    }
+
+    public Employee withId(int id){
+        this.id = id;
+        return this;
+    }
+
+    public Employee withAge(int age){
+        this.age = age;
+        return this;
+    }
+
+    public Employee withPersonalName(String personalName){
+        this.personalName = personalName;
+        return this;
+    }
+
+    public Employee withPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Employee employee = (Employee) o;
+
+        if (age != employee.age) {
+            return false;
+        }
+        if (id != null ? !id.equals(employee.id) : employee.id != null) {
+            return false;
+        }
+        if (personalName != null ? !personalName.equals(employee.personalName) : employee.personalName != null) {
+            return false;
+        }
+        if (phoneNumber != null ? !phoneNumber.equals(employee.phoneNumber) : employee.phoneNumber != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = age;
+        result = 31 * result + (id == null ? 0 : id);
+        result = 31 * result + (personalName != null ? personalName.hashCode() : 0);
+        result = 31 * result + (phoneNumber != null ? phoneNumber.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", age=" + age +
+                ", personalName='" + personalName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
