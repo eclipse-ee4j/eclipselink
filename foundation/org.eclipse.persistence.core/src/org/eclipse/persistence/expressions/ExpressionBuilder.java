1b4cde0faaa84436a2a058ce2329ff45c02dc1f8
Petros Splinakis <petros.splinakis@oracle.com>
2014-12-22 16:22:11 +0100

    Bug #457267 - Fixing NullPointerException in ExpressionBuilder.setSession
    Signed-off-by: Petros Splinakis <petros.splinakis@oracle.com>
    Reviewed-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/expressions/ExpressionBuilder.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/expressions/ExpressionBuilder.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2014 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2015 Oracle and/or its affiliates. All rights reserved.
  * This program and the accompanying materials are made available under the 
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
  * which accompanies this distribution. 
@@ -419,7 +419,11 @@ public class ExpressionBuilder extends ObjectExpression {
      * Stored session shall always be root session.
      */
     public void setSession(AbstractSession session) {
-        this.session = session.getRootSession(null);
+        if (session != null) {
+            this.session = session.getRootSession(null);
+        } else {
+            this.session = null;
+        }
     }
 
     /**
