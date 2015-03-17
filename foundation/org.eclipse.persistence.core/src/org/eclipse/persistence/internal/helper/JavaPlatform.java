3eb2e1ae7b56057c34b5087c48107bf8de84ad75
Petros Splinakis <petros.splinakis@oracle.com>
2015-03-17 16:32:01 +0100

    2.6 port: Bug #462377 - JavaPlatform fails to identify JavaSEPlatform.v1_7
    Signed-off-by: Petros Splinakis <petros.splinakis@oracle.com>
    Reviewed-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/helper/JavaPlatform.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/helper/JavaPlatform.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2015 Oracle and/or its affiliates. All rights reserved.
  * This program and the accompanying materials are made available under the 
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
  * which accompanies this distribution. 
@@ -39,7 +39,7 @@ public class JavaPlatform {
      */
     protected static JDKPlatform getPlatform() {
         if (platform == null) {
-            if (JavaSEPlatform.CURRENT.equals(JavaSEPlatform.v1_6)) {
+            if (JavaSEPlatform.CURRENT.atLeast(JavaSEPlatform.v1_6)) {
                 try {
                     Class platformClass = null;
                     // use class.forName() to avoid loading the JDK 1.6 class unless it is needed.
