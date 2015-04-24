986271eaf89a1cf8af2cce1890a5d8fc2dbb0703
Rick Curtis <curtisr7@gmail.com>
2015-04-24 15:00:52 -0500

    Fix for bug 465452. ServerPlatformUtils.createServerPlatform should not throw a NPE.
    Signed-off-by: Rick Curtis <curtisr7@gmail.com>

--- jpa/eclipselink.jpa.test.jse/src/org/eclipse/persistence/jpa/test/server/TestTargetServerDetector.java
+++ jpa/eclipselink.jpa.test.jse/src/org/eclipse/persistence/jpa/test/server/TestTargetServerDetector.java
@@ -10,11 +10,14 @@
  * Contributors:
  *     01/05/2015 Rick Curtis
  *       - 455683: Automatically detect target server
+ *     04/24/2015 Rick Curtis
+ *       - 465452: Test calling ServerPlatformUtils.createServerPlatform with a null platform class name.
  ******************************************************************************/
 package org.eclipse.persistence.jpa.test.server;
 
 import javax.persistence.EntityManagerFactory;
 
+import org.eclipse.persistence.exceptions.ServerPlatformException;
 import org.eclipse.persistence.internal.jpa.EntityManagerFactoryImpl;
 import org.eclipse.persistence.jpa.test.basic.model.Employee;
 import org.eclipse.persistence.jpa.test.framework.DDLGen;
@@ -23,6 +26,7 @@ import org.eclipse.persistence.jpa.test.framework.EmfRunner;
 import org.eclipse.persistence.jpa.test.framework.Property;
 import org.eclipse.persistence.platform.server.NoServerPlatform;
 import org.eclipse.persistence.platform.server.ServerPlatform;
+import org.eclipse.persistence.platform.server.ServerPlatformUtils;
 import org.eclipse.persistence.platform.server.was.WebSphere_Liberty_Platform;
 import org.junit.Assert;
 import org.junit.Test;
@@ -41,8 +45,19 @@ public class TestTargetServerDetector {
     public void test() {
         // Test that if a target-server property is set we honor that setting
         Assert.assertTrue(getPlatform(libertyEmf) instanceof WebSphere_Liberty_Platform);
-        // Test that if no target-server property is set we get the default server
+        // Test that if no target-server property is set we get the default
+        // server
         Assert.assertTrue(getPlatform(defaultEmf) instanceof NoServerPlatform);
+    }
+
+    @Test
+    public void testCreateNullServerPlatform() {
+        try {
+            ServerPlatformUtils.createServerPlatform(((EntityManagerFactoryImpl) defaultEmf).getServerSession(), null, TestTargetServerDetector.class.getClassLoader());
+            Assert.fail("Shouldn't have been able to create a null ServerPlatform");
+        } catch (ServerPlatformException spe) {
+            // expected
+        }
     }
 
     private ServerPlatform getPlatform(EntityManagerFactory emf) {
