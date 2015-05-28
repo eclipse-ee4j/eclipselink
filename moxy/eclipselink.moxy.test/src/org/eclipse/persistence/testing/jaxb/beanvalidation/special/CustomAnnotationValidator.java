b957e9c5313c908d1cd8e657e14d3ea707a91a4e
Marcel Valovy <marcel.valovy@oracle.com>
2015-05-28 10:56:08 +0200

    Fix of license header. P.S. Last two commits (fa1eb4, de784f) were reviewed by M. Grebac, M. Vojtek
    Signed-off-by: Marcel Valovy <marcel.valovy@oracle.com>

--- moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/beanvalidation/special/CustomAnnotationValidator.java
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/beanvalidation/special/CustomAnnotationValidator.java
@@ -1,22 +1,25 @@
+/**
+ * ****************************************************************************
+ * Copyright (c) 2014, 2015  Oracle and/or its affiliates. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ * <p/>
+ * Contributors:
+ * Marcel Valovy - initial API and implementation
+ * ****************************************************************************
+ */
 package org.eclipse.persistence.testing.jaxb.beanvalidation.special;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 /**
-* ****************************************************************************
-* Copyright (c) 2014, 2015  Oracle and/or its affiliates. All rights reserved.
-* This program and the accompanying materials are made available under the
-* terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
-* which accompanies this distribution.
-* The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
-* and the Eclipse Distribution License is available at
-* http://www.eclipse.org/org/documents/edl-v10.php.
-* <p/>
-* Contributors:
-* Marcel Valovy - initial API and implementation
-* ****************************************************************************
-*/
+ * @author Marcel Valovy
+ */
 public class CustomAnnotationValidator implements ConstraintValidator<CustomAnnotation, Integer> {
 
     @Override
