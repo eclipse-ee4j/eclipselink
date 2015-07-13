fb23904a178b42a8981bcc0fd07fd866a92af672
Will Dazey <dazeydev.3@gmail.com>
2015-07-13 10:45:13 -0500

    Bug #462603: Changed Object types to fix compile failures
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/security/PrivilegedGetConstructorFor.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/security/PrivilegedGetConstructorFor.java
@@ -1,32 +1,34 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2013 Oracle and/or its affiliates. All rights reserved.
- * This program and the accompanying materials are made available under the 
- * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
- * which accompanies this distribution. 
+ * Copyright (c) 1998, 2015 Oracle and/or its affiliates. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
  * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at 
+ * and the Eclipse Distribution License is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *
  * Contributors:
  *     Oracle - initial API and implementation from Oracle TopLink
- ******************************************************************************/  
+ ******************************************************************************/
 package org.eclipse.persistence.internal.security;
 
+import java.lang.reflect.Constructor;
 import java.security.PrivilegedExceptionAction;
 
-public class PrivilegedGetConstructorFor implements PrivilegedExceptionAction {
+public class PrivilegedGetConstructorFor implements PrivilegedExceptionAction<Constructor> {
 
-    private Class javaClass;
-    private Class[] args;
-    private boolean shouldSetAccessible;
-    
+    private final Class javaClass;
+    private final Class[] args;
+    private final boolean shouldSetAccessible;
+
     public PrivilegedGetConstructorFor(Class javaClass, Class[] args, boolean shouldSetAccessible) {
         this.javaClass = javaClass;
         this.args = args;
         this.shouldSetAccessible = shouldSetAccessible;
     }
 
-    public Object run() throws NoSuchMethodException {
+    @Override
+    public Constructor run() throws NoSuchMethodException {
         return PrivilegedAccessHelper.getConstructorFor(javaClass, args, shouldSetAccessible);
     }
 
--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/security/PrivilegedGetMethods.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/security/PrivilegedGetMethods.java
@@ -1,28 +1,30 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2013 Oracle and/or its affiliates. All rights reserved.
- * This program and the accompanying materials are made available under the 
- * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
- * which accompanies this distribution. 
+ * Copyright (c) 1998, 2015 Oracle and/or its affiliates. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
  * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at 
+ * and the Eclipse Distribution License is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *
  * Contributors:
  *     Oracle - initial API and implementation from Oracle TopLink
- ******************************************************************************/  
+ ******************************************************************************/
 package org.eclipse.persistence.internal.security;
 
+import java.lang.reflect.Method;
 import java.security.PrivilegedAction;
 
-public class PrivilegedGetMethods implements PrivilegedAction {
+public class PrivilegedGetMethods implements PrivilegedAction<Method[]> {
 
-    private Class clazz;
-    
+    private final Class clazz;
+
     public PrivilegedGetMethods(Class clazz) {
         this.clazz = clazz;
     }
 
-    public Object run() {
+    @Override
+    public Method[] run() {
         return PrivilegedAccessHelper.getMethods(clazz);
     }
 
