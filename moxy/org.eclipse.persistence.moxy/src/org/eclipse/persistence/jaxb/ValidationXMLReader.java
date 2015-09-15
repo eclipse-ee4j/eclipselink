2fca2e47a6835a1370a514dc1a2daa998cd1df83
Lukas Jungmann <lukas.jungmann@oracle.com>
2015-09-15 12:31:39 +0200

    Bug 444609: Javadoc compilation errors thrown when building using JDK8 (fix javadoc build on JDK8, enforce javadoc errors) (2/3)
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>
    Reviewed-by: TomasK, PetrosS
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>

--- moxy/org.eclipse.persistence.moxy/src/org/eclipse/persistence/jaxb/ValidationXMLReader.java
+++ moxy/org.eclipse.persistence.moxy/src/org/eclipse/persistence/jaxb/ValidationXMLReader.java
@@ -14,13 +14,6 @@
  */
 package org.eclipse.persistence.jaxb;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
-import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
 
 import java.io.File;
 import java.io.IOException;
@@ -34,11 +27,22 @@ import java.util.concurrent.ExecutorService;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.eclipse.persistence.exceptions.BeanValidationException;
+import org.eclipse.persistence.internal.security.PrivilegedAccessHelper;
+import org.eclipse.persistence.internal.security.PrivilegedGetContextClassLoader;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
 import static org.eclipse.persistence.jaxb.BeanValidationHelper.BEAN_VALIDATION_HELPER;
 
 /**
  * Parses validation.xml, scanning for constraints file reference. If found,
- * it will parse the constraints file and put all classes declared under <bean class="clazz"> into
+ * it will parse the constraints file and put all classes declared under &lt;bean class="clazz"&gt; into
  * {@link org.eclipse.persistence.jaxb.BeanValidationHelper#constraintsOnClasses} with value
  * {@link Boolean#TRUE}.
  *
@@ -177,6 +181,7 @@ class ValidationXMLReader implements Callable<Void> {
             private boolean defaultPackageElement = false;
             private String defaultPackage = "";
 
+            @Override
             public void startElement(String uri, String localName, String qName,
                                      Attributes attributes) throws SAXException {
 
@@ -198,8 +203,8 @@ class ValidationXMLReader implements Callable<Void> {
                 }
             }
 
+            @Override
             public void characters(char ch[], int start, int length) throws SAXException {
-
                 if (defaultPackageElement) {
                     defaultPackage = new String(ch, start, length);
                     defaultPackageElement = false;
@@ -242,6 +247,7 @@ class ValidationXMLReader implements Callable<Void> {
 
         private boolean constraintsFileElement = false;
 
+        @Override
         public void startElement(String uri, String localName, String qName,
                                  Attributes attributes) throws SAXException {
 
@@ -250,6 +256,7 @@ class ValidationXMLReader implements Callable<Void> {
             }
         }
 
+        @Override
         public void characters(char ch[], int start, int length) throws SAXException {
 
             if (constraintsFileElement) {
