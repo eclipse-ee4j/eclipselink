42fbdae4796da8c5b87ad5e2a0ed44b37d465270
Will Dazey <dazeydev.3@gmail.com>
2015-11-09 10:10:06 -0600

    Bug# 481734: Update ServerPlatformExceptionResource message
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>
    Reviewed-by: Joe Grassel <fyrewyld@gmail.com>

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/exceptions/i18n/ServerPlatformExceptionResource.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/exceptions/i18n/ServerPlatformExceptionResource.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2014 Oracle and/or its affiliates. All rights reserved.
- * This program and the accompanying materials are made available under the 
- * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
- * which accompanies this distribution. 
+ * Copyright (c) 1998, 2015 Oracle and/or its affiliates. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
  * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at 
+ * and the Eclipse Distribution License is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *
  * Contributors:
@@ -18,7 +18,7 @@ public class ServerPlatformExceptionResource extends ListResourceBundle {
 
     static final Object[][] contents = {
         { "63001", "Server platform class {0} not found." },
-        { "63002", "Server platform class is invalid: " }
+        { "63002", "Server platform class is not valid: {0}" }
     };
 
     /**
