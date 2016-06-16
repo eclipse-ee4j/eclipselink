b9bcae8c8edfb764b7d9071a9ec51600d5653066
Petros Splinakis <petros.splinakis@oracle.com>
2016-06-16 15:56:17 +0200

    Bug #495587 - EntityListener with generic super class causes ValidationException with Java 8
    Signed-off-by: Petros Splinakis <petros.splinakis@oracle.com>
    Reviewed-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/models/jpa22/entitylistener/AbstractEntityListener.java
+++ jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/models/jpa22/entitylistener/AbstractEntityListener.java
@@ -1,41 +1,46 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2016 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016 Oracle and/or its affiliates. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
  * which accompanies this distribution.
  * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
  * and the Eclipse Distribution License is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
- *
- * Contributors:
- *     tware - initial contribution for Bug 366748 - JPA 2.1 Injectable Entity Listeners
  ******************************************************************************/
+
 package org.eclipse.persistence.testing.models.jpa22.entitylistener;
+
+import org.eclipse.persistence.testing.models.jpa22.sessionbean.InjectedBean;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Inject;
 import javax.persistence.PostPersist;
 import javax.persistence.PrePersist;
 
-public class EntityListener extends AbstractEntityListener<EntityListenerHolder> {
-    @Override
+/* Trigger Bug #495587 */
+public class AbstractEntityListener<E extends EntityListenerHolderInterface> {
+    public static boolean INJECTED_RETURN_VALUE = false;
+    public static int POST_CONSTRUCT_CALLS = 0;
+    public static int PRE_DESTROY_CALLS = 0;
+
+    @Inject
+    protected InjectedBean injected;
+
     @PrePersist
-    public void prePersist(EntityListenerHolder object) {
+    public void prePersist(E object) {
         INJECTED_RETURN_VALUE = injected.isCalled();
     }
 
-    @Override
     @PostPersist
-    public void postPersist(EntityListenerHolder object) {
+    public void postPersist(E object) {
     }
 
-    @Override
     @PreDestroy
     public void preDestroy() {
         PRE_DESTROY_CALLS++;
     }
 
-    @Override
     @PostConstruct
     public void postConstruct() {
         POST_CONSTRUCT_CALLS++;
