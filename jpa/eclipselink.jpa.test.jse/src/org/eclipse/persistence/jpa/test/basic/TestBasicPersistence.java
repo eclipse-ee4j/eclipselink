58f04b414c049a7c81be578efd069b9f149cdf6a
Joe Grassel <fyrewyld@gmail.com>
2017-02-16 12:34:35 -0600

    Bug 512255 - Eclipselink JPA/Auditing capablity in EE Environment fails with JNDI name parameter type
    Signed-off-by: Joe Grassel <fyrewyld@gmail.com>
    Reviewed-by: Will Dazey <dazeydev.3@gmail.com>

--- jpa/eclipselink.jpa.test.jse/src/org/eclipse/persistence/jpa/test/basic/TestBasicPersistence.java
+++ jpa/eclipselink.jpa.test.jse/src/org/eclipse/persistence/jpa/test/basic/TestBasicPersistence.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2015 IBM Corporation. All rights reserved.
+ * Copyright (c) 2014, 2017 IBM Corporation. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
  * which accompanies this distribution.
@@ -12,14 +12,32 @@
  *       - 450010 : Add java se test bucket
  *     12/19/2014 - Dalia Abo Sheasha
  *       - 454917 : Added a test to use the IDENTITY strategy to generate values
+ *     02/16/2017 - Jody Grassel
+ *       - 512255 : Eclipselink JPA/Auditing capablity in EE Environment fails with JNDI name parameter type
  ******************************************************************************/
 package org.eclipse.persistence.jpa.test.basic;
 
+import java.io.Serializable;
+import java.lang.management.ManagementFactory;
+import java.rmi.Remote;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
 
+import javax.management.remote.JMXConnectorServer;
+import javax.management.remote.JMXConnectorServerFactory;
+import javax.management.remote.JMXServiceURL;
+import javax.naming.InitialContext;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
+import org.eclipse.persistence.config.PersistenceUnitProperties;
 import org.eclipse.persistence.jpa.test.basic.model.Dog;
 import org.eclipse.persistence.jpa.test.basic.model.Employee;
 import org.eclipse.persistence.jpa.test.basic.model.Person;
@@ -29,19 +47,65 @@ import org.eclipse.persistence.jpa.test.framework.Emf;
 import org.eclipse.persistence.jpa.test.framework.EmfRunner;
 import org.eclipse.persistence.jpa.test.framework.Property;
 import org.eclipse.persistence.jpa.test.framework.SQLListener;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(EmfRunner.class)
 public class TestBasicPersistence {
-    @Emf(createTables = DDLGen.DROP_CREATE, classes = { Dog.class, XmlFish.class, Person.class, Employee.class }, properties = {
-        @Property(name = "eclipselink.cache.shared.default", value = "false") }, mappingFiles = { "META-INF/fish-orm.xml" })
+    @Emf(createTables = DDLGen.DROP_CREATE, classes = { Dog.class, XmlFish.class, Person.class, Employee.class }, 
+            properties = {@Property(name = "eclipselink.cache.shared.default", value = "false")}, 
+            mappingFiles = { "META-INF/fish-orm.xml" })
     private EntityManagerFactory emf;
 
     @SQLListener
     List<String> _sql;
+    
+    private static final int rmiPort;
+    private static final String dsName = "mockDataSource";
+    private static final BogusDataSource mockDataSource = new BogusDataSource("tmpDataSourceImp getConnection called");
+    private static Registry rmiRegistry = null;
+    private static JMXConnectorServer connector = null;
+    
+    static {
+        int rmiPortVal = 1099;
+        
+        String rmiPortProp = System.getProperty("rmi.port");
+        if (!(rmiPortProp == null || rmiPortProp.isEmpty())) {
+            try {
+                rmiPortVal = new Integer(rmiPortProp);
+            } catch (NumberFormatException nfe) {
+                // Use default value.
+            }
+        }
+            
+        rmiPort = rmiPortVal;
+    }
+    
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        rmiRegistry = LocateRegistry.createRegistry(rmiPort);
+        
+        // Create and Bind Mock Data Source
+        rmiRegistry.bind(dsName, mockDataSource);       
 
+        connector = JMXConnectorServerFactory.newJMXConnectorServer(new JMXServiceURL("service:jmx:rmi://localhost:" + rmiPort),
+                new HashMap<String, Object>(), ManagementFactory.getPlatformMBeanServer());
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        if (rmiRegistry != null) {
+            rmiRegistry.unbind(dsName);
+        } 
+        
+        if (connector != null) {
+            connector.stop();
+        }
+    }
+    
     @Test
     public void activeTransaction() {
         Assert.assertNotNull(emf);
@@ -104,5 +168,69 @@ public class TestBasicPersistence {
             }
             em.close();
         }
+    }
+    
+    @Test
+    public void testNonJTADataSourceOverride() throws Exception {
+        if (emf == null)
+            return;
+        
+        InitialContext ic = new InitialContext();
+        Assert.assertNotNull(ic.lookup(dsName));
+        
+        EntityManager em = null;
+        boolean pass = false;
+        Map properties = new HashMap();  
+        properties.put(PersistenceUnitProperties.NON_JTA_DATASOURCE, dsName);
+        properties.put("eclipselink.jdbc.exclusive-connection.mode", "Always");
+        
+        try {
+            em = emf.createEntityManager(properties);
+            em.clear();
+            em.find(Dog.class, 1);
+        } catch (RuntimeException expected) {
+            pass = expected.getMessage().indexOf("tmpDataSourceImp getConnection called") != -1;
+            if (!pass) {
+                throw expected;
+            }
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+        Assert.assertTrue("Non JTA datasource was not set or accessed as expected through map of properties", pass);
+    }
+    
+    /*
+     * Taken from org.eclipse.persistence.testing.tests.jpa.validation.ValidationTestSuite
+     */
+    public static class BogusDataSource implements DataSource, Remote, Serializable {
+        private String text = "foo";
+        
+        public BogusDataSource(String text){
+            super();
+            this.text = text;
+        }
+        
+        public Connection getConnection() throws SQLException {
+            RuntimeException exception = new RuntimeException(text);
+            throw exception;
+        }
+        
+        public Connection getConnection(String username, String password) throws SQLException {
+            return getConnection();
+        }
+        
+        //rest are ignored
+        public java.io.PrintWriter getLogWriter() throws SQLException {
+            return null;
+        }
+        
+        public void setLogWriter(java.io.PrintWriter out) throws SQLException{}
+        public void setLoginTimeout(int seconds) throws SQLException{}
+        public int getLoginTimeout() throws SQLException { return 1; }
+        public <T> T unwrap(Class<T> iface) throws SQLException { return null; }
+        public boolean isWrapperFor(Class<?> iface) throws SQLException { return false; }
+        public Logger getParentLogger() { return null; }
     }
 }
