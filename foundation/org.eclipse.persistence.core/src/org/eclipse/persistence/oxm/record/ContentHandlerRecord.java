792fcceb46856759c9c79f3056a4f33eecff4c33
Vikram Bhatia <vikram.jeet.bhatia@oracle.com>
2017-03-23 20:20:35 +0530

    Bug 514044 - Duplicate xmls entry during marshal with ContentHandler.
    - Fixed marshal issue with ContentHandler com.sun.xml.bind.DataWriter
    - Added a junit test case.
    
    Signed-off-by: Vikram Bhatia <vikram.jeet.bhatia@oracle.com>

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/oxm/record/ContentHandlerRecord.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/oxm/record/ContentHandlerRecord.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2015 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2017 Oracle and/or its affiliates. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
  * which accompanies this distribution.
@@ -251,8 +251,9 @@ public class ContentHandlerRecord extends MarshalRecord {
                 localName = "";
             }
             this.startPrefixMapping(localName, value);
+        } else {
+            attributes.addAttribute(namespaceURI, localName, qName, Constants.CDATA, value);
         }
-        attributes.addAttribute(namespaceURI, localName, qName, Constants.CDATA, value);
     }
 
     /**
--- moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/listofobjects/ns/MapNamespaceTestCases.java
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/listofobjects/ns/MapNamespaceTestCases.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2015 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2017 Oracle and/or its affiliates. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
  * which accompanies this distribution.
@@ -12,7 +12,21 @@
  ******************************************************************************/
 package org.eclipse.persistence.testing.jaxb.listofobjects.ns;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import javax.xml.bind.Marshaller;
+
+import org.eclipse.persistence.jaxb.MarshallerProperties;
+import org.eclipse.persistence.oxm.XMLDescriptor;
+import org.eclipse.persistence.oxm.XMLRoot;
 import org.eclipse.persistence.testing.jaxb.JAXBTestCases;
+import org.w3c.dom.Document;
+
+import com.sun.xml.bind.marshaller.CharacterEscapeHandler;
+import com.sun.xml.bind.marshaller.DataWriter;
 
 public class MapNamespaceTestCases extends JAXBTestCases {
 
@@ -31,4 +45,60 @@ public class MapNamespaceTestCases extends JAXBTestCases {
         return mo;
     }
 
+    public void testObjectToXMLWithContentHandler() throws Exception {
+        Object objectToWrite = getWriteControlObject();
+        StringWriter writer = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(writer);
+        DataWriter dataWriter = new DataWriter(printWriter, "UTF-8", new DummyCharacterEscapeHandler());
+
+        XMLDescriptor desc = null;
+        if (objectToWrite instanceof XMLRoot) {
+            desc = (XMLDescriptor)xmlContext.getSession(0).getProject().getDescriptor(((XMLRoot)objectToWrite).getObject().getClass());
+        } else {
+            desc = (XMLDescriptor)xmlContext.getSession(0).getProject().getDescriptor(objectToWrite.getClass());
+        }
+
+        int sizeBefore = getNamespaceResolverSize(desc);
+        jaxbMarshaller.setProperty(MarshallerProperties.MEDIA_TYPE, "application/xml");
+        jaxbMarshaller.setProperty("com.sun.xml.bind.xmlDeclaration", Boolean.FALSE);
+
+        try {
+            jaxbMarshaller.marshal(objectToWrite, dataWriter);
+        } catch(Exception e) {
+            assertMarshalException(e);
+            return;
+        }
+        if(expectsMarshalException){
+            fail("An exception should have occurred but didn't.");
+            return;
+        }
+
+        int sizeAfter = getNamespaceResolverSize(desc);
+
+        assertEquals(sizeBefore, sizeAfter);
+
+        Document testDocument = getTestDocument(writer.toString());
+
+        writer.close();
+
+        objectToXMLDocumentTest(testDocument);
+    }
+    
+    private class DummyCharacterEscapeHandler implements CharacterEscapeHandler {
+        
+        public void escape(char[] buf, int start, int len, boolean isAttValue, Writer out) throws IOException {
+            if (len == 1 && buf[start] == '\n') {
+                return;
+            }
+
+            StringWriter buffer = new StringWriter();
+
+            for (int i = start; i < start + len; i++) {
+                buffer.write(buf[i]);
+            }
+            
+            out.write(buffer.toString());
+        }
+        
+    }
 }
