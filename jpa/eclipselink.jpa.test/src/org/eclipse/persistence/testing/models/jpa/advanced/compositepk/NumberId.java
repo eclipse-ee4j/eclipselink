e7a9c2555b802b367000df8b32ebc8c1e7f0f23d
Will Dazey <dazeydev.3@gmail.com>
2017-10-23 13:51:46 -0500

    Bug 520387: EclipseLink metadata processing sets incorrect owning descriptors
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>
    Reviewed-by: Lukas Jungmann <lukas.jungmann@oracle.com>

---
+++ jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/models/jpa/advanced/compositepk/NumberId.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved.
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
+ * which accompanies this distribution. 
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     09/11/2017-2.1 Will Dazey 
+ *       - 520387: multiple owning descriptors for an embeddable are not set
+ ******************************************************************************/ 
+package org.eclipse.persistence.testing.models.jpa.advanced.compositepk;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.GeneratedValue;
+import javax.persistence.SequenceGenerator;
+
+@Embeddable
+public class NumberId implements Serializable {
+
+    private static final long serialVersionUID = 4263409730381544959L;
+
+    @Column(name = "ID_NUMBER")
+    @SequenceGenerator(name = "ID_SEQ", sequenceName = "ID_SEQUENCE", allocationSize = 50, initialValue = 1000)
+    @GeneratedValue(generator = "ID_SEQ")
+    private Long value;
+
+    public NumberId() {
+        this(Long.valueOf(0L));
+    }
+
+    public NumberId(Long value) {
+        this.value = value;
+    }
+
+    public Long getValue() {
+        return value;
+    }
+
+    public final boolean equals(Object other) {
+        if (other == null) {
+            return false;
+        }
+        if (this == other) {
+            return true;
+        }
+        if (getClass() != other.getClass()) {
+            return false;
+        }
+        return Objects.equals(this.value, ((NumberId) other).value);
+    }
+
+    public final int hashCode() {
+        return Objects.hash(new Object[] { this.value });
+    }
+
+    public final String toString() {
+        return getClass().getSimpleName() + "[value=" + this.value + "]";
+    }
+}
