adea020d6f941c8afe12b50db36b89ef99d9c07e
Lukas Jungmann <lukas.jungmann@oracle.com>
2017-11-15 05:52:27 +0100

    [nobug] fix failing testMappedSuperclassWeaving test - server side
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/tests/jpa21/advanced/WeaverTestSuite.java
+++ jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/tests/jpa21/advanced/WeaverTestSuite.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2015, 2017  Oracle and/or its affiliates. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     Lukas - Initial implementation
+ ******************************************************************************/
 package org.eclipse.persistence.testing.tests.jpa21.advanced;
 
 import java.util.Arrays;
@@ -7,7 +19,7 @@ import javax.persistence.metamodel.ManagedType;
 
 import org.eclipse.persistence.internal.descriptors.PersistenceEntity;
 import org.eclipse.persistence.testing.framework.junit.JUnitTestCase;
-import org.eclipse.persistence.testing.models.jpa21.advanced.Athlete;
+import org.eclipse.persistence.testing.models.jpa21.advanced.WovenMS;
 import org.junit.Assert;
 
 import junit.framework.Test;
@@ -17,6 +29,7 @@ public class WeaverTestSuite extends JUnitTestCase {
 
     public WeaverTestSuite(String name) {
         super(name);
+        setPuName("pu-with-mappedsuperclass");
     }
 
     @Override
@@ -37,8 +50,8 @@ public class WeaverTestSuite extends JUnitTestCase {
     //bug #466271 - @MappedSuperclass with no implementations should be woven
     public void testMappedSuperclassWeaving() {
         EntityManagerFactory emf = getEntityManagerFactory();
-        ManagedType<Athlete> managedType = emf.getMetamodel().managedType(Athlete.class);
-        Class<Athlete> javaClass = emf.getMetamodel().managedType(Athlete.class).getJavaType();
+        ManagedType<WovenMS> managedType = emf.getMetamodel().managedType(WovenMS.class);
+        Class<WovenMS> javaClass = emf.getMetamodel().managedType(WovenMS.class).getJavaType();
         Assert.assertTrue(Arrays.asList(javaClass.getInterfaces()).contains(PersistenceEntity.class));
     }
 }
