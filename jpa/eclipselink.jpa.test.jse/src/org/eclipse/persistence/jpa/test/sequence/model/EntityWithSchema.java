cf513e54ede4928860c75fa48bcbeeefefecd35c
Lukas Jungmann <lukas.jungmann@oracle.com>
2017-12-15 13:39:15 +0100

    [nobug] move table qualifier test to the better place
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- jpa/eclipselink.jpa.test.jse/src/org/eclipse/persistence/jpa/test/sequence/model/EntityWithSchema.java
+++ jpa/eclipselink.jpa.test.jse/src/org/eclipse/persistence/jpa/test/sequence/model/EntityWithSchema.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2015 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2017 Oracle and/or its affiliates. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
  * which accompanies this distribution.
@@ -10,7 +10,7 @@
  * Contributors:
  *     dminsky - initial implementation
  ******************************************************************************/
-package org.eclipse.persistence.testing.models.jpa.advanced.entities;
+package org.eclipse.persistence.jpa.test.sequence.model;
 
 import java.io.Serializable;
 import javax.persistence.*;
