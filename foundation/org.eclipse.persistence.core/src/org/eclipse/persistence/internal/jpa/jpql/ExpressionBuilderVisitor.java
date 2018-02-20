62ed683d42cedf28df2740c3156da59e6e4c2e75
Will Dazey <dazeydev.3@gmail.com>
2018-02-20 15:36:15 -0600

    Bug 531062: Part 2: jse tests
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/jpa/jpql/ExpressionBuilderVisitor.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/jpa/jpql/ExpressionBuilderVisitor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2018 Oracle and/or its affiliates, IBM Corporation. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
  * which accompanies this distribution.
@@ -12,7 +12,7 @@
  *
  *     04/11/2017-2.6 Will Dazey
  *       - 512386: Concat expression return type Boolean -> String
- *     02/13/2018-2.7 Will Dazey
+ *     02/20/2018-2.7 Will Dazey
  *       - 531062: Incorrect expression type created for CollectionExpression
  ******************************************************************************/
 package org.eclipse.persistence.internal.jpa.jpql;
