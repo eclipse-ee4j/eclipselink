0f840bb75230ea97f2ac7ff75660c9380f7550af
Lukas Jungmann <lukas.jungmann@oracle.com>
2018-03-20 00:07:13 +0100

    Bug 496836: NullPointerException on ObjectChangeSet.mergeObjectChanges
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/models/jpa/advanced/embeddable/VisitorPopulator.java
+++ jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/models/jpa/advanced/embeddable/VisitorPopulator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2015  Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2018  Oracle and/or its affiliates. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
  * which accompanies this distribution.
@@ -10,6 +10,9 @@
  * Contributors:
  *     07/07/2014-2.6 Tomas Kraus
  *       - 439127: Create sample instances for EntityEmbeddableTest jUnit test.
+ *     03/19/2018-2.7.2 Lukas Jungmann
+ *       - 413120: Nested Embeddable Null pointer
+ *       - 496836: NullPointerException on ObjectChangeSet.mergeObjectChanges
  ******************************************************************************/
 package org.eclipse.persistence.testing.models.jpa.advanced.embeddable;
 
@@ -34,6 +37,23 @@ public class VisitorPopulator {
     public static Visitor visitorExample2() {
         return new Visitor("2", "Arthur D. Frechette",
                 new Country("GBR", "England"));
+    }
+
+    public static Visitor visitorExample3() {
+        return new Visitor("10", "Pepa Novak",
+                new Country("CZE", "Czech Republic", "Europe"));
+    }
+
+    public static Visitor visitorExample4() {
+        return new Visitor("11", "Arthur D. Frechette",
+                new Country("GBR", "England", "ANT"));
+    }
+
+    public static Visitor visitorExample5() {
+        Visitor v = new Visitor("12", "Arthur D. Frechette",
+                new Country("GBR", "England", "AFR"));
+        v.getCountry().getContinent().setDescription(new Description("Note about Africa"));
+        return v;
     }
 
 }
