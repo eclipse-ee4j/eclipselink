7ef695230a5ceb65b3c1285b9acd788584f8e770
Radek Felcman <radek.felcman@oracle.com>
2018-03-22 14:28:48 +0100

    Bug 531349 - XmlSchema Prefix is not honoured if root element is  nil
    MOXy marshaller ignores custom namespace prefix specified in package-info class. It generates default one "ns0" instead custom "PRE".
    
    Signed-off-by: Radek Felcman <radek.felcman@oracle.com>

--- moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/prefixmapper/PrefixMapperContextTestCases.java
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/prefixmapper/PrefixMapperContextTestCases.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2015 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2018 Oracle and/or its affiliates. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
  * which accompanies this distribution.
@@ -28,6 +28,14 @@ public class PrefixMapperContextTestCases extends TestCase {
     private static final String CONTROL_XML = "<newPrefix:employeeContext xmlns:ns0=\"extraUri\" xmlns:newPrefix=\"someuri\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:somePrefix=\"my.uri\">" +
               "<firstName>Jon</firstName><lastName>Doe</lastName><acmeNS:employeeId>123</acmeNS:employeeId>" +
               "</newPrefix:employeeContext>";
+
+    public PrefixMapperContextTestCases(String name) {
+        super(name);
+    }
+
+    public String getName() {
+        return "JAXB set/getProperty Tests: " + super.getName();
+    }
 
     public void testMarshalWithContextualNamespaces() throws Exception  {
         JAXBContext ctx = JAXBContextFactory.createContext(new Class[]{EmployeeContext.class}, null);
