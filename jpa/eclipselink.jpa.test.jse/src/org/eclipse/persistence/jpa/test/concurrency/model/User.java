299af56fa3621ac3fb9853093d8173701fa584c0
Will Dazey <dazeydev.3@gmail.com>
2018-04-09 11:26:17 -0500

    Bug 500753: clean up implementation
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>

--- jpa/eclipselink.jpa.test.jse/src/org/eclipse/persistence/jpa/test/concurrency/model/User.java
+++ jpa/eclipselink.jpa.test.jse/src/org/eclipse/persistence/jpa/test/concurrency/model/User.java
@@ -22,8 +22,10 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="CONCURR_USER")
 public class User {
 
     @Id private int id;
