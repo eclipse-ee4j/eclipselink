b603c635c98d933cd53adc12f68cdf9a87c244e2
Will Dazey <dazeydev.3@gmail.com>
2018-04-11 10:33:20 -0500

    Bug 533148: Add new 'sql-call-deferral' property
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>

--- jpa/eclipselink.jpa.test.jse/src/org/eclipse/persistence/jpa/test/sequence/model/EntityWithSchema.java
+++ jpa/eclipselink.jpa.test.jse/src/org/eclipse/persistence/jpa/test/sequence/model/EntityWithSchema.java
@@ -19,6 +19,8 @@ import javax.persistence.*;
 @Table(name="ENTITY_WITH_SCHEMA", schema=EntityWithSchema.CUSTOM_SCHEMA_NAME)
 public class EntityWithSchema implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     public static final String CUSTOM_SCHEMA_NAME = "CUSTOM_SCHEMA";
     public static final String CUSTOM_SEQUENCE_NAME = "CUSTOM_SEQUENCE";
 
