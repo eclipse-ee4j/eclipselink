1824e4ff503313a5434ad87983bc638c9ed2c88d
Radek Felcman <radek.felcman@oracle.com>
2018-04-26 12:23:27 +0200

    Bug 420746 - Unmarshal of JAXBElement field annotated with @XmlElementRef nil not unmarshalling xsi:nil="true" correctly.
    MOXy unmarshaller incorrectly unmarshal element declared with @XmlElementRef if attribute (namespace declaration) xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" is positioned outside element with xsi:nil="true" attribute.
    There are no other null policies applied, than xsi:nil="true" in the source document. See https://bugs.eclipse.org/bugs/show_bug.cgi?id=420746 .
    
    Signed-off-by: Radek Felcman <radek.felcman@oracle.com>

---
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/xmlelementref/nills2/Bar.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     Oracle - initial API and implementation from Oracle TopLink
+ ******************************************************************************/
+package org.eclipse.persistence.testing.jaxb.xmlelementref.nills2;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Bar {
+
+    @XmlAttribute
+    private String data;
+
+    public Bar() {
+    }
+
+    public Bar(final String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return this.data;
+    }
+
+    public void setData(final String data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Bar)) {
+            return false;
+        }
+        Bar a = (Bar) obj;
+        if (a.getData() == null & this.getData() == null) {
+            return true;
+        } else if (a.getData() != null && this.getData() != null && a.getData().equals(this.getData())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
