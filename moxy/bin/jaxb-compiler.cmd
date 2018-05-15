9812a25f3b16513cb077c7537ab3c2ac53840876
Radek Felcman <radek.felcman@oracle.com>
2018-05-15 16:16:14 +0200

    Update JSON-P to 1.1.2 version and separate dependencies to API and implementation
    Update buildsystem and compdeps (Eclipse features).
    
    Signed-off-by: Radek Felcman <radek.felcman@oracle.com>

--- moxy/bin/jaxb-compiler.cmd
+++ moxy/bin/jaxb-compiler.cmd
@@ -13,8 +13,8 @@ set JVM_ARGS=-Xmx256m
 set _FIXPATH=
 call :fixpath "%~dp0"
 set THIS=%_FIXPATH:~1%
-set CLASSPATH=%THIS%..\jlib\moxy\javax.json-api-1.1.2.jar
-set CLASSPATH=%CLASSPATH%;%THIS%..\jlib\moxy\javax.json-1.1.2.jar
+set CLASSPATH=%THIS%..\jlib\moxy\javax.json-api_1.1.2.jar
+set CLASSPATH=%CLASSPATH%;%THIS%..\jlib\moxy\javax.json_1.1.2.jar
 set CLASSPATH=%CLASSPATH%;%THIS%..\jlib\moxy\jaxb-core_2.2.11.v201407311112.jar
 set CLASSPATH=%CLASSPATH%;%THIS%..\jlib\moxy\jaxb-core_2.2.11.v201407311112.jar
 set CLASSPATH=%CLASSPATH%;%THIS%..\jlib\moxy\jaxb-xjc_2.2.11.v201407311112.jar
