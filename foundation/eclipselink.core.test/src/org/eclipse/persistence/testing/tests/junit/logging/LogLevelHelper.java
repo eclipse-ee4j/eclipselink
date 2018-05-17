e3bc43cf1a53ae95c0687b53f95800d8ddb8f5e1
Lukas Jungmann <lukas.jungmann@oracle.com>
2018-05-18 01:48:36 +0200

    prepare test-core for running against modules
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- foundation/eclipselink.core.test/src/org/eclipse/persistence/testing/tests/junit/logging/LogLevelHelper.java
+++ foundation/eclipselink.core.test/src/org/eclipse/persistence/testing/tests/junit/logging/LogLevelHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015  Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2018 Oracle and/or its affiliates. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
  * which accompanies this distribution.
@@ -10,10 +10,12 @@
  * Contributors:
  *      Tomas Kraus - Initial implementation
  ******************************************************************************/
-package org.eclipse.persistence.logging;
+package org.eclipse.persistence.testing.tests.junit.logging;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+
+import org.eclipse.persistence.logging.LogLevel;
 
 /**
  * jUnit tests helper that allows {@link org.eclipse.persistence.logging.LogLevel}
