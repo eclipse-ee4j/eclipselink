d617ac2ab257219330dc31792ef25fdc5498d706
Ravi Babu Tummuru <39968150+rtummuru@users.noreply.github.com>
2018-06-07 11:04:30 -0700

    Update Oracle9Platform.java

--- foundation/org.eclipse.persistence.oracle/src/org/eclipse/persistence/platform/database/oracle/Oracle9Platform.java
+++ foundation/org.eclipse.persistence.oracle/src/org/eclipse/persistence/platform/database/oracle/Oracle9Platform.java
@@ -211,12 +211,8 @@ public class Oracle9Platform extends Oracle8Platform {
             SQLXML sqlXml = resultSet.getSQLXML(columnNumber);
             String str = sqlXml.getString();
             sqlXml.free();
-            if (str != null ) { 
-                // Oracle 12c appends a \n character to the xml string
-                return str.endsWith("\n") ? str.substring(0, str.length() - 1) : str;
-            } else {
-                return str;
-            }
+            // Oracle 12c appends a \n character to the xml string
+            return (str != null && str.endsWith("\n")) ? str.substring(0, str.length() - 1) : str;
         } else if (type == OracleTypes.OPAQUE) {
             try {
                 Object result = resultSet.getObject(columnNumber);
