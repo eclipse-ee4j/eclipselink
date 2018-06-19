0962d36dbd86b93c7e23a1e4fb8b037756933f3f
Tomas Kraus <tomas.kraus@oracle.com>
2018-06-19 11:25:41 +0200

    Bug 535250 - Recursive annotations triggers a StackOverflowError
    Signed-off-by: Tomas Kraus <tomas.kraus@oracle.com>

---
+++ jpa/eclipselink.jpa.test/src/org/eclipse/samples/annotations/CycleC.java
@@ -0,0 +1,5 @@
+package org.eclipse.samples.annotations;
+
+@CycleA
+public @interface CycleC {
+}
---
+++ jpa/eclipselink.jpa.test/src/org/eclipse/samples/annotations/CycleSelf.java
@@ -0,0 +1,5 @@
+package org.eclipse.samples.annotations;
+
+@CycleSelf
+public @interface CycleSelf {
+}
