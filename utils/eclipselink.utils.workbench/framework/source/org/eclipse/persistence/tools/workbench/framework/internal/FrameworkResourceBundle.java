e870a61ab0aa59f34fe4b4aebc74c240a8bd98a1
Lukas Jungmann <lukas.jungmann@oracle.com>
2018-06-19 23:50:50 +0200

    final tweaks
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- utils/eclipselink.utils.workbench/framework/source/org/eclipse/persistence/tools/workbench/framework/internal/FrameworkResourceBundle.java
+++ utils/eclipselink.utils.workbench/framework/source/org/eclipse/persistence/tools/workbench/framework/internal/FrameworkResourceBundle.java
@@ -23,7 +23,7 @@ public final class FrameworkResourceBundle extends ListResourceBundle {
 
     static final Object[][] contents = {
 
-        {"COPYRIGHT", "Copyright \u00A9 1997, 2012 Oracle and/or its affiliates. All rights reserved. <p> This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html"},
+        {"COPYRIGHT", "Copyright \u00A9 1997, 2018 Oracle and/or its affiliates. All rights reserved. <p> This program and the accompanying materials are made available under the terms Eclipse Public License 2.0 which is available at http://www.eclipse.org/legal/epl-2.0.</p><p>SPDX-License-Identifier: EPL-2.0</p>"},
 
         // Helpful when having more than one Browse button in a panel
         {"BROWSE_BUTTON_1", "&Browse..."},
