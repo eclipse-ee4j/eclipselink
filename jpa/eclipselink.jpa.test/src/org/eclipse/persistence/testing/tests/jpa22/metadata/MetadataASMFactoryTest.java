89741691539f55db544c855072a8122501f5b909
Tomáš Kraus <tomas.kraus@oracle.com>
2018-06-21 14:10:14 +0200

    Bug 535250 - Recursive annotations triggers a StackOverflowError (#137)
    * Bug 535250 - Recursive annotations triggers a StackOverflowError
    
    Signed-off-by: Tomas Kraus <tomas.kraus@oracle.com>

--- jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/tests/jpa22/metadata/MetadataASMFactoryTest.java
+++ jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/tests/jpa22/metadata/MetadataASMFactoryTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2018 Oracle and/or its affiliates. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
  * which accompanies this distribution.
@@ -8,19 +8,25 @@
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *
  * Contributors:
- *              ljungmann - initial implementation
+ *     ljungmann - initial implementation
+ *     06/206/2018-2.7.2 Tomas Kraus
+ *       - 535250: Test meta-annotations with dependency cycle
  ******************************************************************************/
 package org.eclipse.persistence.testing.tests.jpa22.metadata;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
 import org.eclipse.persistence.internal.jpa.deployment.PersistenceUnitProcessor;
 import org.eclipse.persistence.internal.jpa.metadata.MetadataLogger;
 import org.eclipse.persistence.internal.jpa.metadata.accessors.objects.MetadataAnnotation;
 import org.eclipse.persistence.internal.jpa.metadata.accessors.objects.MetadataAsmFactory;
 import org.eclipse.persistence.internal.jpa.metadata.accessors.objects.MetadataClass;
 import org.eclipse.persistence.testing.framework.junit.JUnitTestCase;
+import org.eclipse.samples.annotations.CycleA;
+import org.eclipse.samples.annotations.CycleB;
+import org.eclipse.samples.annotations.CycleSelf;
 import org.junit.Assert;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
 
 public class MetadataASMFactoryTest extends JUnitTestCase {
     
@@ -35,6 +41,8 @@ public class MetadataASMFactoryTest extends JUnitTestCase {
         TestSuite suite = new TestSuite();
         suite.setName("MetadataASMFactoryTest");
         suite.addTest(new MetadataASMFactoryTest("testMetadataAnnotations"));
+        suite.addTest(new MetadataASMFactoryTest("testAnnotationsWithCycle"));
+        suite.addTest(new MetadataASMFactoryTest("testAnnotationsWithPrimitiveCycle"));
         return suite;
     }
 
@@ -48,6 +56,34 @@ public class MetadataASMFactoryTest extends JUnitTestCase {
 
         annotation = metadataClass.getAnnotation("javax.persistence.EntityListeners");
         Assert.assertNotNull(annotation);
+    }
+
+    /**
+     * Check meta-annotations graph with cycle A -> B -> C -> A
+     */
+    public void testAnnotationsWithCycle() {
+        try {
+            MetadataAsmFactory fact = new MetadataAsmFactory(new MetadataLogger(null), MetadataASMFactoryTest.class.getClassLoader());
+            MetadataClass metadataClass = fact.getMetadataClass(CycleA.class.getName());
+            MetadataAnnotation annotation = metadataClass.getAnnotation("org.eclipse.samples.annotations.CycleC");
+            metadataClass = fact.getMetadataClass(CycleB.class.getName());
+            annotation = metadataClass.getAnnotation("org.eclipse.samples.annotations.CycleA");
+        } catch (StackOverflowError e) {
+            fail("Stack overflow while processing cycle in meta-annotations");
+        }
+    }
+
+    /**
+     * Check meta-annotations graph with primitive cycle Self -> Self
+     */
+    public void testAnnotationsWithPrimitiveCycle() {
+        try {
+            MetadataAsmFactory fact = new MetadataAsmFactory(new MetadataLogger(null), MetadataASMFactoryTest.class.getClassLoader());
+            MetadataClass metadataClass = fact.getMetadataClass(CycleSelf.class.getName());
+            MetadataAnnotation annotation = metadataClass.getAnnotation("org.eclipse.samples.annotations.CycleSelf");
+        } catch (StackOverflowError e) {
+            fail("Stack overflow while processing cycle in meta-annotations");
+        }
     }
 
 }
