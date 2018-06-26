4c15988b0be973d438853c8a0caf82af868e70b9
Will Dazey <dazeydev.3@gmail.com>
2018-06-26 16:02:50 -0500

    Bug 532160: Add support for OracleXPlatforms, without extension bundle
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>

---
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/platform/database/Oracle8Platform.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 1998, 2018 Oracle and/or its affiliates. All rights reserved.
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
+ * which accompanies this distribution. 
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     06/26/2018 - Will Dazey
+ *       - 532160 : Add support for non-extension OracleXPlatform classes
+ ******************************************************************************/
+package org.eclipse.persistence.platform.database;
+
+import org.eclipse.persistence.platform.database.OraclePlatform;
+
+
+/**
+ * <p><b>Purpose:</b>
+ * Provides Oracle version specific behavior when 
+ * org.eclipse.persistence.oracle bundle is not available.
+ */
+public class Oracle8Platform extends OraclePlatform {
+}
