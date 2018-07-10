8630a69f67f0ab22722c1a775b026d4a3e2708c4
Joe Grassel <jgrassel@us.ibm.com>
2018-07-10 10:00:30 -0500

    Bug 536853: Update unit test headers with proper pattern.
    Signed-off-by: Joe Grassel <jgrassel@us.ibm.com>

--- jpa/eclipselink.jpa.test.jse/src/org/eclipse/persistence/jpa/compositeid/TestCompositeId.java
+++ jpa/eclipselink.jpa.test.jse/src/org/eclipse/persistence/jpa/compositeid/TestCompositeId.java
@@ -1,6 +1,5 @@
 /********************************************************************************
- * Copyright (c) 1998, 2018 Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 1998, 2018 IBM Corporation. All rights reserved.
+ * Copyright (c) 2018 IBM Corporation. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -9,10 +8,11 @@
  * http://www.eclipse.org/org/documents/edl-v10.php.
  *
  * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
- * Contributors:
- *     07/09/2018-2.6 Jody Grassel
- *       - 536853: MapsID processing sets up to fail validation
  ******************************************************************************/
+
+// Contributors:
+//     07/09/2018-2.6 Jody Grassel
+//        - 536853: MapsID processing sets up to fail validation
 
 package org.eclipse.persistence.jpa.compositeid;
 
