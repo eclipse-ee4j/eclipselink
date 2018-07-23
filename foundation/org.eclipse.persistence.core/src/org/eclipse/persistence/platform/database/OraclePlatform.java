8073e531d09274090fb9515b1d4d91b1561ab38d
Tomáš Kraus <tomas.kraus@oracle.com>
2018-07-23 13:38:06 +0200

    Bug 425150 - Fixed bug in query result evaluation. (#182)
    Signed-off-by: Tomas Kraus <tomas.kraus@oracle.com>

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/platform/database/OraclePlatform.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/platform/database/OraclePlatform.java
@@ -60,6 +60,7 @@ import org.eclipse.persistence.internal.helper.Helper;
 import org.eclipse.persistence.internal.helper.NonSynchronizedVector;
 import org.eclipse.persistence.internal.localization.ExceptionLocalization;
 import org.eclipse.persistence.internal.sessions.AbstractSession;
+import org.eclipse.persistence.internal.sessions.DatabaseSessionImpl;
 import org.eclipse.persistence.queries.DataModifyQuery;
 import org.eclipse.persistence.queries.DataReadQuery;
 import org.eclipse.persistence.queries.DatabaseQuery;
@@ -80,6 +81,7 @@ import org.eclipse.persistence.tools.schemaframework.TableDefinition;
  * @since TOPLink/Java 1.0
  */
 public class OraclePlatform extends org.eclipse.persistence.platform.database.DatabasePlatform {
+
     protected static DataModifyQuery vpdSetIdentifierQuery;
     protected static DataModifyQuery vpdClearIdentifierQuery;
 
@@ -1187,18 +1189,40 @@ public class OraclePlatform extends org.eclipse.persistence.platform.database.Da
         return super.createExpressionFor(field, builder);
     }
 
+    // Value of shouldCheckResultTableExistsQuery must be true.
     /**
      * INTERNAL:
-     * Returns query to check whether given table exists in MySQL database.
-     * Returned query must be completely prepared so it can be just executed by calling code.
+     * Returns query to check whether given table exists.
+     * Query execution returns a row when table exists or empty result otherwise.
      * @param table database table meta-data
      * @return query to check whether given table exists
      */
-    public DataReadQuery getTableExistsQuery(final TableDefinition table) {
-        final String sql = "SELECT table_name FROM user_tables WHERE table_name='" + table.getFullName() + "'";
-        final DataReadQuery query = new DataReadQuery(sql);
+    @Override
+    protected DataReadQuery getTableExistsQuery(final TableDefinition table) {
+        final DataReadQuery query = new DataReadQuery(
+                "SELECT table_name FROM user_tables WHERE table_name='" + table.getFullName() + "'");
         query.setMaxRows(1);
         return query;
+    }
+
+    /**
+     * INTERNAL:
+     * Executes and evaluates query to check whether given table exists.
+     * Returned value depends on returned result set being empty or not.
+     * @param session current database session
+     * @param table database table meta-data
+     * @param suppressLogging whether to suppress logging during query execution
+     * @return value of {@code true} if given table exists or {@code false} otherwise
+     */
+    public boolean checkTableExists(final DatabaseSessionImpl session,
+            final TableDefinition table, final boolean suppressLogging) {
+        try {
+            session.setLoggingOff(suppressLogging);
+            final Vector result = (Vector)session.executeQuery(getTableExistsQuery(table));
+            return !result.isEmpty();
+        } catch (Exception notFound) {
+            return false;
+        }
     }
 
 }
