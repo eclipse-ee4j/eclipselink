f88a85a1aba66441252ac5376c8f46edaa2db0fc
Jan Vissers <vissers.j@gmail.com>
2018-10-05 00:05:55 +0200

    Bug 539822** - MetadataProject processStage2 to use getMetamodelMappedSuperclasses
    Signed-off-by: Jan Vissers <vissers.j@gmail.com>

---
+++ jpa/eclipselink.jpa.test.jse/src/org/eclipse/persistence/jpa/test/canonical/DomainInterface.java
@@ -0,0 +1,6 @@
+package org.eclipse.persistence.jpa.test.canonical;
+
+public interface DomainInterface extends BasePersistable {
+
+    Long doSomeBusinessOperation();
+}
---
+++ jpa/eclipselink.jpa.test.jse/src/org/eclipse/persistence/jpa/test/canonical/DomainInterface_.java
@@ -0,0 +1,13 @@
+package org.eclipse.persistence.jpa.test.canonical;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "Simulated Generation", date = "2018-10-09T11:24:45")
+@StaticMetamodel(DomainPersistable.class)
+public class DomainInterface_ {
+
+    public static volatile SingularAttribute<DomainPersistable, Long> id;
+    public static volatile SingularAttribute<DomainPersistable, String> name;
+}
