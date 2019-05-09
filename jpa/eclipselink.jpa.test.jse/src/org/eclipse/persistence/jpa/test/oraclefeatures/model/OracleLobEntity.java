4ac55269fe0969422566953d503858d96df5e676
Jody Grassel <fyrewyld@gmail.com>
2019-05-09 14:10:42 -0500

    Bug 547023 - Add LOB Locator support to core Oracle platform (#433)
    Signed-off-by: Joe Grassel <fyrewyld@gmail.com>

---
+++ jpa/eclipselink.jpa.test.jse/src/org/eclipse/persistence/jpa/test/oraclefeatures/model/OracleLobEntity.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * Copyright (c) 2019 IBM Corporation. All rights reserved.
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
+ * which accompanies this distribution. 
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     05/06/2019 - Jody Grassel
+ *       - 547023: Add LOB Locator support for core Oracle platform.
+ ******************************************************************************/
+
+package org.eclipse.persistence.jpa.test.oraclefeatures.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+@Entity
+public class OracleLobEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String strData;
+
+    @Lob
+    private byte[] blobData;
+
+    @Lob
+    @Column(columnDefinition = "CLOB NOT NULL")
+    private String clobData;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStrData() {
+        return strData;
+    }
+
+    public void setStrData(String strData) {
+        this.strData = strData;
+    }
+
+    public byte[] getBlobData() {
+        return blobData;
+    }
+
+    public void setBlobData(byte[] blobData) {
+        this.blobData = blobData;
+    }
+
+    public String getClobData() {
+        return clobData;
+    }
+
+    public void setClobData(String clobData) {
+        this.clobData = clobData;
+    }
+
+    @Override
+    public String toString() {
+        return "OracleBlobEntity [id=" + id + ", strData=" + strData + "]";
+    }
+
+}
