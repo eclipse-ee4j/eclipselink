a6ddff55c4a2041d2e2825f53e8befbcd8d81af2
Will Dazey <dazeydev.3@gmail.com>
2019-09-13 11:45:11 -0500

    Bug 471144: Bad offset for AggregateObjectMapping using eclipselink.cursor
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>

---
+++ jpa/eclipselink.jpa.test.jse/src/org/eclipse/persistence/jpa/test/mapping/model/SimpleMappingEntity.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2019 IBM Corporation. All rights reserved.
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
+ * which accompanies this distribution. 
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     09/12/2019 - Will Dazey
+ *       - 471144: Add support for AggregateObjectMappings to eclipselink.cursor impl
+ ******************************************************************************/
+package org.eclipse.persistence.jpa.test.mapping.model;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class SimpleMappingEntity {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private int mappingField1;
+
+    @Embedded
+    private SimpleMappingEmbeddable aggregateObjectMapping;
+
+    public SimpleMappingEntity() { }
+
+    public SimpleMappingEntity(int mappingField1, SimpleMappingEmbeddable aggregateObjectMapping) {
+        this.mappingField1 = mappingField1;
+        this.aggregateObjectMapping = aggregateObjectMapping;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public int getMappingField1() {
+        return mappingField1;
+    }
+    public void setMappingField1(int mappingField1) {
+        this.mappingField1 = mappingField1;
+    }
+    public SimpleMappingEmbeddable getAggregateObjectMapping() {
+        return aggregateObjectMapping;
+    }
+    public void setAggregateObjectMapping(SimpleMappingEmbeddable aggregateObjectMapping) {
+        this.aggregateObjectMapping = aggregateObjectMapping;
+    }
+}
