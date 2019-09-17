84975afe3d2d39e9fd3bd200b8122d398ee585b7
Will Dazey <dazeydev.3@gmail.com>
2019-09-17 10:24:44 -0500

    Bug 547173: em.unwrap returns NULL in EE application (#535)
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>
    Reviewed-by: Joe Grassel <fyrewyld@gmail.com>

--- jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/internal/jpa/EntityManagerImpl.java
+++ jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/internal/jpa/EntityManagerImpl.java
@@ -2928,7 +2928,7 @@ public class EntityManagerImpl implements org.eclipse.persistence.jpa.JpaEntityM
                 return (T) this.getSessionBroker();
             } else if (cls.equals(java.sql.Connection.class)) {
                 final UnitOfWorkImpl unitOfWork = (UnitOfWorkImpl) this.getUnitOfWork();
-                final Accessor accessor = unitOfWork.getAccessor();
+                Accessor accessor = unitOfWork.getAccessor();
                 if (unitOfWork.getParent().isExclusiveIsolatedClientSession()) {
                     // If the ExclusiveIsolatedClientSession hasn't serviced a query prior to the unwrap,
                     // there will be no available Connection.
@@ -2954,6 +2954,7 @@ public class EntityManagerImpl implements org.eclipse.persistence.jpa.JpaEntityM
                 
                 if (checkForTransaction(false) != null) {
                     unitOfWork.beginEarlyTransaction();
+                    accessor = unitOfWork.getAccessor();
                     // Ensure external connection is acquired.
                     accessor.incrementCallCount(unitOfWork.getParent());
                     accessor.decrementCallCount();
