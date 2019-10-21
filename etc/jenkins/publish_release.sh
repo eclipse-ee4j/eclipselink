13ec9ec758b32537d12fbf7b9b090982feb6a145
Radek Felcman <radek.felcman@oracle.com>
2019-10-21 16:08:41 +0200

    Jenkins jobs - publish releases
    This is fix for Jenkins promote/release job to publish final release into right
    directory in Eclipse.org download server.
    During init phase there is download of specified milestone directory to verify it.

---
+++ etc/jenkins/publish_release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+#
+# Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Distribution License v. 1.0, which is available at
+# http://www.eclipse.org/org/documents/edl-v10.php.
+#
+# SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+
+#
+# Arguments:
+#  N/A
+#==========================
+#   Basic Env Setup
+#
+. ${HOME}/etc/jenkins/setEnvironment.sh
+
+
+echo '-[ EclipseLink Publish to Releases ]-----------------------------------------------------------'
+scp -r ${RELEASE_DNLD_DIR}/* genie.eclipselink@projects-storage.eclipse.org:${RELEASE_DNLD_DIR_REMOTE}
