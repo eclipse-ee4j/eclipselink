2235a9c32c0c7aa1a69a08aa2481b700862a86e2
Lukas Jungmann <lukas.jungmann@oracle.com>
2020-01-06 10:13:28 +0100

    jpa callback validation not being invoked for changed object (#656)
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/internal/jpa/metadata/listeners/BeanValidationListener.java
+++ jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/internal/jpa/metadata/listeners/BeanValidationListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -95,6 +95,11 @@ public class BeanValidationListener extends DescriptorEventAdapter {
         if(!unitOfWork.isObjectDeleted(source)) {
             validateOnCallbackEvent(event, "preUpdate", groupPreUpdate);
         }
+    }
+
+    @Override
+    public void preUpdateWithChanges(DescriptorEvent event) {
+        aboutToUpdate(event);
     }
 
     @Override
