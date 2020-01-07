bb27b2eb2befe7a0ded50b18d503a84311222668
Radek Felcman <radek.felcman@oracle.com>
2020-01-07 13:55:05 +0100

    [2.7] BUG#558528 - SOAPBINDING ANNOTATION WITH SOAPBINDING.STYLE.RPC MAKES DEPLOYMENT IMPOSSIBLE (#666)
    * [2.7] BUG#558528 - SOAPBINDING ANNOTATION WITH SOAPBINDING.STYLE.RPC MAKES DEPLOYMENT IMPOSSIBLE
    
    Signed-off-by: Radek Felcman <radek.felcman@oracle.com>

--- moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/moxy/unit/jaxb/compiler/SchemaGeneratorTestCase.java
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/moxy/unit/jaxb/compiler/SchemaGeneratorTestCase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -15,7 +15,10 @@
 package org.eclipse.persistence.testing.moxy.unit.jaxb.compiler;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertFalse;
 
+import java.lang.reflect.Type;
+import java.util.Collection;
 import java.util.ArrayList;
 
 import mockit.Deencapsulation;
@@ -32,6 +35,14 @@ import org.eclipse.persistence.jaxb.compiler.SchemaGenerator;
 import org.eclipse.persistence.jaxb.compiler.TypeInfo;
 import org.eclipse.persistence.jaxb.compiler.builder.TransformerPropertyBuilder;
 import org.eclipse.persistence.jaxb.javamodel.Helper;
+import org.eclipse.persistence.jaxb.javamodel.JavaClass;
+import org.eclipse.persistence.jaxb.javamodel.JavaMethod;
+import org.eclipse.persistence.jaxb.javamodel.JavaField;
+import org.eclipse.persistence.jaxb.javamodel.JavaModel;
+import org.eclipse.persistence.jaxb.javamodel.JavaConstructor;
+import org.eclipse.persistence.jaxb.javamodel.JavaPackage;
+import org.eclipse.persistence.jaxb.javamodel.JavaAnnotation;
+import org.eclipse.persistence.jaxb.javamodel.JavaClassInstanceOf;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -64,5 +75,52 @@ public class SchemaGeneratorTestCase {
          }};
 
         Deencapsulation.invoke(schemaGenerator, "addTransformerToSchema", property, typeInfo, typeDefParticle, complexType, schema);
+    }
+
+    @Test
+    public void javaxXmlRpcStringHolderTest(final @Mocked Helper helper) {
+        assertFalse(helper.isBuiltInJavaType(new JavaClass() { public String getRawName() { return "javax.xml.rpc.StringHolder";}
+            public JavaClassInstanceOf instanceOf() { return null; }
+            public boolean isSynthetic() { return false; }
+            public Collection getActualTypeArguments() { return null; }
+            public JavaClass getComponentType() { return null; }
+            public String getQualifiedName() { return null; }
+            public boolean hasActualTypeArguments() { return false; }
+            public Collection getDeclaredClasses() { return null; }
+            public JavaField getDeclaredField(String arg0) { return null; }
+            public Collection getDeclaredFields() { return null; }
+            public JavaMethod getDeclaredMethod(String arg0, JavaClass[] arg1) { return null; }
+            public Collection getDeclaredMethods() { return null; }
+            public JavaMethod getMethod(String arg0, JavaClass[] arg1) { return null; }
+            public Collection getMethods() { return null ;}
+            public JavaConstructor getConstructor(JavaClass[] parameterTypes) { return null; }
+            public Collection getConstructors() { return null; }
+            public JavaConstructor getDeclaredConstructor(JavaClass[] parameterTypes) { return null; }
+            public Collection getDeclaredConstructors() { return null; }
+            public int getModifiers() { return 0; }
+            public String getName() { return null; }
+            public JavaPackage getPackage() { return null; }
+            public String getPackageName() { return null; }
+            public JavaClass getSuperclass() { return null; }
+            public Type[] getGenericInterfaces() { return null; }
+            public Type getGenericSuperclass() { return null; }
+            public boolean isAbstract() { return false; }
+            public boolean isAnnotation() { return false; }
+            public boolean isArray() { return false; }
+            public boolean isAssignableFrom(JavaClass arg0) { return false; }
+            public boolean isEnum() { return false; }
+            public boolean isFinal() { return false; }
+            public boolean isInterface() { return false; }
+            public boolean isMemberClass() { return false; }
+            public boolean isPrimitive() { return false; }
+            public boolean isPrivate() { return false; }
+            public boolean isProtected() { return false; }
+            public boolean isPublic() { return false; }
+            public boolean isStatic() { return false; }
+            public JavaAnnotation getAnnotation(JavaClass arg0) { return null; }
+            public Collection getAnnotations() { return null; }
+            public JavaAnnotation getDeclaredAnnotation(JavaClass arg0) { return null; }
+            public Collection getDeclaredAnnotations() { return null; }
+        }));
     }
 }
