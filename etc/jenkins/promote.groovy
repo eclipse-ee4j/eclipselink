3644282ab90111e8b00be6b7048b71a7417ac5e9
Radek Felcman <radek.felcman@oracle.com>
2020-01-24 11:52:28 +0100

    [2.6] EclipseLink - Jenkins pipeline
    Jenkins pipeline and bash scripts used for a build and promotion.
    
    Backport from 2.7.

---
+++ etc/jenkins/promote.groovy
@@ -0,0 +1,151 @@
+// Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Distribution License v. 1.0, which is available at
+// http://www.eclipse.org/org/documents/edl-v10.php.
+//
+// SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+
+// Job input parameters:
+//  RELEASE                - Release switch
+//  MAJOR_VERSION          - EclipseLink major version like 2.6 , 2.7 , 3.0
+//  VERSION                - EclipseLink version. Must match with first part of NIGHTLY_BUILD_ID parameter like 2.7.5
+//  NIGHTLY_BUILD_ID       - Nightly build ID in following form <version>-<classifier>-<git commit short id> .
+//                           For example -  2.7.5.v20190614-4681d7f571
+//                           Check directory with nightly builds.
+//  RELEASE_CANDIDATE_ID   - Typical values are: RC1, RC2....
+//  SIGN                   - Sing content of promoted jar files true - sign false - don't sign.
+//  DEBUG                  - Show debug messages.
+
+
+pipeline {
+    agent {
+        kubernetes {
+            label 'el-master-agent-pod'
+            yaml """
+apiVersion: v1
+kind: Pod
+spec:
+
+  volumes:
+  - name: tools
+    persistentVolumeClaim:
+      claimName: tools-claim-jiro-eclipselink
+  - name: volume-known-hosts
+    configMap:
+      name: known-hosts      
+  - name: settings-xml
+    secret:
+      secretName: m2-secret-dir
+      items:
+      - key: settings.xml
+        path: settings.xml
+  - name: toolchains-xml
+    configMap:
+      name: m2-dir
+      items:
+      - key: toolchains.xml
+        path: toolchains.xml
+  - name: settings-security-xml
+    secret:
+      secretName: m2-secret-dir
+      items:
+      - key: settings-security.xml
+        path: settings-security.xml
+  - name: m2-repo
+    emptyDir: {}
+    
+  containers:
+
+  - name: el-build
+    resources:
+      limits:
+        memory: "2Gi"
+        cpu: "1"
+      requests:
+        memory: "2Gi"
+        cpu: "1"
+    image: tkraus/el-build:1.1.8
+    volumeMounts:
+    - name: tools
+      mountPath: /opt/tools
+    - name: volume-known-hosts
+      mountPath: /home/jenkins/.ssh      
+    - name: settings-xml
+      mountPath: /home/jenkins/.m2/settings.xml
+      subPath: settings.xml
+      readOnly: true
+    - name: toolchains-xml
+      mountPath: /home/jenkins/.m2/toolchains.xml
+      subPath: toolchains.xml
+      readOnly: true
+    - name: settings-security-xml
+      mountPath: /home/jenkins/.m2/settings-security.xml
+      subPath: settings-security.xml
+      readOnly: true
+    - name: m2-repo
+      mountPath: /home/jenkins/.m2/repository
+    tty: true
+    command:
+    - cat
+"""
+        }
+    }
+    stages {
+        // Prepare and promote EclipseLink artifacts to oss.sonatype.org (staging) and to the Eclipse.org Milestone Builds area
+        stage('Promote') {
+            steps {
+                container('el-build') {
+                    git branch: '${GIT_BRANCH}', url: '${GIT_REPOSITORY_URL}'
+                    sshagent(['projects-storage.eclipse.org-bot-ssh']) {
+                        withCredentials([file(credentialsId: 'secret-subkeys.asc', variable: 'KEYRING')]) {
+                            sh label: '', script: '''
+                                gpg --batch --import "${KEYRING}"
+                                for fpr in $(gpg --list-keys --with-colons  | awk -F: \'/fpr:/ {print $10}\' | sort -u);
+                                do
+                                    echo -e "5\\ny\\n" |  gpg --batch --command-fd 0 --expert --edit-key $fpr trust;
+                                done'''
+                        }
+                        // Download selected nightly build from Eclipse.org Nightly Builds
+                        sh """
+                            mkdir -p ${HOME}/etc/jenkins
+                            cp -r ${WORKSPACE}/etc/jenkins/* ${HOME}/etc/jenkins
+                            cp ${WORKSPACE}/buildsystem/ant_customizations.jar ${HOME}/etc/jenkins
+                            ${HOME}/etc/jenkins/promote_init.sh
+                            """
+                        // Prepare and promote EclipseLink artifacts to oss.sonatype.org (staging)
+                        sh """
+                            . /etc/profile
+                            curl --version
+                            echo ${RELEASE}
+                            echo ${MAJOR_VERSION}
+                            echo ${NIGHTLY_BUILD_ID}
+                            echo ${RELEASE_CANDIDATE_ID}
+                            echo ${MAJOR_VERSION}
+                            echo ${SIGN}
+                            echo ${DEBUG}
+                            if [ ${RELEASE} == 'false' ]
+                            then
+                                echo calling "promote.sh ${NIGHTLY_BUILD_ID} ${RELEASE_CANDIDATE_ID} ${MAJOR_VERSION} ${SIGN} ${DEBUG}"
+                                ${HOME}/etc/jenkins/promote.sh ${NIGHTLY_BUILD_ID} ${RELEASE_CANDIDATE_ID} ${MAJOR_VERSION} ${SIGN} ${DEBUG}
+                            else
+                                echo calling "promote.sh release ${RELEASE_CANDIDATE_ID} ${MAJOR_VERSION} ${SIGN} ${DEBUG}"
+                                ${HOME}/etc/jenkins/promote.sh release ${RELEASE_CANDIDATE_ID} ${MAJOR_VERSION} ${SIGN} ${DEBUG}
+                            fi
+                        """
+                        // Promote EclipseLink bundles to the Eclipse.org Milestone Builds area
+                        sh """
+                            echo ${RELEASE}
+                            if [ ${RELEASE} == 'false' ]
+                            then
+                                ${HOME}/etc/jenkins/publish_milestone.sh
+                            else
+                                ${HOME}/etc/jenkins/publish_release.sh                            
+                            fi
+                            """
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ etc/jenkins/promote_init.sh
@@ -0,0 +1,59 @@
+#!/bin/bash -e
+#
+# Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Distribution License v. 1.0, which is available at
+# http://www.eclipse.org/org/documents/edl-v10.php.
+#
+# SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+
+#
+# Arguments:
+#  N/A
+
+echo '-[ EclipseLink Promotion Init ]-----------------------------------------------------------'
+. ${HOME}/etc/jenkins/setEnvironment.sh
+mkdir -p ${DNLD_DIR}/nightly/${VERSION}
+mkdir -p ${RELEASE_DNLD_DIR}/${VERSION}
+mkdir -p ${RELEASE_SITE_DIR}
+mkdir -p ${MILESTONE_SITE_DIR}
+mkdir -p ${NIGHTLY_SITE_DIR}
+mkdir -p ${SIGN_DIR}
+scp -r genie.eclipselink@projects-storage.eclipse.org:${DNLD_DIR_REMOTE}/nightly/${VERSION}/* ${DNLD_DIR}/nightly/${VERSION}
+if [ ${RELEASE} = "true" ]; then
+  mkdir -p ${MILESTONE_DNLD_DIR}/${VERSION}/${RELEASE_CANDIDATE_ID}
+  scp -r genie.eclipselink@projects-storage.eclipse.org:${MILESTONE_DNLD_DIR_REMOTE}/${VERSION}/${RELEASE_CANDIDATE_ID}/* ${MILESTONE_DNLD_DIR}/${VERSION}/${RELEASE_CANDIDATE_ID}
+fi
+
+echo '-[ EclipseLink Init ]-----------------------------------------------------------'
+
+mkdir -p $HOME/extension.lib.external/mavenant
+
+#DOWNLOAD SOME DEPENDENCIES
+wget -nc http://repo1.maven.org/maven2/junit/junit/4.12/junit-4.12.jar -O $HOME/extension.lib.external/junit-4.12.jar
+wget -nc http://repo1.maven.org/maven2/org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar -O $HOME/extension.lib.external/hamcrest-core-1.3.jar
+wget -nc http://repo1.maven.org/maven2/org/jmockit/jmockit/1.35/jmockit-1.35.jar -O $HOME/extension.lib.external/jmockit-1.35.jar
+wget -nc http://repo1.maven.org/maven2/org/hibernate/validator/hibernate-validator/6.0.7.Final/hibernate-validator-6.0.7.Final.jar -O $HOME/extension.lib.external/hibernate-validator-6.0.7.Final.jar
+wget -nc http://repo1.maven.org/maven2/org/jboss/logging/jboss-logging/3.3.0.Final/jboss-logging-3.3.0.Final.jar -O $HOME/extension.lib.external/jboss-logging-3.3.0.Final.jar
+wget -nc http://repo1.maven.org/maven2/org/glassfish/javax.el/3.0.1-b08/javax.el-3.0.1-b08.jar -O $HOME/extension.lib.external/javax.el-3.0.1-b08.jar
+wget -nc http://repo1.maven.org/maven2/com/fasterxml/classmate/1.3.1/classmate-1.3.1.jar -O $HOME/extension.lib.external/classmate-1.3.1.jar
+wget -nc http://repo1.maven.org/maven2/mysql/mysql-connector-java/5.1.48/mysql-connector-java-5.1.48.jar -O $HOME/extension.lib.external/mysql-connector-java-5.1.48.jar
+wget -nc https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.7-bin.tar.gz -O $HOME/extension.lib.external/apache-ant-1.10.7-bin.tar.gz
+wget -nc http://archive.apache.org/dist/maven/ant-tasks/2.1.3/binaries/maven-ant-tasks-2.1.3.jar -O $HOME/extension.lib.external/mavenant/maven-ant-tasks-2.1.3.jar
+wget -nc https://download.eclipse.org/eclipse/downloads/drops4/R-4.10-201812060815/eclipse-SDK-4.10-linux-gtk-x86_64.tar.gz -O $HOME/extension.lib.external/eclipse-SDK-4.10-linux-gtk-x86_64.tar.gz
+
+#UNPACK SOME  DEPENDENCIES
+tar -x -z -C $HOME/extension.lib.external -f $HOME/extension.lib.external/eclipse-SDK-4.10-linux-gtk-x86_64.tar.gz
+
+#PREPARE build.properties FILE
+echo "extensions.depend.dir=$HOME/extension.lib.external" >> $HOME/build.properties
+echo "junit.lib=$HOME/extension.lib.external/junit-4.12.jar:$HOME/extension.lib.external/hamcrest-core-1.3.jar" >> $HOME/build.properties
+echo 'jdbc.driver.jar=$HOME/extension.lib.external/mysql-connector-java-5.1.48.jar' >> $HOME/build.properties
+echo 'db.driver=com.mysql.jdbc.Driver' >> $HOME/build.properties
+echo "db.url=$TEST_DB_URL" >> $HOME/build.properties
+echo "db.user=$TEST_DB_USERNAME" >> $HOME/build.properties
+echo "db.pwd=$TEST_DB_PASSWORD" >> $HOME/build.properties
+echo 'db.platform=org.eclipse.persistence.platform.database.MySQLPlatform' >> $HOME/build.properties
+echo 'eclipse.install.dir=$HOME/extension.lib.external/eclipse' >> $HOME/build.properties
+echo hudson.workspace=$WORKSPACE
