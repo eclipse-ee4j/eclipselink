e102f874d1423fa06884fa91f6faee3407e9a638
Joe Grassel <fyrewyld@gmail.com>
2020-03-09 10:54:32 -0500

    Bug 548814 - EL not Auto Detecting Oracle 19C
    Signed-off-by: Joe Grassel <fyrewyld@gmail.com>

---
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/platform/database/Oracle19Platform.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2020 IBM Corporation. All rights reserved.
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
+ * which accompanies this distribution. 
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     03/09/2020-2.6.8 Jody Grassel
+ *       - 548814: EL not Auto Detecting Oracle 19C
+ ******************************************************************************/
+
+package org.eclipse.persistence.platform.database;
+
+public class Oracle19Platform extends Oracle18Platform {
+	 public Oracle19Platform() {
+	        super();
+	    }
+}
