c205d678fb0d29f6dc1e3ff438a5bf0922149d5f
Radek Felcman <radek.felcman@oracle.com>
2020-03-24 08:29:46 +0100

    Bugfix for #624 - New Object created and deleted in nested UnitOfWork is commited to DB
    Bugfix + unit test.

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/sessions/MergeManager.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/sessions/MergeManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -697,7 +697,7 @@ public class MergeManager {
         AbstractSession parent = unitOfWork.getParent();
 
         // If the clone is deleted, avoid this merge and simply return the clone.
-        if (unitOfWork.isObjectDeleted(clone)) {
+        if (!unitOfWork.isNestedUnitOfWork() && unitOfWork.isObjectDeleted(clone)) {
             return clone;
         }
         ClassDescriptor descriptor = unitOfWork.getDescriptor(clone.getClass());
