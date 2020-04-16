999ca8fd6fb45ec41d1f98a9f3b249c5d96c3fe2
Will Dazey <dazeydev.3@gmail.com>
2020-04-16 14:01:09 -0500

    Bug 561664: JoinColumn with same name and referencedColumnName throws exception
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>

---
+++ jpa/eclipselink.jpa.test.jse/src/org/eclipse/persistence/jpa/test/mapping/model/BaseParent.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2020 IBM Corporation. All rights reserved.
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0 
+ * which accompanies this distribution. 
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     04/17/2020 - Will Dazey
+ *       - 561664: JoinColumn with same name as referencedColumnName
+ ******************************************************************************/
+package org.eclipse.persistence.jpa.test.mapping.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "BASE_PARENT")
+public class BaseParent {
+
+    @Id
+    @Column(name = "BASE_PARENT_ID")
+    private Long id;
+}
