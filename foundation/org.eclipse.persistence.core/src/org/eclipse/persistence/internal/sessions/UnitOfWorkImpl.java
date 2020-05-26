2a2e67c759ff5d43f985a3229cc56fb07cdccacd
Ravi Babu Tummuru <ravi.babu.tummuru@oracle.com>
2020-05-26 16:55:39 +0000

    BUG#562811 - HEAP DUMP HAS LARGE SIZED ECLIPSELINK REPEATABLEWRITEUNI
    Signed-off-by: Ravi Babu Tummuru <ravi.babu.tummuru@oracle.com>

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/sessions/UnitOfWorkImpl.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/sessions/UnitOfWorkImpl.java
@@ -5874,6 +5874,7 @@ public class UnitOfWorkImpl extends AbstractSession implements org.eclipse.persi
         this.optimisticReadLockObjects = null;
         this.batchQueries = null;
         this.privateOwnedObjects = null;
+        this.newObjectsCloneToMergeOriginal = null;
         if(shouldClearCache) {
             clearIdentityMapCache();
         }
