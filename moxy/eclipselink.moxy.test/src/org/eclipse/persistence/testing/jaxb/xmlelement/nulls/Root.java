e728c0f5638abff9b120efcca624656bd78c381f
Radek Felcman <radek.felcman@oracle.com>
2020-05-29 09:41:45 +0200

    Bugfix MOXy 561757 unmarshalling JSON null value ignored in the payload
    Bigfix + unit test
    
    Signed-off-by: Radek Felcman <radek.felcman@oracle.com>

--- moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/xmlelement/nulls/Root.java
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/xmlelement/nulls/Root.java
@@ -19,7 +19,7 @@ import java.util.List;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElements;
 import javax.xml.bind.annotation.XmlRootElement;
-import jakarta.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlTransient;
 
 @XmlRootElement
 public class Root {
