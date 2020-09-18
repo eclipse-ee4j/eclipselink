4ab5bc742613df9c20a6509f7b1f8688f0c1a7be
Will Dazey <dazeydev.3@gmail.com>
2020-09-17 23:33:21 -0500

    Bug 567087: Minor fix to whitespace characters used in COALESCE function
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>

--- foundation/org.eclipse.persistence.core/src/main/java/org/eclipse/persistence/expressions/ExpressionOperator.java
+++ foundation/org.eclipse.persistence.core/src/main/java/org/eclipse/persistence/expressions/ExpressionOperator.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 1998, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2020 IBM Corporation. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -673,8 +674,8 @@ public class ExpressionOperator implements Serializable {
         exOperator.bePrefix();
         exOperator.setNodeClass(ClassConstants.ArgumentListFunctionExpression_Class);
         exOperator.setStartString("COALESCE(");
-        exOperator.setSeparator(",");
-        exOperator.setTerminationString(" )");
+        exOperator.setSeparator(", ");
+        exOperator.setTerminationString(")");
         return exOperator;
     }
 
