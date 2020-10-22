a22f2580b3968401e462396b6ebf5e80535aae41
Radek Felcman <radek.felcman@oracle.com>
2020-10-22 16:43:13 +0200

    Bugfix for stored procedures call in H2 database (#937)
    Signed-off-by: Radek Felcman <radek.felcman@oracle.com>

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/platform/database/H2Platform.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/platform/database/H2Platform.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -265,5 +265,14 @@ public class H2Platform extends DatabasePlatform {
     @Override
     public boolean isH2() {
         return true;
+    }
+
+    /**
+     * INTERNAL:
+     * Used for stored procedure calls.
+     */
+    @Override
+    public String getProcedureCallHeader() {
+        return "CALL ";
     }
 }
