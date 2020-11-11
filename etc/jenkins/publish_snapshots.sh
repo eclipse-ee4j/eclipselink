339a37c4afde1060b92f806b716056a9860344f8
Radek Felcman <radek.felcman@oracle.com>
2020-11-11 14:46:34 +0100

    Jenkins build - Snapshots promotion during Nightly build (#957)
    Signed-off-by: Radek Felcman <radek.felcman@oracle.com>

---
+++ etc/jenkins/publish_snapshots.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+#
+# Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Distribution License v. 1.0, which is available at
+# http://www.eclipse.org/org/documents/edl-v10.php.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+#
+# Arguments:
+#  N/A
+
+echo '-[ EclipseLink Publish to Jakarta Snapshots ]-----------------------------------------------------------'
+. /etc/profile
+mvn --no-transfer-progress -U -C -B -V \
+      -Psnapshots -DskipTests \
+      -Ddoclint=none -Ddeploy \
+      clean deploy
