b8efe36c1a97b7a9f233519059c8e1b806dd2bf6
Radek Felcman <radek.felcman@oracle.com>
2021-02-22 11:31:35 +0100

    EclipseLink doesn't generate SQL UPDATE correctly if @ReturnUpdate is used in InheritanceType.JOINED (#1019)
    bugfix + unit test
    
    Signed-off-by: Radek Felcman <radek.felcman@oracle.com>

--- jpa/eclipselink.jpa.test.jse/src/org/eclipse/persistence/jpa/test/returninsert/model/ReturnInsertDetailJoined.java
+++ jpa/eclipselink.jpa.test.jse/src/org/eclipse/persistence/jpa/test/returninsert/model/ReturnInsertDetailJoined.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2021 Oracle and/or its affiliates. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
  * which accompanies this distribution.
@@ -12,13 +12,13 @@
  ******************************************************************************/
 package org.eclipse.persistence.jpa.test.returninsert.model;
 
-import org.eclipse.persistence.annotations.ReturnInsert;
-
 import javax.persistence.*;
+
+import org.eclipse.persistence.annotations.ReturnInsert;
+import org.eclipse.persistence.annotations.ReturnUpdate;
 
 /**
  * The parent persistent class for the JPA22_RETURNINSERT_DETAIL_JOINED database table.
- * 
  */
 @Entity
 @Table(name = "JPA22_RETURNINSERT_DETAIL_JOINED")
@@ -26,15 +26,20 @@ import javax.persistence.*;
 @DiscriminatorValue("A")
 public class ReturnInsertDetailJoined extends ReturnInsertMasterJoined {
 
-	@Column(name = "detail_nr", nullable = false, unique = true, updatable = false)
-	@ReturnInsert(returnOnly = true)
+	@Column(name = "detail_nr")
 	Long detailNumber = null;
+
+	@Column(name = "detail_nr_virtual", nullable = false, unique = true, updatable = false)
+	@ReturnInsert(returnOnly = true)
+	@ReturnUpdate
+	Long detailNumberVirtual = null;
 
 	public ReturnInsertDetailJoined() {
 	}
 
-	public ReturnInsertDetailJoined(Long id, String type) {
+	public ReturnInsertDetailJoined(Long id, Long detailNumber, String type) {
 		super(id, type);
+		this.detailNumber = detailNumber;
 	}
 
 	public Long getDetailNumber() {
@@ -43,5 +48,13 @@ public class ReturnInsertDetailJoined extends ReturnInsertMasterJoined {
 
 	public void setDetailNumber(Long detailNumber) {
 		this.detailNumber = detailNumber;
+	}
+
+	public Long getDetailNumberVirtual() {
+		return detailNumberVirtual;
+	}
+
+	public void setDetailNumberVirtual(Long detailNumber) {
+		this.detailNumberVirtual = detailNumber;
 	}
 }
