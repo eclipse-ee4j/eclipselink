45af2606ebff2668c3fca6f59db47d972a6e93ec
Will Dazey <dazeydev.3@gmail.com>
2021-03-09 13:42:33 -0600

    Bug 571794: Enable Input parameters for Criteria API SELECT clause
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>

--- jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/internal/jpa/querydef/AbstractQueryImpl.java
+++ jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/internal/jpa/querydef/AbstractQueryImpl.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2011, 2018 Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2011, 2018 IBM Corporation. All rights reserved.
+ * Copyright (c) 2011, 2021 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2021 IBM Corporation. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -26,7 +26,6 @@ import javax.persistence.criteria.AbstractQuery;
 import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import javax.persistence.criteria.Selection;
 import javax.persistence.metamodel.EntityType;
 import javax.persistence.metamodel.Metamodel;
 
@@ -239,14 +238,6 @@ public abstract class AbstractQueryImpl<T> extends CommonAbstractCriteriaImpl<T>
 
     protected void findJoins(FromImpl root) {
         root.findJoins(this);
-    }
-
-    protected void findRootAndParameters(Selection<?> selection) {
-        if (selection.isCompoundSelection()) {
-            for (Selection subSelection : selection.getCompoundSelectionItems()) {
-                findRootAndParameters(subSelection);
-            }
-        }
     }
 
     /**
