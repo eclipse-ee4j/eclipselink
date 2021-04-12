5a47791bb933ab38f84fe83d38f602fe88604071
Radek Felcman <radek.felcman@oracle.com>
2021-04-12 09:58:31 +0200

    ConcurrencySemaphores unit test (#1073)
    This is unit test for org.eclipse.persistence.internal.helper.ConcurrencySemaphore.
    
    Signed-off-by: Radek Felcman <radek.felcman@oracle.com>

--- foundation/eclipselink.core.test/src/org/eclipse/persistence/testing/tests/helper/HelperTestModel.java
+++ foundation/eclipselink.core.test/src/org/eclipse/persistence/testing/tests/helper/HelperTestModel.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2014 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2021 Oracle and/or its affiliates. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
  * which accompanies this distribution.
@@ -31,6 +31,7 @@ public class HelperTestModel extends TestModel {
 
         suite.addTestSuite(org.eclipse.persistence.testing.tests.helper.JavaUtilTest.class);
         suite.addTestSuite(org.eclipse.persistence.testing.tests.helper.JavaVersionTest.class);
+        suite.addTestSuite(org.eclipse.persistence.testing.tests.helper.ConcurrencySemaphoreTest.class);
 
         suite.addTest(new CompareArrayContentTest());
         suite.addTest(new CompareArrayLengthTest());
