84196f01376e2af3990dec7975c7593a6047d14e
Will Dazey <dazeydev.3@gmail.com>
2021-05-07 14:07:52 -0500

    Bug 573435: CriteriaBuilder construct throws argument type mismatch exception with Case Select
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>

--- jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/internal/jpa/querydef/SelectionImpl.java
+++ jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/internal/jpa/querydef/SelectionImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2021 Oracle, IBM Corporation, and/or its affiliates. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
  * which accompanies this distribution.
@@ -74,6 +74,11 @@ public abstract class SelectionImpl<X> implements Selection<X>, InternalSelectio
     public Class<? extends X> getJavaType() {
         return this.javaType;
     }
+
+    public void setJavaType(Class<X> javaType) {
+        this.javaType = javaType;
+    }
+
     /**
      * Return selection items composing a compound selection
      * @return list of selection items
