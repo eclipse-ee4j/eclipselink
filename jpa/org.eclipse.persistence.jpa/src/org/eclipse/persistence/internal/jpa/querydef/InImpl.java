8a9d6e306f8607e981ec84a752322d9b473355e2
Will Dazey <dazeydev.3@gmail.com>
2021-05-27 09:32:16 -0500

    Bug 573794: Add support for parameters within CriteriaBuilder IN expressions
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>

--- jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/internal/jpa/querydef/InImpl.java
+++ jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/internal/jpa/querydef/InImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2014 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2021 Oracle, IBM Corporation, and/or its affiliates. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
  * which accompanies this distribution.
@@ -118,8 +118,12 @@ public class InImpl<T> extends CompoundExpressionImpl implements In<T> {
         } else {
             if (this.currentNode.isRelationExpression()) {
                 RelationExpression baseIn = (RelationExpression) this.currentNode;
-                ((InternalSelection) value).getCurrentNode().setLocalBase(baseIn.getFirstChild());
-                ((Collection) ((CollectionExpression) baseIn.getSecondChild()).getValue()).add(((InternalSelection) value).getCurrentNode());
+                org.eclipse.persistence.expressions.Expression resultExp = ((InternalSelection)value).getCurrentNode();
+                resultExp = org.eclipse.persistence.expressions.Expression.from(resultExp, baseIn.getFirstChild());
+                ((Collection) ((CollectionExpression) baseIn.getSecondChild()).getValue()).add(resultExp);
+
+                // Add to the expression list for #findRootAndParameters()
+                this.expressions.add(value);
             } else {
                 throw new IllegalStateException(ExceptionLocalization.buildMessage("CANNOT_ADD_CONSTANTS_TO_SUBQUERY_IN"));
             }
