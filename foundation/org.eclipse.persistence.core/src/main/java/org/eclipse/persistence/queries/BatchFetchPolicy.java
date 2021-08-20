aa175fc28699f337645d348e1932a4050e77cd57
Will Dazey <dazeydev.3@gmail.com>
2021-08-20 18:44:51 -0500

    Bug 1245: NullPointerException from BatchFetchPolicy dataResults
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>

--- foundation/org.eclipse.persistence.core/src/main/java/org/eclipse/persistence/queries/BatchFetchPolicy.java
+++ foundation/org.eclipse.persistence.core/src/main/java/org/eclipse/persistence/queries/BatchFetchPolicy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2021 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -52,11 +52,13 @@ public class BatchFetchPolicy implements Serializable, Cloneable {
     protected transient Map<Object, Object> batchObjects;
 
     public BatchFetchPolicy() {
-        this.type = BatchFetchType.JOIN;
+        this(BatchFetchType.JOIN);
     }
 
     public BatchFetchPolicy(BatchFetchType type) {
         this.type = type;
+        this.dataResults = new HashMap<Object, List<AbstractRecord>>();
+        this.dataResults.put(this, new ArrayList<AbstractRecord>());
     }
 
     @Override
@@ -236,10 +238,6 @@ public class BatchFetchPolicy implements Serializable, Cloneable {
      * This is used for IN batching in batches.
      */
     public void addDataResults(AbstractRecord row) {
-        if (this.dataResults == null) {
-            this.dataResults = new HashMap<>();
-            this.dataResults.put(this, new ArrayList<AbstractRecord>());
-        }
         for (List<AbstractRecord> results : this.dataResults.values()) {
             results.add(row);
         }
