75bbde1650f014787d7ddfae29ff90537a60a08f
Radek Felcman <radek.felcman@oracle.com>
2021-09-03 08:55:38 +0200

    Jenkins build fix to reflect/use JDK specified in the Jenkins pipeline
    This fix removes `. /etc/profile` call from scripts called by various Jenkins pipelines.
    In `. /etc/profile` was hardcoded JDK 8 so Jenkins build/test didn't reflect JDK settings from pipeline.
    
    Signed-off-by: Radek Felcman <radek.felcman@oracle.com>

--- etc/jenkins/release.sh
+++ etc/jenkins/release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #
-# Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2020, 2021 Oracle and/or its affiliates. All rights reserved.
 #
 # This program and the accompanying materials are made available under the
 # terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -20,7 +20,6 @@ HELP_PLUGIN='org.apache.maven.plugins:maven-help-plugin:3.1.0'
 # $5 -  OVERWRITE_STAGING           - Allows to overwrite existing version in OSSRH (Jakarta) staging repositories
 
 echo '-[ EclipseLink Release ]-----------------------------------------------------------'
-. /etc/profile
 
 ECLIPSELINK_VERSION="${1}"
 NEXT_ECLIPSELINK_VERSION="${2}"
