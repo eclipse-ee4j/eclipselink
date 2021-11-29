e6856015fb958564d3359013e15ebede6708ab37
Radek Felcman <radek.felcman@oracle.com>
2021-11-29 13:06:27 +0100

    problems with session customizer when upgrading to 2.7.9 - bugfix + unit test (#1364) (#1365)
    Standalone usage ReturnUpdate annotation was broken in PR #1016.
    fixes #1363
    
    Signed-off-by: Radek Felcman <radek.felcman@oracle.com>
    (cherry picked from commit fb1cc69d09a70bf74935132181a105ddd37e07d6)

--- foundation/org.eclipse.persistence.core/src/main/java/org/eclipse/persistence/internal/queries/ExpressionQueryMechanism.java
+++ foundation/org.eclipse.persistence.core/src/main/java/org/eclipse/persistence/internal/queries/ExpressionQueryMechanism.java
@@ -837,12 +837,12 @@ public class ExpressionQueryMechanism extends StatementQueryMechanism {
         if (getDescriptor().hasReturningPolicies() && getDescriptor().getReturnFieldsToGenerateUpdate() != null) {
             // In case of RelationalDescriptor only return fields for current table must be used.
             List<DatabaseField> returnFieldsForTable = new ArrayList<>();
-            for (DatabaseField item: getDescriptor().getReturnFieldsToGenerateInsert()) {
+            for (DatabaseField item: getDescriptor().getReturnFieldsToGenerateUpdate()) {
                 if (table.equals(item.getTable())) {
                     returnFieldsForTable.add(item);
                 }
                 if (!returnFieldsForTable.isEmpty()) {
-                    updateStatement.setReturnFields(getDescriptor().getReturnFieldsToGenerateInsert());
+                    updateStatement.setReturnFields(getDescriptor().getReturnFieldsToGenerateUpdate());
                 }
             }
         }
