25305ca92d792feacfa067fc6f3c3cd20a239aaa
Will Dazey <dazeydev.3@gmail.com>
2022-01-19 12:55:47 -0600

    Bug 578262: Bulk update queries reuse the same version locking value
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/descriptors/TimestampLockingPolicy.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/descriptors/TimestampLockingPolicy.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 1998, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2022 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022 IBM Corporation. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -194,7 +195,7 @@ public class TimestampLockingPolicy extends VersionLockingPolicy {
      * Return an expression that updates the write lock
      */
     public Expression getWriteLockUpdateExpression(ExpressionBuilder builder, AbstractSession session) {
-        return builder.value(getInitialWriteValue(session));
+        return builder.currentTimeStamp();
     }
 
     /**
