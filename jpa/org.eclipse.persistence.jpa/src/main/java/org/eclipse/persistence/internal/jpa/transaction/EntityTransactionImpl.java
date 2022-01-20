ba7e9299ee86cf2ea9404480ac7acb35c24477c3
Tomas Kraus <tomas.kraus@oracle.com>
2022-01-20 14:47:57 +0100

    Removed debug output.
    Signed-off-by: Tomas Kraus <tomas.kraus@oracle.com>

--- jpa/org.eclipse.persistence.jpa/src/main/java/org/eclipse/persistence/internal/jpa/transaction/EntityTransactionImpl.java
+++ jpa/org.eclipse.persistence.jpa/src/main/java/org/eclipse/persistence/internal/jpa/transaction/EntityTransactionImpl.java
@@ -159,7 +159,6 @@ public class EntityTransactionImpl implements jakarta.persistence.EntityTransact
             } else if (exception instanceof org.eclipse.persistence.exceptions.OptimisticLockException) {
                 throw new RollbackException(new jakarta.persistence.OptimisticLockException(exception));
             } else {
-                exception.printStackTrace();
                 throw new RollbackException(exception);
             }
         } finally {
