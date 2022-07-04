4fd7a32b2cbb154351390e9062088d968d75cc6c
Lukas Jungmann <lukas.jungmann@oracle.com>
2022-07-04 17:27:25 +0200

    move remaining composite tests under test apps (xml.composite and xml.extended.composite)
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- jpa/eclipselink.jpa.test/src/it/java/org/eclipse/persistence/testing/tests/jpa/xml/EntityMappingsJUnitTestSuite.java
+++ jpa/eclipselink.jpa.test/src/it/java/org/eclipse/persistence/testing/tests/jpa/xml/EntityMappingsJUnitTestSuite.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2022 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -70,8 +70,6 @@ public class EntityMappingsJUnitTestSuite extends TestCase {
 
         suite.addTest(AdvancedCompositePKJunitTest.suite());
         suite.addTest(AdvancedJunitTest.suite());
-
-        suite.addTest(org.eclipse.persistence.testing.tests.jpa.xml.composite.advanced.EntityMappingsAdvancedJUnitTestCase.suite());
 
         return suite;
     }
