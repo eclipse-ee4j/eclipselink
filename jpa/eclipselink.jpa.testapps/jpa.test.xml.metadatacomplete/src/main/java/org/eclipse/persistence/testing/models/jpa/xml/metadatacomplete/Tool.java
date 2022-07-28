0b7961a84801fd4232d83fe5f08c09003d36c8f2
Lukas Jungmann <lukas.jungmann@oracle.com>
2022-07-28 10:02:04 +0200

    move (xml)metadatacomplete tests
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

---
+++ jpa/eclipselink.jpa.testapps/jpa.test.xml.metadatacomplete/src/main/java/org/eclipse/persistence/testing/models/jpa/xml/metadatacomplete/Tool.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2011, 2022 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0,
+ * or the Eclipse Distribution License v. 1.0 which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+ */
+
+// Contributors:
+//     12/01/2010-2.2 Guy Pelletier
+//       - 331234: xml-mapping-metadata-complete overriden by metadata-complete specification
+package org.eclipse.persistence.testing.models.jpa.xml.metadatacomplete;
+
+import jakarta.persistence.Column;
+
+/**
+ * Included in the following mapping files:
+ * - eclipselink-xml-only-model/metadata-complete-mappings.
+ * - eclipselink-xml-only-model/xml-mapping-metadata-complete.
+ *
+ * Which are used in two separate persistence units from:
+ * - eclipselink-annotation-model/persistence.xml, namely:
+ *   - METADATA_COMPLETE
+ *   - XML_MAPPING_METADATA_COMPLETE
+ *
+ * @author gpelleti
+ */
+public class Tool {
+    @Column(name="IGNORED_BRAND")
+    public String brand;
+
+    public int id;
+
+    public Tool() {}
+}
---
+++ jpa/eclipselink.jpa.testapps/jpa.test.xml.xmlmetadatacomplete/src/main/java/org/eclipse/persistence/testing/models/jpa/xml/xmlmetadatacomplete/XLMMappingMetadataCompleteTableManager.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2014, 2022 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2022 IBM Corporation and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0,
+ * or the Eclipse Distribution License v. 1.0 which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+ */
+
+// Contributors:
+//     08/18/2014-2.5 Jody Grassel (IBM Corporation)
+//       - 440802: xml-mapping-metadata-complete does not exclude @Entity annotated entities
+
+package org.eclipse.persistence.testing.models.jpa.xml.xmlmetadatacomplete;
+
+import org.eclipse.persistence.tools.schemaframework.FieldDefinition;
+import org.eclipse.persistence.tools.schemaframework.TableCreator;
+import org.eclipse.persistence.tools.schemaframework.TableDefinition;
+
+public class XLMMappingMetadataCompleteTableManager extends TableCreator {
+    public XLMMappingMetadataCompleteTableManager() {
+        setName("XMLMappingMetadataComplete");
+        addTableDefinition(buildXMLOnlyEntityTable());
+        addTableDefinition(buildAnoOnlyEntityTable());
+    }
+
+    public static TableDefinition buildXMLOnlyEntityTable() {
+        TableDefinition table = new TableDefinition();
+
+        table.setName("XMLONLYENTITY");
+
+        FieldDefinition fieldID = new FieldDefinition();
+        fieldID.setName("ID");
+        fieldID.setTypeName("NUMERIC");
+        fieldID.setSize(15);
+        fieldID.setShouldAllowNull(false);
+        fieldID.setIsPrimaryKey(true);
+        fieldID.setUnique(false);
+        fieldID.setIsIdentity(false);
+        table.addField(fieldID);
+
+        FieldDefinition fieldSTRDATA = new FieldDefinition();
+        fieldSTRDATA.setName("STRDATA");
+        fieldSTRDATA.setTypeName("VARCHAR2");
+        fieldSTRDATA.setSize(80);
+        fieldSTRDATA.setSubSize(0);
+        fieldSTRDATA.setIsPrimaryKey(false);
+        fieldSTRDATA.setIsIdentity(false);
+        fieldSTRDATA.setUnique(false);
+        fieldSTRDATA.setShouldAllowNull(true);
+        table.addField(fieldSTRDATA);
+
+        return table;
+    }
+
+    public static TableDefinition buildAnoOnlyEntityTable() {
+        TableDefinition table = new TableDefinition();
+
+        table.setName("ANOONLYENT");
+
+        FieldDefinition fieldID = new FieldDefinition();
+        fieldID.setName("ID");
+        fieldID.setTypeName("NUMERIC");
+        fieldID.setSize(15);
+        fieldID.setShouldAllowNull(false);
+        fieldID.setIsPrimaryKey(true);
+        fieldID.setUnique(false);
+        fieldID.setIsIdentity(false);
+        table.addField(fieldID);
+
+        FieldDefinition fieldSTRDATA = new FieldDefinition();
+        fieldSTRDATA.setName("STRDATA");
+        fieldSTRDATA.setTypeName("VARCHAR2");
+        fieldSTRDATA.setSize(80);
+        fieldSTRDATA.setSubSize(0);
+        fieldSTRDATA.setIsPrimaryKey(false);
+        fieldSTRDATA.setIsIdentity(false);
+        fieldSTRDATA.setUnique(false);
+        fieldSTRDATA.setShouldAllowNull(true);
+        table.addField(fieldSTRDATA);
+
+        return table;
+    }
+}
