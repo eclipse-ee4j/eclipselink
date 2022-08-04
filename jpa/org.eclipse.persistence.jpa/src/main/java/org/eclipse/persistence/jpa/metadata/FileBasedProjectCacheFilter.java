f7e8f920560c34a6c5bb31b820eb3bd779159a1d
Tomas Kraus <tomas.kraus@oracle.com>
2022-08-04 12:08:52 +0200

    Issue 1613: Minimal version of the fix
    Signed-off-by: Tomas Kraus <tomas.kraus@oracle.com>

--- jpa/org.eclipse.persistence.jpa/src/main/java/org/eclipse/persistence/jpa/metadata/FileBasedProjectCacheFilter.java
+++ jpa/org.eclipse.persistence.jpa/src/main/java/org/eclipse/persistence/jpa/metadata/FileBasedProjectCacheFilter.java
@@ -15,11 +15,7 @@
 package org.eclipse.persistence.jpa.metadata;
 
 import java.io.ObjectInputFilter;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.rmi.Remote;
 
-import org.eclipse.persistence.Version;
 import org.eclipse.persistence.logging.SessionLog;
 import org.eclipse.persistence.sessions.Project;
 
@@ -35,7 +31,6 @@ class FileBasedProjectCacheFilter implements ObjectInputFilter {
      */
     FileBasedProjectCacheFilter(SessionLog log) {
         this.log = log;
-        //log.setLevel(SessionLog.FINEST);
     }
 
     // ObjectInputStream data verification entry point.
@@ -52,27 +47,12 @@ class FileBasedProjectCacheFilter implements ObjectInputFilter {
                     return status;
                 }
             }
-            // Check current object instance.
-            final Class<?> serialClass = info.serialClass();
-            if (serialClass != null) {
-                log.log(SessionLog.FINE, String.format(
-                        "FileBasedProjectCacheFilter: processing level %d class %s",
-                        info.depth(), serialClass.getName()));
-                // Only Project class instance is allowed on top level of the structure
-                // and class must also come from known .jar file name.
-                if (info.depth() == 1L) {
-                    if (serialClass != Project.class || !isEclipseLinkFileJar(serialClass, log)) {
-                        log.log(SessionLog.WARNING, String.format(
-                                "FileBasedProjectCacheFilter: rejected illegal top level FileBasedProjectCache class: %s",
-                                serialClass.getName()));
-                        return Status.REJECTED;
-                    }
-                }
-                // Remote interface must be rejected
-                if (Remote.class.isAssignableFrom(serialClass)) {
+            // The only allowed instance on top of the structure is Project class
+            if (info.serialClass() != null && info.depth() == 1L) {
+                if (info.serialClass() != Project.class) {
                     log.log(SessionLog.WARNING, String.format(
-                            "FileBasedProjectCacheFilter: rejected illegal Remote FileBasedProjectCache class: %s",
-                            serialClass.getName()));
+                            "FileBasedProjectCacheFilter: rejected illegal top level FileBasedProjectCache class: %s",
+                            info.serialClass().getName()));
                     return Status.REJECTED;
                 }
             }
@@ -81,48 +61,6 @@ class FileBasedProjectCacheFilter implements ObjectInputFilter {
             log.log(SessionLog.WARNING, "Exception in FileBasedProjectCacheFilter check", t);
             throw t;
         }
-    }
-
-    // Check whether Eclipselink own class is from known .jar file name.
-    private static boolean isEclipseLinkFileJar(final Class<?> serialClass, final SessionLog log) {
-        final URL classUrl = serialClass.getResource(
-                '/' + serialClass.getName().replace('.', '/') + ".class");
-        if (classUrl != null && "jar".equals(classUrl.getProtocol())) {
-            final String classFile = classUrl.getPath();
-            try {
-                final URL fileUrl = new URL(classFile);
-                if ("file".equals(fileUrl.getProtocol())) {
-                    final String filePaths = fileUrl.getPath();
-                    final int delimPos = filePaths.indexOf('!');
-                    String file;
-                    if (delimPos > 0) {
-                        file = filePaths.substring(0, delimPos);
-                    } else {
-                        file = filePaths;
-                    }
-                    final int pathSepPos = file.lastIndexOf('/');
-                    if (pathSepPos > 0 && file.length() > pathSepPos + 1) {
-                        file = file.substring(pathSepPos + 1);
-                    }
-                    if ((file.startsWith("org.eclipse.persistence") && file.contains(Version.getVersion()))
-                            || file.equals("eclipselink.jar")) {
-                        log.log(SessionLog.FINER, String.format(
-                                "FileBasedProjectCacheFilter: class %s from known jar file %s",
-                                serialClass.getName(), file));
-                        return true;
-                    }
-                }
-            } catch (MalformedURLException e) {
-                log.log(SessionLog.WARNING, String.format(
-                        "FileBasedProjectCacheFilter: could not find source .jar of class %s",
-                        serialClass.getName()));
-            }
-        } else {
-            log.log(SessionLog.WARNING, String.format(
-                    "FileBasedProjectCacheFilter: could not find source .jar of class %s",
-                    serialClass.getName()));
-        }
-        return false;
     }
 
 }
