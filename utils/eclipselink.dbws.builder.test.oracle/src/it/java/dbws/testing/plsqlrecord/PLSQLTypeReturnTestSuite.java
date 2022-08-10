59c8eb9f1a6a89fc8dbbe7d9ef68ea8eec522f64
lananda <lalitha.ananda@oracle.com>
2022-08-10 14:12:13 +0530

    Bug1562: Edit comment for PLSQLTypeReturnTestSuite.java

--- utils/eclipselink.dbws.builder.test.oracle/src/it/java/dbws/testing/plsqlrecord/PLSQLTypeReturnTestSuite.java
+++ utils/eclipselink.dbws.builder.test.oracle/src/it/java/dbws/testing/plsqlrecord/PLSQLTypeReturnTestSuite.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/*
  * Copyright (c) 2022 Oracle and/or its affiliates. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
@@ -9,7 +9,7 @@
  *
  * Contributors:
  *     Oracle  - Initial implementation
- ******************************************************************************/
+ */
 package dbws.testing.plsqlrecord;
 
 import static org.junit.Assert.assertNotNull;
