7843a764f5a18a60575e18085f83ed91164bf607
Lukas Jungmann <lukas.jungmann@oracle.com>
2022-08-12 15:51:27 +0200

    #1650: Explicitely use MOXy for XML de/serialization
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- foundation/org.eclipse.persistence.core/src/main/java/org/eclipse/persistence/sessions/serializers/XMLSerializer.java
+++ foundation/org.eclipse.persistence.core/src/main/java/org/eclipse/persistence/sessions/serializers/XMLSerializer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2021 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2022 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -16,19 +16,24 @@ package org.eclipse.persistence.sessions.serializers;
 
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.Map;
 
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Marshaller;
 import jakarta.xml.bind.Unmarshaller;
 
+import org.eclipse.persistence.internal.security.PrivilegedAccessHelper;
 import org.eclipse.persistence.sessions.Session;
 
 /**
- * Uses JAXB to convert an object to XML.
+ * Uses JAXB (MOXy runtime) to convert an object to XML.
  * @author James Sutherland
  */
 public class XMLSerializer extends AbstractSerializer {
+
+    private static final Map<String, Object> XML_FACTORY_OPTS = Map.of(JAXBContext.JAXB_CONTEXT_FACTORY, "org.eclipse.persistence.jaxb.XMLBindingContextFactory");
+
     JAXBContext context;
 
     public XMLSerializer() {
@@ -36,7 +41,9 @@ public class XMLSerializer extends AbstractSerializer {
 
     public XMLSerializer(String packageName) {
         try {
-            this.context = JAXBContext.newInstance(packageName);
+            this.context = JAXBContext.newInstance(packageName,
+                    PrivilegedAccessHelper.callDoPrivileged(() -> Thread.currentThread().getContextClassLoader()),
+                    XML_FACTORY_OPTS);
         } catch (JAXBException exception) {
             throw new RuntimeException(exception);
         }
@@ -53,7 +60,7 @@ public class XMLSerializer extends AbstractSerializer {
                 serializePackage = serializeClass.getPackage().getName();
             }
             try {
-                this.context = JAXBContext.newInstance(serializePackage, serializeClass.getClassLoader());
+                this.context = JAXBContext.newInstance(serializePackage, serializeClass.getClassLoader(), XML_FACTORY_OPTS);
             } catch (JAXBException exception) {
                 throw new RuntimeException(exception);
             }
