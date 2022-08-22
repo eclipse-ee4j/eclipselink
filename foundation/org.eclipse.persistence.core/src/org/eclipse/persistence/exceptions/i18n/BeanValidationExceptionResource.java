dee92c652ce0dcab3774aa46ac325f27beac4aed
Will Dazey <dazeydev.3@gmail.com>
2022-08-22 10:50:26 -0500

    Minor messaging improvements
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/exceptions/i18n/BeanValidationExceptionResource.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/exceptions/i18n/BeanValidationExceptionResource.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2022 Oracle, IBM Corporation and/or its affiliates. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
  * which accompanies this distribution.
@@ -26,12 +26,12 @@ public final class BeanValidationExceptionResource extends ListResourceBundle {
     static final Object[][] contents = {
 
             { "7500", "Bean validation mode on {0}marshaller is set to ON, but no Bean validation provider was found." },
-            { "7501", "Bean validation mode on {0}marshaller is set to illegal value, \"{1}\"." },
+            { "7501", "Bean validation mode on {0}marshaller is set to an unsupported value, \"{1}\"." },
 
             { "7510", "Constraints violated on {0}marshalled bean:\n{1}{2}" },
 
-            { "7525", "Cannot parse property {0}, because it is both annotated with @NotNull and has attribute " +
-                    "\"xs:nillable=true\"." }
+            { "7525", "Cannot parse the \"{0}\" property, because it is both annotated with @NotNull and has the " +
+                    "\"xs:nillable=true\" attribute." }
 
     };
 
