42efe757ac72717147379f1ef9d9ab5ca2123bd9
Will Dazey <dazeydev.3@gmail.com>
2022-08-22 10:53:33 -0500

    Minor messaging improvements
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/exceptions/i18n/DBWSExceptionResource.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/exceptions/i18n/DBWSExceptionResource.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 1998, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2022 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022 IBM Corporation and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -22,19 +23,19 @@ import java.util.ListResourceBundle;
  */
 public class DBWSExceptionResource extends ListResourceBundle {
     static final Object[][] contents = {
-        {"47000", "Could not locate file [{0}]"},
-        {"47001", "Could not locate descriptor [{0}] for operation [{1}] in the O-R project"},
-        {"47002", "Could not locate query [{0}] for descriptor [{1}]"},
-        {"47003", "Could not locate query [{0}] for session [{1}]"},
-        {"47004", "Parameter type [{0}] for operation [{1}] does not exist in the schema"},
-        {"47005", "Parameter type [{0}] for operation [{1}] has no O-X mapping"},
-        {"47006", "Result type [{0}] for operation [{1}] does not exist in the schema"},
-        {"47007", "Result type [{0}] for operation [{1}] has no O-X mapping"},
-        {"47008", "Only Simple XML Format queries support multiple output arguments"},
-        {"47009", "INOUT cursor parameters are not supported"},
-        {"47010", "Could not locate O-R session for service [{0}]"},
-        {"47011", "Could not locate O-X session for service [{0}]"},
-        {"47012", "Could not parse DBWS file"},
+            {"47000", "Cannot locate \"[{0}]\" file"},
+            {"47001", "Cannot locate [{0}] descriptor for [{1}] operation in the O-R project"},
+            {"47002", "Cannot locate \"[{0}]\" query for [{1}] descriptor"},
+            {"47003", "Cannot locate \"[{0}]\" query for [{1}] session"},
+            {"47004", "[{0}] parameter type, for [{1}] operation, does not exist in the schema"},
+            {"47005", "[{0}] parameter type, for [{1}] operation, has no O-X mapping"},
+            {"47006", "[{0}] result type, for [{1}] operation, does not exist in the schema"},
+            {"47007", "[{0}] result type, for [{1}] operation, has no O-X mapping"},
+            {"47008", "Only Simple XML Format queries support multiple output arguments"},
+            {"47009", "INOUT cursor parameters are not supported"},
+            {"47010", "Cannot locate O-R session for [{0}] service"},
+            {"47011", "Cannot locate O-X session for [{0}] service"},
+            {"47012", "Cannot parse DBWS file"},
     };
 
     /**
