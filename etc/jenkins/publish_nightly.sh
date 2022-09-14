fd9e4ecbee675d15ac3b0a5081189e4f88cfa0e9
Radek Felcman <radek.felcman@oracle.com>
2022-09-14 09:00:20 +0200

    Nightly build fix - Jenkins pipeline (#1704)
    This fix related with #1703 and etc/jenkins/build.groovy pipeline. Due security reason "Publish to nightly" stage is executed under different container, than EclipseLink build and tests. There should be Maven, but in different version, than el-buildcontainer used for a build and tests.
    
    Signed-off-by: Radek Felcman radek.felcman@oracle.com

--- etc/jenkins/publish_nightly.sh
+++ etc/jenkins/publish_nightly.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #
-# Copyright (c) 2019, 2020 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2019, 2022 Oracle and/or its affiliates. All rights reserved.
 #
 # This program and the accompanying materials are made available under the
 # terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -16,6 +16,5 @@ if [ ${CONTINUOUS_BUILD} = "true" ]; then
     echo '-[ EclipseLink Continuous Build -> No publishing any artifacts]-------------------------------'
 else
     echo '-[ EclipseLink Publish to Nightly ]-----------------------------------------------------------'
-    mvn -V -B clean package -f bundles/nightly/pom.xml
     scp -r $WORKSPACE/bundles/nightly/target/nightlybuild/* genie.eclipselink@projects-storage.eclipse.org:$BUILD_RESULTS_TARGET_DIR
 fi
