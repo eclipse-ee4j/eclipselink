9dad7171bb21dd36fee5487288a315a89d07a010
Will Dazey <dazeydev.3@gmail.com>
2022-09-23 18:26:07 -0500

    Issue 1711: ClassCastException while updating foreignkeys with identity generation
    Signed-off-by: Will Dazey <dazeydev.3@gmail.com>

--- jpa/eclipselink.jpa.test.jse/src/it/java/org/eclipse/persistence/jpa/test/sequence/TestIdentityGeneration.java
+++ jpa/eclipselink.jpa.test.jse/src/it/java/org/eclipse/persistence/jpa/test/sequence/TestIdentityGeneration.java
@@ -27,6 +27,7 @@ import org.eclipse.persistence.jpa.test.framework.Emf;
 import org.eclipse.persistence.jpa.test.framework.EmfRunner;
 import org.eclipse.persistence.jpa.test.framework.Property;
 import org.eclipse.persistence.jpa.test.sequence.model.Coffee;
+import org.eclipse.persistence.jpa.test.sequence.model.StepExecutionEntity;
 import org.eclipse.persistence.jpa.test.sequence.model.Tea;
 import org.eclipse.persistence.jpa.test.sequence.model.TeaShop;
 import org.eclipse.persistence.platform.database.DatabasePlatform;
@@ -37,7 +38,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(EmfRunner.class)
 public class TestIdentityGeneration {
-    @Emf(createTables = DDLGen.DROP_CREATE, classes = { Coffee.class, Tea.class, TeaShop.class }, 
+    @Emf(createTables = DDLGen.DROP_CREATE, classes = { Coffee.class, Tea.class, TeaShop.class, StepExecutionEntity.class }, 
             properties = { 
                     @Property(name="eclipselink.logging.level", value="FINE")})
     private EntityManagerFactory emf;
@@ -85,6 +86,34 @@ public class TestIdentityGeneration {
             Assert.assertEquals("wrong set size", teas.size(), referenceSet.size());
             Assert.assertTrue("missing element", referenceSet.contains(tea1));
             Assert.assertTrue("missing element", referenceSet.contains(tea2));
+        } finally {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            if(em.isOpen()) {
+                em.close();
+            }
+        }
+    }
+
+    /**
+     * Test to cover updating foreign key field after insert.
+     * 
+     * https://github.com/eclipse-ee4j/eclipselink/issues/1711
+     */
+    @Test
+    public void testPersistWithSecondaryTables2() {
+        if (emf == null)
+            return;
+
+        EntityManager em = emf.createEntityManager();
+        try {
+            StepExecutionEntity e1 = new StepExecutionEntity();
+            e1.setChildEntity(e1);
+
+            em.getTransaction().begin();
+            em.persist(e1);
+            em.getTransaction().commit();
         } finally {
             if (em.getTransaction().isActive()) {
                 em.getTransaction().rollback();
