61e1d740b5fb288f2f9f2aa2b81472a8a9c1964b
Tomáš Kraus <tomas.kraus@oracle.com>
2022-12-06 10:43:33 +0100

    Issue #1727 - Fixed missing default FQN entity alias name. (#1729)
    Signed-off-by: Tomáš Kraus <tomas.kraus@oracle.com>

--- jpa/org.eclipse.persistence.jpa.jpql/src/main/java/org/eclipse/persistence/jpa/jpql/parser/FullyQualifyPathExpressionVisitor.java
+++ jpa/org.eclipse.persistence.jpa.jpql/src/main/java/org/eclipse/persistence/jpa/jpql/parser/FullyQualifyPathExpressionVisitor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006, 2021 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2022 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -68,6 +68,7 @@ public final class FullyQualifyPathExpressionVisitor extends AbstractTraverseChi
     @Override
     public void visit(CollectionValuedPathExpression expression) {
         visitAbstractPathExpression(expression);
+        variableName = expression.toActualText().toLowerCase(Locale.ROOT);
     }
 
     @Override
