bf23a281b80eb9286f95eda833a4826881f5529f
Tomas Moreno <sirlordt@gmail.com>
2023-01-05 01:37:36 -0800

    Added support for optional connection parameter authSource in NOSQL MONGODB (#1770)
    * Added support for optional connection parameter authSource in NOSQL MongoDB server plus unit test. More info about https://www.mongodb.com/docs/manual/reference/connection-string/#mongodb-urioption-urioption.authSource
    
    Signed-off-by: Tomas Moreno <sirlordt@gmail.com>

--- foundation/org.eclipse.persistence.nosql/src/main/java/org/eclipse/persistence/internal/nosql/adapters/mongo/MongoDatabaseConnectionFactory.java
+++ foundation/org.eclipse.persistence.nosql/src/main/java/org/eclipse/persistence/internal/nosql/adapters/mongo/MongoDatabaseConnectionFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2023 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -95,7 +95,15 @@ public class MongoDatabaseConnectionFactory implements ConnectionFactory {
                 isExternal = false;
                 List<MongoCredential> credentialsList =  new ArrayList<>();
                 if ((connectionSpec.getUser() != null) && (connectionSpec.getUser().length() > 0)) {
-                    MongoCredential credential = MongoCredential.createCredential(connectionSpec.getUser(), connectionSpec.getDB(), connectionSpec.getPassword());
+                    MongoCredential credential = null;
+
+                    if ( connectionSpec.getAuthSource() != null ) {
+                        credential = MongoCredential.createCredential(connectionSpec.getUser(), connectionSpec.getAuthSource(), connectionSpec.getPassword());
+                    }
+                    else {
+                        credential = MongoCredential.createCredential(connectionSpec.getUser(), connectionSpec.getDB(), connectionSpec.getPassword());
+                    }
+
                     credentialsList.add(credential);
                 }
                 Builder optionsBuilder = new MongoClientOptions.Builder();
--- foundation/org.eclipse.persistence.nosql/src/main/java/org/eclipse/persistence/internal/nosql/adapters/mongo/MongoJCAConnectionSpec.java
+++ foundation/org.eclipse.persistence.nosql/src/main/java/org/eclipse/persistence/internal/nosql/adapters/mongo/MongoJCAConnectionSpec.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2021 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2023 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -32,6 +32,9 @@ public class MongoJCAConnectionSpec implements ConnectionSpec {
 
     /** Mongo database name. */
     protected String db = "mydb";
+
+    /** Optional authSource https://www.mongodb.com/docs/manual/reference/connection-string/#mongodb-urioption-urioption.authSource */
+    protected String authSource;
 
     /** Optional user name. */
     protected String user;
@@ -157,5 +160,13 @@ public class MongoJCAConnectionSpec implements ConnectionSpec {
 
     public void setServerSelectionTimeout(int serverSelectionTimeout) {
         this.serverSelectionTimeout = serverSelectionTimeout;
+    }
+
+    public String getAuthSource() {
+        return authSource;
+    }
+
+    public void setAuthSource(String authSource) {
+        this.authSource = authSource;
     }
 }
