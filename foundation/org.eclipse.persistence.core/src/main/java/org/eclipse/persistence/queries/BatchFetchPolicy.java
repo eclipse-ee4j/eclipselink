06348e3a0cd435b869126778e5dfaea26e4cf00e
Radek Felcman <radek.felcman@oracle.com>
2023-01-09 14:54:08 +0100

    Copyright year update
    Signed-off-by: Radek Felcman <radek.felcman@oracle.com>

--- foundation/org.eclipse.persistence.core/src/main/java/org/eclipse/persistence/queries/BatchFetchPolicy.java
+++ foundation/org.eclipse.persistence.core/src/main/java/org/eclipse/persistence/queries/BatchFetchPolicy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2022 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2023 Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2022 IBM Corporation. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
