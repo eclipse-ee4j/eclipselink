1bb5bdd6b825af36bd0d4a92d901e599e3f6c4eb
Radek Felcman <radek.felcman@oracle.com>
2023-01-18 10:36:45 +0100

    Jenkins build - MySQL driver German characters test
    Signed-off-by: Radek Felcman <radek.felcman@oracle.com>

--- etc/jenkins/pr_verify.groovy
+++ etc/jenkins/pr_verify.groovy
@@ -104,6 +104,7 @@ spec:
                         /opt/bin/mysql-start.sh
                         mysql -e 'status;' -uroot -proot                        
                         mysql -e 'SET GLOBAL character_set_server = utf8mb4;' -uroot -proot                        
+                        mysql -e 'SET GLOBAL connectionCollation = utf8mb4;' -uroot -proot                        
                         mysql -e 'status;' -uroot -proot                        
                         mkdir ~/.eclipselinktests
                     """
