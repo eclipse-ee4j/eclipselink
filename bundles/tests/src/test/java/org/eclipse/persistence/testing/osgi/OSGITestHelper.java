13a3baa725ec3ebc6d2a6c1a16baf8f3618b6cd0
Lukas Jungmann <lukas.jungmann@oracle.com>
2023-08-11 18:28:31 +0200

    #1919: 4.0.2 tag fails to run the Bundles tests
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- bundles/tests/src/test/java/org/eclipse/persistence/testing/osgi/OSGITestHelper.java
+++ bundles/tests/src/test/java/org/eclipse/persistence/testing/osgi/OSGITestHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2021 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2023 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -15,6 +15,9 @@
 package org.eclipse.persistence.testing.osgi;
 
 import org.ops4j.pax.exam.Option;
+
+import java.net.MalformedURLException;
+import java.nio.file.Path;
 
 import static org.ops4j.pax.exam.CoreOptions.bundle;
 import static org.ops4j.pax.exam.CoreOptions.cleanCaches;
@@ -41,10 +44,10 @@ public class OSGITestHelper {
 
         return options(
                 // JAXB API
-                bundle("file:" + PLUGINS_DIR + ACTIVATION_JAR),
-                bundle("file:" + PLUGINS_DIR + JAXB_API_JAR),
+                bundle(pathToUrl(PLUGINS_DIR + ACTIVATION_JAR)),
+                bundle(pathToUrl(PLUGINS_DIR + JAXB_API_JAR)),
                 //JAXB_OSGI
-                bundle("file:" + PLUGINS_DIR + JAXB_OSGI),
+                bundle(pathToUrl(PLUGINS_DIR + JAXB_OSGI)),
 
                 systemPackage("javax.rmi"),
                 systemPackage("javax.rmi.CORBA"),
@@ -52,12 +55,12 @@ public class OSGITestHelper {
                 systemPackage("org.omg.CORBA.portable"),
                 systemPackage("org.omg.CORBA_2_3.portable"),
                 // JAX-RS API
-                bundle("file:" + PLUGINS_DIR + JAXRS_JAR),
+                bundle(pathToUrl(PLUGINS_DIR + JAXRS_JAR)),
 
                 // EclipseLink bundles
-                bundle("file:" + PLUGINS_DIR + "org.eclipse.persistence.moxy.jar"),
-                bundle("file:" + PLUGINS_DIR + "org.eclipse.persistence.core.jar"),
-                bundle("file:" + PLUGINS_DIR + ASM_JAR),
+                bundle(pathToUrl(PLUGINS_DIR + "org.eclipse.persistence.moxy.jar")),
+                bundle(pathToUrl(PLUGINS_DIR + "org.eclipse.persistence.core.jar")),
+                bundle(pathToUrl(PLUGINS_DIR + ASM_JAR)),
 
                 cleanCaches(),
                 junitBundles());
@@ -86,5 +89,13 @@ public class OSGITestHelper {
                 cleanCaches(),
                 junitBundles()
         );
+    }
+
+    private static String pathToUrl(String path) {
+        try {
+            return Path.of(path).toUri().toURL().toExternalForm();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
