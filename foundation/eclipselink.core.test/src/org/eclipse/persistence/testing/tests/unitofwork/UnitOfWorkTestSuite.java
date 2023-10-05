8d5a6133648e5529b844fa1dde0fa542fe4ce88a
Patrick Schmitt <patrick_98@t-online.de>
2023-10-05 09:47:18 +0200

    Issue 1950: Duplicate objects in UnitOfWorkImpl.primaryKeyToNewObjects (#1956)
    - use IdentityHashSet instead of ArrayList for primaryKeyToNewObjects to prevent duplicates in the future
    - added unit tests
    
    Signed-off-by: Patrick Schmitt <Patrick.Schmitt@cpb-software.com>

--- foundation/eclipselink.core.test/src/org/eclipse/persistence/testing/tests/unitofwork/UnitOfWorkTestSuite.java
+++ foundation/eclipselink.core.test/src/org/eclipse/persistence/testing/tests/unitofwork/UnitOfWorkTestSuite.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2023 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -68,6 +68,9 @@ public class UnitOfWorkTestSuite extends TestSuite {
         addTest(buildRefreshDeletedObjectTest());
 
         addTest(new UnregisterUnitOfWorkTest());
+
+        // Issue 1950 - Duplicate objects in UnitOfWorkImpl.primaryKeyToNewObjects
+        addTest(new UOWPrimaryKeyToNewObjectsDuplicateObjectsTest());
 
         // EL Bug 252047 - Mutable attributes are not cloned when isMutable is enabled on a Direct Mapping
         addTest(new CloneAttributeIfMutableTest());
