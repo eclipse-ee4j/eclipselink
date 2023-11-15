c332ea3cad6a0aee19462b04fd1c6d722820e8ae
Radek Felcman <radek.felcman@oracle.com>
2023-11-15 15:09:18 +0100

    Installer and OSGi test fixes (#1999)
    - installer test is more responsive now and prints in case of failure STDOUT and STDERR
    - Remove some warning messages in OSGi test on JDK 21
    
    Signed-off-by: Radek Felcman <radek.felcman@oracle.com>

--- moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/installer/JaxbCompilerTestCase.java
+++ moxy/eclipselink.moxy.test/src/org/eclipse/persistence/testing/jaxb/installer/JaxbCompilerTestCase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2023 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -14,7 +14,10 @@
 //     Martin Vojtek - 2.6.0 - initial implementation
 package org.eclipse.persistence.testing.jaxb.installer;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 import java.util.logging.Logger;
 
 /**
@@ -53,9 +56,15 @@ public class JaxbCompilerTestCase extends junit.framework.TestCase {
         File compilerFile = new File(binDir, jaxbCompiler);
 
         Process process = Runtime.getRuntime().exec(new String[] { compilerFile.getAbsolutePath(), resourceFile.getAbsolutePath() }, null, binDir);
+        String stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()))
+                .lines()
+                .collect(Collectors.joining("\n"));
+        String stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()))
+                .lines()
+                .collect(Collectors.joining("\n"));
         int result = process.waitFor();
 
-        assertTrue("Process has not completed successfully.", result == 0);
+        assertTrue("Process has not completed successfully with:\nStdOut:\n" + stdInput + "\nStdError:\n" + stdError, result == 0);
 
         String packagePrefix = "perf" + File.separator + "testing" + File.separator + "persistence" + File.separator + "eclipse" + File.separator + "org" + File.separator + "workitem";
 
