d240802cac757bb8af5d7d0e04bb6e2787a81ea5
Radek Felcman <radek.felcman@oracle.com>
2024-02-01 14:11:02 +0100

    Unused import removal
    Signed-off-by: Radek Felcman <radek.felcman@oracle.com>

--- jpa/eclipselink.jpa.testapps/jpa.test.jpql/src/test/java/org/eclipse/persistence/testing/tests/jpa/jpql/advanced/JUnitJPQLComplexTest.java
+++ jpa/eclipselink.jpa.testapps/jpa.test.jpql/src/test/java/org/eclipse/persistence/testing/tests/jpa/jpql/advanced/JUnitJPQLComplexTest.java
@@ -76,7 +76,6 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
