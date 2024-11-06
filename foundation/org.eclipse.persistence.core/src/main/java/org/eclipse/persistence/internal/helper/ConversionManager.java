962ccc827579ae2f3a64f5a04a37992486d2f303
TheChronicler <41308075+TheChronicler@users.noreply.github.com>
2024-11-06 11:53:39 +0100

    Allow conversion from OffsetDateTime to java.sql.Timestamp

--- foundation/org.eclipse.persistence.core/src/main/java/org/eclipse/persistence/internal/helper/ConversionManager.java
+++ foundation/org.eclipse.persistence.core/src/main/java/org/eclipse/persistence/internal/helper/ConversionManager.java
@@ -43,6 +43,7 @@ import java.sql.Time;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.time.Year;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
@@ -838,6 +839,8 @@ public class ConversionManager extends CoreConversionManager implements Serializ
             timestamp = Helper.timestampFromLong((Long)sourceObject);
         } else if (sourceObject instanceof LocalDateTime) {
             timestamp = Timestamp.valueOf((LocalDateTime) sourceObject);
+        } else if (sourceObject instanceof OffsetDateTime) {
+            timestamp = Timestamp.valueOf(((OffsetDateTime)sourceObject).toLocalDateTime());
         } else {
             throw ConversionException.couldNotBeConverted(sourceObject, ClassConstants.TIMESTAMP);
         }
