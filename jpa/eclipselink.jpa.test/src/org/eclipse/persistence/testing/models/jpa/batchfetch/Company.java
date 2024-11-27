2db479829e184362624d3a0c5354fd993772de11
Sheikah45 <66929319+Sheikah45@users.noreply.github.com>
2024-11-27 03:18:57 -0500

    Fix for ReportQuery not fetching relationships with BatchFetch.IN (#2316)
    Co-authored-by: Radek Felcman <radek.felcman@oracle.com>

---
+++ jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/models/jpa/batchfetch/Company.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2024 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0,
+ * or the Eclipse Distribution License v. 1.0 which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+ */
+
+// Contributors:
+
+package org.eclipse.persistence.testing.models.jpa.batchfetch;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import org.eclipse.persistence.annotations.BatchFetch;
+import org.eclipse.persistence.annotations.BatchFetchType;
+
+import java.util.List;
+
+@Entity
+@Table(name = "BATCH_IN_COMPANY")
+public class Company {
+    @Id
+    private long id;
+
+    public Company() {
+    }
+
+    public Company(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
