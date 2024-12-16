ed4a3a31c901b27fdcecf0d0e2f16751783ae283
Radek Felcman <radek.felcman@oracle.com>
2024-12-16 18:08:50 +0100

    JDK 24 build fixes
    There are just some javadoc related issues which leads into errors and block EclipseLink on JDK 24
    
    Signed-off-by: Radek Felcman <radek.felcman@oracle.com>

--- moxy/org.eclipse.persistence.moxy/src/test/java/org/eclipse/persistence/testing/jaxb/xmlelementrefs/adapter/ObjectFactory.java
+++ moxy/org.eclipse.persistence.moxy/src/test/java/org/eclipse/persistence/testing/jaxb/xmlelementrefs/adapter/ObjectFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2021 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2024 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -53,7 +53,7 @@ public class ObjectFactory {
  }
 
  /**
-  * Create an instance of {@link JAXBElement }{@code byte[]}
+  * Create an instance of {@code JAXBElement<byte[]>}
   *
   */
  @XmlElementDecl(namespace = "", name = "e1", scope = Foo.class)
@@ -63,7 +63,7 @@ public class ObjectFactory {
  }
 
  /**
-  * Create an instance of {@link JAXBElement }{@code byte[]}
+  * Create an instance of {@code JAXBElement<byte[]>}
   *
   */
  @XmlElementDecl(namespace = "", name = "e2", scope = Foo.class)
