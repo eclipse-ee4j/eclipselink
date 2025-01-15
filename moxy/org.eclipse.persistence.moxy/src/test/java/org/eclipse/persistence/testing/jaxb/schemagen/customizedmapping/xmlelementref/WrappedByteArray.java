48530ea46457725112ce9797f71dbe99cddeb938
Radek Felcman <radek.felcman@oracle.com>
2025-01-15 08:23:25 +0100

    JDK 24 build fixes (#2327)
    There are just some javadoc related issues which leads into errors and block EclipseLink on JDK 24
    
    Signed-off-by: Radek Felcman <radek.felcman@oracle.com>

--- moxy/org.eclipse.persistence.moxy/src/test/java/org/eclipse/persistence/testing/jaxb/schemagen/customizedmapping/xmlelementref/WrappedByteArray.java
+++ moxy/org.eclipse.persistence.moxy/src/test/java/org/eclipse/persistence/testing/jaxb/schemagen/customizedmapping/xmlelementref/WrappedByteArray.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2021 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2024 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -36,7 +36,7 @@ public class WrappedByteArray {
      *
      * @return
      *     possible object is
-     *     {@link JAXBElement }{@code <}{@link byte[]}{@code >}
+     *     {@code JAXBElement<byte[]>}
      *
      */
     public JAXBElement<byte[]> getInByteArray() {
@@ -48,7 +48,7 @@ public class WrappedByteArray {
      *
      * @param value
      *     allowed object is
-     *     {@link JAXBElement }{@code <}{@link byte[]}{@code >}
+     *     {@code JAXBElement<byte[]>}
      *
      */
     public void setInByteArray(JAXBElement<byte[]> value) {
