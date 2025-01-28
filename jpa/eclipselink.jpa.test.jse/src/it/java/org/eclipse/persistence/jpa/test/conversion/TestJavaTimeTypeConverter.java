eaba2ce4debf9473ee8db5cf9b48b30dd6f82c42
Radek Felcman <radek.felcman@oracle.com>
2025-01-28 13:30:27 +0100

    java.util.Date to java.time.LocalDate conversion fix
    Signed-off-by: Radek Felcman <radek.felcman@oracle.com>

--- jpa/eclipselink.jpa.test.jse/src/it/java/org/eclipse/persistence/jpa/test/conversion/TestJavaTimeTypeConverter.java
+++ jpa/eclipselink.jpa.test.jse/src/it/java/org/eclipse/persistence/jpa/test/conversion/TestJavaTimeTypeConverter.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2020 IBM Corporation. All rights reserved.
+ * Copyright (c) 2020, 2025 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2025 IBM Corporation. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -26,7 +26,10 @@ import java.time.LocalTime;
 import java.time.Month;
 import java.time.OffsetDateTime;
 import java.time.OffsetTime;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.temporal.TemporalField;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -74,8 +77,12 @@ public class TestJavaTimeTypeConverter {
         Assert.assertEquals(2, date.getDate());
 
         LocalDate ld = (LocalDate) cm.convertObject(date, ClassConstants.TIME_LDATE);
+        ZonedDateTime check = ZonedDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
 
         Assert.assertNotNull(ld);
+        Assert.assertEquals(check.getYear(), ld.getYear());
+        Assert.assertEquals(check.getMonth(), ld.getMonth());
+        Assert.assertEquals(check.getDayOfMonth(), ld.getDayOfMonth());
         Assert.assertEquals(2020, ld.getYear());
         Assert.assertEquals(Month.JANUARY, ld.getMonth());
         Assert.assertEquals(2, ld.getDayOfMonth());
