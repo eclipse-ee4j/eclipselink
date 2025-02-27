e1de36ddc68288617dcc1337d6e6aa82f57e99a3
Radek Felcman <radek.felcman@oracle.com>
2025-02-27 09:40:42 +0100

    jpa.wdf.test - TestGetReference.testNegativ
    Signed-off-by: Radek Felcman <radek.felcman@oracle.com>

--- jpa/eclipselink.jpa.wdf.test/src/it/java/org/eclipse/persistence/testing/tests/wdf/jpa1/entitymanager/TestFlush.java
+++ jpa/eclipselink.jpa.wdf.test/src/it/java/org/eclipse/persistence/testing/tests/wdf/jpa1/entitymanager/TestFlush.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2021 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2025 Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2005, 2015 SAP. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
