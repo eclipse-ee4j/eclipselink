115e40b7b0fe64e7110dff4b03857dc45920482a
Jonathan S. Fisher <exabrial@gmail.com>
2025-05-19 11:56:04 -0500

    [2.7] Close #1806 - Have to the ability to dynamically set the Validation Group during a transaction. Add tests, update copyright headers.

--- jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/tests/jpa/beanvalidation/BeanValidationJunitTest.java
+++ jpa/eclipselink.jpa.test/src/org/eclipse/persistence/testing/tests/jpa/beanvalidation/BeanValidationJunitTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2025 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -27,13 +27,17 @@ import javax.persistence.TypedQuery;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 
+import org.eclipse.persistence.config.EntityManagerProperties;
 import org.eclipse.persistence.logging.SessionLog;
 import org.eclipse.persistence.mappings.ForeignReferenceMapping;
 import org.eclipse.persistence.testing.framework.junit.JUnitTestCase;
 import org.eclipse.persistence.testing.models.jpa.beanvalidation.Address;
 import org.eclipse.persistence.testing.models.jpa.beanvalidation.BeanValidationTableCreator;
 import org.eclipse.persistence.testing.models.jpa.beanvalidation.Employee;
+import org.eclipse.persistence.testing.models.jpa.beanvalidation.GermanPhone;
+import org.eclipse.persistence.testing.models.jpa.beanvalidation.Phone;
 import org.eclipse.persistence.testing.models.jpa.beanvalidation.Project;
+import org.eclipse.persistence.testing.models.jpa.beanvalidation.USPhone;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -65,6 +69,13 @@ public class BeanValidationJunitTest extends JUnitTestCase {
             suite.addTest(new BeanValidationJunitTest("testTraversableResolverPreventsLoadingOfLazyRelationships"));
             suite.addTest(new BeanValidationJunitTest("testTraversableResolverPreventsTraversingRelationshipMultipleTimes"));
             suite.addTest(new BeanValidationJunitTest("testValidateChangedData"));
+            suite.addTest(new BeanValidationJunitTest("testPersistIrishPhoneNoGroups"));
+            suite.addTest(new BeanValidationJunitTest("testPersistUSPhoneWithGermanGroup"));
+            suite.addTest(new BeanValidationJunitTest("testPersistGermanPhoneWithUSGroup"));
+            suite.addTest(new BeanValidationJunitTest("testPersistGermanPhoneWithGermanGroup"));
+            suite.addTest(new BeanValidationJunitTest("testRemoveGermanPhoneWithGermanGroup"));
+            suite.addTest(new BeanValidationJunitTest("testRemoveInvalidGermanPhoneWithGermanGroup"));
+            suite.addTest(new BeanValidationJunitTest("testUpdateInvalidGermanPhoneWithGermanGroup"));
         }
         return suite;
     }
@@ -396,6 +407,241 @@ public class BeanValidationJunitTest extends JUnitTestCase {
         }
     }
 
+    public void testPersistIrishPhoneNoGroups() {
+      EntityManager em = createEntityManager();
+      boolean gotConstraintViolations = false;
+      try {
+          // Persist an object with Irish phone number (valid default-phone)
+          beginTransaction(em);
+          Employee e = new Employee();
+          e.setPhone(new Phone("+353 (1) 555-5678"));
+          em.persist(e);
+      } catch (ConstraintViolationException e) {
+          assertTrue("Transaction not marked for roll back when ConstraintViolation is thrown", getRollbackOnly(em));
+          gotConstraintViolations = true;
+      } finally {
+          if (isTransactionActive(em)) {
+              rollbackTransaction(em);
+          }
+          closeEntityManager(em);
+      }
+      assertFalse("Did not expect Constraint Violation while persisting IrishPhone without groups", gotConstraintViolations);
+    }
+    
+    public void testPersistUSPhoneWithGermanGroup() {
+      Map<String, Object> props = new HashMap<>();
+      props.put(EntityManagerProperties.VALIDATION_GROUP_PRE_PERSIST, new Class[]{ GermanPhone.class });
+      EntityManager em = createEntityManager(props);
+      boolean gotConstraintViolations = false;
+      try {
+          // Persist an object with US phone number
+          beginTransaction(em);
+          Employee e = new Employee();
+          e.setPhone(new Phone("+1 (316) 555-5555"));
+          em.persist(e);
+      } catch (ConstraintViolationException e) {
+          assertTrue("Transaction not marked for roll back when ConstraintViolation is thrown", getRollbackOnly(em));
+          gotConstraintViolations = true;
+      } finally {
+          if (isTransactionActive(em)) {
+              rollbackTransaction(em);
+          }
+          closeEntityManager(em);
+      }
+      assertTrue("Did not get Constraint Violation while persisting USPhone with German group active", gotConstraintViolations);
+    }
+
+    public void testPersistGermanPhoneWithUSGroup() {
+      Map<String, Object> props = new HashMap<>();
+      props.put(EntityManagerProperties.VALIDATION_GROUP_PRE_PERSIST, new Class[]{ USPhone.class });
+      EntityManager em = createEntityManager(props);
+      boolean gotConstraintViolations = false;
+      try {
+          // Persist an object with German phone number (invalid without German group)
+          beginTransaction(em);
+          Employee e = new Employee();
+          e.setPhone(new Phone("+49 (221) 1555-5678")); 
+          em.persist(e);
+      } catch (ConstraintViolationException e) {
+          assertTrue("Transaction not marked for roll back when ConstraintViolation is thrown", getRollbackOnly(em));
+          gotConstraintViolations = true;
+      } finally {
+          if (isTransactionActive(em)) {
+              rollbackTransaction(em);
+          }
+          closeEntityManager(em);
+      }
+      assertTrue("Did not get Constraint Violation while persisting GermanPhone with US group active", gotConstraintViolations);
+    }
+
+    public void testPersistGermanPhoneWithGermanGroup() {
+      Map<String, Object> props = new HashMap<>();
+      props.put(EntityManagerProperties.VALIDATION_GROUP_PRE_PERSIST, new Class[]{ GermanPhone.class });
+      EntityManager em = createEntityManager(props);
+      boolean gotConstraintViolations = false;
+      try {
+          // Persist an object with German phone number (valid DE-phone)
+          beginTransaction(em);
+          Employee e = new Employee();
+          e.setPhone(new Phone("+49 (221) 1555-5678"));
+          em.persist(e);
+      } catch (ConstraintViolationException e) {
+          assertTrue("Transaction not marked for roll back when ConstraintViolation is thrown", getRollbackOnly(em));
+          gotConstraintViolations = true;
+      } finally {
+          if (isTransactionActive(em)) {
+              rollbackTransaction(em);
+          }
+          closeEntityManager(em);
+      }
+      assertFalse("Did not expect Constraint Violation while persisting GermanPhone with German group active", gotConstraintViolations);
+    }
+    
+    public void testRemoveGermanPhoneWithGermanGroup() {
+      Map<String, Object> props = new HashMap<>();
+      props.put(EntityManagerProperties.VALIDATION_GROUP_PRE_REMOVE, new Class[]{ GermanPhone.class });
+      EntityManager em = createEntityManager(props);
+      boolean removeSuccessful = false;
+      final int PK = 200;
+
+      try {
+          beginTransaction(em);
+          Employee e1 = new Employee(PK, "name", "name", 1337);
+          e1.setPhone(new Phone("+49 (221) 1555-5678"));
+          em.persist(e1);
+          commitTransaction(em);
+      } catch (RuntimeException ex) {
+          if (isTransactionActive(em)) {
+              rollbackTransaction(em);
+          }
+          throw ex;
+      }
+
+      try {
+          beginTransaction(em);
+          Employee e = em.find(Employee.class, PK);
+          em.remove(e);
+          commitTransaction(em);
+          removeSuccessful = true;
+      } catch (RuntimeException ex) {
+          if (isTransactionActive(em)) {
+              rollbackTransaction(em);
+          }
+          throw ex;
+      }
+
+      closeEntityManager(em);
+      assertTrue("Automatic Validation should not be executed for remove with German group", removeSuccessful);
+    }
+
+    public void testUpdateGermanPhoneWithGermanGroup() {
+      Map<String, Object> props = new HashMap<>();
+      props.put(EntityManagerProperties.VALIDATION_GROUP_PRE_UPDATE, new Class[]{ GermanPhone.class });
+      EntityManager em = createEntityManager(props);
+      boolean updateSuccessful = false;
+      final int PK = 201;
+
+      try {
+          beginTransaction(em);
+          Employee e1 = new Employee(PK, "name", "name", 1337);
+          em.persist(e1);
+          commitTransaction(em);
+      } catch (RuntimeException ex) {
+          if (isTransactionActive(em)) {
+              rollbackTransaction(em);
+          }
+          throw ex;
+      }
+
+      try {
+          beginTransaction(em);
+          Employee e = em.find(Employee.class, PK);
+          e.setPhone(new Phone("+49 (221) 1555-5678"));
+          commitTransaction(em);
+          updateSuccessful = true;
+      } catch (RuntimeException ex) {
+          if (isTransactionActive(em)) {
+              rollbackTransaction(em);
+          }
+          throw ex;
+      }
+
+      closeEntityManager(em);
+      assertTrue("Automatic Validation should not be executed for update with German group", updateSuccessful);
+    }
+    
+    public void testRemoveInvalidGermanPhoneWithGermanGroup() {
+      Map<String, Object> props = new HashMap<>();
+      props.put(EntityManagerProperties.VALIDATION_GROUP_PRE_REMOVE, new Class[]{ GermanPhone.class });
+      EntityManager em = createEntityManager(props);
+      boolean gotConstraintViolations = false;
+      final int PK = 300;
+
+      // first persist with a valid German phone
+      try {
+          beginTransaction(em);
+          Employee e1 = new Employee(PK, "name", "name", 0);
+          e1.setPhone(new Phone("+49 (221) 1555-5678"));
+          em.persist(e1);
+          commitTransaction(em);
+      } catch (RuntimeException ex) {
+          if (isTransactionActive(em)) rollbackTransaction(em);
+          closeEntityManager(em);
+          throw ex;
+      }
+
+      // now change to a US phone and attempt remove
+      try {
+          beginTransaction(em);
+          Employee e = em.find(Employee.class, PK);
+          e.setPhone(new Phone("123-456-7890"));
+          em.remove(e);
+          commitTransaction(em);
+      } catch (ConstraintViolationException e) {
+          assertTrue("Transaction not marked for roll back when ConstraintViolation is thrown", getRollbackOnly(em));
+          gotConstraintViolations = true;
+      } finally {
+          if (isTransactionActive(em)) rollbackTransaction(em);
+          closeEntityManager(em);
+      }
+      assertTrue("Expected Constraint Violation while removing with invalid GermanPhone under German group", gotConstraintViolations);
+    }
+
+    public void testUpdateInvalidGermanPhoneWithGermanGroup() {
+      Map<String, Object> props = new HashMap<>();
+      props.put(EntityManagerProperties.VALIDATION_GROUP_PRE_UPDATE, new Class[]{ GermanPhone.class });
+      EntityManager em = createEntityManager(props);
+      boolean gotConstraintViolations = false;
+      final int PK = 301;
+
+      // first persist without phone
+      try {
+          beginTransaction(em);
+          Employee e1 = new Employee(PK, "name", "name", 0);
+          em.persist(e1);
+          commitTransaction(em);
+      } catch (RuntimeException ex) {
+          if (isTransactionActive(em)) rollbackTransaction(em);
+          closeEntityManager(em);
+          throw ex;
+      }
+
+      // now change to a US phone and attempt update
+      try {
+          beginTransaction(em);
+          Employee e = em.find(Employee.class, PK);
+          e.setPhone(new Phone("123-456-7890"));
+          commitTransaction(em);
+      } catch (ConstraintViolationException e) {
+          assertTrue("Transaction not marked for roll back when ConstraintViolation is thrown", getRollbackOnly(em));
+          gotConstraintViolations = true;
+      } finally {
+          if (isTransactionActive(em)) rollbackTransaction(em);
+          closeEntityManager(em);
+      }
+      assertTrue("Expected Constraint Violation while updating with invalid GermanPhone under German group", gotConstraintViolations);
+    }
+    
     //--------------------Helper Methods ---------------//
     private boolean isInstantiated(Object entityObject, String attributeName, org.eclipse.persistence.sessions.Project project) {
         ForeignReferenceMapping mapping = (ForeignReferenceMapping) project.getDescriptor(Employee.class).getObjectBuilder().getMappingForAttributeName(attributeName);
