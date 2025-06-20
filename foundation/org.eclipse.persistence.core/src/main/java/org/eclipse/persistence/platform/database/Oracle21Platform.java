8a58fa1c415210364347589045dc851d9acf9b69
Tomáš Kraus <tomas.kraus@oracle.com>
2025-06-20 16:05:37 +0200

    Issue #2451 - Added fractional seconds precision support in a SQL time or timestamp data type for MySQL and Oracle
    Signed-off-by: Tomáš Kraus <tomas.kraus@oracle.com>

--- foundation/org.eclipse.persistence.core/src/main/java/org/eclipse/persistence/platform/database/Oracle21Platform.java
+++ foundation/org.eclipse.persistence.core/src/main/java/org/eclipse/persistence/platform/database/Oracle21Platform.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2025 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -14,8 +14,26 @@
 //     13/01/2022-4.0.0 Tomas Kraus - 1391: JSON support in JPA
 package org.eclipse.persistence.platform.database;
 
+import java.util.Hashtable;
+
+import org.eclipse.persistence.internal.databaseaccess.FieldTypeDefinition;
+
 public class Oracle21Platform extends Oracle19Platform {
     public Oracle21Platform() {
         super();
     }
+
+    @Override
+    protected Hashtable<Class<?>, FieldTypeDefinition> buildFieldTypes() {
+        Hashtable<Class<?>, FieldTypeDefinition> fieldTypes = super.buildFieldTypes();
+        fieldTypes.put(java.time.LocalDateTime.class, new FieldTypeDefinition("TIMESTAMP", 9));
+        fieldTypes.put(java.time.LocalTime.class, new FieldTypeDefinition("TIMESTAMP", 9));
+        return fieldTypes;
+    }
+
+    @Override
+    public boolean supportsFractionalTime() {
+        return true;
+    }
+
 }
