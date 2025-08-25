6adf215331eb90caa44021304246907e03de6045
Radek Felcman <radek.felcman@oracle.com>
2025-08-25 20:27:35 +0200

    JPA Server side test fixes - part 5
    Main changes there:
    - new Maven profile all-sources and assembly config due a Toplink
    - eclipselink.jar new exports due a Toplink
    - jpa.testapps.jta server platform, database platform substitution strings in resources-ejb/META-INF/persistence.xml
    - jpa.testapps.sessionbean.ha server platform, database platform substitution strings in resources-ejb/META-INF/persistence.xml
    
    Signed-off-by: Radek Felcman <radek.felcman@oracle.com>

--- bundles/eclipselink/src/main/java/module-info.java
+++ bundles/eclipselink/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, 2022 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2025 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -173,6 +173,8 @@ module eclipselink {
     exports org.eclipse.persistence.jaxb.metadata;
     exports org.eclipse.persistence.jaxb.plugins;
     exports org.eclipse.persistence.jaxb.rs;
+    exports org.eclipse.persistence.jpa.rs.util;
+    exports org.eclipse.persistence.jpa.rs.logging;
     exports org.eclipse.persistence.jaxb.xmlmodel;
 
     exports org.eclipse.persistence.jaxb.xjc;
