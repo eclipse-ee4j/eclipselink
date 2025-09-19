373ce6f5b94293664f37ffc0dc8c3d34df956485
Tomáš Kraus <tomas.kraus@oracle.com>
2025-09-19 16:30:22 +0200

    Issue #2245 - test with @Embeddable record
    Signed-off-by: Tomáš Kraus <tomas.kraus@oracle.com>

--- jpa/eclipselink.jpa.testapps/jpa.test.advanced.embeddable/src/main/java/org/eclipse/persistence/testing/models/jpa/advanced/embeddable/EmbeddableRecordTableCreator.java
+++ jpa/eclipselink.jpa.testapps/jpa.test.advanced.embeddable/src/main/java/org/eclipse/persistence/testing/models/jpa/advanced/embeddable/EmbeddableRecordTableCreator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024, 2025 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -22,6 +22,7 @@ public class EmbeddableRecordTableCreator extends TogglingFastTableCreator {
     public EmbeddableRecordTableCreator() {
         setName("EmbeddableProject");
         addTableDefinition(buildCmp3EmbedRecVisitorTable());
+        addTableDefinition(buildCmp3EmbedSegmentTable());
     }
 
     public static TableDefinition buildCmp3EmbedRecVisitorTable() {
@@ -34,4 +35,15 @@ public class EmbeddableRecordTableCreator extends TogglingFastTableCreator {
         table.addField(createStringColumn("NAME4"));
         return table;
     }
+
+    public static TableDefinition buildCmp3EmbedSegmentTable() {
+        TableDefinition table = createTable("CMP3_EMBED_SEGMENT");
+        table.addField(createNumericPk("ID"));
+        table.addField(createNumericColumn("POINTAX"));
+        table.addField(createNumericColumn("POINTAY"));
+        table.addField(createNumericColumn("POINTBX"));
+        table.addField(createNumericColumn("POINTBY"));
+        return table;
+    }
+
 }
