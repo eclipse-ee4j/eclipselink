9ee68d59bef23fdf8fdd1507af8feb73700d9f15
pardhiv-krishna <pardhiv.krishna@ibm.com>
2025-09-24 10:26:26 +0530

    Copyright modified and comment added

--- foundation/org.eclipse.persistence.core/src/main/java/org/eclipse/persistence/platform/database/DB2Platform.java
+++ foundation/org.eclipse.persistence.core/src/main/java/org/eclipse/persistence/platform/database/DB2Platform.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1998, 2024 Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 1998, 2024 IBM Corporation. All rights reserved.
+ * Copyright (c) 1998, 2025 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2025 IBM Corporation. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -616,6 +616,7 @@ public class DB2Platform extends org.eclipse.persistence.platform.database.Datab
     
     private static final class DB2ExtractOperator extends ExtractOperator {
 
+    	// DATE emulation: CAST(:first AS DATE)
         private static final String[] DATE_STRINGS = new String[] {"CAST(", " AS DATE)"};
 
         private DB2ExtractOperator() {
